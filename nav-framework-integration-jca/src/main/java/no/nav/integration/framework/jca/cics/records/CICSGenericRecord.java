e79dff4b8a492f79d8326a36e4c9e332e9e9402b
cab2920 <cab2920@localhost>
2006-04-27 08:05:04 +0000

    renamed

---
+++ nav-framework-integration-jca/src/main/java/no/nav/integration/framework/jca/cics/records/CICSGenericRecord.java
@@ -0,0 +1,224 @@
+package no.nav.integration.framework.jca.cics.records;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+/**
+ * This class provides a simple example of using the Streamable interface.
+ * This custom record can be used with the ECI Resource adapter.
+ * Based on IBM Redbook example.
+ *
+ * @author  person5b7fd84b3197, Accenture
+ * @version $Id: CICSGenericRecord.java 2828 2006-03-05 10:17:18Z skb2930 $
+ */
+public class CICSGenericRecord implements javax.resource.cci.Streamable, javax.resource.cci.Record {
+	private static final Log log = LogFactory.getLog(CICSGenericRecord.class);
+
+	//-------------------------------------------------------------------------
+    // Internal properties
+    //-------------------------------------------------------------------------
+    private String recordName;
+    private String desc;
+    private String contents = "";
+    private String enc = "IBM037";
+
+	/**
+	 * Creates a new instance of CICSGenericRecord.
+	 * @param encoding String
+	 */
+	public CICSGenericRecord(String encoding) {
+		enc = encoding;
+	}
+
+	/**
+	 * Creates a new instance of CICSGenericRecord.
+	 * This constructor defaults the encoding to IBM037
+	 */
+	public CICSGenericRecord() {
+	}
+
+    //-------------------------------------------------------------------------
+    //  the following methods are required for the Record interface
+    //-------------------------------------------------------------------------
+    /**
+     * Get the name of the Record.
+     * 
+     * @return a String representing the Record Name
+     */
+    public java.lang.String getRecordName() {
+        return recordName;
+    }
+
+    /**
+     * Set the name of the Record.
+     * 
+     * @param newName The Name of the Record
+     */
+    public void setRecordName(String newName) {
+        recordName = newName;
+    }
+
+
+    /**
+     * Set the record short destription.
+     *
+     * @param newDesc The Description for this record
+     */
+    public void setRecordShortDescription(String newDesc) {
+        desc = newDesc;
+    }
+
+    /**
+     * Get the short description for this Record.
+     * 
+     * @return a String representing the Description
+     */
+    public java.lang.String getRecordShortDescription() {
+        return desc;
+    }
+
+    /**
+     * Return a hashcode for this object.
+     *
+     * @return hashcode
+     */
+    public int hashCode() {
+        if (contents != null) {
+            return contents.hashCode();
+        } else {
+            return super.hashCode();
+        }
+    }
+
+    /**
+     * Determines if objects are equal. Objects are equal if they
+     * have the same reference or the text contained is identical.
+     * @param comp The object to compare.
+     * @return flag indicating true or false
+     */
+    public boolean equals(Object comp) {
+        if (!(comp instanceof CICSGenericRecord)) {
+            return false;
+        }
+
+        if (comp == this) {
+            return true;
+        }
+
+        CICSGenericRecord realComp = (CICSGenericRecord) comp;
+
+        return realComp.getText().equals(getText());
+    }
+
+    /**
+     * Use the superclass clone method for this class.
+     * @return Object - The clone
+     * @throws CloneNotSupportedException - Unable to clone
+     */
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    //-------------------------------------------------------------------------
+    // These methods are required for the streamable interface
+    //-------------------------------------------------------------------------
+    /**
+     * This method is invoked by the ECI Resource Adapter when it is
+     * transmitting data to the Record. This means we are receiving a commarea
+     * back from the ECI Resource Adapter and must have been passed as an 
+     * output record.
+     * 
+     * @param is the inputStream where we read the information from.
+     */
+    public void read(InputStream is) {
+	    try {
+		    int total = is.available();
+		    byte[] bytes = new byte[total];
+		    is.read(bytes);
+
+		    //-----------------------------------------------------------------
+		    // convert the bytes to a string based on the selected encoding
+		    //-----------------------------------------------------------------
+		    contents = new String(bytes, enc);
+	    } catch (UnsupportedEncodingException e) {
+		    if (log.isWarnEnabled()) {
+			    log.warn("Encoding not supported, change to another", e);
+		    }
+	    } catch (IOException e) {
+		    if (log.isWarnEnabled()) {
+			    log.warn("Could not read bytes", e);
+		    }
+	    }
+
+    }
+
+	/**
+     * This method is invoked by the ECI Resource Adapter when it wants to
+     * read the record. This means we have been passed as an input record.
+     * 
+     * @param os the output stream to write the information to.
+     */
+    public void write(OutputStream os) {
+        try {
+
+            //-----------------------------------------------------------------
+            // output the string as bytes in the selected encoding
+            //-----------------------------------------------------------------
+            os.write(contents.getBytes(enc));
+        } catch (UnsupportedEncodingException e) {
+	        if (log.isWarnEnabled()) {
+		        log.warn("Encoding not supported, change to another", e);
+	        }
+        } catch (IOException e) {
+	        if (log.isWarnEnabled()) {
+		        log.warn("Could not read bytes", e);
+	        }
+        }
+	}
+
+    /**
+     * Return the text of this java record.
+     *
+     * @return The text
+     */
+    public String getText() {
+        return contents;
+    }
+
+
+    /**
+     * Set the text for this java record.
+     * 
+     * @param newStr The new text
+     */
+    public void setText(String newStr) {
+        contents = newStr;
+    }
+
+    /**
+     * Return the current java encoding used for this record.
+     *
+     * @return the java encoding
+     */
+    public String getEncoding() {
+        return enc;
+    }
+
+    /**
+     * Set the java encoding to be used for this record.
+     * <p>
+     * Note: No checks are made at this time to see if the encoding is a valid
+     * java encoding. If you wish you can modify the code to include this.
+     *
+     * @param newEnc the new java encoding
+     */
+    public void setEncoding(String newEnc) {
+        enc = newEnc;
+    }
+}
---
+++ nav-framework-integration-jca/src/main/java/no/nav/integration/framework/jca/cics/service/CicsStaticRecordMapper.java
@@ -0,0 +1,186 @@
+package no.nav.integration.framework.jca.cics.service;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.resource.ResourceException;
+import javax.resource.cci.InteractionSpec;
+import javax.resource.cci.Record;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.ibm.connector2.cics.ECIInteractionSpec;
+
+import net.sf.hibernate.MappingException;
+import net.sf.hibernate.mapping.RootClass;
+
+import no.nav.common.framework.FrameworkError;
+import no.nav.common.framework.error.SystemException;
+import no.nav.common.framework.service.ServiceFailedException;
+import no.nav.integration.framework.hibernate.cfg.Configuration;
+import no.nav.integration.framework.hibernate.helper.RecordConverter;
+import no.nav.integration.framework.hibernate.helper.RecordHelper;
+import no.nav.integration.framework.jca.cics.records.CICSGenericRecord;
+import no.nav.integration.framework.jca.service.RecordMapper;
+
+
+/**
+ * Creates interaction and records to communicate with CICS Resource Adapter.
+ *
+ * @author person5b7fd84b3197, Accenture
+ * @version $Id: CicsStaticRecordMapper.java 2800 2006-03-01 10:48:32Z skb2930 $
+ */
+public class CicsStaticRecordMapper implements RecordMapper {
+	private static final Log log = LogFactory.getLog(CicsStaticRecordMapper.class);
+	private Map cache = new HashMap(12);
+	private RecordConverter recordConverter;
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * The record type holds a String.
+	 */
+	public Record classToRecord(String recordName, InteractionProperties interProps, Object bean)
+		throws ServiceFailedException {
+
+		Configuration configuration = getConfiguration(interProps, retrieveClassloader());
+		RootClass clazz = RecordHelper.getServiceRootClass(configuration, recordName);
+
+		if (log.isDebugEnabled()) {
+			log.debug("RootClassName: " + clazz.getName());
+		}
+
+		StringBuffer buffer = new StringBuffer();
+
+		// Create a string from the given service
+		try {
+			recordConverter.classToRecord(configuration, clazz, bean, buffer);
+		} catch (Exception e) {
+			throw new ServiceFailedException(FrameworkError.JCA_RECORD_CONVERSION_ERROR, e, clazz.getName());
+		}
+
+		if (log.isDebugEnabled()) {
+			log.debug("InputRecord length : " + buffer.length());
+			log.debug("Input Record:  '" + buffer.toString() + "'");
+		}
+
+		// Create the CICS Record
+		CICSGenericRecord record = new CICSGenericRecord();
+		record.setText(buffer.toString());
+
+		return record;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * The record type holds a String.
+	 */
+	public Object recordToClass(String recordName, InteractionProperties interProps, Record record)
+		throws ServiceFailedException {
+
+		CICSGenericRecord cicsRecord = (CICSGenericRecord) record;
+
+		if (log.isDebugEnabled()) {
+			log.debug("ReturnRecord from Cics: " + cicsRecord.getText());
+		}
+
+		Configuration configuration = getConfiguration(interProps, retrieveClassloader());
+		RootClass clazz = RecordHelper.getServiceRootClass(configuration, recordName);
+
+		try {
+			return recordConverter.recordToClass(configuration, cicsRecord.getText(), clazz);
+		} catch (Exception e) {
+			throw new ServiceFailedException(FrameworkError.JCA_RECORD_CONVERSION_ERROR, e, clazz.getName());
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public InteractionSpec createInteractionSpec(InteractionProperties interProps) {
+		ECIInteractionSpec spec = new ECIInteractionSpec();
+
+		try {
+			spec.setFunctionName(interProps.getFunctionName());
+			spec.setCommareaLength(interProps.getCommareaLength());
+			spec.setReplyLength(interProps.getReplyLength());
+			spec.setExecuteTimeout(interProps.getExecuteTimeout());
+			spec.setInteractionVerb(ECIInteractionSpec.SYNC_SEND_RECEIVE);
+		} catch (ResourceException re) {
+			throw new SystemException(FrameworkError.JCA_CREATE_INTERACTION_ERROR, re);
+		}
+
+		return spec;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public InteractionSpec createInteraction(InteractionProperties interProps) {
+		return createInteractionSpec(interProps);
+	}
+
+	/**
+	 * Helper method to load the mapping files.
+	 *
+	 * @param interaction the interactionProperties.
+	 * @param contextClassLoader the <code>ClassLoader</code> to use for loading the mapping files.
+	 * @return Configuration the record mapper configuration.
+	 */
+	private synchronized Configuration getConfiguration(InteractionProperties interaction, final ClassLoader contextClassLoader) {
+
+		Configuration config = (Configuration) cache.get(interaction);
+
+		if (null != config) {
+			return config;
+		}
+
+		Configuration newConfig = newConfig();
+
+		for (int i = 0; i < interaction.getMappingFiles().size(); i++) {
+			String mapping = (String) interaction.getMappingFiles().get(i);
+			InputStream stream = contextClassLoader.getResourceAsStream(mapping);
+
+			try {
+				newConfig.addInputStream(stream);
+			} catch (MappingException e) {
+				throw new SystemException(FrameworkError.JCA_GET_CONFIG_ERROR, e, mapping);
+			}
+		}
+
+		cache.put(interaction, newConfig);
+
+		return newConfig;
+	}
+
+	/**
+	 * Creates an empty configuration.
+	 *
+	 * @return an empty configuration.
+	 */
+	Configuration newConfig() {
+		return new Configuration();
+	}
+
+	/**
+	 * Helper method for retrieving the class loader to use for loading mapping files.
+	 *
+	 * @return the class loader to use for loading mapping files.
+	 */
+	private ClassLoader retrieveClassloader() {
+		return Thread.currentThread().getContextClassLoader();
+	}
+
+	/**
+	 * Sets the <code>RecordConverter</code> to use when converting from record to instance and vice versa.
+	 *
+	 * @param recordConverter the <code>RecordConverter</code> to use.
+	 * @see RecordConverter
+	 */
+	public void setRecordConverter(final RecordConverter recordConverter) {
+		this.recordConverter = recordConverter;
+	}
+}
