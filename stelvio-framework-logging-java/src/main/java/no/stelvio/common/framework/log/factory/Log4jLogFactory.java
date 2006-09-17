9cf3c3691d8ebcdd89ca3aaa7b679c24e5a8517c
ska2812 <ska2812@localhost>
2006-09-17 11:12:19 +0000

    Renamed name from nav to stelvio

--- stelvio-framework-logging-java/src/main/java/no/stelvio/common/framework/log/factory/Log4jLogFactory.java
+++ stelvio-framework-logging-java/src/main/java/no/stelvio/common/framework/log/factory/Log4jLogFactory.java
@@ -48,7 +48,7 @@ public class Log4jLogFactory extends LogFactory {
 	 * the log4j configuration (in milliseconds).
 	 */
 	public static final String REFRESH_INTERVAL =
-		"no.nav.common.framework.log.factory.Log4jLogFactory.refreshInterval";
+		"no.stelvio.common.framework.log.factory.Log4jLogFactory.refreshInterval";
 
 	/** Wether or not to use the default init. sequence in LogManager */
 	public static final String DEFAULT_INIT_OVERRIDE_KEY = "log4j.defaultInitOverride";
--- stelvio-framework-sso-was/src/main/java/no/stelvio/framework/sso/StelvioTrustInterceptor.java
+++ stelvio-framework-sso-was/src/main/java/no/stelvio/framework/sso/StelvioTrustInterceptor.java
@@ -10,25 +10,24 @@ import com.ibm.websphere.security.WebTrustAssociationUserException;
 
 /**
  * Konfigureres ved å sette denne i LTPA > Trust Association > Trust Interceptor
- * og sette ws.ext.dirs på serveren til å inkludere hvor denne filen ligger (E:\data\workspace\head\stelvio-framework-sso-was\bin)
- * og
+ * og sette ws.ext.dirs på serveren til å inkludere hvor denne filen ligger
+ * (E:\data\workspace\head\stelvio-framework-sso-was\bin) og
  * E:\apps\IBM\WebSphere Studio\Application Developer\v5.1.1\runtimes\base_v51\lib\wssec.jar.
+ *
  * @author person1f201b37d484, Accenture
- * @version $Id: NAVTrustInterceptor.java 2654 2005-11-28 11:14:54Z shc2920 $
+ * @version $Id: stelvioTrustInterceptor.java 2654 2005-11-28 11:14:54Z shc2920 $
  * @deprecated bruk no.stelvio.framework.sso.NtlmTrustInterceptor istedenfor.
  */
-public class NAVTrustInterceptor
-	extends com.ibm.websphere.security.WebSphereBaseTrustAssociationInterceptor
-	implements com.ibm.websphere.security.TrustAssociationInterceptor {
+public class StelvioTrustInterceptor
+        extends com.ibm.websphere.security.WebSphereBaseTrustAssociationInterceptor
+        implements com.ibm.websphere.security.TrustAssociationInterceptor {
 
 	public boolean isTargetInterceptor(HttpServletRequest req) throws WebTrustAssociationException {
 		// return true if this is the target interceptor, else return false.
 		String ntlmuser = (String) req.getSession().getAttribute("ntlmuser");
-		if (ntlmuser != null) {
-			return true;
-		}
-		return false;
-	}
+
+        return ntlmuser != null;
+    }
 
 	public void validateEstablishedTrust(HttpServletRequest req) throws WebTrustAssociationFailedException {
 		//validate if the request is from the trusted proxy server.
