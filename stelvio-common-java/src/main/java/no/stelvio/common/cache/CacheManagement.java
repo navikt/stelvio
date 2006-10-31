108ea0f38219ca92c5f855092ab79da39334ff1e
esa2812 <esa2812@localhost>
2006-10-31 11:00:05 +0000

    - Added cache components

---
+++ stelvio-common-java/src/main/java/no/stelvio/common/cache/CacheManagement.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package no.stelvio.common.cache;
+
+import net.sf.ehcache.CacheManager;
+
+/**
+ * CacheManagement component for stelvio.
+ * 
+ * Interface that defines the management operations on the system cache services.
+ * Implementations are standard POJO's that can be JMX-enabled using Spring configuration.
+ * 
+ * @author person4f9bc5bd17cc, Accenture
+ */
+public interface CacheManagement {
+	/**
+	 * Flushes all elements in all configured caches.
+	 */
+	public void flushCache();
+	
+	/**
+	 * @return the Ehcache CacheManager
+	 */
+	public CacheManager getCacheManager();
+}
---
+++ stelvio-common-java/src/main/java/no/stelvio/common/cache/CacheManagementImpl.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package no.stelvio.common.cache;
+
+import net.sf.ehcache.CacheManager;
+
+import org.apache.log4j.Logger;
+
+/**
+ * CacheManagement component for stelvio.
+ * 
+ * This class makes cache-administration accessible as a
+ * JMX-component using Spring configuration.
+ * 
+ * @author person4f9bc5bd17cc, Accenture
+ */
+public class CacheManagementImpl implements CacheManagement {
+	private Logger log = Logger.getLogger(CacheManagementImpl.class);
+	private CacheManager cacheManager;
+	
+	/**
+	 * {@inheritDoc CacheManagement#clearAll()}
+	 */
+	public void flushCache() {
+		if (log.isDebugEnabled()) {
+			log.debug("Flushing all elements in "+getCacheManager().getCacheNames().length+" configured cache(s).");
+		}
+		
+		cacheManager.clearAll();
+	}
+
+	/**
+	 * {@inheritDoc CacheManagement#getCacheManager()}
+	 */
+	public CacheManager getCacheManager() {
+		return cacheManager;
+	}
+
+	/**
+	 * 
+	 * @param cacheManager to set.
+	 */
+	public void setCacheManager(CacheManager cacheManager) {
+		this.cacheManager = cacheManager;
+	}
+}
