a08605119aff30845244bbeb6747e9d518813926
ska2812 <ska2812@localhost>
2006-11-05 17:12:16 +0000

    Multicasts the workload to others

---
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/error/strategy/support/ExceptionHandlerStrategyMulticaster.java
@@ -0,0 +1,13 @@
+package no.stelvio.common.error.strategy.support;
+
+import no.stelvio.common.error.strategy.ExceptionHandlerStrategy;
+
+/**
+ * Will multicast to others {@link ExceptionHandlerStrategy} to do the real work.
+ *
+ * @author personf8e9850ed756
+ * @todo write javadoc
+ * @todo maybe output from one should go into input to the next? maybe not call it multicast then? 
+ */
+public class ExceptionHandlerStrategyMulticaster implements ExceptionHandlerStrategy {
+}
