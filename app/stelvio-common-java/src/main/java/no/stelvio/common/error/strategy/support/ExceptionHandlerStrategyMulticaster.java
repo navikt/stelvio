185d4da95f97d37790e955d453ff6f7b8270a5e7
ska2812 <ska2812@localhost>
2006-11-06 19:36:22 +0000

    Adding to comment about using chain in the class name some how

--- app/stelvio-common-java/src/main/java/no/stelvio/common/error/strategy/support/ExceptionHandlerStrategyMulticaster.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/error/strategy/support/ExceptionHandlerStrategyMulticaster.java
@@ -7,7 +7,7 @@ import no.stelvio.common.error.strategy.ExceptionHandlerStrategy;
  *
  * @author personf8e9850ed756
  * @todo write javadoc
- * @todo maybe output from one should go into input to the next? maybe not call it multicast then? 
+ * @todo maybe output from one should go into input to the next? maybe not call it multicast then --> chain 
  */
 public class ExceptionHandlerStrategyMulticaster implements ExceptionHandlerStrategy {
 }
