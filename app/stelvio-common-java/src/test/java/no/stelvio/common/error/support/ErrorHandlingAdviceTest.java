2e8e628fa1808cafcd129737aebcd262b15d6e6d
ska2812 <ska2812@localhost>
2006-11-20 01:43:11 +0000

    Added unit test for ErrorHandlingAdvice

---
+++ app/stelvio-common-java/src/test/java/no/stelvio/common/error/support/ErrorHandlingAdviceTest.java
@@ -0,0 +1,30 @@
+package no.stelvio.common.error.support;
+
+import org.jmock.InAnyOrder;
+import org.jmock.Mockery;
+import org.junit.Test;
+
+import no.stelvio.common.error.ExceptionHandlerFacade;
+
+/**
+ * @author personf8e9850ed756
+ * @todo write javadoc
+ */
+public class ErrorHandlingAdviceTest {
+    @Test
+    public void exceptionIsSentToFacade() throws Throwable {
+        Mockery context = new Mockery();
+        final ExceptionHandlerFacade facade = context.mock(ExceptionHandlerFacade.class);
+        final IllegalArgumentException throwable = new IllegalArgumentException("test");
+
+        context.expects(new InAnyOrder() {{
+            one (facade).handleException(with(same(throwable)));
+        }});
+
+        ErrorHandlingAdvice advice = new ErrorHandlingAdvice();
+        advice.setExceptionHandlerFacade(facade);
+
+        advice.afterThrowing(null, null, null, throwable);
+        context.assertIsSatisfied();
+    }
+}
