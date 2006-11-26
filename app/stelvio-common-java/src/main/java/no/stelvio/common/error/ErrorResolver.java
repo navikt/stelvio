8a520367bced716120135f603243213ed3a7a583
ska2812 <ska2812@localhost>
2006-11-26 08:50:26 +0000

    Removed fixed comment

--- app/stelvio-common-java/src/main/java/no/stelvio/common/error/ErrorResolver.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/error/ErrorResolver.java
@@ -3,7 +3,6 @@ package no.stelvio.common.error;
 /**
  * @author personf8e9850ed756
  * @todo write javadoc
- * @todo should the name be ErrorHolder instead?
  */
 public interface ErrorResolver {
     Err resolve(Throwable throwable);
