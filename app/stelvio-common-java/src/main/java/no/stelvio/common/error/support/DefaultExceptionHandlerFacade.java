5fd4c523eb202eab93380d4a7cbe7d684aeebdce
ska2812 <ska2812@localhost>
2006-12-21 13:42:45 +0000

    Fixed merge error

--- app/stelvio-common-java/src/main/java/no/stelvio/common/error/support/DefaultExceptionHandlerFacade.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/error/support/DefaultExceptionHandlerFacade.java
@@ -1,23 +1,17 @@
 package no.stelvio.common.error.support;
 
+import com.sun.beans.ObjectHandler;
+import no.stelvio.common.error.ExceptionHandlerFacade;
+import no.stelvio.common.error.strategy.ExceptionHandlerStrategy;
+import no.stelvio.common.error.strategy.support.FallbackExceptionHandlerStrategy;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-
-import com.sun.beans.ObjectHandler;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Transformer;
-
-import no.stelvio.common.error.ExceptionHandlerFacade;
-import no.stelvio.common.error.StelvioException;
-import no.stelvio.common.error.strategy.ExceptionHandlerStrategy;
-import no.stelvio.common.error.strategy.support.FallbackExceptionHandlerStrategy;
-
-import java.util.*;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 /**
  * @author personf8e9850ed756
@@ -137,70 +131,13 @@ public class DefaultExceptionHandlerFacade implements ExceptionHandlerFacade {
             t = defaultStrategy.handleException(e);
         } catch (Throwable throwable) {
             // Original exception
-            fallbackStrategy.handleException(e);
+            t = fallbackStrategy.handleException(e);
             // Just print out new exception (database down, etc)
             fallbackStrategy.handleException(throwable);
         }
 
         return t;
     }
-
-
-	public <T extends Throwable> void rethrow(T e) throws Throwable {
-		T t;
-
-		try {
-		    t = defaultStrategy.handleException(e);
-		} catch (Throwable throwable) {
-			// Original exception
-		    t = fallbackStrategy.handleException(e);
-			// Just print out new exception (database down, etc)
-		    fallbackStrategy.handleException(throwable);
-		}
-
-		throw t;
-	}
-
-	public <T extends Throwable> void throwNew(Class<T> exceptionToThrow, Class<? extends StelvioException> cause, Object... params) throws Throwable {
-		T se = createException(exceptionToThrow, cause, params);
-		T t;
-
-		try {
-			t = defaultStrategy.handleException(se);
-		} catch (Throwable throwable) {
-			// Original exception
-			t = fallbackStrategy.handleException(se);
-			// Just print out new exception (database down, etc)
-			fallbackStrategy.handleException(throwable);
-		}
-
-		throw t;
-	}
-
-	private <T extends Throwable> T createException(final Class<T> exceptionToThrow, final Class<? extends StelvioException> cause, final Object... params) {
-		final List<Class> classes = new ArrayList<Class>();
-		classes.add(cause.getClass());
-
-		for (Object param : params) {
-			classes.add(param.getClass());
-		}
-
-		T t = null;
-		try {
-			final Constructor<T> constructor = exceptionToThrow.getConstructor(classes.toArray(new Class[classes.size()]));
-			t = constructor.newInstance(cause, params);
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();  // TODO: implement body
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();  // TODO: implement body
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();  // TODO: implement body
-		} catch (InstantiationException e) {
-			e.printStackTrace();  // TODO: implement body
-		}
-
-		return t;
-	}
 
 	public void setFallbackStrategy(ExceptionHandlerStrategy fallbackStrategy) {
         this.fallbackStrategy = fallbackStrategy;
