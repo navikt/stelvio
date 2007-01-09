727258e9a477b79798062d689d29312dce4229e3
ska2812 <ska2812@localhost>
2007-01-09 21:57:16 +0000

    The implementation of the codes table factory

---
+++ app/stelvio-star-example/layers/service/starexample/stelvio-service-star-example-ejb/src/main/java/no/nav/service/pensjon/DefaultCodesTableFactoryBean.java
@@ -0,0 +1,26 @@
+package no.nav.service.pensjon;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.ejb.CreateException;
+
+import no.stelvio.common.codestable.CodesTableItem;
+import no.stelvio.common.codestable.CodesTableItemPeriodic;
+import no.stelvio.common.codestable.CodesTableNotFoundException;
+import no.stelvio.common.codestable.factory.CodesTableFactory;
+
+import org.springframework.ejb.support.AbstractStatelessSessionBean;
+
+/**
+ * Bean implementation class for CodesTableFactory.
+ */
+public class DefaultCodesTableFactoryBean implements CodesTableFactory {
+	public <T extends CodesTableItem> List<T> retrieveCodesTable(Class<T> codesTableClass) throws CodesTableNotFoundException {
+		return Collections.EMPTY_LIST;
+	}
+
+	public <T extends CodesTableItemPeriodic> List<T> retrieveCodesTablePeriodic(Class<T> codesTableClass) throws CodesTableNotFoundException {
+		return Collections.EMPTY_LIST;
+	}
+}
