f7529ffaac2b7c2c0ea4eaaed2c7dc07ba679276
maa2812 <maa2812@localhost>
2007-01-12 14:54:21 +0000

    New files, moved from stelvio-common

---
+++ app/stelvio-service-java/src/main/java/no/stelvio/refactor/menu/service/support/DefaultMenuItemService.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package no.stelvio.refactor.menu.service.support;
+
+import no.stelvio.service.menu.service.MenuItemService;
+
+/**
+ * Implements services for accessing data related to menu. 
+ * 
+ * @author person4f9bc5bd17cc, Accenture
+ */
+public class DefaultMenuItemService implements MenuItemService {
+//	private MenuRepository menuRepository;
+//
+//	/**
+//	 * {@inheritDoc MenuItemService#getMenuItems()}
+//	 */
+//	public List<MenuItem> getMenuItems() {
+//		return menuRepository.getParents();
+//	}
+//	
+//	/**
+//	 * @param menuRepository the menuRepository to set
+//	 */
+//	public void setMenuRepository(MenuRepository menuRepository) {
+//		this.menuRepository = menuRepository;
+//	}
+}
