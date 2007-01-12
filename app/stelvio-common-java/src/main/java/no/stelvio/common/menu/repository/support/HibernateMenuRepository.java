be4f316b9a3e9652845ebb9dafd181b4d8066c94
maa2812 <maa2812@localhost>
2007-01-12 09:44:32 +0000

    Hibernate implementation of menurespository

---
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/menu/repository/support/HibernateMenuRepository.java
@@ -0,0 +1,44 @@
+package no.stelvio.common.menu.repository.support;
+
+import java.util.List;
+
+import org.springframework.orm.hibernate3.HibernateTemplate;
+
+import no.stelvio.common.menu.domain.MenuItem;
+import no.stelvio.common.menu.repository.MenuRepository;
+
+public class HibernateMenuRepository implements MenuRepository {
+
+	public HibernateTemplate hibernateTemplate;
+	
+	/**
+	 * Default no-arg constructor
+	 * Calls to this constructor must be followed by the injection of hibernateTemplate
+	 */
+	public HibernateMenuRepository(){}
+	
+	/**
+	 * {@inheritDoc MenuRepository#getParents()}
+	 */	
+	@SuppressWarnings("unchecked")
+	public List<MenuItem> getParents() {
+		return hibernateTemplate.findByNamedQuery("MenuItem.findParents");
+	}
+
+	/**
+	 * Gets a HibernateTemplate
+	 * @return hibernateTemplate 
+	 */
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+	
+	/**
+	 * Sets a HibernateTemplate
+	 * @param hibernateTemplate instance
+	 */
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
+
+}
