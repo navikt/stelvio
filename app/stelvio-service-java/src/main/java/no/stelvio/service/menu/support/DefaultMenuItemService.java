86eaba006aa37d5fb95966e9b7d2f73b84e53c05
maa2812 <maa2812@localhost>
2007-01-15 14:12:52 +0000

    Code has been added, after it was removed as a work around before refactoring

--- app/stelvio-service-java/src/main/java/no/stelvio/service/menu/support/DefaultMenuItemService.java
+++ app/stelvio-service-java/src/main/java/no/stelvio/service/menu/support/DefaultMenuItemService.java
@@ -1,8 +1,9 @@
-/**
- * 
- */
 package no.stelvio.service.menu.support;
 
+import java.util.List;
+
+import no.stelvio.domain.menu.MenuItem;
+import no.stelvio.repository.menu.MenuRepository;
 import no.stelvio.service.menu.MenuItemService;
 
 /**
@@ -11,19 +12,19 @@ import no.stelvio.service.menu.MenuItemService;
  * @author person4f9bc5bd17cc, Accenture
  */
 public class DefaultMenuItemService implements MenuItemService {
-//	private MenuRepository menuRepository;
-//
-//	/**
-//	 * {@inheritDoc MenuItemService#getMenuItems()}
-//	 */
-//	public List<MenuItem> getMenuItems() {
-//		return menuRepository.getParents();
-//	}
-//	
-//	/**
-//	 * @param menuRepository the menuRepository to set
-//	 */
-//	public void setMenuRepository(MenuRepository menuRepository) {
-//		this.menuRepository = menuRepository;
-//	}
+	private MenuRepository menuRepository;
+
+	/**
+	 * {@inheritDoc MenuItemService#getMenuItems()}
+	 */
+	public List<MenuItem> getMenuItems() {
+		return menuRepository.getParents();
+	}
+	
+	/**
+	 * @param menuRepository the menuRepository to set
+	 */
+	public void setMenuRepository(MenuRepository menuRepository) {
+		this.menuRepository = menuRepository;
+	}
 }
