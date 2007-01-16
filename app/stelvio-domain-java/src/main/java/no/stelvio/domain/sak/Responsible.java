30d7d32850ccfc498db33b6fcc349396d8724ea0
maa2812 <maa2812@localhost>
2007-01-16 13:22:41 +0000

    refactor package name

---
+++ app/stelvio-domain-java/src/main/java/no/stelvio/domain/sak/Responsible.java
@@ -0,0 +1,57 @@
+package no.stelvio.domain.sak;
+
+import java.util.List;
+
+/**
+ * TODO: Document me
+ * 
+ * @author person4f9bc5bd17cc, Accenture
+ * @version $id$
+ */
+public class Responsible {
+	private String id;
+	private String description;
+	private List<Area> areas;
+	
+	/**
+	 * @return the areas
+	 */
+	public List<Area> getAreas() {
+		return areas;
+	}
+	
+	/**
+	 * @param areas the areas to set
+	 */
+	public void setAreas(List<Area> areas) {
+		this.areas = areas;
+	}
+	
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+}
