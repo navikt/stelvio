77971e463cbf041c3939e01c732ec50cc9ce979b
kkb2812 <kkb2812@localhost>
2007-01-16 13:45:14 +0000

    Changed class name, enhanced documentation, and removed checkstyle errors

--- app/stelvio-service-java/src/main/java/no/stelvio/service/menu/support/DefaultMenuService.java
+++ app/stelvio-service-java/src/main/java/no/stelvio/service/menu/support/DefaultMenuService.java
@@ -4,25 +4,31 @@ import java.util.List;
 
 import no.stelvio.domain.menu.MenuItem;
 import no.stelvio.repository.menu.MenuRepository;
-import no.stelvio.service.menu.MenuItemService;
+import no.stelvio.service.menu.MenuService;
 
 /**
- * Implements services for accessing data related to menu. 
+ * Menu service implementation of the {@link MenuService} business interface.
  * 
  * @author person4f9bc5bd17cc, Accenture
+ * @version $Id$ 
  */
-public class DefaultMenuItemService implements MenuItemService {
+public class DefaultMenuService implements MenuService {
 	private MenuRepository menuRepository;
 
 	/**
-	 * {@inheritDoc MenuItemService#getMenuItems()}
+	 * {@inheritDoc MenuService#getMenuItems()}
+	 * 
+	 * @return List of all the menu items.
 	 */
 	public List<MenuItem> getMenuItems() {
 		return menuRepository.getParents();
 	}
-	
+
 	/**
-	 * @param menuRepository the menuRepository to set
+	 * Sets the menu repository (dependency injected).
+	 * 
+	 * @param menuRepository
+	 *            The menu repository to set
 	 */
 	public void setMenuRepository(MenuRepository menuRepository) {
 		this.menuRepository = menuRepository;
