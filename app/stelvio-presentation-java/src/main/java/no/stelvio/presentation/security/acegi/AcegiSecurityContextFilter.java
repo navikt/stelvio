d4e4254be8edadb65d10b6ffd0bb7645ab8c72fc
ska2812 <ska2812@localhost>
2007-01-17 08:48:59 +0000

    Cleaned up code

--- app/stelvio-presentation-java/src/main/java/no/stelvio/presentation/security/acegi/AcegiSecurityContextFilter.java
+++ app/stelvio-presentation-java/src/main/java/no/stelvio/presentation/security/acegi/AcegiSecurityContextFilter.java
@@ -2,13 +2,10 @@ package no.stelvio.presentation.security.acegi;
 
 import java.io.IOException;
 import java.security.Principal;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import no.stelvio.presentation.filter.AbstractFilter;
 
 import org.acegisecurity.Authentication;
 import org.acegisecurity.GrantedAuthority;
@@ -16,62 +13,52 @@ import org.acegisecurity.GrantedAuthorityImpl;
 import org.acegisecurity.context.SecurityContextHolder;
 import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
 
+import no.stelvio.presentation.filter.AbstractFilter;
+
 /**
- * A filter that populates the
- * <code>org.acegisecurity.context.SecurityContext</code> in the
- * <code>org.acegisecurity.context.SecurityContextHolder</code> with an
- * <code>org.acegisecurity.Authentication</code> object using the principal
- * obtained from java security. The filter is dependent on that the
- * <code>SecurityContextHolder</code> is set up with a
- * <code>SecurityContext</code> in order to perform its intended operations,
- * so the filter
- * <code>org.acegisecurity.context.HttpSessionContextIntegrationFilter</code>
- * which handles this should be configured before the
- * <code>AcegiSecurityContextFilter</code> in the filterchain.
- * 
+ * A filter that populates the <code>org.acegisecurity.context.SecurityContext</code> in the
+ * <code>org.acegisecurity.context.SecurityContextHolder</code> with an <code>org.acegisecurity.Authentication</code>
+ * object using the principal obtained from java security. The filter is dependent on that the
+ * <code>SecurityContextHolder</code> is set up with a <code>SecurityContext</code> in order to perform its intended
+ * operations, so the filter <code>org.acegisecurity.context.HttpSessionContextIntegrationFilter</code> which handles
+ * this should be configured before the <code>AcegiSecurityContextFilter</code> in the filterchain.
+ *
  * @author persondab2f89862d3, Accenture
  * @version $Id$
  */
 public class AcegiSecurityContextFilter extends AbstractFilter {
-
-	/**
-	 * {@inheritDoc}
-	 */
+	/** {@inheritDoc} */
 	@Override
-	public void doFilter(HttpServletRequest request,
-			HttpServletResponse response, FilterChain chain)
+	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		
+
 		Principal principal = request.getUserPrincipal();
-		Authentication auth = (SecurityContextHolder.getContext() != null) ? SecurityContextHolder
-				.getContext().getAuthentication()
-				: null;
-		if (auth == null && principal != null) {
-			setAuthenticationObject(principal);
-		} else {
-			if (principal != null && !principal.equals(auth.getPrincipal())) {
+		Authentication auth = (SecurityContextHolder.getContext() != null) ?
+				SecurityContextHolder.getContext().getAuthentication() : null;
+
+		if (principal != null) {
+			if (auth == null || !principal.equals(auth.getPrincipal())) {
 				setAuthenticationObject(principal);
 			}
 		}
+
 		chain.doFilter(request, response);
 	}
 
 	/**
-	 * Sets the <code>org.acegisecurity.Authentication</code> object in the
-	 * acegi security context with the <code>java.security.Principal</code>.
-	 * 
-	 * @param principal
-	 *            the principal
+	 * Sets the <code>org.acegisecurity.Authentication</code> object in the Acegi security context with the
+	 * <code>java.security.Principal</code>.
+	 *
+	 * @param principal the principal to generate a token from
 	 */
 	private void setAuthenticationObject(Principal principal) {
 		Object credentials = "";
-		GrantedAuthority granted = new GrantedAuthorityImpl("Granted");
-		GrantedAuthority[] authorities = { granted };
-		// This authentication object is fully trusted when created with the 3
-		// param contructor,
+		GrantedAuthority[] authorities = {new GrantedAuthorityImpl("Granted")};
+
+		// This authentication object is fully trusted when created with the 3 param contructor,
 		// i.e. isAuthenticated=true.
-		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-				principal, credentials, authorities);
+		UsernamePasswordAuthenticationToken token =
+				new UsernamePasswordAuthenticationToken(principal, credentials, authorities);
 		SecurityContextHolder.getContext().setAuthentication(token);
 	}
 }
