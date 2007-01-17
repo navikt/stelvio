6414addac092d4f20e035191cc9c4382f76a9d92
ska2812 <ska2812@localhost>
2007-01-17 11:29:32 +0000

    Made into concrete classes that should be extended by transfer objects

--- app/stelvio-common-java/src/main/java/no/stelvio/common/transferobject/ServiceRequest.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/transferobject/ServiceRequest.java
@@ -1,21 +1,39 @@
 package no.stelvio.common.transferobject;
 
-import java.io.Serializable
+import java.io.Serializable;
 
 import no.stelvio.common.context.RequestContext;
 
 
 /**
- * Business requests are serializable and (may) contain a RequestContext.
+ * The base class for all service requests, which are serializable and may contain a <code>RequestContext</code>.
  * 
  * @author personff564022aedd
  * @author person15754a4522e7
  * @author personf8e9850ed756
+ * @see RequestContext
  * @version $Id$
  */
-public interface ServiceRequest extends Serializable {
+public class ServiceRequest implements Serializable {
+	/** Used to ensure backwards compatability when serializing instances. */
+	private static final long serialVersionUID = -3970002011961058329L;
+	/** The <code>RequestContext</code> instance to use. */
+	private RequestContext requestContext;
+
 	/**
 	 * @return {@link RequestContext}
 	 */
-	RequestContext getRequestContext();
+	public RequestContext getRequestContext() {
+		return requestContext;
+	}
+
+	/**
+	 * Should only be set by subclasses or by reflection through interceptors used when the transfer object goes through
+	 * layers.
+	 *
+	 * @param requestContext The <code>RequestContext</code> instance to use.
+	 */
+	protected void setRequestContext(RequestContext requestContext) {
+		this.requestContext = requestContext;
+	}
 }
