4e420d146f6eb2744435486718ac1d3decc385d6
ska2812 <ska2812@localhost>
2007-01-17 08:19:39 +0000

    Exception is used when no item is found for the given code

---
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/codestable/ItemNotFoundException.java
@@ -0,0 +1,28 @@
+package no.stelvio.common.codestable;
+
+import no.stelvio.common.codestable.support.CodesTableException;
+
+/**
+ * Exception thrown when <code>CodesTable</code> or a <code>CodesTablePeriodic</code> doesn't hold a requested
+ * item.
+ *
+ * @author personf8e9850ed756
+ * @version $Id$
+ */
+public class ItemNotFoundException extends CodesTableException {
+	/**
+	 * Creates an instance of the exception with the given code.
+	 *
+	 * @param code the item is not found for this code.
+	 */
+	public ItemNotFoundException(Object code) {
+		super(code);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected String messageTemplate(final int numArgs) {
+		return "No codes table item found for code {0}";
+	}
+}
