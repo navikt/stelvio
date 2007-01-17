09432d574e1a4d66a48775e8048d55f52004f467
ska2812 <ska2812@localhost>
2007-01-17 14:42:41 +0000

    Changed to use HenvendelseCti

--- app/stelvio-star-example/layers/repository/starexample/stelvio-repository-star-example-java/src/main/java/no/stelvio/repository/codestable/support/HibernateCodesTableRepository.java
+++ app/stelvio-star-example/layers/repository/starexample/stelvio-repository-star-example-java/src/main/java/no/stelvio/repository/codestable/support/HibernateCodesTableRepository.java
@@ -1,24 +1,19 @@
 package no.stelvio.repository.codestable.support;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-import no.stelvio.common.codestable.CodesTableItem;
+import no.nav.domain.pensjon.codestable.HenvendelseTypeCti;
 import no.stelvio.repository.codestable.CodesTableRepository;
 
 public class HibernateCodesTableRepository implements CodesTableRepository {
 
 	public List fetchCodesTable(Class codestable) {
 		List list = new ArrayList();
-		list.add(new Cti("code", "decode", Locale.ENGLISH, true));
+		list.add(new HenvendelseTypeCti("code", "decode", new Date(), new Date(), Locale.ENGLISH, true));
 		
 		return list;
-	}
-	
-	public static class Cti extends CodesTableItem {
-		public Cti(String code, String decode, Locale locale, boolean isValid) {
-			super(code, decode, locale, isValid);
-		}
 	}
 }
