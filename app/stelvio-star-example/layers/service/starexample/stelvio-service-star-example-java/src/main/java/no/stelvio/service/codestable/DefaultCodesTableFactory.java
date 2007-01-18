e694a153dd10a8515daa7f12ea5589da6910a8da
ska2812 <ska2812@localhost>
2007-01-18 06:18:50 +0000

    Cleaned up directoy structure

---
+++ app/stelvio-star-example/layers/service/starexample/stelvio-service-star-example-java/src/main/java/no/stelvio/service/codestable/DefaultCodesTableFactory.java
@@ -0,0 +1,28 @@
+package no.stelvio.service.codestable;
+
+import java.util.List;
+
+import no.stelvio.common.codestable.CodesTableItem;
+import no.stelvio.common.codestable.CodesTableItemPeriodic;
+import no.stelvio.common.codestable.CodesTableNotFoundException;
+import no.stelvio.common.codestable.factory.CodesTableFactory;
+import no.stelvio.repository.codestable.CodesTableRepository;
+
+/**
+ * Bean implementation class for CodesTableFactory.
+ */
+public class DefaultCodesTableFactory implements CodesTableFactory {
+	private CodesTableRepository codesTableRepository;
+	
+	public <T extends CodesTableItem> List<T> createCodesTable(Class<T> codesTableClass) throws CodesTableNotFoundException {
+		return codesTableRepository.fetchCodesTable(codesTableClass); 
+	}
+
+	public <T extends CodesTableItemPeriodic> List<T> createCodesTablePeriodic(Class<T> codesTableClass) throws CodesTableNotFoundException {
+		return codesTableRepository.fetchCodesTable(codesTableClass); 
+	}
+	
+	public void setCodesTableRepository(CodesTableRepository codesTableRepository) {
+		this.codesTableRepository = codesTableRepository;
+	}
+}
