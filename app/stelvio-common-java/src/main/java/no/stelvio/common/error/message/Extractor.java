aca93d63f9d6a27bfbe61c25a315615d50ffa8f4
ska2812 <ska2812@localhost>
2007-01-18 06:12:50 +0000

    Improvement of javadoc

--- app/stelvio-common-java/src/main/java/no/stelvio/common/error/message/Extractor.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/error/message/Extractor.java
@@ -1,17 +1,15 @@
 package no.stelvio.common.error.message;
 
 /**
- * Used to extract a messageFrom which is used to describe an exception.
+ * Used to extract a message which is used to describe an exception.
  *
  * @author personf8e9850ed756
- * @todo better javadoc
- * @todo is extractor a good name?
  */
 public interface Extractor {
     /**
-     * Retrieves the message that describes the given exception.
+     * Extract the message that describes the given exception.
      *
-     * @param throwable the exception to retrieve a message for.
+     * @param throwable the exception to extract a message for.
      * @return the message that describes the given exception.
      */
     String messageFor(Throwable throwable);
