c9b6dfb3d0f5d93ecb11061e67f0b5bf8888c618
ska2812 <ska2812@localhost>
2007-01-22 11:34:58 +0000

    Codestable support classes

---
+++ app/stelvio-repository-java/src/main/java/no/stelvio/repository/codestable/CodesTableRepository.java
@@ -0,0 +1,7 @@
+package no.stelvio.repository.codestable;
+
+import java.util.List;
+
+public interface CodesTableRepository {
+	List fetchCodesTable(Class codestable);
+}
---
+++ app/stelvio-repository-java/src/main/java/no/stelvio/repository/codestable/support/HibernateCodesTableRepository.java
@@ -0,0 +1,15 @@
+package no.stelvio.repository.codestable.support;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import no.stelvio.repository.codestable.CodesTableRepository;
+
+public class HibernateCodesTableRepository implements CodesTableRepository {
+
+	public List fetchCodesTable(Class codestable) {
+		List list = new ArrayList();
+		
+		return list;
+	}
+}
