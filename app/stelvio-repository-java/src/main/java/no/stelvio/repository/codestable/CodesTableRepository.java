6c9d54852ec4b15cdc41fac0857ca6dae5bc9f22
maa2812 <maa2812@localhost>
2007-01-23 08:53:31 +0000

    Changed method signature, using generics to specify that the class passed as a param to the fetchCodesTable-method must be a AbstractCodestableItem implementation

--- app/stelvio-repository-java/src/main/java/no/stelvio/repository/codestable/CodesTableRepository.java
+++ app/stelvio-repository-java/src/main/java/no/stelvio/repository/codestable/CodesTableRepository.java
@@ -2,6 +2,14 @@ package no.stelvio.repository.codestable;
 
 import java.util.List;
 
+import no.stelvio.common.codestable.support.AbstractCodesTableItem;
+
 public interface CodesTableRepository {
-	List fetchCodesTable(Class codestable);
+	
+	/**
+	 * Retrieves all codestable items specified by the CodesTableItem class
+	 * @param codestableItem AbstractCodesTableItem implementation that may be instantiated (non-abstract)
+	 * @return list of specified CodesTableItem
+	 */
+	List fetchCodesTable(Class<AbstractCodesTableItem>  codestableItem);
 }
