4a38cc71b92ea6228336d4a08d343db5c7e5c954
maa2812 <maa2812@localhost>
2007-01-23 08:00:24 +0000

    Changed table-mapping. Physical database uses T_-prefix for tables. Added javadoc

--- app/stelvio-star-example/layers/domain/starexample/stelvio-domain-star-example-java/src/main/java/no/nav/domain/pensjon/codestable/HenvendelseTypeCti.java
+++ app/stelvio-star-example/layers/domain/starexample/stelvio-domain-star-example-java/src/main/java/no/nav/domain/pensjon/codestable/HenvendelseTypeCti.java
@@ -28,23 +28,36 @@ import no.stelvio.common.codestable.CodesTableItemPeriodic;
  *
  */
 @Entity
-@Table(name="K_HENVENDELSE_T")
+@Table(name="T_K_HENVENDELSE_T")
 @AttributeOverrides({
     @AttributeOverride(name="code", column=@Column(name="K_HENVENDELSE_T")),
     @AttributeOverride(name="decode", column=@Column(name="dekode")),
     @AttributeOverride(name="valid", column=@Column(name="er_gyldig")),
     @AttributeOverride(name="fromDate", column=@Column(name="dato_fom")),
-    @AttributeOverride(name="toDate", column=@Column(name="dato_tom"))
+    @AttributeOverride(name="toDate", column=@Column(name="dato_tom"))    
 })
 @NamedQuery(name="HenvendelseTypeCti.findAll",query="select htcti from HenvendelseTypeCti htcti")
 public class HenvendelseTypeCti extends CodesTableItemPeriodic {
 
 	private static final long serialVersionUID = 4989579646384579677L;
 	
+	/**
+	 * Default no-arg constructor.
+	 * Should only be called by persistence provider
+	 */
 	protected HenvendelseTypeCti() {
 		super();
 	}
 
+	/**
+	 * Constructs a new HenvendelseTypeCti
+	 * @param code the unique identifier for this object
+	 * @param decode the decode
+	 * @param fromDate valid from date
+	 * @param toDate valid to date
+	 * @param locale locale for this instance
+	 * @param valid <code>true</code> if this item is valid, <code>false</code> if it's invalid
+	 */
 	public HenvendelseTypeCti(String code, String decode, Date fromDate, Date toDate, Locale locale, boolean valid) {
 		super(code, decode, fromDate, toDate, locale, valid);
 	}
