b7664a94cabca10c1dca27d527361cb2db20475f
ska2812 <ska2812@localhost>
2007-01-24 14:29:24 +0000

    Forgotten to create the codes table holder for the lists

--- app/stelvio-service-java/src/main/java/no/stelvio/service/codestable/RepositoryCodesTableFactory.java
+++ app/stelvio-service-java/src/main/java/no/stelvio/service/codestable/RepositoryCodesTableFactory.java
@@ -1,11 +1,15 @@
 package no.stelvio.service.codestable;
 
+import java.util.List;
+
 import no.stelvio.common.codestable.CodesTable;
 import no.stelvio.common.codestable.CodesTableItem;
 import no.stelvio.common.codestable.CodesTableItemPeriodic;
 import no.stelvio.common.codestable.CodesTableNotFoundException;
 import no.stelvio.common.codestable.CodesTablePeriodic;
 import no.stelvio.common.codestable.factory.CodesTableFactory;
+import no.stelvio.common.codestable.support.DefaultCodesTable;
+import no.stelvio.common.codestable.support.DefaultCodesTablePeriodic;
 import no.stelvio.repository.codestable.CodesTableRepository;
 
 /**
@@ -14,12 +18,18 @@ import no.stelvio.repository.codestable.CodesTableRepository;
 public class RepositoryCodesTableFactory implements CodesTableFactory {
 	private CodesTableRepository codesTableRepository;
 	
+	@SuppressWarnings("unchecked")
 	public <T extends CodesTableItem> CodesTable<T> createCodesTable(Class<T> codesTableClass) throws CodesTableNotFoundException {
-		return (CodesTable<T>) codesTableRepository.findCodesTableItems(codesTableClass);
+		List<T> items = codesTableRepository.findCodesTableItems(codesTableClass);
+		
+		return new DefaultCodesTable<T>(items); 
 	}
 
+	@SuppressWarnings("unchecked")
 	public <T extends CodesTableItemPeriodic> CodesTablePeriodic<T> createCodesTablePeriodic(Class<T> codesTableClass) throws CodesTableNotFoundException {
-		return (CodesTablePeriodic<T>) codesTableRepository.findCodesTableItems(codesTableClass); 
+		List<T> items = codesTableRepository.findCodesTableItems(codesTableClass);
+
+		return new DefaultCodesTablePeriodic<T>(items); 
 	}
 	
 	public void setCodesTableRepository(CodesTableRepository codesTableRepository) {
