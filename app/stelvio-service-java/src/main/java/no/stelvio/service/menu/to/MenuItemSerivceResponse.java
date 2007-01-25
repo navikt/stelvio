7d7219a8a16edf0da63958b1a610127b8ef2fa1e
maa2812 <maa2812@localhost>
2007-01-25 11:50:56 +0000

    Changed name from MenuItemResponse

---
+++ app/stelvio-service-java/src/main/java/no/stelvio/service/menu/to/MenuItemSerivceResponse.java
@@ -0,0 +1,55 @@
+package no.stelvio.service.menu.to;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import no.stelvio.common.transferobject.ServiceResponse;
+import no.stelvio.domain.menu.MenuItem;
+
+public class MenuItemSerivceResponse extends ServiceResponse {
+
+
+	private static final long serialVersionUID = 2721957614150130964L;
+	
+	private List<MenuItem> menuItemList;
+	
+	/**
+	 * Constructs a new MenuItemSerivceResponse without any menu items
+	 * The MenuItemSerivceResponse will be set to hold an empty list of <code>MenuItem</code>s
+	 *
+	 */
+	public MenuItemSerivceResponse(){
+		super();
+		this.menuItemList = new ArrayList<MenuItem>();
+	}
+	
+	/**
+	 * Constructs a new MenuItemSerivceResponse with holding the list of <code>MenuItem</code>s specified by the parameter
+	 * If the parameter is <code>null</code> the MenuItemSerivceResponse will be set to hold an empty list
+	 * @param menuItemList
+	 */
+	public MenuItemSerivceResponse(List<MenuItem> menuItemList){
+		this();
+		//Only set a list if it's not null
+		if(menuItemList != null){
+			this.menuItemList = menuItemList;
+		}
+	}
+
+	/**
+	 * Gets a list of <code>MenuItem</code>s
+	 * @return list of <code>MenuItem</code>s
+	 */
+	public List<MenuItem> getMenuItemList() {
+		return menuItemList;
+	}
+
+	/**
+	 * Sets a list of <code>MenuItem</code>s
+	 * @param menuItemList list of <code>MenuItem</code>s
+	 */
+	public void setMenuItemList(List<MenuItem> menuItemList) {
+		this.menuItemList = menuItemList;
+	}
+
+}
