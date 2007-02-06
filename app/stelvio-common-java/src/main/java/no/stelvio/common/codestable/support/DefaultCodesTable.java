cf2507c4426c5955c45b81aa61c19155563689ef
maa2812 <maa2812@localhost>
2007-02-06 10:49:47 +0000

    Implementation uses map instead of list as internal store for items

--- app/stelvio-common-java/src/main/java/no/stelvio/common/codestable/support/DefaultCodesTable.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/codestable/support/DefaultCodesTable.java
@@ -1,15 +1,16 @@
 package no.stelvio.common.codestable.support;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import no.stelvio.common.codestable.CodesTable;
 import no.stelvio.common.codestable.CodesTableItem;
 import no.stelvio.common.codestable.DecodeNotFoundException;
 import no.stelvio.common.codestable.ItemNotFoundException;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
 
 /**
@@ -22,15 +23,16 @@ import org.apache.commons.collections.Predicate;
  * @todo add safe copying of input/output, that is, constructor and getItems()
  */
 public class DefaultCodesTable<T extends CodesTableItem> implements CodesTable {
-	/** List of <code>CodesTableItem</code>s this <code>DefaultCodesTable</code> consists of. */
-	private List<T> codesTableItems;
 	
+	/** Map containing Codes-CodesTableItem pairs*/
+	private Map<Object, T> codeCodesTableItemMap;
 	
 	/**
-	 * Holds the filtered list of <code>CodesTableItem</code>s created by taking the full list and applying the
+	 * Holds the filtered map of <code>CodesTableItem</code>s created by taking the full list and applying the
 	 * predicates.
 	 */
-	private List<T> filteredCodesTableItems;
+	private Map<Object, T> filteredCodeCodesTableItemMap;	
+	
 
 	/** List of predicates to use for filtering the list of <code>CodesTableItem</code>s */
 	private List<Predicate> predicates = new ArrayList<Predicate>();
@@ -41,15 +43,23 @@ public class DefaultCodesTable<T extends CodesTableItem> implements CodesTable {
 	 * @param codesTableItems list of <code>CodesTableItem</code>s this <code>DefaultCodesTable</code> consists of. 
 	 */
 	public DefaultCodesTable(List<T> codesTableItems) {	
-		this.codesTableItems = codesTableItems;
+		this.codeCodesTableItemMap = new HashMap<Object, T>();
+		if(codesTableItems != null){
+			for (T codesTableItem : codesTableItems) {
+				codeCodesTableItemMap.put(codesTableItem.getCode(), codesTableItem);
+			}
+		}
+		this.filteredCodeCodesTableItemMap = new HashMap<Object, T>(codeCodesTableItemMap);
 	}
 	
 	/** 
 	 * {@inheritDoc}
 	 */
 	public List<T> getItems() {
-		return codesTableItems;
-		
+		if(predicates.isEmpty()){
+			return new ArrayList<T>(codeCodesTableItemMap.values());
+		}
+		return new ArrayList<T>(filteredCodeCodesTableItemMap.values());	
 	}
 
 	/** 
@@ -60,19 +70,9 @@ public class DefaultCodesTable<T extends CodesTableItem> implements CodesTable {
 								
 		//There are no predicates for the items in the codestable  
 		if (this.predicates.isEmpty()){
-			for (T c : this.codesTableItems){
-				if (c.getCode().equals(code.toString())){
-					cti = c;
-					break;
-				}
-			}
+			cti = this.codeCodesTableItemMap.get(code);
 		} else { 
-			for(T c : this.filteredCodesTableItems){
-				if (c.getCode().equals(code.toString())){
-					cti = c;
-					break;
-				}
-			}
+			cti = this.filteredCodeCodesTableItemMap.get(code);
 		}
 
 		if (cti == null) {
@@ -86,17 +86,27 @@ public class DefaultCodesTable<T extends CodesTableItem> implements CodesTable {
 	 * {@inheritDoc}
 	 */
 	public void addPredicate(Predicate predicate) {
-		//If there are no previous predicates added to a codestable, all of the codetableitems in a codetable
-		//are filtered, or else the codestableitems in the filtered collection are filtered.
-		synchronized (this.filteredCodesTableItems){
-			if(this.predicates.isEmpty()){
-				this.filteredCodesTableItems = (ArrayList<T>) CollectionUtils.select(this.codesTableItems, predicate);
-			} else{
-				this.filteredCodesTableItems = (ArrayList<T>)
-						CollectionUtils.select(this.filteredCodesTableItems, predicate);
-			}
-		}
 		
+		//If there are no previous predicates added to a codestable, all of the codetableitems in a codetable
+		//are filtered, or else the codestableitems in the filtered map are filtered.
+		synchronized (this.filteredCodeCodesTableItemMap){
+			if(this.predicates.isEmpty()){
+				this.filteredCodeCodesTableItemMap = new HashMap<Object, T>(codeCodesTableItemMap);
+			}
+			
+			ArrayList<Object> keysFilteredOut = new ArrayList<Object>();
+			
+			//Loop through filter map and remove items that fails eval of filter rules
+			for (Object code : filteredCodeCodesTableItemMap.keySet()) {
+				//If CodesTableItem doesn't evaluate according to predicate, remove from filtered list
+				if(!predicate.evaluate(filteredCodeCodesTableItemMap.get(code))){
+					keysFilteredOut.add(code);
+				}
+			}
+			for (Object filteredOutKey : keysFilteredOut) {
+				filteredCodeCodesTableItemMap.remove(filteredOutKey);
+			}
+		}				
 		this.predicates.add(predicate);	
 	}
 
@@ -105,37 +115,26 @@ public class DefaultCodesTable<T extends CodesTableItem> implements CodesTable {
 	 */
 	public void resetPredicates() {
 		this.predicates.clear();
-		this.filteredCodesTableItems.clear();
+		//This is done in addPredicate if no predicates exists, 
+		//doing it here anyway to avoid stale version of filteredCodeCodesTableItemMap
+		this.filteredCodeCodesTableItemMap = new HashMap<Object, T>(codeCodesTableItemMap);
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public String getDecode(Object code) {		
+	public String getDecode(Object code) throws ItemNotFoundException, DecodeNotFoundException{		
 		String decode = null;
-		String defaultDecode = null;
 
-		// If there are predicates added to the codestableitems in a codestable,
+		// If there are predicates added to the codestable,
 		// getCodesTableItem() will only return an item if it belongs to the filtered collection
-		// of codestableitems
-		// TODO: optimize
-		for(T cti : this.codesTableItems){
-			if(cti.equals(getCodesTableItem(code)) ){
-				 decode = cti.getDecode();
-				 break;
-			} else if (cti.equals(getCodesTableItem(code))){
-				defaultDecode = cti.getDecode();
-			}
-		}
+		// of codestableitems, otherwise it will throw an exception
+		T codesTableItem = getCodesTableItem(code);
+		decode = codesTableItem.getDecode();
 		
-		// If there doesn't exist a decode for the input code and locale, use the
-		// default decode of the input code, or throw an exception
+		//If for some reason a code in the map maps to a null value, throw exception
 		if(decode == null){
-			if(defaultDecode != null){
-				return defaultDecode;
-			} else{
-				throw new DecodeNotFoundException(code.toString());
-			}
+			throw new DecodeNotFoundException(code.toString());
 		}
 		
 		return decode;
--- app/stelvio-common-java/src/main/java/no/stelvio/common/codestable/support/DefaultCodesTablePeriodic.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/codestable/support/DefaultCodesTablePeriodic.java
@@ -2,15 +2,16 @@ package no.stelvio.common.codestable.support;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import no.stelvio.common.codestable.CodesTableItemPeriodic;
 import no.stelvio.common.codestable.CodesTablePeriodic;
 import no.stelvio.common.codestable.DecodeNotFoundException;
 import no.stelvio.common.codestable.ItemNotFoundException;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
 
 /**
@@ -23,14 +24,15 @@ import org.apache.commons.collections.Predicate;
  * throws exception when 2 rows have wrapping periods, hashcode uses code/date_from/is_approved, equals checks for overlapping  
  */
 public class DefaultCodesTablePeriodic<T extends CodesTableItemPeriodic> implements CodesTablePeriodic {
-	/** List of <code>CodesTableItemPeriodic</code>s this <code>DefaultCodesTablePeriodic</code> consists of. */
-	private List<T> codesTableItems;
+	
+	/** Map containing Codes-CodesTableItemPeriodic pairs*/
+	private Map<Object, T> codeCodesTableItemMap;
 	
 	/**
-	 * Holds the filtered list of <code>CodesTableItemPeriodic</code>s created by taking the full list and applying the
+	 * Holds the filtered map of <code>CodesTableItemPeriodic</code>s created by taking the full list and applying the
 	 * predicates.
 	 */
-	private List<T> filteredCodesTableItems;
+	private Map<Object, T> filteredCodeCodesTableItemMap;		
 	
 	/** List of predicates to use for filtering the list of <code>CodesTableItemPeriodic</code>s */
 	private List<Predicate> predicates = new ArrayList<Predicate>();
@@ -41,14 +43,24 @@ public class DefaultCodesTablePeriodic<T extends CodesTableItemPeriodic> impleme
 	 * @param codesTableItems list of <code>CodesTableItemPeriodic</code>s this <code>DefaultCodesTablePeriodic</code> consists of.
 	 */	                                                                                    
 	public DefaultCodesTablePeriodic(List<T> codesTableItems) {
-		this.codesTableItems = codesTableItems;
+		this.codeCodesTableItemMap = new HashMap<Object, T>();
+		if(codesTableItems != null){
+			for (T codesTableItem : codesTableItems) {
+				codeCodesTableItemMap.put(codesTableItem.getCode(), codesTableItem);
+			}
+		}
+		this.filteredCodeCodesTableItemMap = new HashMap<Object, T>(codeCodesTableItemMap);
+		
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public List<T> getItems() {
-		return codesTableItems;
+		if(predicates.isEmpty()){
+			return new ArrayList<T>(codeCodesTableItemMap.values());
+		}
+		return new ArrayList<T>(filteredCodeCodesTableItemMap.values());
 	}
 	
 	/**
@@ -56,22 +68,12 @@ public class DefaultCodesTablePeriodic<T extends CodesTableItemPeriodic> impleme
 	 */
 	public T getCodesTableItem(Object code) throws ItemNotFoundException {
 		T cti = null;
-								
-		// There are no predicates for the items of the codestableperiodic
+		
+		//There are no predicates for the items in the codestable  
 		if (this.predicates.isEmpty()){
-			for (T c : this.codesTableItems) {
-				if (c.getCode().equals(code.toString())) {
-					cti = c;
-					break;
-				}
-			}
+			cti = this.codeCodesTableItemMap.get(code);
 		} else { 
-			for (T c : this.filteredCodesTableItems){
-				if (c.getCode().equals(code.toString())){
-					cti = c;
-					break;
-				}
-			}
+			cti = this.filteredCodeCodesTableItemMap.get(code);
 		}
 
 		if (cti == null) {
@@ -85,19 +87,21 @@ public class DefaultCodesTablePeriodic<T extends CodesTableItemPeriodic> impleme
 	 * {@inheritDoc)}
 	 */
 	public void addPredicate(Predicate predicate) {
-		// If there are no previous predicates added to a codestableperiodic,
-		// all of the codetableitemperiodics in a codetableperiodic
-		// are filtered, or else the codestableitemperiodics in the filtered
-		// collection are filtered.
-		synchronized (this.filteredCodesTableItems){
-			if (this.predicates.isEmpty()){
-				this.filteredCodesTableItems = (ArrayList<T>)
-					CollectionUtils.select(this.codesTableItems, predicate);
-			} else {
-				this.filteredCodesTableItems = (ArrayList<T>)
-					CollectionUtils.select(this.filteredCodesTableItems, predicate);
+	
+		//If there are no previous predicates added to a codestable, all of the codetableitems in a codetable
+		//are filtered, or else the codestableitems in the filtered map are filtered.
+		synchronized (this.filteredCodeCodesTableItemMap){
+			if(this.predicates.isEmpty()){
+				this.filteredCodeCodesTableItemMap = new HashMap<Object, T>(codeCodesTableItemMap);
 			}
-		}
+			//Loop through filter map and remove items that fails eval of filter rules
+			for (Object code : filteredCodeCodesTableItemMap.keySet()) {
+				//If CodesTableItem doesn't evaluate according to predicate, remove from filtered list
+				if(!predicate.evaluate(filteredCodeCodesTableItemMap.get(code))){
+					filteredCodeCodesTableItemMap.remove(code);
+				}
+			}
+		}		
 		
 		this.predicates.add(predicate);	
 	}
@@ -107,39 +111,28 @@ public class DefaultCodesTablePeriodic<T extends CodesTableItemPeriodic> impleme
 	 */
 	public void resetPredicates() {
 		this.predicates.clear();
-		this.filteredCodesTableItems.clear();
+		//This is done in addPredicate if no predicates exists, 
+		//doing it here anyway to avoid stale version of filteredCodeCodesTableItemMap
+		this.filteredCodeCodesTableItemMap = new HashMap<Object, T>(codeCodesTableItemMap);
 	}
 
 	/**
+	 * FIXME: This method doesn't use the input Date to retrieve decode. 
+	 * FIXME: Method should probably change implementation or signature in the future.
 	 * {@inheritDoc}
 	 */
-	public String getDecode(Object code, Date date) throws DecodeNotFoundException {
+	public String getDecode(Object code, Date date) throws ItemNotFoundException, DecodeNotFoundException {
 		String decode = null;
-		String defaultDecode = null;
+
+		// If there are predicates added to the codestable,
+		// getCodesTableItem() will only return an item if it belongs to the filtered collection
+		// of codestableitems, otherwise it will throw an exception
+		T codesTableItem = getCodesTableItem(code);
+		decode = codesTableItem.getDecode();
 		
-		//If there are predicates added to the codestableitems in a codestable,
-		//codesTableItem will only return an item if it belongs to the filtered collection
-		//of codestableitems
-		//TODO: optimize loop
-		for(T cti : codesTableItems){
-			if(cti.equals(getCodesTableItem(code)) 
-					&& cti.getFromDate().before(date) && cti.getToDate().after(date)){ 
-				decode = cti.getDecode();
-				break;
-			} else if(cti.equals(getCodesTableItem(code))
-					&& cti.getFromDate().before(date) && cti.getToDate().after(date)){
-				defaultDecode = cti.getDecode();
-			}
-		}
-		
-		// If there doesn't exist a decode for the input code, date and locale, use the
-		// default decode of the input code and date, or throw an exception
-		if (decode == null){
-			if (defaultDecode != null){
-				return defaultDecode;
-			} else {
-				throw new DecodeNotFoundException(code.toString());
-			}
+		//If for some reason a code in the map maps to a null value, throw exception
+		if(decode == null){
+			throw new DecodeNotFoundException(code.toString());
 		}
 		
 		return decode;
