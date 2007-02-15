8bc9cdedcc819b9fe118bafbc21757f6d6d8a8cd
ska2812 <ska2812@localhost>
2007-02-15 10:32:39 +0000

    Added ASBO objects for PenHenvendelse service on the bus

---
+++ app/stelvio-star-example/layers/consumer/stelvio-consumer-star-example-wsclient/src/main/java/no/stelvio/consumer/star/example/henvendelse/map/ASBOPenHenvendelse.java
@@ -0,0 +1,8 @@
+package no.stelvio.consumer.star.example.henvendelse.map;
+
+/**
+ * @author personf8e9850ed756, Accenture
+ * @todo write javadoc
+ */
+public class ASBOPenHenvendelse {
+}
---
+++ app/stelvio-star-example/layers/consumer/stelvio-consumer-star-example-wsclient/src/main/java/no/stelvio/consumer/star/example/henvendelse/map/historikk/ASBOPenHenvendelseHistorikk.java
@@ -0,0 +1,8 @@
+package no.stelvio.consumer.star.example.henvendelse.map.historikk;
+
+/**
+ * @author personf8e9850ed756, Accenture
+ * @todo write javadoc
+ */
+public class ASBOPenHenvendelseHistorikk {
+}
