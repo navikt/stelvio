26b9cc4c958a8815c4183fdf7c6f1f5725928f0b
ska2812 <ska2812@localhost>
2007-02-15 11:01:03 +0000

    Added javadoc

--- app/stelvio-common-java/src/main/java/no/stelvio/common/error/retriever/ErrorDefinitionRetriever.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/error/retriever/ErrorDefinitionRetriever.java
@@ -5,9 +5,17 @@ import java.util.Set;
 import no.stelvio.common.error.support.ErrorDefinition;
 
 /**
+ * Defines the interfaces to be implemented for retrieving error definitions.
+ *
  * @author personf8e9850ed756
- * @todo write javadoc
  */
 public interface ErrorDefinitionRetriever {
+	/**
+	 * Retrieves a set of <code>ErrorDefinitions</code>s to be used in error handling.
+	 *
+	 * @return a set of <code>ErrorDefinitions</code>s.
+	 * @throws RetrieverFailedException if retrieving error definitions failed.
+	 * @see ErrorDefinition
+	 */
     Set<ErrorDefinition> retrieve() throws RetrieverFailedException;
 }
