3a38e42397f2b2cff0c481273de3e61999517bb3
ska2812 <ska2812@localhost>
2007-02-15 11:24:16 +0000

    Added an example of a call to henvendelse service

--- app/stelvio-star-example/layers/service/starexample/stelvio-service-star-example-java/src/main/java/no/stelvio/service/star/example/saksbehandling/support/RepositorySaksbehandlerService.java
+++ app/stelvio-star-example/layers/service/starexample/stelvio-service-star-example-java/src/main/java/no/stelvio/service/star/example/saksbehandling/support/RepositorySaksbehandlerService.java
@@ -3,8 +3,15 @@ package no.stelvio.service.star.example.saksbehandling.support;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import no.stelvio.domain.star.example.henvendelse.Fagomrade;
+import no.stelvio.domain.star.example.henvendelse.HenvendelseStatisticsCriteria;
+import no.stelvio.domain.star.example.henvendelse.Spesifikasjon;
+import no.stelvio.domain.star.example.henvendelse.Tidsperiode;
 import no.stelvio.domain.star.example.saksbehandling.Saksbehandler;
 import no.stelvio.repository.star.example.saksbehandling.SaksbehandlerRepository;
+import no.stelvio.service.star.example.henvendelse.HenvendelseServiceBi;
+import no.stelvio.service.star.example.henvendelse.to.HenvendelseStatisticsRequest;
+import no.stelvio.service.star.example.henvendelse.to.HenvendelseStatisticsResponse;
 import no.stelvio.service.star.example.saksbehandling.SaksbehandlerNotFoundException;
 import no.stelvio.service.star.example.saksbehandling.SaksbehandlerServiceBi;
 import no.stelvio.service.star.example.saksbehandling.to.SaksbehandlerRequest;
@@ -20,12 +27,21 @@ public class RepositorySaksbehandlerService implements SaksbehandlerServiceBi {
 
 	/** The repository to retrieve the saksbehandler with. */
 	private SaksbehandlerRepository saksbehandlerRepository;
+	/** The service used to generate statistics of henvendelser. */
+	private HenvendelseServiceBi henvendelseService;
 
+	/** {@inheritDoc} */
 	public SaksbehandlerResponse hentSaksbehandler(SaksbehandlerRequest request) throws SaksbehandlerNotFoundException {
 		Saksbehandler saksbehandler = saksbehandlerRepository.findSaksbehandlerById(request.getSaksbehandlerNr());
 
+		HenvendelseStatisticsCriteria criteria = new HenvendelseStatisticsCriteria(
+				"enhetId", Tidsperiode.SISTE_4_UKER, Fagomrade.PENSJON, Spesifikasjon.ANTALL);
+		HenvendelseStatisticsResponse statisticsResponse =
+				henvendelseService.genererHenvendelseStatistikk(new HenvendelseStatisticsRequest(criteria));
+
 		if (log.isDebugEnabled()) {
 			log.debug("saksbehandler = " + saksbehandler);
+			log.debug("statistics = " + statisticsResponse.getStatistics());
 		}
 
 		return new SaksbehandlerResponse(saksbehandler);
@@ -39,4 +55,15 @@ public class RepositorySaksbehandlerService implements SaksbehandlerServiceBi {
 	public void setSaksbehandlerRepository(no.stelvio.repository.star.example.saksbehandling.SaksbehandlerRepository saksbehandlerRepository) {
 		this.saksbehandlerRepository = saksbehandlerRepository;
 	}
+
+	/**
+	 * Sets the value of henvendelseService.
+	 *
+	 * @param henvendelseService value to set on henvendelseService.
+	 */
+	public void setHenvendelseService(HenvendelseServiceBi henvendelseService) {
+		this.henvendelseService = henvendelseService;
+	}
 }
+
+
