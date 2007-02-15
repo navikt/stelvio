62508b622072e767fbeb8ecc2991149333551545
ska2812 <ska2812@localhost>
2007-02-15 11:21:22 +0000

    - Added logging - Fixed comments

--- app/stelvio-common-java/src/main/java/no/stelvio/common/codestable/CodesTableManager.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/codestable/CodesTableManager.java
@@ -1,36 +1,36 @@
 package no.stelvio.common.codestable;
 
-/** * 
- * Interface defining functionality for accessing and caching persistent codes tables.
- * The class is not implemented as a Singleton, but should be externally managed
- * as a singleton, e.g. by using Spring Framework.
- * 
+/**
+ * Interface defining functionality for accessing and caching persistent codes tables. The class is not implemented as a
+ * Singleton, but should be externally managed as a singleton, e.g. by using Spring Framework.
+ *
  * @author personb66fa0b5ff6e, Accenture
  * @version $Id $
  */
 public interface CodesTableManager {
 
 	/**
-	 * Retrieves a <code>CodesTable</code> from the database by using <code>CodesTableFactory</code>.
-	 * The <code>CodesTable</code> that the input <code>CodesTableItem</code> belongs to, 
-	 * is the <code>CodesTable</code> that shall be retrieved. 
-	 * 
-	 * @param <T> <code>CodesTableItem</code>'s or subclasses of <code>CodesTableItem</code> that are valid input 
-	 * parameters, and that the <code>CodesTable</code> returned by the method holds values of. 
+	 * Retrieves a <code>CodesTable</code> from the database by using <code>CodesTableFactory</code>. The
+	 * <code>CodesTable</code> that the input <code>CodesTableItem</code> belongs to, is the <code>CodesTable</code> that
+	 * shall be retrieved.
+	 *
+	 * @param <T> <code>CodesTableItem</code>'s or subclasses of <code>CodesTableItem</code> that are valid input
+	 * parameters, and that the <code>CodesTable</code> returned by the method holds values of.
 	 * @param codesTableItem the <code>CodesTable</code> to retieve - represented by a <code>CodesTableItem</code>s class.
 	 * @return The fetched <code>CodesTable</code>.
 	 */
 	<T extends CodesTableItem> CodesTable<T> getCodesTable(Class<T> codesTableItem);
-	
+
 	/**
-	  * Retrieves a <code>CodesTablePeriodic</code> from the database by using <code>CodesTableFactory</code>. 
-	  * The <code>CodesTablePeriodic</code> that the input <code>CodesTableItemPeriodic</code> belongs to, 
-	  * is the <code>CodesTablePeriodic</code> that shall be retrieved. 
-	  * 
-	  * @param <T> <code>CodesTableItemPeriodic</code>'s or subclasses of <code>CodesTableItemPeriodic</code> that are valid input 
-	  * parameters, and that the <code>CodesTablePeriodic</code> returned by the method holds values of. 
-	  * @param codesTableItemPeriodic the <code>CodesTablePeriodic</code> to retieve - represented by a <code>CodesTableItemPeriodic</code>s class.
-	  * @return The fetched <code>CodesTablePeriodic</code>.
-	  */
+	 * Retrieves a <code>CodesTablePeriodic</code> from the database by using <code>CodesTableFactory</code>. The
+	 * <code>CodesTablePeriodic</code> that the input <code>CodesTableItemPeriodic</code> belongs to, is the
+	 * <code>CodesTablePeriodic</code> that shall be retrieved.
+	 *
+	 * @param <T> <code>CodesTableItemPeriodic</code>'s or subclasses of <code>CodesTableItemPeriodic</code> that are valid
+	 * input parameters, and that the <code>CodesTablePeriodic</code> returned by the method holds values of.
+	 * @param codesTableItemPeriodic the <code>CodesTablePeriodic</code> to retieve - represented by a
+	 * <code>CodesTableItemPeriodic</code>s class.
+	 * @return The fetched <code>CodesTablePeriodic</code>.
+	 */
 	<T extends CodesTableItemPeriodic> CodesTablePeriodic<T> getCodesTablePeriodic(Class<T> codesTableItemPeriodic);
 }
--- app/stelvio-common-java/src/main/java/no/stelvio/common/codestable/support/DefaultCodesTableManager.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/codestable/support/DefaultCodesTableManager.java
@@ -1,5 +1,8 @@
 package no.stelvio.common.codestable.support;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import no.stelvio.common.codestable.CodesTable;
 import no.stelvio.common.codestable.CodesTableItem;
 import no.stelvio.common.codestable.CodesTableItemPeriodic;
@@ -10,62 +13,69 @@ import no.stelvio.common.codestable.factory.CodesTableFactory;
 
 /**
  * Implementation of CodesTableManager used for accessing and caching persistent codes tables.
- * 
+ *
  * @author personb66fa0b5ff6e, Accenture
  * @version $Id$
  */
 public class DefaultCodesTableManager implements CodesTableManager {
+	private Log log = LogFactory.getLog(DefaultCodesTableManager.class);
+
 	/** The CodesTableFactory used for retrieval of codestables. */
 	private CodesTableFactory codesTableFactory;
-	
-	/**
-	 * {@inheritDoc}
-	 */
+
+	/** {@inheritDoc} */
 	public <T extends CodesTableItem> CodesTable<T> getCodesTable(Class<T> codesTableItem) {
-        validateCodesTableClass(codesTableItem);
-        
+		validateCodesTableClass(codesTableItem);
+
+		if (log.isDebugEnabled()) {
+			log.debug("Creating a normal codes table for " + codesTableItem);
+		}
+
 		return codesTableFactory.createCodesTable(codesTableItem);
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
+	/** {@inheritDoc} */
 	public <T extends CodesTableItemPeriodic> CodesTablePeriodic<T> getCodesTablePeriodic(Class<T> codesTableItem) {
 		validateCodesTablePeriodicClass(codesTableItem);
-		
-        return codesTableFactory.createCodesTablePeriodic(codesTableItem);
+
+		if (log.isDebugEnabled()) {
+			log.debug("Creating a periodic codes table for " + codesTableItem);
+		}
+
+		return codesTableFactory.createCodesTablePeriodic(codesTableItem);
 	}
 
 	/**
 	 * Checks that the class to load a codestable for is a subclass of <code>CodesTableItem</>.
-	 * 
+	 *
 	 * @param codesTableClass the class to load a codestable for.
 	 * @throws NotCodesTableException if the class to load a codestable for is not a subclass of <code>CodesTable</code>
 	 */
-	private void validateCodesTableClass(final Class<? extends AbstractCodesTableItem> codesTableClass){
-		if(null!= codesTableClass && !CodesTableItem.class.isAssignableFrom(codesTableClass)){
+	private void validateCodesTableClass(final Class<? extends AbstractCodesTableItem> codesTableClass) {
+		if (null != codesTableClass && !CodesTableItem.class.isAssignableFrom(codesTableClass)) {
 			throw new NotCodesTableException(codesTableClass);
 		}
-	}	
-	
+	}
+
 	/**
 	 * Checks that the class to load a codestable for is a subclass of <code>CodesTableItemPeriodic</>.
-	 * 
+	 *
 	 * @param codesTablePeriodicClass the class to load a codestable for.
-	 * @throws NotCodesTableException if the class to load a codestable for is not a subclass of <code>CodesTablePeriodic</code>.
+	 * @throws NotCodesTableException if the class to load a codestable for is not a subclass of
+	 * <code>CodesTablePeriodic</code>.
 	 */
-	private void validateCodesTablePeriodicClass(final Class<? extends AbstractCodesTableItem> codesTablePeriodicClass){
-		if(null!= codesTablePeriodicClass && !CodesTableItemPeriodic.class.isAssignableFrom(codesTablePeriodicClass)){
+	private void validateCodesTablePeriodicClass(final Class<? extends AbstractCodesTableItem> codesTablePeriodicClass) {
+		if (null != codesTablePeriodicClass && !CodesTableItemPeriodic.class.isAssignableFrom(codesTablePeriodicClass)) {
 			throw new NotCodesTableException(codesTablePeriodicClass);
 		}
-	}	
-	
+	}
+
 	/**
-	 * Sets the <code>CodesTableFactory</code> that is used to retrieve 
-	 * the codestables from the database.
+	 * Sets the <code>CodesTableFactory</code> that is used to retrieve the codestables from the database.
+	 *
 	 * @param codesTableFactory a reference to the interface for retrieving codestables from the database.
 	 */
-	public void setCodesTableFactory(CodesTableFactory codesTableFactory){
+	public void setCodesTableFactory(CodesTableFactory codesTableFactory) {
 		this.codesTableFactory = codesTableFactory;
 	}
 }
