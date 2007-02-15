a1ca06d6faa300d6be39fbc8b57129e624a5c69e
ska2812 <ska2812@localhost>
2007-02-15 11:22:18 +0000

    Added hack for handling an uninitialized RequestContext/SecurityContext

--- app/stelvio-common-java/src/main/java/no/stelvio/common/error/support/CommonExceptionLogic.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/error/support/CommonExceptionLogic.java
@@ -19,127 +19,145 @@ import no.stelvio.common.util.SequenceNumberGenerator;
  * @todo better class name
  */
 public abstract class CommonExceptionLogic implements Serializable {
-    private long errorId;
-    private String userId;
-    private String screenId;
-    private String processId;
-    private String transactionId;
-    private Object[] templateArguments;
-    /** Says whether the exception has been logged or not */
-    private boolean logged;
-    private String message;
+	private long errorId;
+	private String userId;
+	private String screenId;
+	private String processId;
+	private String transactionId;
+	private Object[] templateArguments;
+	/** Says whether the exception has been logged or not */
+	private boolean logged;
+	private String message;
 
-    protected CommonExceptionLogic(Object[] templateArguments) {
-	    RequestContext requestContext = RequestContextHolder.currentRequestContext();
-	    SecurityContext securityContext = SecurityContextHolder.currentSecurityContext();
-	    init(SequenceNumberGenerator.getNextId("ErrorId"), securityContext.getUserId(), requestContext.getScreenId(),
-                requestContext.getProcessId(), requestContext.getTransactionId(), templateArguments, false);
-    }
+	protected CommonExceptionLogic(Object[] templateArguments) {
+		RequestContext requestContext;
+		SecurityContext securityContext;
+		String userIdLocal = null;
+		String screenIdLocal = null;
+		String processIdLocal = null;
+		String transactionIdLocal = null;
 
-    protected CommonExceptionLogic(CommonExceptionLogic cel) {
-        init(cel.errorId, cel.userId, cel.screenId, cel.processId, cel.transactionId, cel.templateArguments, cel.logged);
-    }
+		try {
+			requestContext = RequestContextHolder.currentRequestContext();
+			securityContext = SecurityContextHolder.currentSecurityContext();
 
-    private void init(long errorId,
-                      String userId,
-                      String screenId,
-                      String processId,
-                      String transactionId,
-                      Object[] templateArguments,
-                      boolean logged) {
-        this.templateArguments = copyArguments(templateArguments);
+			userIdLocal = securityContext.getUserId();
+			screenIdLocal = requestContext.getScreenId();
+			processIdLocal = requestContext.getProcessId();
+			transactionIdLocal = requestContext.getTransactionId();
+		} catch (IllegalStateException e) {
+			// Could happen, but not fatal
+			// TODO find a better way to handle this, maybe have a getXxxxContext() that returns null
+		}
 
-        for (Object templateArgument : this.templateArguments) {
-            if (null == templateArgument) {
-                throw new IllegalArgumentException("None of the template arguments can be null");
-            }
-        }
-        
-        constructMessage();
+		init(SequenceNumberGenerator.getNextId("ErrorId"), userIdLocal, screenIdLocal,
+				processIdLocal, transactionIdLocal, templateArguments, false);
+	}
 
-        this.errorId = errorId;
-        this.userId = userId;
-        this.screenId = screenId;
-        this.processId = processId;
-        this.transactionId = transactionId;
-        this.logged = logged;
-    }
+	protected CommonExceptionLogic(CommonExceptionLogic cel) {
+		init(cel.errorId, cel.userId, cel.screenId, cel.processId, cel.transactionId, cel.templateArguments, cel.logged);
+	}
 
-    /**
-     * Implemented by subclasses by returning the template to use for constructing the exception's messageFrom.
-     *
-     * @param numArgs the number of arguments to the exception's constructor. Can be used if there is a need to
-     * dynamically build a template to fit the number of arguments.
-     * @return the template to use for constructing the exception's messageFrom.
-     */
-    protected abstract String messageTemplate(final int numArgs);
+	private void init(long errorId,
+	                  String userId,
+	                  String screenId,
+	                  String processId,
+	                  String transactionId,
+	                  Object[] templateArguments,
+	                  boolean logged) {
+		this.templateArguments = copyArguments(templateArguments);
 
-    public long getErrorId() {
-        return errorId;
-    }
+		for (Object templateArgument : this.templateArguments) {
+			if (null == templateArgument) {
+				throw new IllegalArgumentException("None of the template arguments can be null");
+			}
+		}
 
-    public String getUserId() {
-        return userId;
-    }
+		constructMessage();
 
-    public String getScreenId() {
-        return screenId;
-    }
+		this.errorId = errorId;
+		this.userId = userId;
+		this.screenId = screenId;
+		this.processId = processId;
+		this.transactionId = transactionId;
+		this.logged = logged;
+	}
 
-    public String getProcessId() {
-        return processId;
-    }
+	/**
+	 * Implemented by subclasses by returning the template to use for constructing the exception's messageFrom.
+	 *
+	 * @param numArgs the number of arguments to the exception's constructor. Can be used if there is a need to
+	 * dynamically build a template to fit the number of arguments.
+	 * @return the template to use for constructing the exception's messageFrom.
+	 */
+	protected abstract String messageTemplate(final int numArgs);
 
-    public String getTransactionId() {
-        return transactionId;
-    }
+	public long getErrorId() {
+		return errorId;
+	}
 
-    public Object[] getTemplateArguments() {
-        return copyArguments(templateArguments);
-    }
+	public String getUserId() {
+		return userId;
+	}
 
-    public String getMessage() {
-        return message;
-    }
+	public String getScreenId() {
+		return screenId;
+	}
 
-    public boolean isLogged() {
-        return logged;
-    }
+	public String getProcessId() {
+		return processId;
+	}
 
-    public void setLogged() {
-        logged = true;
-    }
+	public String getTransactionId() {
+		return transactionId;
+	}
 
-    private Object[] copyArguments(Object[] templateArguments) {
-        if (null == templateArguments || templateArguments.length == 0) {
-            throw new IllegalArgumentException("The template arguments cannot be empty");
-        }
+	public Object[] getTemplateArguments() {
+		return copyArguments(templateArguments);
+	}
 
-        Object[] copy = new Object[templateArguments.length];
-        System.arraycopy(templateArguments, 0, copy, 0, templateArguments.length);
+	public String getMessage() {
+		return message;
+	}
 
-        return copy;
-    }
+	public boolean isLogged() {
+		return logged;
+	}
 
-    private void constructMessage() {
-        String template = messageTemplate(templateArguments.length);
+	public void setLogged() {
+		logged = true;
+	}
 
-        if (StringUtils.isBlank(template)) {
-            throw new IllegalArgumentException("The messageFrom template should not be empty");
-        }
+	private Object[] copyArguments(Object[] templateArguments) {
+		if (null == templateArguments || templateArguments.length == 0) {
+			throw new IllegalArgumentException("The template arguments cannot be empty");
+		}
 
-        MessageFormat messageFormat = new MessageFormat(template);
-        Format[] formats = messageFormat.getFormats();
+		Object[] copy = new Object[templateArguments.length];
+		System.arraycopy(templateArguments, 0, copy, 0, templateArguments.length);
 
-        if (formats.length < 1) {
-            throw new IllegalArgumentException("The template must have at least 1 argument");
-        }
+		return copy;
+	}
 
-        if (formats.length != templateArguments.length) {
-            throw new IllegalArgumentException("The template arguments doesn't match template; should be " +
-                    formats.length + " arguments, but is " + templateArguments.length);
-        }
-        
-        message = messageFormat.format(templateArguments);
-    }
+	private void constructMessage() {
+		String template = messageTemplate(templateArguments.length);
+
+		if (StringUtils.isBlank(template)) {
+			throw new IllegalArgumentException("The messageFrom template should not be empty");
+		}
+
+		MessageFormat messageFormat = new MessageFormat(template);
+		Format[] formats = messageFormat.getFormats();
+
+		if (formats.length < 1) {
+			throw new IllegalArgumentException("The template must have at least 1 argument");
+		}
+
+		if (formats.length != templateArguments.length) {
+			throw new IllegalArgumentException("The template arguments doesn't match template; should be " +
+					formats.length + " arguments, but is " + templateArguments.length);
+		}
+
+		message = messageFormat.format(templateArguments);
+	}
 }
