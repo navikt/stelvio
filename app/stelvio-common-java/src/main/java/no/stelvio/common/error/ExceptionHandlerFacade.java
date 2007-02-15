90061fc38ddda7d66c775a7df682a5a439f44b4f
ska2812 <ska2812@localhost>
2007-02-15 11:22:11 +0000

    Removed todo for the naming of the class

--- app/stelvio-common-java/src/main/java/no/stelvio/common/error/ExceptionHandlerFacade.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/error/ExceptionHandlerFacade.java
@@ -3,11 +3,13 @@ package no.stelvio.common.error;
 /**
  * @author personf8e9850ed756
  * @todo write javadoc
- * @todo should it be called facade?
  */
 public interface ExceptionHandlerFacade {
-    <T extends Throwable> void handle(T e);
-    <T extends Throwable> void rethrow(T e) throws T;
+	<T extends Throwable> void handle(T e);
+
+	<T extends Throwable> void rethrow(T e) throws T;
+
 	<T extends Throwable> void throwNew(Class<T> exceptionToThrow, Object... params) throws T;
+
 	<T extends Throwable> void throwNew(Class<T> exceptionToThrow, Throwable cause, Object... params) throws T;
 }
