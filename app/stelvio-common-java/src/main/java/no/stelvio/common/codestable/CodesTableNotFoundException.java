a524d5f8959c96d2508111c36c749fbad2e91573
maa2812 <maa2812@localhost>
2007-02-27 09:58:26 +0000

    Change to adhere exception guidelines. See defect 441

--- app/stelvio-common-java/src/main/java/no/stelvio/common/codestable/CodesTableNotFoundException.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/codestable/CodesTableNotFoundException.java
@@ -1,7 +1,5 @@
 package no.stelvio.common.codestable;
 
-import java.text.MessageFormat;
-
 import no.stelvio.common.codestable.support.AbstractCodesTableItem;
 import no.stelvio.common.codestable.support.CodesTableException;
 
@@ -15,22 +13,27 @@ import no.stelvio.common.codestable.support.CodesTableException;
 public class CodesTableNotFoundException extends CodesTableException {
 
 	private static final long serialVersionUID = -6427780265990487437L;
-	private static final MessageFormat FORMAT = new MessageFormat("Codestable {0} does not exist");
 
+    
     /**
+     * Constructs a new CodesTableNotFoundException
+     * @param cause the underlying cause of this exception
+     * @param cti CodestableItem that was not found
+     */
+    public <T extends AbstractCodesTableItem> CodesTableNotFoundException(Throwable cause, Class<T> cti) {
+        super(cause, cti);
+    }	
+	
+	/**
      * Constructs a new CodesTableNotFoundException
      * @param cti CodestableItem that was not found
      */
     public <T extends AbstractCodesTableItem> CodesTableNotFoundException(Class<T> cti) {
-        super(create(cti));
+        super(cti);
     }
 
-    private static <T extends AbstractCodesTableItem> String create(Class<T> cti) {
-        String s = FORMAT.format(new Object[]{cti});
-        return s;
-    }
 
     protected String messageTemplate(final int numArgs) {
-        return "Problems with handling codes table: {0}";
+        return "Codestable {0} does not exist";
     }
 }
