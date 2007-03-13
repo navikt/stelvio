de9891d24981998c81341e3459bd39fd30026894
ska2812 <ska2812@localhost>
2007-03-13 08:50:37 +0000

    - Improved usage of JDK 5 - Improved formatting of comments

--- app/stelvio-common-java/src/main/java/no/stelvio/common/security/authorization/method/RoleAccessDecisionVoter.java
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/security/authorization/method/RoleAccessDecisionVoter.java
@@ -1,11 +1,7 @@
 package no.stelvio.common.security.authorization.method;
 
 import java.lang.reflect.Method;
-import java.util.Iterator;
 import java.util.List;
-
-import no.stelvio.common.security.SecurityContextHolder;
-import no.stelvio.common.security.authorization.method.RolesUtil;
 
 import org.acegisecurity.Authentication;
 import org.acegisecurity.ConfigAttribute;
@@ -15,17 +11,20 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.aop.framework.ReflectiveMethodInvocation;
 
+import no.stelvio.common.security.SecurityContextHolder;
+
 /**
  * An access-controller, i.e. an <code>AccessDecisionVoter</code> implementation that participate in an access-controll 
- * decision based on the roles found in the Roles annotation for the current intercepted method. If no such annotation is found
- * the class simply abstains from voting, i.e its vote method returns <code>AccessDecisionVoter.ACCESS_ABSTAIN</code>.  
+ * decision based on the roles found in the Roles annotation for the current intercepted method. If no such annotation
+ * is found the class simply abstains from voting, i.e its vote method returns
+ * <code>AccessDecisionVoter.ACCESS_ABSTAIN</code>.  
  * 
  * @author persondab2f89862d3, Accenture
  * @version $Id$
  */
 public class RoleAccessDecisionVoter implements AccessDecisionVoter {
-
 	protected static final Log logger = LogFactory.getLog(RoleAccessDecisionVoter.class);
+
 	/**
 	 * Indication of wheter or not this class supports the presented config attrbute.
 	 * @param attr the config attribute
@@ -34,6 +33,7 @@ public class RoleAccessDecisionVoter implements AccessDecisionVoter {
 	public boolean supports(ConfigAttribute attr) {
 		return true;    
 	}
+
 	/**
 	 * Indication of wheter or not this class supports the secure object represented by the clazz argument.
 	 * @param clazz the secure object class.
@@ -42,16 +42,17 @@ public class RoleAccessDecisionVoter implements AccessDecisionVoter {
 	public boolean supports(Class clazz) {
 		return ReflectiveMethodInvocation.class.isAssignableFrom(clazz);
 	}
+
 	/**
 	 * Checks if the current user has the required role to proceed with the method invocation. The roles required are
-	 * obtained from the intercepted method's Roles annotation. If no such annotation is present AccessDecisionVoter.ACCESS_ABSTAIN
-	 * will be returned.
+	 * obtained from the intercepted method's Roles annotation. If no such annotation is present
+	 * AccessDecisionVoter.ACCESS_ABSTAIN will be returned.
 	 * 
 	 * @param authentication the authentication object containing the user principal 
 	 * @param secureObject the secured object, should be a ReflectiveMethodInvocation
 	 * @param config the configuration attributes for a particular method invocation
-	 * @return AccessDecisionVoter.ACCESS_GRANTED, AccessDecisionVoter.ACCESS_DENIED or AccessDecisionVoter.ACCESS_ABSTAIN.
-	 * 
+	 * @return AccessDecisionVoter.ACCESS_GRANTED, AccessDecisionVoter.ACCESS_DENIED or
+	 * AccessDecisionVoter.ACCESS_ABSTAIN.
 	 */
 	public int vote(Authentication authentication, Object secureObject,
 			ConfigAttributeDefinition config) {
@@ -62,13 +63,12 @@ public class RoleAccessDecisionVoter implements AccessDecisionVoter {
 		
 		if(logger.isDebugEnabled()){
 			logger.debug("Getting roles from annotation for the intercepted method.");
-			Iterator<String> iter = roles.iterator();
-			while(iter.hasNext()){
-				String role = iter.next();
+			for (String role : roles) {
 				logger.debug("Role: " + role);
 			}
 		}
-		if(roles == null || roles.isEmpty()){
+
+		if (roles == null || roles.isEmpty()){
 			return AccessDecisionVoter.ACCESS_ABSTAIN;
 		} else if(SecurityContextHolder.currentSecurityContext().isUserInRoles(roles)){
 			return AccessDecisionVoter.ACCESS_GRANTED;
