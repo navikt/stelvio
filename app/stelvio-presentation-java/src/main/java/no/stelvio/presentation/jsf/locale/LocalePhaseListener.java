c3e593d32eae676316da05a9a1f43f46a77cf88c
ska2812 <ska2812@localhost>
2007-03-13 09:25:47 +0000

    Comment and code cleanup

--- app/stelvio-presentation-java/src/main/java/no/stelvio/presentation/jsf/locale/LocalePhaseListener.java
+++ app/stelvio-presentation-java/src/main/java/no/stelvio/presentation/jsf/locale/LocalePhaseListener.java
@@ -4,7 +4,6 @@
 package no.stelvio.presentation.jsf.locale;
 
 import java.util.Locale;
-
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
@@ -12,16 +11,13 @@ import javax.faces.event.PhaseListener;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 /**
- * Phaselistener for setting the current locale from FacesContext in
- * the Spring LocaleContextHolder. This phaselistener is invoked before
- * the <code>INVOKE_APPLICATION<code> phase.
+ * Phaselistener for setting the current locale from FacesContext in the Spring LocaleContextHolder. This phaselistener
+ * is invoked before the <code>INVOKE_APPLICATION<code> phase.
  * 
  * @author person6045563b8dec, Accenture
  * @version $Id$
- *
  */
 public class LocalePhaseListener implements PhaseListener {
-
 	/**
 	 * auto generated UID
 	 */
@@ -38,12 +34,11 @@ public class LocalePhaseListener implements PhaseListener {
 
 
 	/**
-	 * Before the 'invoke application' phase is entered the current locale in the
-	 * Spring LocaleContextHolder is updated with the locale set in the application. 
-	 * Retrieves the current locale from the current view root and sets it in the Spring 
-	 * LocaleContextHolder
+	 * Before the 'invoke application' phase is entered the current locale in the Spring LocaleContextHolder is updated
+	 * with the locale set in the application. Retrieves the current locale from the current view root and sets it in
+	 * the Spring LocaleContextHolder.
 	 * 
-	 * @param event The phase event that has occured
+	 * @param event The phase event that has occured.
 	 */
 	public void beforePhase(PhaseEvent event) {
 		Locale currentLocale = event.getFacesContext().getViewRoot().getLocale(); 
@@ -51,10 +46,10 @@ public class LocalePhaseListener implements PhaseListener {
 	}
 
 	/**
-	 * Returns the phase id of the phase this phase listener is listening on,
-	 * this phaselistener is listening on the <code>INVOKE_APPLICATION<code> phase
+	 * Returns the phase id of the phase this phase listener is listening on, this phaselistener is listening on the
+	 * <code>INVOKE_APPLICATION<code> phase.
 	 * 
-	 * @return the phase id of the <code>INVOKE_APPLICATION<code> phase
+	 * @return the phase id of the <code>INVOKE_APPLICATION<code> phase.
 	 */
 	public PhaseId getPhaseId() {
 		return PhaseId.INVOKE_APPLICATION;
--- app/stelvio-presentation-java/src/test/java/no/stelvio/presentation/jsf/locale/LocalePhaseListenerTest.java
+++ app/stelvio-presentation-java/src/test/java/no/stelvio/presentation/jsf/locale/LocalePhaseListenerTest.java
@@ -1,30 +1,30 @@
 package no.stelvio.presentation.jsf.locale;
 
 import java.util.Locale;
-import static org.junit.Assert.*;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 
-import no.stelvio.presentation.security.page.AbstractPhaselistenerTestCase;
-
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.springframework.context.i18n.LocaleContextHolder;
 
+import no.stelvio.presentation.security.page.AbstractPhaselistenerTestCase;
+
 /**
  * Test class for the LocalePhaseListener, which sets the locale from
- * faces context in Spring LocaleContextHolder 
+ * faces context in Spring LocaleContextHolder.
+ *  
  * @author person6045563b8dec
  * @version $Id$
- *
  */
 public class LocalePhaseListenerTest extends AbstractPhaselistenerTestCase {
-
 	LocalePhaseListener localePhaseListener;
 	Locale defaultLocale;
 	
 	/**
-	 * Set up mock objects necessary to run the test
-	 * @throws Exception if an error occurs during set up
+	 * Set up mock objects necessary to run the test.
+	 *
+	 * @throws Exception if an error occurs during set up.
 	 */
 	@Override
 	protected void onSetUp() throws Exception {		
@@ -32,27 +32,16 @@ public class LocalePhaseListenerTest extends AbstractPhaselistenerTestCase {
 		defaultLocale = Locale.FRANCE;
 		facesContext.getApplication().setDefaultLocale(defaultLocale);
 	}
+
 	/**
-	 * Tear down mock objects used in the test
-	 * 
-	 * @throws Exception if an error occurs during tear down
-	 */
-	
-	protected void onTearDown() throws Exception {
-		
-	}
-	
-	/**
-	 * Test that the default locale from faces context is set in the
-	 * Spring LocaleContextHolder object.
-	 *
+	 * Test that the default locale from faces context is set in the Spring LocaleContextHolder object.
 	 */
 	@Test
 	public void testBefore() {
 		assertTrue(LocaleContextHolder.getLocale() != defaultLocale);
 		PhaseEvent event = new PhaseEvent(facesContext, PhaseId.INVOKE_APPLICATION, lifecycle);
 		localePhaseListener.beforePhase(event);
+		
 		assertTrue(LocaleContextHolder.getLocale() == defaultLocale);
 	}
-
 }
