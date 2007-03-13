cd8a421d7e614427e905e99b1a2b88a12243ccdb
ska2812 <ska2812@localhost>
2007-03-13 09:04:11 +0000

    Improved usage of JUnit 4

--- app/stelvio-test-java/src/test/java/no/stelvio/test/context/StelvioContextSetterTest.java
+++ app/stelvio-test-java/src/test/java/no/stelvio/test/context/StelvioContextSetterTest.java
@@ -1,8 +1,11 @@
 package no.stelvio.test.context;
 
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
 
 import no.stelvio.common.context.RequestContext;
 import no.stelvio.common.context.RequestContextHolder;
@@ -11,47 +14,40 @@ import no.stelvio.common.security.SecurityContext;
 import no.stelvio.common.security.SecurityContextHolder;
 import no.stelvio.common.security.support.SimpleSecurityContext;
 
-import org.junit.Test;
-
 public class StelvioContextSetterTest {
-
 	@Test
-	public void testSetSecurityContextSecurityContext() {
+	public void setSecurityContextSecurityContext() {
 		SecurityContext ctx = new SimpleSecurityContext("UserId", new ArrayList<String>());
 		StelvioContextSetter.setSecurityContext(ctx);
 		assertEquals("Context is not set on the thread.", ctx, SecurityContextHolder.currentSecurityContext());
 	}
 
 	@Test
-	public void testSetSecurityContextStringStringArray() {
+	public void setSecurityContextStringStringArray() {
 		SecurityContext ctx = new SimpleSecurityContext("UserId", new ArrayList<String>());
 		StelvioContextSetter.setSecurityContext("UserId","Role1","Role2");
 		assertEquals("Context is not set correctly on the thread.", ctx.getUserId(), SecurityContextHolder.currentSecurityContext().getUserId());
 	}
 
 	@Test
-	public void testResetSecurityContext() {
+	public void resetSecurityContext() {
 		assertTrue("Will come at a later stage. Currently the securityContext will never be null.",true);
 	}
 
 	@Test
-	public void testSetRequestContext() {
+	public void setRequestContext() {
 		RequestContext ctx = new SimpleRequestContext("id","module","process","transaction");
 		StelvioContextSetter.setRequestContext(ctx);
 		assertEquals("Context is not set on the thread.", ctx, RequestContextHolder.currentRequestContext());
 	}
 
-	@Test
-	public void testResetRequestContext() {
+	@Test(expected = IllegalStateException.class)
+	public void resetRequestContext() {
 		RequestContext ctx = new SimpleRequestContext("id","module","process","transaction");
 		StelvioContextSetter.setRequestContext(ctx);
-		try{
-			StelvioContextSetter.resetRequestContext();
-			RequestContextHolder.currentRequestContext();
-		} catch (IllegalStateException expected){
-			assertTrue(true);
-			return;
-		}
+
+		StelvioContextSetter.resetRequestContext();
+		RequestContextHolder.currentRequestContext();
 		fail("An IllegalStateException should have been thrown if no requestcontext is on the current thread.");
 	}
 }
