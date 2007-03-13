22778f289e0362db7ebc52f1431cdbe980386465
ska2812 <ska2812@localhost>
2007-03-13 15:54:14 +0000

    Made our own version of IllegalArgumentException

---
+++ app/stelvio-common-java/src/main/java/no/stelvio/common/error/InvalidArgumentException.java
@@ -0,0 +1,19 @@
+package no.stelvio.common.error;
+
+/**
+ * Thrown to indicate that a method has been passed an invalid/illegal or inappropriate argument.
+ *  
+ * @author personf8e9850ed756
+ */
+public class InvalidArgumentException extends SystemUnrecoverableException {
+	private static final long serialVersionUID = 123345612346L;
+
+	protected InvalidArgumentException(String argumentname, Object argument) {
+        super(argumentname, argument);
+    }
+
+	@Override
+	protected String messageTemplate(int numArgs) {
+		return "Invalid argument specified ({0} --> {1})";
+	}
+}
