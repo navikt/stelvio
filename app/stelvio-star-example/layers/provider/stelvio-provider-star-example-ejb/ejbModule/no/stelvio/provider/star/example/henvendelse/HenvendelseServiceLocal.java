9ae7af72c1015f7932d4469a69dd53853e38f4a0
ska2812 <ska2812@localhost>
2007-03-14 21:36:14 +0000

    Added ejb for provider layer

---
+++ app/stelvio-star-example/layers/provider/stelvio-provider-star-example-ejb/ejbModule/no/stelvio/provider/star/example/henvendelse/HenvendelseServiceLocal.java
@@ -0,0 +1,10 @@
+/**
+ * @author personf8e9850ed756, Accenture
+ * @todo write javadoc
+ */
+package no.stelvio.service.star.example.henvendelse;
+
+import javax.ejb.EJBLocalObject;
+
+public interface HenvendelseServiceLocal extends HenvendelseServiceBi, EJBLocalObject {
+}
