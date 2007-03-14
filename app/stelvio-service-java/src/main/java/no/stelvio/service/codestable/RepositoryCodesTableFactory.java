7e6e8d353ea36f8fede544fe61d74de741e8c724
ska2812 <ska2812@localhost>
2007-03-14 21:04:57 +0000

    - Renamed CodesTablePeriodicItems (from CodesTableItemsPeriodic) - Create new instance of CodesTable/CodesTablePeriodic on each call to DefaultCodesTableManager (defect #164)   - deprecated CodesTableFactory   - new CodesTableItemsFactory that returns the list of codes table items

--- app/stelvio-service-java/src/main/java/no/stelvio/service/codestable/RepositoryCodesTableFactory.java
+++ app/stelvio-service-java/src/main/java/no/stelvio/service/codestable/RepositoryCodesTableFactory.java
@@ -2,9 +2,12 @@ package no.stelvio.service.codestable;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import no.stelvio.common.codestable.CodesTable;
 import no.stelvio.common.codestable.CodesTableItem;
-import no.stelvio.common.codestable.CodesTableItemPeriodic;
+import no.stelvio.common.codestable.CodesTablePeriodicItem;
 import no.stelvio.common.codestable.CodesTableNotFoundException;
 import no.stelvio.common.codestable.CodesTablePeriodic;
 import no.stelvio.common.codestable.factory.CodesTableFactory;
@@ -16,6 +19,7 @@ import no.stelvio.repository.codestable.CodesTableRepository;
  * Bean implementation class for CodesTableFactory.
  */
 public class RepositoryCodesTableFactory implements CodesTableFactory {
+	private Log log = LogFactory.getLog(RepositoryCodesTableFactory.class);
 	private CodesTableRepository codesTableRepository;
 
 	/**
@@ -24,6 +28,7 @@ public class RepositoryCodesTableFactory implements CodesTableFactory {
 	@SuppressWarnings("unchecked")
 	public <T extends CodesTableItem<K, V>, K extends Enum, V> CodesTable<T, K, V> createCodesTable(Class<T> codesTableItemClass)
 			throws CodesTableNotFoundException {
+		log.warn("DEPRECATED - Use RepositoryCodesTableItemsFactory instead - DEPRECATED");
 		final List<T> items = codesTableRepository.findCodesTableItems(codesTableItemClass);
 
 		return new DefaultCodesTable<T, K, V>(items);
@@ -33,16 +38,18 @@ public class RepositoryCodesTableFactory implements CodesTableFactory {
 	 * {@inheritDoc}
 	 */
 	@SuppressWarnings("unchecked")
-	public <T extends CodesTableItemPeriodic<K, V>, K extends Enum, V> CodesTablePeriodic<T, K, V>
+	public <T extends CodesTablePeriodicItem<K, V>, K extends Enum, V> CodesTablePeriodic<T, K, V>
 	                                       createCodesTablePeriodic(Class<T> codesTableItemPeriodicClass)
 			throws CodesTableNotFoundException {
+		log.warn("DEPRECATED - Use RepositoryCodesTableItemsFactory instead - DEPRECATED");
 		final List<T> items = codesTableRepository.findCodesTableItems(codesTableItemPeriodicClass);
 
 		return new DefaultCodesTablePeriodic<T, K, V>(items);
 	}
 	
 	/**
-	 * Sets the repository used to retrieve codestable from persistent repository
+	 * Sets the repository used to retrieve codestable from persistent repository.
+	 * 
 	 * @param codesTableRepository repository to use in thus factory
 	 */
 	public void setCodesTableRepository(CodesTableRepository codesTableRepository) {
