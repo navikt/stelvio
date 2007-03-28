4893d057b8d9be9e3822b6b917af998a1137cb27
bhc2812 <bhc2812@localhost>
2007-03-28 11:03:48 +0000

    lagt til for første gang

---
+++ BuildIntProjects/build.bat
@@ -0,0 +1,99 @@
+@echo off
+setlocal
+
+rem ==============================================================================
+rem This file is used to invoke build tasks
+rem build.bat
+rem
+rem  Available targets:
+rem
+rem      init               - displays the currently set variables and displays this help message
+rem      build-all-projects - Task to build all WID projects
+rem
+rem   To run a full build:
+rem
+rem    $AUTOMATION_HOME/build.bat init build-all-projects
+rem ==============================================================================
+
+rem ==============================================================================
+rem Load properties file setupBuild.bat
+rem ==============================================================================
+
+call setupBuild.bat
+
+rem ==============================================================================
+rem perform check of environment variable WID_HOME.
+rem ==============================================================================
+
+if exist %WID_HOME% goto continue3
+echo Error: Please set WID_HOME
+goto done
+  
+:continue3  
+echo WID_HOME= "%WID_HOME%"
+
+rem ==============================================================================
+rem perform check of environment variable WID_LIB.
+rem ==============================================================================
+
+if exist %WID_LIB% goto continue3
+echo Error: Please set WID_LIB
+goto done
+  
+:continue3  
+echo WID_LIB= "%WID_LIB%"
+
+
+rem ==============================================================================
+rem perform check of environment variable JAVA_HOME.
+rem ==============================================================================
+
+if exist %JAVA_HOME% goto continue4
+echo Error: Please set JAVA_HOME
+goto done
+  
+:continue4  
+echo JAVA_HOME= "%JAVA_HOME%"
+
+rem ==============================================================================
+rem perform check of environment variable AUTOMATION_HOME.
+rem ==============================================================================
+
+if exist %AUTOMATION_HOME% goto continue5
+echo Error: Please set AUTOMATION_HOME
+goto done
+
+:continue5
+echo AUTOMATION_HOME= "%AUTOMATION_HOME%"
+
+rem ==============================================================================
+rem perform check of environment variable SERVICEDEPLOY.
+rem ==============================================================================
+
+if defined SERVICEDEPLOY goto continue6
+echo Error: Please set SERVICEDEPLOY
+goto done
+
+:continue6
+echo SERVICEDEPLOY= "%SERVICEDEPLOY%"
+
+rem ==============================================================================
+rem call ant.
+rem ==============================================================================
+
+:run
+rmdir /S /Q %WORKSPACE%\.metadata
+%JAVA_HOME%\bin\java -Xms100M -Xmx500M -Dwid.home=%WID_HOME% -Dworkspace=%WORKSPACE% -Dlog.dir=%LOGDIR% -Doutput.dir.wsdl.zip=%OUTPUT_WSDL_DIR_ZIP% -Doutput.dir.ear=%OUTPUT_DIR_EAR% -Doutput.dir.zip=%OUTPUT_DIR_ZIP% -Dbuildscripts.dir=%AUTOMATION_HOME% -Dservice.deploy=%ServiceDeploy% -cp %WID_LIB%\sca-style.jar;%WID_LIB%\wsanttasks.jar;%WID_LIB%\wsprofile.jar;%ECLIPSE_HOME%\startup.jar org.eclipse.core.launcher.Main -logfile eclipse.log  -application com.ibm.etools.j2ee.ant.RunAnt  -buildfile %AUTOMATION_HOME%\masterBuild.xml -data "%WORKSPACE%" %*
+
+
+echo RunAnt returned %ERRORLEVEL%
+
+if "%ERRORLEVEL%"=="23" goto run
+if "%ERRORLEVEL%"=="15" echo ERROR WORKSPACE "%WORKSPACE%" is already BEING USED
+if "%ERRORLEVEL%"=="13" echo ERROR runAnt BUILD FAILED
+if NOT "%ERRORLEVEL%"=="0" echo ERROR runAnt BUILD FAILED. ErrorCode = "%ERRORLEVEL%"
+
+:done
+endlocal
+
+
---
+++ BuildIntProjects/setupBuild.bat
@@ -0,0 +1,60 @@
+@echo off
+
+
+rem -----------------------------------------------------------------------------------------
+rem This file is used to set up the necessary variables to run build automation.
+rem Run build.bat from the same directory.
+rem
+rem Important:  Use 8.3 file and directory names (ie. "PROGRA~1", not "Program Files")
+rem -----------------------------------------------------------------------------------------
+
+
+rem WID installation directory - use back slashes
+SET WID_HOME=C:\IBM\WID6
+
+rem Root directory of the automation scripts and workspaces (usually /Automation) - use back slashes
+SET AUTOMATION_HOME=E:\ws-test\BuildIntProjects
+
+
+rem Rational Software Development Platform Directory (install directory of the first RSDP product installed locally)
+SET RSDP_HOME=%WID_HOME%
+
+rem -----------------------------------------------------------------------------------------
+rem You shouldn't have to change any of the following environment variables
+rem -----------------------------------------------------------------------------------------
+
+rem JAVA home directory (usually %WID_HOME%\runtimes\base_v5\java) - use back slashes
+SET JAVA_HOME=%WID_HOME%\runtimes\bi_v6\java
+rem SET JAVA_HOME=C:\apps\Java\jdk1.5.0_09
+
+rem The directory where startup.jar is located
+SET ECLIPSE_HOME=%RSDP_HOME%\eclipse
+
+rem WID installation directory - use back slashes
+SET WID_LIB=%WID_HOME%\runtimes\bi_v6\lib
+
+rem We want a date in the format YYYYMMDD
+rem SET TODAYS_DATE=%DATE:~6%%DATE:~0,2%%DATE:~3,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
+SET TODAYS_DATE=%DATE:~6%%DATE:~0,2%%DATE:~3,2%
+
+
+
+rem The directory where the log files will be written - use back slashes
+SET LOGDIR=%AUTOMATION_HOME%\dailybuilds\%TODAYS_DATE%\logs
+
+rem Directory on local machine where EARs etc.. will be created and from where these
+rem files will be sent to the FTP_SERVER machine - use forward slashes
+SET OUTPUT_DIR_ZIP=%AUTOMATION_HOME%\dailybuilds\%TODAYS_DATE%\zip
+
+SET OUTPUT_WSDL_DIR_ZIP=%AUTOMATION_HOME%\dailybuilds\%TODAYS_DATE%\wsdl-zip
+
+rem Directory on local machine where EARs etc.. will be created and from where these
+rem files will be sent to the FTP_SERVER machine - use forward slashes
+SET OUTPUT_DIR_EAR=%AUTOMATION_HOME%\dailybuilds\%TODAYS_DATE%\ear
+
+rem WID workspace directory - use back slashes
+SET WORKSPACE=%AUTOMATION_HOME%\dailybuilds\%TODAYS_DATE%\workspace
+
+
+rem serviceDeploy path and file name
+SET SERVICEDEPLOY=%WID_HOME%\runtimes\bi_v6\bin\serviceDeploy.bat
