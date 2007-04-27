fea3780f3f12b4c1a5acc009f2006bf667042fa7
bhc2812 <bhc2812@localhost>
2007-04-27 08:03:54 +0000

    finnished up for now

--- maven-jaxhandler-plugin/src/main/java/no/nav/maven/plugins/JaxRPCHandlerMojo.java
+++ maven-jaxhandler-plugin/src/main/java/no/nav/maven/plugins/JaxRPCHandlerMojo.java
@@ -1,27 +1,7 @@
 package no.nav.maven.plugins;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.io.*;
-import java.util.Enumeration;
 import java.util.Iterator;
-import java.util.jar.JarFile;
-import java.util.jar.JarOutputStream;
-import java.util.zip.*;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -29,17 +9,18 @@ import org.dom4j.*;
 import org.dom4j.io.*;
 
 /**
- * Goal which touches a timestamp file.
+ * Goal which that adds the Stelvio jaxrpc handler to webservices.xml
  * 
- * @goal touch
+ * @goal addHandler
  * 
- 
+ * 
  */
-public class MyMojo extends AbstractMojo {
+public class JaxRPCHandlerMojo extends AbstractMojo {
 
 	private final String TEMP_OUTPUT = "target";
 
 	/**
+	 * This parameter is the directory where the wid ear files are placed.
 	 * @parameter
 	 * @required
 	 */
