67e216365f4a4ce0f96e939a2449b74973a4512d
fda2812 <fda2812@localhost>
2007-08-30 12:52:16 +0000

    Lagt til NAV-bruker-id

--- int/stelvio-commons-lib/gen/src/no/stelvio/common/bus/util/StelvioContextHelper.java
+++ int/stelvio-commons-lib/gen/src/no/stelvio/common/bus/util/StelvioContextHelper.java
@@ -2,12 +2,12 @@
  * StelvioConctextHelper.java Created on Feb 26, 2006 Author: persona2c5e3b49756 Schnell
  * (test@example.com)
  * 
- * This is a utility class that provides context information from StelvioContext or 
+ * This is a utility class that provides context information from StelvioContext
+ * or
  * 
- * @Updated:
- *  26-02-2006 -> initial Version
- *  08-03-2006 -> fixed NullPointerException WBISessionManager -  CC NAV00000491
- *    
+ * @Updated: 26-02-2006 -> initial Version 08-03-2006 -> fixed
+ * NullPointerException WBISessionManager - CC NAV00000491
+ *  
  */
 package no.stelvio.common.bus.util;
 
@@ -19,74 +19,90 @@ import java.util.logging.Logger;
 
 import javax.naming.InitialContext;
 import javax.security.auth.Subject;
+import javax.security.auth.login.CredentialExpiredException;
 
+import com.ibm.websphere.security.WSSecurityException;
+import com.ibm.websphere.security.auth.CredentialDestroyedException;
 import com.ibm.websphere.security.auth.WSSubject;
 import com.ibm.websphere.security.cred.WSCredential;
 import com.ibm.websphere.workarea.UserWorkArea;
 import com.ibm.ws.session.WBISessionManager;
 
 /*
- * <p> This is a utility class that provides helper methods around StelvioContext, useful
- * for different purpose as get an userId or the WBISessionId or other funtionality around StelvioContext</p>
+ * <p> This is a utility class that provides helper methods around
+ * StelvioContext, useful for different purpose as get an userId or the
+ * WBISessionId or other funtionality around StelvioContext </p>
  * 
-* @usage 
- * <p>
- *  no.stelvio.common.bus.util.StelvioContextHelper stelvioCtx = new StelvioContextHelper();
- *  String myUserId = stelvioCtx.getUserId();
+ * @usage <p> no.stelvio.common.bus.util.StelvioContextHelper stelvioCtx = new
+ * StelvioContextHelper(); String myUserId = stelvioCtx.getUserId();
  * 
- * Don't use System.out it is only an example. * 
- * @author persona2c5e3b49756 Schnell, test@example.com
+ * Don't use System.out it is only an example. * @author persona2c5e3b49756 Schnell,
+ * test@example.com
  *  
  */
 public class StelvioContextHelper {
 
-    // Sample - log.logp(Level.FINEST, className, <yourMethod>, <yourText>);
-	private final static String className = StelvioContextHelper.class.getName();
+	// Sample - log.logp(Level.FINEST, className, <yourMethod>, <yourText>);
+	private final static String className = StelvioContextHelper.class
+			.getName();
+
 	private final Logger log = Logger.getLogger(className);
 
 	//	static variables
 	final static String workAreaName = "BUS_STELVIO_CONTEXT";
-	final static String DEFAULT_USER_NAME = "WPS2812";
+
+	final static String DEFAULT_USER_NAME = "UNKNOWN";
+
 	final static String DEFAULT_LANGUAGE = "no";
-	final static String DEFAULT_APPLICATION_NAME = "PENSJON";
-	final static String DEFAULT_NO_WA  = "NO_BUS_STELVIO_CONTEXT";
-	
+
+	final static String DEFAULT_APPLICATION_NAME = "UNKN";
+
+	final static String DEFAULT_NO_WA = "NO_BUS_STELVIO_CONTEXT";
+
 	// initial UserWorkArea
 	private UserWorkArea workArea = null;
+
 	private InitialContext workAreaCtx = null;
-	private WBISessionManager sessionManager = null; 
-		
-	// variables 
+
+	private WBISessionManager sessionManager = null;
+
+	// variables
 	private String userId = null;
+
 	private String correlationId = null;
+
 	private String languageId = null;
+
 	private String applicationId = null;
+
 	private String wbiSessionId = null;
-	
-	
-		
+
+	private String navUserId = null;
+
 	/**
 	 * <p>
 	 * Constructor
 	 * </p>
-	 * 
-	 */	
+	 *  
+	 */
 	public StelvioContextHelper() {
-	
-		workArea= null;
+
+		workArea = null;
 		try {
-		    // 1. WorkArea
+			// 1. WorkArea
 			InitialContext initialContext = new InitialContext();
-			workArea = (UserWorkArea) initialContext.lookup("java:comp/websphere/UserWorkArea");
-			
+			workArea = (UserWorkArea) initialContext
+					.lookup("java:comp/websphere/UserWorkArea");
+
 			// 2. WBISession
 			sessionManager = WBISessionManager.getInstance();
-			
+
 			// 3. Initial variables
 			setStelvioBusContext();
-			
+
 		} catch (Exception e) {
-			log.logp(Level.SEVERE, className, "constructor()", "CatchedError: " + getExceptionTrace(e));
+			log.logp(Level.SEVERE, className, "constructor()", "CatchedError: "
+					+ getExceptionTrace(e));
 		}
 	}
 
@@ -96,136 +112,155 @@ public class StelvioContextHelper {
 	public String getApplicationId() {
 		return applicationId;
 	}
+
 	/**
-	 * @param applicationId The applicationId to set.
+	 * @param applicationId
+	 *            The applicationId to set.
 	 */
 	public void setApplicationId(String applicationId) {
 		this.applicationId = applicationId;
 	}
+
 	/**
 	 * @return Returns the corrleationId.
 	 */
 	public String getCorrelationId() {
 		return correlationId;
 	}
+
 	/**
-	 * @param corrleationId The corrleationId to set.
+	 * @param corrleationId
+	 *            The corrleationId to set.
 	 */
 	public void setCorrelationId(String correlationId) {
 		this.correlationId = correlationId;
 	}
+
 	/**
 	 * @return Returns the languageId.
 	 */
 	public String getLanguageId() {
 		return languageId;
 	}
+
 	/**
-	 * @param languageId The languageId to set.
+	 * @param languageId
+	 *            The languageId to set.
 	 */
 	public void setLanguageId(String languageId) {
 		this.languageId = languageId;
 	}
+
 	/**
 	 * @return Returns the userId.
 	 */
 	public String getUserId() {
 		return userId;
 	}
+
 	/**
-	 * @param userId The userId to set.
+	 * @param userId
+	 *            The userId to set.
 	 */
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
 
 	/**
+	 * @return Returns the navUserId.
+	 */
+	public String getNavUserId() {
+		return navUserId;
+	}
+
+	/**
 	 * getWBISessionId
 	 */
-	private String getWBISessionId()
-	{
-		
-		if (sessionManager.isSessionExisted())
-		{
-			wbiSessionId = sessionManager.getSessionContext().getSessionId();	
-		}
-		else
-		{			
+	private String getWBISessionId() {
+
+		if (sessionManager.isSessionExisted()) {
+			wbiSessionId = sessionManager.getSessionContext().getSessionId();
+		} else {
 			//TODO
 			wbiSessionId = null;
 		}
 
-		log.logp(Level.FINE, className, "getWBISessioId()", "WBISessionId=" + wbiSessionId);
-		
+		log.logp(Level.FINE, className, "getWBISessioId()", "WBISessionId="
+				+ wbiSessionId);
+
 		return wbiSessionId;
 	}
-	
+
 	/**
 	 * @return internal systemUserId.
 	 */
-	private String getSecurityIdentity()
-	{
+	private String getSecurityIdentity() {
 		String tName = Thread.currentThread().getName();
 		String sysUser = null;
-		
-		log.logp(Level.FINE, className, "getInternIdentity()", "Credential executing on the thread " + tName);
+
+		log.logp(Level.FINE, className, "getInternIdentity()",
+				"Credential executing on the thread " + tName);
 		try {
 			Subject runAsSecurity = WSSubject.getRunAsSubject();
 			if (runAsSecurity != null) {
-				Set security_credentials = runAsSecurity.getPublicCredentials(WSCredential.class);
-				WSCredential security_credential = (WSCredential)security_credentials.iterator().next();
+				Set security_credentials = runAsSecurity
+						.getPublicCredentials(WSCredential.class);
+				WSCredential security_credential = (WSCredential) security_credentials
+						.iterator().next();
 				sysUser = (String) security_credential.getSecurityName();
-			}
-			else
+			} else
 				sysUser = DEFAULT_USER_NAME;
-			
+
 		} catch (Exception e) {
-			log.logp(Level.SEVERE, className, "getInternIdentity()", "CatchedError: " + getExceptionTrace(e));
+			log.logp(Level.SEVERE, className, "getInternIdentity()",
+					"CatchedError: " + getExceptionTrace(e));
 		}
-		log.logp(Level.FINE, className, "getInternIdentity()", "return " + sysUser);
+		log.logp(Level.FINE, className, "getInternIdentity()", "return "
+				+ sysUser);
 		return sysUser;
 	}
-	
-	
+
 	/**
 	 * internal method to set the context
 	 */
 	private void setStelvioBusContext() {
-		
+
 		if (workArea != null) {
-			
+
 			String wName = workArea.getName();
-			if (wName==null) wName=DEFAULT_NO_WA;
-			
-			if (wName.equalsIgnoreCase(workAreaName))
-			{
-				log.logp(Level.INFO, className, "setStelvioBusContext()", "StelvioContext exists in WorkArea");
-				
+			if (wName == null)
+				wName = DEFAULT_NO_WA;
+
+			if (wName.equalsIgnoreCase(workAreaName)) {
+				log.logp(Level.INFO, className, "setStelvioBusContext()",
+						"StelvioContext exists in WorkArea");
+
 				userId = (String) workArea.get("userId");
-				if (userId == null || userId.length() <= 0 ) {
-					userId = getSecurityIdentity(); 
+				if (userId == null || userId.length() <= 0) {
+					userId = getSecurityIdentity();
 				}
-				
+
 				languageId = (String) workArea.get("languageId");
-				if (languageId == null || languageId.length() <= 0 ) {
+				if (languageId == null || languageId.length() <= 0) {
 					languageId = DEFAULT_LANGUAGE;
 				}
 
 				applicationId = (String) workArea.get("applicationId");
-				if (applicationId == null || applicationId.length() <= 0 ) {
+				if (applicationId == null || applicationId.length() <= 0) {
 					applicationId = DEFAULT_APPLICATION_NAME;
 				}
 
 				correlationId = (String) workArea.get("correlationId");
-				if (correlationId == null || correlationId.length() <= 0 ) {
-				  correlationId = getWBISessionId();
+				if (correlationId == null || correlationId.length() <= 0) {
+					correlationId = getWBISessionId();
 				}
 
-				
 			}
 			// another WorkArea
 			else {
-				log.logp(Level.FINE, className, "setStelvioBusContext()", "StelvioContext doesn't exists within WorkArea - use default values");			
+				log
+						.logp(Level.FINE, className, "setStelvioBusContext()",
+								"StelvioContext doesn't exists within WorkArea - use default values");
 				userId = DEFAULT_USER_NAME;
 				languageId = DEFAULT_LANGUAGE;
 				applicationId = DEFAULT_APPLICATION_NAME;
@@ -234,12 +269,36 @@ public class StelvioContextHelper {
 		}
 		// set the default values in general
 		else {
-			log.logp(Level.FINE, className, "setStelvioBusContext()", "StelvioContext doesn't exists within WorkArea - use default values");			
+			log
+					.logp(Level.FINE, className, "setStelvioBusContext()",
+							"StelvioContext doesn't exists within WorkArea - use default values");
 			userId = DEFAULT_USER_NAME;
 			languageId = DEFAULT_LANGUAGE;
 			applicationId = DEFAULT_APPLICATION_NAME;
-			correlationId = getWBISessionId(); 			
+			correlationId = getWBISessionId();
 		}
+
+		//Set NAV-user from security-credential
+		try {
+			Subject security_subject = WSSubject.getRunAsSubject();
+			if (security_subject != null) {
+				// Get all security credentials from the security subject
+				Set security_credentials = security_subject
+						.getPublicCredentials(WSCredential.class);
+				// Get the first credential
+				WSCredential security_credential = (WSCredential) security_credentials
+						.iterator().next();
+				navUserId = (String) security_credential.getSecurityName();
+			}
+			//Set user = Default if failed.
+		} catch (CredentialDestroyedException e) {
+			navUserId = DEFAULT_USER_NAME;
+		} catch (CredentialExpiredException e1) {
+			navUserId = DEFAULT_USER_NAME;
+		} catch (WSSecurityException e) {
+			navUserId = DEFAULT_USER_NAME;
+		}
+
 	}
 
 	/**
@@ -247,31 +306,39 @@ public class StelvioContextHelper {
 	 * Pretty print the Stelvio workArea context.
 	 * </p>
 	 * 
-	 * @param sModulIdent for logging
-	 *            
+	 * @param sModulIdent
+	 *            for logging
+	 * 
 	 * @return void
 	 */
-	public void printStelvioContext(String sModulIdent)
-	{
-		if (workArea != null)
-		{
-			log.logp(Level.INFO, className, "printStelvioContext()", "-- " + sModulIdent + ": " + workAreaName);
-			log.logp(Level.INFO, className, "printStelvioContext()", "--- NameWorkArea:" + workArea.getName());
-			log.logp(Level.INFO, className, "printStelvioContext()", "--- userId:" + userId);
-			log.logp(Level.INFO, className, "printStelvioContext()", "--- correlationId:" + correlationId);
-			log.logp(Level.INFO, className, "printStelvioContext()", "--- languageId:" + languageId);
-			log.logp(Level.INFO, className, "printStelvioContext()", "--- applicationId:" + applicationId);
-			log.logp(Level.INFO, className, "printStelvioContext()", "-- " + sModulIdent + ": " + workAreaName);
-		}
-		else
-			log.logp(Level.INFO, className, "printStelvioContext()", "-- " + sModulIdent + ": Work Area <" + workAreaName + "> doesn't exists");	
+	public void printStelvioContext(String sModulIdent) {
+		if (workArea != null) {
+			log.logp(Level.INFO, className, "printStelvioContext()", "-- "
+					+ sModulIdent + ": " + workAreaName);
+			log.logp(Level.INFO, className, "printStelvioContext()",
+					"--- NameWorkArea:" + workArea.getName());
+			log.logp(Level.INFO, className, "printStelvioContext()",
+					"--- userId:" + userId);
+			log.logp(Level.INFO, className, "printStelvioContext()",
+					"--- correlationId:" + correlationId);
+			log.logp(Level.INFO, className, "printStelvioContext()",
+					"--- languageId:" + languageId);
+			log.logp(Level.INFO, className, "printStelvioContext()",
+					"--- applicationId:" + applicationId);
+			log.logp(Level.INFO, className, "printStelvioContext()", "-- "
+					+ sModulIdent + ": " + workAreaName);
+		} else
+			log.logp(Level.INFO, className, "printStelvioContext()", "-- "
+					+ sModulIdent + ": Work Area <" + workAreaName
+					+ "> doesn't exists");
 	}
-	
+
 	/**
 	 * Convert exception stacktrace to string
 	 * 
-	 * @param exception	the Exception to convert
-	 * @return 	the string representation of the Exception
+	 * @param exception
+	 *            the Exception to convert
+	 * @return the string representation of the Exception
 	 */
 	private String getExceptionTrace(Exception ex) {
 		StringWriter sw = new StringWriter();
