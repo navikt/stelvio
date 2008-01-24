4ab1dfcfa62cb8f2282e059bc7058799a1bcb125
paa2812 <paa2812@localhost>
2008-01-24 17:10:56 +0000

    sdfasdfasdfasdfasdf

--- dev/maven2/maven-datapower-configurer/src/main/java/no/nav/maven/plugins/Configurer.java
+++ dev/maven2/maven-datapower-configurer/src/main/java/no/nav/maven/plugins/Configurer.java
@@ -1,27 +1,24 @@
 package no.nav.maven.plugins;
 
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
-import org.dom4j.Element;
 import org.dom4j.XPath;
 import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
-
-import sun.misc.BASE64Encoder;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+
 
 /**
  * Goal which touches a timestamp file.
@@ -32,26 +29,56 @@ import java.util.Date;
 public class Configurer
 extends AbstractMojo
 {
+	
+	private static char[] map1 = new char[64];
+	   static {
+	      int i=0;
+	      for (char c='A'; c<='Z'; c++) map1[i++] = c;
+	      for (char c='a'; c<='z'; c++) map1[i++] = c;
+	      for (char c='0'; c<='9'; c++) map1[i++] = c;
+	      map1[i++] = '+'; map1[i++] = '/'; }
+
 	/**
 	 * Location of the file.
 	 * @parameter expression="${outDir}"
 	 * @required
 	 */
-	private File outputDirectory; //= new File("E:/maven-plugins/maven-datapower-configurer/target");
+	private File outputDirectory; // = new File("E:/maven-plugins/maven-datapower-configurer/target");
 	
 	/**
 	 * Location of the file.
 	 * @parameter expression="${wsdl.zip}"
 	 * @required
 	 */
-	private File wsdlFile; // = new File("E:/maven-plugins/maven-datapower-configurer/wsdl-pselv.zip");
+	private File wsdlFile; // = new File("E:/deploy_scripts/kjempen/target/classes/builds/wsdl/wsdl-pselv.zip");
 	
 	/**
 	 * Location of the file.
 	 * @parameter expression="${template}"
 	 * @required
 	 */
-	private File template; // = new File("E:/maven-plugins/maven-datapower-configurer/template.xcfg");
+	private File template; // = new File("E:/maven-plugins/maven-datapower-configurer/src/main/resources/template.xcfg");
+	
+	/**
+	 * Location of the file.
+	 * @parameter expression="${WSHost}"
+	 * @required
+	 */
+	private String WSHost; // = "https://secgw-01.utv.internsone.local:5550/service/mgmt/current";
+	
+	/**
+	 * Location of the file.
+	 * @parameter expression="${user}"
+	 * @required
+	 */
+	private String user = "";
+	
+	/**
+	 * Location of the file.
+	 * @parameter expression="${password}"
+	 * @required
+	 */
+	private String password = "";
 	
 	/**
 	 * Private variables
@@ -60,9 +87,9 @@ extends AbstractMojo
 	
 	private Document doc;
 	
-	private Element files;
-	
 	private XPath xpath;
+	
+	private int retries = 0;
 	
 	public void execute() throws MojoExecutionException
 	{
@@ -74,17 +101,12 @@ extends AbstractMojo
 			
 			//extracting wsdl files
 			extract();
-			
-			//adding files to DataPower config file
-			xpath = doc.createXPath("//files");
-			files = (Element)xpath.selectSingleNode(doc);
-			if(files == null) throw new DocumentException("Unable to find files node in template!");
-			getLog().info("Adding wsdl archive content to configuration file...");
-			addFilesToTemplate(tmpFolder);
-			getLog().info("Done!");
+		
+			//String test = new Base64Codec(null,"").encodeBase64(user + ":" + password);
+			uploadFilesAndFolders(tmpFolder);
 			
 			//writing merged template
-			writeXML();
+			//writeXML();
 			
 			//cleaning up temporary files
 			cleanUp(tmpFolder);
@@ -109,32 +131,148 @@ extends AbstractMojo
 		doc = reader.read(template);    	
 		getLog().info("Done!");
 	}
+
+	private void uploadFilesAndFolders(File root) throws IOException{
+		List folders = new ArrayList();
+		List files = new ArrayList();
+		DataPowerRequest request, request2;
+		Iterator iter;
+		String AuthString = "Basic " + new Base64Codec(null,"").encodeBase64(user + ":" + password);
+		scanFilesAndFolders(tmpFolder, folders, files);
+		
+		//Creating folders
+		request = new DataPowerRequest();
+		request.setContent(getSOAPEnvelope_mkdir(folders));
+		request.getHTTPHeader().put("Authorization",AuthString);
+		getLog().info("Creating the following remote folders:");
+		iter = folders.iterator();
+		while(iter.hasNext()) getLog().info(iter.next().toString());
+		sendHTTPSRequest(request);
+		
+		//Uploading files
+		request2 = new DataPowerRequest();
+		request2.setContent(getSOAPEnvelope_setfile(files));
+		request2.getHTTPHeader().put("Authorization",AuthString);
+		getLog().info("Uploading following files to datapower");
+		iter = files.iterator();
+		while(iter.hasNext()) getLog().info(getSubPath(new File(iter.next().toString())));
+		sendHTTPSRequest(request2);
+	}
 	
-	private void addFilesToTemplate(File root) throws DocumentException{
+	private void scanFilesAndFolders(File root, List folders, List files){
 		File[] children = root.listFiles();
 		File child;
-		Element file;
+		String path;
 		
 		for (int i = 0; i < children.length; i++) {
 			child = children[i];
-			if(child.isDirectory()) addFilesToTemplate(child);
-			else{
-				if(child.getName().toLowerCase().compareTo("manifest.mf") == 0) continue;
-				String path = getSubPath(child);
-				file = files.addElement("file");
-				
-				file.addAttribute("name","local:///" + path);
-				file.addAttribute("src","local/" + path);
-				file.addAttribute("location","local");
-				getLog().info("Adding " + path);
-				try {
-					file.addAttribute("hash",getHash(child));
-					file.setText(base64Encode(child));
-				} catch (IOException e) {
-					throw new DocumentException("Unable to get Base64Encoding/MD5Hash for " + child,e);
-				}
+			if(child.isDirectory()){
+				folders.add("local:///" + getSubPath(child));
+				scanFilesAndFolders(child, folders, files);
+			}else{
+				if(child.getName().toLowerCase().compareTo("manifest.mf") != 0) files.add(child);
 			}
 		}
+	}
+	
+	private void sendHTTPSRequest(DataPowerRequest request) throws IOException{
+		HttpURLConnection conn = null;
+		URL gatewayURL = null;
+		OutputStream out = null;
+		InputStream in = null;
+		byte[] buf;
+		StringBuffer response = null;
+		int character;
+		
+		
+		try {
+			gatewayURL = new URL(WSHost);
+			
+			getLog().info("Connecting to datapower...");
+			conn = (HttpURLConnection)gatewayURL.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			
+			Enumeration enum = request.getHTTPHeader().keys();
+			while(enum.hasMoreElements()){
+				String key = enum.nextElement().toString();
+				conn.setRequestProperty(key,request.getHTTPHeader().get(key).toString());
+			}
+			
+			conn.connect();
+			getLog().info("Connected");
+			
+			
+			getLog().info("Sending request...");
+			out = conn.getOutputStream();
+			//write headr info as well??????
+			out.write(request.getContent().getBytes("UTF-8"));
+
+			in = new BufferedInputStream(conn.getInputStream(),32 * 1024);
+			response = new StringBuffer();
+			buf = new byte[4096];
+			
+			int status = 0;
+			while((in.read(buf) != -1)){
+				response.append(new String(buf));
+			}
+		} catch (IOException e) {
+			if(retries++ < 10){
+				getLog().warn(e.getMessage() + ", retrying " + retries + "/10");
+				sendHTTPSRequest(request);
+			}else {
+				getLog().error(e.getMessage());
+				throw e;
+			}	
+		}
+		
+		
+		if(response.indexOf("<dp:result>OK</dp:result>") < 0){ //failure
+			getLog().error("Server error message:\n\n" + response);
+		}else getLog().info("Request successfull!\n");
+		
+		in.close();
+		out.close();
+		conn.disconnect();
+	}
+	
+	private String getSOAPEnvelope_mkdir(List folders){
+		String env = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:man=\"http://www.datapower.com/schemas/management\">\n" +
+				"<soapenv:Header/>\n" +
+				"<soapenv:Body>\n" +
+					"<man:request domain=\"test-config\">\n" +
+						"<man:do-action>\n";
+		
+		Iterator iter = folders.iterator();
+		while(iter.hasNext()){
+			env += 	"<CreateDir>\n" +
+			"<Dir>" + iter.next().toString() + "</Dir>\n" +
+			"</CreateDir>\n";
+		}
+		env +=	"</man:do-action>\n" +
+					"</man:request>\n" +
+				"</soapenv:Body>\n" +
+			"</soapenv:Envelope>";
+		return env;
+	}
+	
+	private String getSOAPEnvelope_setfile(List files) throws IOException{
+		String base64;
+		String env = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:man=\"http://www.datapower.com/schemas/management\">\n" +
+		   "<soapenv:Header/>\n" +
+		   "<soapenv:Body>\n" +
+		      "<man:request domain=\"test-config\">\n";
+		Iterator iter = files.iterator();
+		while(iter.hasNext()){
+			String file = iter.next().toString();
+			base64 = base64Encode(new File(file));
+			env += "<man:set-file name=\"local:///" + getSubPath(new File(file)) + "\">" + base64 + "</man:set-file>\n";
+		}
+		env += 	"</man:request>\n" +
+		   			"</soapenv:Body>\n" +
+				"</soapenv:Envelope>";
+		return env;
 	}
 	
 	private String getHash(File file) throws IOException{
@@ -145,7 +283,7 @@ extends AbstractMojo
 		try {
 			is = new FileInputStream(file);
 			len = new Long(file.length());
-			digest = MessageDigest.getInstance("MD5");
+			digest = MessageDigest.getInstance("SHA");
 			
 			byte[] content = new byte[len.intValue()];
 			
@@ -155,26 +293,51 @@ extends AbstractMojo
 			is.close();
 			
 			digest.update(content);
+			byte[] hash = digest.digest();
 			
-			return new BASE64Encoder().encode(digest.digest());
+			String test = new Base64Codec(null,"").encodeBase64("petterasskildt:gy59inku");
+			return new String(base64Encode(hash,hash.length));
 		} catch (Exception e) {
 			throw new IOException("Error while creating hash of " + file.getAbsolutePath() + ": " + e.getMessage());
 		}
 	}
 	
+	public char[] base64Encode (byte[] in, int iLen) {
+		   int oDataLen = (iLen*4+2)/3;       // output length without padding
+		   int oLen = ((iLen+2)/3)*4;         // output length including padding
+		   char[] out = new char[oLen];
+		   int ip = 0;
+		   int op = 0;
+		   while (ip < iLen) {
+		      int i0 = in[ip++] & 0xff;
+		      int i1 = ip < iLen ? in[ip++] & 0xff : 0;
+		      int i2 = ip < iLen ? in[ip++] & 0xff : 0;
+		      int o0 = i0 >>> 2;
+		      int o1 = ((i0 &   3) << 4) | (i1 >>> 4);
+		      int o2 = ((i1 & 0xf) << 2) | (i2 >>> 6);
+		      int o3 = i2 & 0x3F;
+		      out[op++] = map1[o0];
+		      out[op++] = map1[o1];
+		      out[op] = op < oDataLen ? map1[o2] : '='; op++;
+		      out[op] = op < oDataLen ? map1[o3] : '='; op++; }
+		   return out; }
+	
 	private String base64Encode(File file) throws IOException{
-		BASE64Encoder encoder = new BASE64Encoder();
-		InputStream is = new FileInputStream(file);
-		Long len = new Long(file.length());
+		Base64Codec codec = new Base64Codec(null, "");
+		Reader reader = new InputStreamReader(new FileInputStream(file),"UTF-8");
+		int length = new Long(file.length()).intValue();
+		char[] buf = new char[length];
 		
-		byte[] content = new byte[len.intValue()];
+		int readChars = reader.read(buf, 0, length);
+		if(readChars < length) throw new IOException("Unable to read all content from " + file.getAbsolutePath());
+		return codec.encodeBase64(new String(buf));
+	}
+	
+	private String base64Encode(byte[] buf) throws IOException{
+		String str = new String(buf);
+		Base64Codec codec = new Base64Codec(null, "");
 		
-		//reading bytes from file
-		int readBytes = is.read(content,0,len.intValue());
-		if(readBytes != len.intValue()) throw new IOException("Unable to read all content from " + file.getAbsolutePath());
-		is.close();
-		
-		return encoder.encode(content);
+		return codec.encodeBase64(str);
 	}
 	
 	private String getSubPath(File path){
@@ -205,7 +368,7 @@ extends AbstractMojo
 		
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(configFile),"UTF-8"));
 		XMLWriter writer = new XMLWriter(bw,OutputFormat.createPrettyPrint());
-		writer.write(doc);
+		writer.write(doc.getRootElement());
 		writer.close();
 		
 		getLog().info("\n\n\t\tWritten configuration to " + configFile.getAbsolutePath());
---
+++ dev/maven2/maven-datapower-configurer/src/main/java/no/nav/maven/plugins/DataPowerRequest.java
@@ -0,0 +1,35 @@
+/*
+ * Created on Jan 24, 2008
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package no.nav.maven.plugins;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+/**
+ * @author utvikler
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class DataPowerRequest {
+
+	private String content;
+	private Dictionary HTTPHeader = new Hashtable();
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Dictionary getHTTPHeader() {
+		return HTTPHeader;
+	}
+	public void setHTTPHeader(Dictionary header) {
+		HTTPHeader = header;
+	}
+}
