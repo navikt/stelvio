0467a2d8ad1b51368fdc5a65f82848fd4efb73f9
paa2812 <paa2812@localhost>
2008-02-13 12:45:02 +0000

    added info statement

--- dev/maven2/maven-datapower-configurer/src/main/java/no/nav/maven/plugins/Configurer.java
+++ dev/maven2/maven-datapower-configurer/src/main/java/no/nav/maven/plugins/Configurer.java
@@ -227,6 +227,7 @@ extends AbstractMojo
 		File LTPAFolder = new File(environment.getAbsolutePath().replaceAll(environment.getName(), "") + "/ltpa-keys");
 		File[] keys = LTPAFolder.listFiles();
 		getLog().info("------------- LTPA Keys Import -------------");
+		getLog().info("Searching for keys in " + LTPAFolder.getAbsolutePath());
 		if(keys == null) getLog().info("No keys to import...");
 		else{
 			for (int i = 0; i < keys.length; i++) {
