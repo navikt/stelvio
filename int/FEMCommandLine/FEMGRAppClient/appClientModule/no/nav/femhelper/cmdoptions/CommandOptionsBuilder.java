7a23b3ae367d42e467deff52167fb0bd22d94314
ard2812 <ard2812@localhost>
2008-09-04 13:52:05 +0000

    Added new class to support use of Apache CLI

---
+++ int/FEMCommandLine/FEMGRAppClient/appClientModule/no/nav/femhelper/cmdoptions/CommandOptionsBuilder.java
@@ -0,0 +1,77 @@
+package no.nav.femhelper.cmdoptions;
+
+import no.nav.femhelper.common.Constants;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.lang.ArrayUtils;
+
+public class CommandOptionsBuilder {
+	
+	// Default constructor
+	public CommandOptionsBuilder() {
+		
+	}
+	
+	public Options getOptions() {
+		
+		// Create command line option for 'help'
+		Option help = OptionBuilder.withLongOpt("help").create();
+		help.setDescription("This help index");
+		
+		// Create command line option for 'configFile'
+		// This option is requiered
+		Option configFile = OptionBuilder.withLongOpt("configFile").withValueSeparator().hasArg().create();
+		configFile.setArgName("full path");
+		configFile.setDescription("Mandatory. Full path to configuration file for system environment spesifications (hostname etc.).");
+		
+		// Create command line option for 'logFilePath'
+		// This option is requiered
+		Option logFilePath = OptionBuilder.withLongOpt("logFilePath").withValueSeparator().hasArg().create();
+		logFilePath.setArgName("path");
+		logFilePath.setDescription("Path for directory where the output file will be located.");
+		
+		// Create command line option for 'logFileName'
+		// This option is requiered
+		Option logFileName = OptionBuilder.withLongOpt("logFileName").withValueSeparator().hasArg().create();
+		logFileName.setArgName("filename");
+		logFileName.setDescription("Name of output log file. Default value is xxx");
+		
+		// Create command line option for 'messageType'
+		// This option is requiered
+		Option messageType = OptionBuilder.withLongOpt("messageType").withValueSeparator().hasArg().create();
+		messageType.setArgName(ArrayUtils.toString(Constants.messageTypeOptions));
+		messageType.setDescription("Mandatory");
+		
+		// Create command line option for 'action'
+		// This option is requiered
+		Option action = OptionBuilder.withLongOpt("action").withValueSeparator().hasArg().create();
+		action.setArgName(ArrayUtils.toString(Constants.actionOptions));
+		action.setDescription("Mandatory");
+		
+//		 Create command line option for 'maxResultSet'
+		// This option is requiered
+		Option maxResultSet = OptionBuilder.withLongOpt("maxResultSet").withValueSeparator().hasArg().create();
+		maxResultSet.setArgName("integer");
+		maxResultSet.setDescription("Default value is xxx. Recommended to use range between x and y ");
+		
+		// Create command line option for 'maxResultSetPaging'
+		// This option is requiered
+		Option maxResultSetPaging = OptionBuilder.withLongOpt("maxResultSetPaging").withValueSeparator().hasArg().create();
+		maxResultSetPaging.setArgName("boolean");
+		maxResultSetPaging.setDescription("Mandatory");
+		
+		Options options = new Options();
+		options.addOption(help);
+		options.addOption(configFile);
+		options.addOption(logFilePath);
+		options.addOption(logFileName);
+		options.addOption(messageType);
+		options.addOption(action);
+		options.addOption(maxResultSet);
+		options.addOption(maxResultSetPaging);
+		return options;
+	}
+	
+}
