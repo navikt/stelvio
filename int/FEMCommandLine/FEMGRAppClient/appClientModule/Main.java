fc6dcac229035d40a2546f99634346fccbb243b0
sbc2812 <sbc2812@localhost>
2008-09-13 14:41:27 +0000

    Renamed variable LOGGER to logger, shortened command line output.

--- int/FEMCommandLine/FEMGRAppClient/appClientModule/Main.java
+++ int/FEMCommandLine/FEMGRAppClient/appClientModule/Main.java
@@ -32,12 +32,12 @@ import utils.PropertyUtil;
  */
 public class Main {
 
-	private static Logger LOGGER = Logger.getLogger(Main.class.getName());
+	private static Logger logger = Logger.getLogger(Main.class.getName());
 	
 	public static void main(String[] args) throws IOException {
 
-		LOGGER.log(Level.FINE, Constants.METHOD_ENTER + "main");
-		LOGGER.log(Level.INFO, "FEMGRAppClient for WPS 6.1 - Version 0.9");
+		logger.log(Level.FINE, Constants.METHOD_ENTER + "main");
+		logger.log(Level.INFO, "FEMGRAppClient for WPS 6.1 - Version 0.9");
 		
 		CommandOptionsBuilder optionsBuilder = new CommandOptionsBuilder();
 		Options options = optionsBuilder.getOptions();
@@ -46,7 +46,7 @@ public class Main {
 		try {
 			cl = new PosixParser().parse(options, args);
 		} catch (ParseException parseEx) {
-			System.out.println("Incorrect arguments (listed below). Due to this will the application now terminate");
+			System.out.println("Incorrect arguments (listed below) - application will terminate.");
 			System.out.println(parseEx.getMessage());
 			System.exit(-1); // TODO AR Find correct return code for error
 		}
@@ -56,7 +56,7 @@ public class Main {
 			formatter.setWidth(500);
 			
 			System.out.println("============================================================");
-			System.out.println("Fail Event Manager (FEM) Helper - Version 0.9");
+			System.out.println("Failed Event Manager (FEM) Helper - Version 0.9");
 			System.out.println("============================================================");
 			System.out.println("Sample usage: launchClient.sh FEMHelper.ear " +
 					"--configFile=/was_app/config/fem/fem.properties " +
@@ -67,14 +67,14 @@ public class Main {
 		}
 		
 		if (!cl.hasOption(Constants.configFile))	{	
-			LOGGER.log(Level.WARNING, Constants.METHOD_ERROR + "The property file is not spesified");
+			logger.log(Level.WARNING, Constants.METHOD_ERROR + "Property file not spesified.");
 			System.exit(0);
 		} 
 		
 		String configFilePath = cl.getOptionValue(Constants.configFile);
 		File propertyFile = new File(configFilePath);
 		if (!propertyFile.exists()) {
-			LOGGER.log(Level.WARNING, Constants.METHOD_ERROR + "The property file does not exist (" + configFilePath + ")");
+			logger.log(Level.WARNING, Constants.METHOD_ERROR + "The property file does not exist (" + configFilePath + ")");
 			System.exit(0);
 		}
 		
@@ -83,10 +83,10 @@ public class Main {
 		if (!validatedArguments.isEmpty()) {
 			// Log all errors that occured in validation
 			for (int i = 0; i < validatedArguments.size(); i++) {
-				LOGGER.log(Level.WARNING, (String) validatedArguments.get(i));
+				logger.log(Level.WARNING, (String) validatedArguments.get(i));
 			}
 			
-			LOGGER.log(Level.WARNING, "Exiting due insufficient arguments. See details listed above");
+			logger.log(Level.WARNING, "Exiting due to insufficient number of arguments. See details listed above");
 			System.exit(0);
 		}
 		
@@ -103,23 +103,23 @@ public class Main {
 		
 		if (!validatedProperties.isEmpty()) {
 			for (int i = 0; i < validatedProperties.size(); i++) {
-				LOGGER.log(Level.WARNING, (String) validatedProperties.get(i));
+				logger.log(Level.WARNING, (String) validatedProperties.get(i));
 			}
-			LOGGER.log(Level.SEVERE, "FEMGRAppClient will now end due to issues with missing or invalid parameter in the configuration file - logged above!");
+			logger.log(Level.SEVERE, "FEMGRAppClient terminating due to issues with missing or invalid parameters in configuration file - logged above!");
 			System.exit(0);
 		}
 
 		try {
-			LOGGER.log(Level.FINE, "Create status action instance");
+			logger.log(Level.FINE, "Create status action instance");
 			AbstractAction statusAction = ActionFactory.getAction(Constants.actionOptions[3], connectProps);
 
 			// Get the total number of events to match with the page size to avoid hughe amount of retrieving
 			Long numberOfEvents = (Long) statusAction.process(null, null, null, false, 0, 0, cl);
 			if (numberOfEvents.intValue() < 1) {
-				LOGGER.log(Level.INFO, "The is no events on this Fail Event Manager. The application will terminate");
+				logger.log(Level.INFO, "There are no events on Failed Event Manager. The application will terminate");
 				System.exit(0);
 			} else {
-				LOGGER.log(Level.INFO, "ACTION REQUIERED! There is events on Fail Event Manager");
+				logger.log(Level.INFO, "ACTION REQUIERED! There are events on Failed Event Manager");
 			}
 			
 			// Set to max. fem entries if pagsizse is greater
@@ -127,7 +127,7 @@ public class Main {
 			if (pagesize > numberOfEvents)
 			{
 				pagesize = numberOfEvents.intValue();
-				LOGGER.log(Level.INFO, "Set working result set to total number of events (" + pagesize + ") because MAX_RESULT_SET > TotalEvents.");
+				logger.log(Level.INFO, "Set working result set to total number of events (" + pagesize + ") because MAX_RESULT_SET > TotalEvents.");
 			}
 			
 			// Determine paging
@@ -145,11 +145,9 @@ public class Main {
 			action.process(path, filename, arguments, paging, numberOfEvents, pagesize, cl);
 						
 		} catch (Exception e) {
-			LOGGER.log(Level.SEVERE, Constants.METHOD_ERROR + "Exception:StackTrace:");
+			logger.log(Level.SEVERE, Constants.METHOD_ERROR + "Exception:StackTrace:");
 			e.printStackTrace();
-		}
-		
-		LOGGER.log(Level.INFO, "FEMGRAppClient done...Thx for using it!");
-		LOGGER.log(Level.FINE, Constants.METHOD_EXIT + "main");
+		}		
+		logger.log(Level.INFO, "FEMGRAppClient done.");
 	}
 }
