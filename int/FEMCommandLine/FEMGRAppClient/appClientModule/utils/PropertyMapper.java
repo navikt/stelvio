11ab861f2235c9d27c5c002e2795dc179a15acf7
sbc2812 <sbc2812@localhost>
2008-09-13 14:32:06 +0000

    Corrected indentation.

--- int/FEMCommandLine/FEMGRAppClient/appClientModule/utils/PropertyMapper.java
+++ int/FEMCommandLine/FEMGRAppClient/appClientModule/utils/PropertyMapper.java
@@ -11,8 +11,9 @@ import org.apache.commons.lang.StringUtils;
 import com.ibm.websphere.management.AdminClient;
 
 /**
- * <p>Mapper class to ensure consistency of the <code>java.util.Properties</code>
- * object passed to <code>AdminClientFactory.createAdminClient().<p>
+ * <p>
+ * Mapper class to ensure consistency of the <code>java.util.Properties</code> object passed to
+ * <code>AdminClientFactory.createAdminClient().<p>
  * 
  * <p>Without this mapper the properties are read directly from the 
  * <code>.properties</code> file, and that may cause a runtime exception of 
@@ -23,70 +24,70 @@ import com.ibm.websphere.management.AdminClient;
  * @author Andreas Røe
  */
 public class PropertyMapper {
-	
+
 	/**
 	 * Logger instance
 	 */
 	private static Logger logger = Logger.getLogger(PropertyMapper.class.getName());
-		
+
 	/**
-	 * <p>This method perfomes the actual mapping. No validation of the content
-	 * and consistency of the configuration here. This is allready performed in
-	 * <code>PropertyUtil.validateProperties()</code>. See class documentation
-	 * for the purpose of this.</p>
+	 * <p>
+	 * This method perfomes the actual mapping. No validation of the content and consistency of the configuration here. This is
+	 * allready performed in <code>PropertyUtil.validateProperties()</code>. See class documentation for the purpose of this.
+	 * </p>
 	 * 
-	 * @param src source properties read directly from the configuration file
+	 * @param src
+	 *            source properties read directly from the configuration file
 	 * @return a properly mapped set of properties
 	 */
 	public Properties getMappedProperties(Properties src) {
 		logger.log(Level.FINE, Constants.METHOD_ENTER + "getMappedProperties");
-		
+
 		Properties result = new Properties();
 
 		if (!StringUtils.isEmpty(src.getProperty(Constants.CONNECTOR_HOST))) {
 			result.setProperty(AdminClient.CONNECTOR_HOST, src.getProperty(Constants.CONNECTOR_HOST));
 		}
-		
+
 		if (!StringUtils.isEmpty(src.getProperty(Constants.CONNECTOR_PORT))) {
 			result.setProperty(AdminClient.CONNECTOR_PORT, src.getProperty(Constants.CONNECTOR_PORT));
 		}
-		
+
 		if (!StringUtils.isEmpty(src.getProperty(Constants.CONNECTOR_TYPE))) {
 			result.setProperty(AdminClient.CONNECTOR_TYPE, src.getProperty(Constants.CONNECTOR_TYPE));
 		}
-		
+
 		if (!StringUtils.isEmpty(src.getProperty(Constants.CONNECTOR_SECURITY_ENABLED))) {
 			result.setProperty(AdminClient.CONNECTOR_SECURITY_ENABLED, src.getProperty(Constants.CONNECTOR_SECURITY_ENABLED));
 		}
-		
+
 		if (!StringUtils.isEmpty(src.getProperty(Constants.USERNAME))) {
 			result.setProperty(AdminClient.USERNAME, src.getProperty(Constants.USERNAME));
 		}
-		
+
 		if (!StringUtils.isEmpty(src.getProperty(Constants.PASSWORD))) {
 			result.setProperty(AdminClient.PASSWORD, src.getProperty(Constants.PASSWORD));
 		}
-		
-		if (!"RMI".equals(src.getProperty(Constants.CONNECTOR_TYPE))) 
-		{
-				if (!StringUtils.isEmpty(src.getProperty(Constants.SSL_KEYSTORE))) {
-					result.setProperty("javax.net.ssl.keyStore", src.getProperty(Constants.SSL_KEYSTORE));
-				}
-		
-				if (!StringUtils.isEmpty(src.getProperty(Constants.SSL_KEYSTORE_PASSWORD))) {
-					result.setProperty("javax.net.ssl.keyStorePassword", src.getProperty(Constants.SSL_KEYSTORE_PASSWORD));
-				}
-		
-				if (!StringUtils.isEmpty(src.getProperty(Constants.SSL_TRUSTSTORE))) {
-					result.setProperty("javax.net.ssl.trustStore", src.getProperty(Constants.SSL_TRUSTSTORE));
-				}
-		
-				if (!StringUtils.isEmpty(src.getProperty(Constants.SSL_TRUSTSTORE_PASSWORD))) {
-					result.setProperty("javax.net.ssl.trustStorePassword", src.getProperty(Constants.SSL_TRUSTSTORE_PASSWORD));
-				}
+
+		if (!"RMI".equals(src.getProperty(Constants.CONNECTOR_TYPE))) {
+			if (!StringUtils.isEmpty(src.getProperty(Constants.SSL_KEYSTORE))) {
+				result.setProperty("javax.net.ssl.keyStore", src.getProperty(Constants.SSL_KEYSTORE));
+			}
+
+			if (!StringUtils.isEmpty(src.getProperty(Constants.SSL_KEYSTORE_PASSWORD))) {
+				result.setProperty("javax.net.ssl.keyStorePassword", src.getProperty(Constants.SSL_KEYSTORE_PASSWORD));
+			}
+
+			if (!StringUtils.isEmpty(src.getProperty(Constants.SSL_TRUSTSTORE))) {
+				result.setProperty("javax.net.ssl.trustStore", src.getProperty(Constants.SSL_TRUSTSTORE));
+			}
+
+			if (!StringUtils.isEmpty(src.getProperty(Constants.SSL_TRUSTSTORE_PASSWORD))) {
+				result.setProperty("javax.net.ssl.trustStorePassword", src.getProperty(Constants.SSL_TRUSTSTORE_PASSWORD));
+			}
 		}
-	
-		logger.log(Level.FINE, Constants.METHOD_EXIT+ "getMappedProperties");
+
+		logger.log(Level.FINE, Constants.METHOD_EXIT + "getMappedProperties");
 		return result;
 	}
 }
