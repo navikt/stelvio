04c4c3d7f8d54eab8a55745443c02f788e8d6439
ebf2812 <ebf2812@localhost>
2008-09-15 10:55:05 +0000

    Refactoring

---
+++ int/BPCCommandLine/BPCHelperAppClient/appClientModule/no/nav/bpchelper/service/BusinessFlowManagerServiceAdapter.java
@@ -0,0 +1,23 @@
+package no.nav.bpchelper.service;
+
+import com.ibm.bpe.api.QueryResultSet;
+
+public class BusinessFlowManagerServiceAdapter implements BusinessFlowManagerService {
+	private com.ibm.bpe.api.BusinessFlowManagerService adaptee;
+
+	public BusinessFlowManagerServiceAdapter(com.ibm.bpe.api.BusinessFlowManagerService adaptee) {
+		this.adaptee = adaptee;
+	}
+	
+	public com.ibm.bpe.api.BusinessFlowManagerService getAdaptee() {
+		return adaptee;
+	}
+	
+	public QueryResultSet query(String selectClause, String whereClause, String orderByClause, Integer threshold) {
+		try {
+			return adaptee.query(selectClause, whereClause, orderByClause, threshold, null);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
--- int/BPCCommandLine/BPCHelperAppClient/appClientModule/no/nav/bpchelper/service/ServiceFactory.java
+++ int/BPCCommandLine/BPCHelperAppClient/appClientModule/no/nav/bpchelper/service/ServiceFactory.java
@@ -1,19 +1,14 @@
 package no.nav.bpchelper.service;
 
-import javax.naming.InitialContext;
-import javax.rmi.PortableRemoteObject;
-
-import com.ibm.bpe.api.BusinessFlowManager;
-import com.ibm.bpe.api.BusinessFlowManagerHome;
+import com.ibm.bpe.clientmodel.BFMConnection;
 
 public class ServiceFactory {
 	public static BusinessFlowManagerService getBusinessFlowManagerService() {
 		try {
-			InitialContext ctx= new InitialContext();
-			Object result=ctx.lookup("java:comp/env/ejb/BusinessFlowManagerHome");
-			BusinessFlowManagerHome processHome=(BusinessFlowManagerHome) PortableRemoteObject.narrow(result, BusinessFlowManagerHome.class);
-			BusinessFlowManager businessFlowManager = processHome.create();
-			 return new BusinessFlowManagerServiceImpl(businessFlowManager);
+			BFMConnection bfmConnection = new BFMConnection();
+			bfmConnection.setRemote(Boolean.TRUE.toString());
+			bfmConnection.setJndiName("java:comp/env/ejb/BusinessFlowManagerHome");
+			return new BusinessFlowManagerServiceAdapter(bfmConnection.getBusinessFlowManagerService());
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
