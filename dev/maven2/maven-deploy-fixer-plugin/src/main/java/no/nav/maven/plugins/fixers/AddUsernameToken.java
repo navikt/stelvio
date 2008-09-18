f1e022727b71305ab95f68b0be98d2483ef30804
bwa2812 <bwa2812@localhost>
2008-09-18 09:24:56 +0000

    added functionality to handle multiple export modules in cons-modules.

--- dev/maven2/maven-deploy-fixer-plugin/src/main/java/no/nav/maven/plugins/fixers/AddUsernameToken.java
+++ dev/maven2/maven-deploy-fixer-plugin/src/main/java/no/nav/maven/plugins/fixers/AddUsernameToken.java
@@ -2,6 +2,7 @@ package no.nav.maven.plugins.fixers;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -49,7 +50,8 @@ public class AddUsernameToken extends AbstractMojo {
 
 	private void setupIbmWebservicesExt() throws DocumentException, IOException{
 		Document doc = null;
-		Element pcBinding, serviceConfig, usernameCaller, ltpaCaller, usernameToken, ltpaToken;
+		Element serviceConfig, usernameCaller, ltpaCaller, usernameToken, ltpaToken;
+		ArrayList<Element> pcBindings ;
 		XPath search = null;
 		SAXReader reader = null;
 		Map<String, String> uris = null;
@@ -62,44 +64,49 @@ public class AddUsernameToken extends AbstractMojo {
 
 		search = doc.createXPath("//pcBinding");
 		search.setNamespaceURIs(uris);
-		pcBinding = (Element)search.selectSingleNode(doc);
-		if(pcBinding != null){
-			pcBinding.clearContent();
-			
-			serviceConfig = pcBinding.addElement("serverServiceConfig").addElement("securityRequestConsumerServiceConfig");
-			
-			usernameCaller = serviceConfig.addElement("caller");
-			usernameCaller.addAttribute("name", "UsernameTokenCaller");
-			usernameCaller.addAttribute("part", "");
-			usernameCaller.addAttribute("uri", "");
-			usernameCaller.addAttribute("localName", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#UsernameToken");
-			
-			ltpaCaller = serviceConfig.addElement("caller");
-			ltpaCaller.addAttribute("name", "LTPATokenCaller");
-			ltpaCaller.addAttribute("part", "");
-			ltpaCaller.addAttribute("uri", "http://www.ibm.com/websphere/appserver/tokentype/5.0.2");
-			ltpaCaller.addAttribute("localName", "LTPA");
-			
-			usernameToken = serviceConfig.addElement("requiredSecurityToken");
-			usernameToken.addAttribute("name", "OptionalUsernameToken");
-			usernameToken.addAttribute("uri", "");
-			usernameToken.addAttribute("localName", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#UsernameToken");
+		pcBindings = (ArrayList<Element>)search.selectNodes(doc);
+		
+		for(Element pcBinding : pcBindings){
+			if(pcBinding != null){
+				pcBinding.clearContent();
+				
+				serviceConfig = pcBinding.addElement("serverServiceConfig").addElement("securityRequestConsumerServiceConfig");
+				
+				usernameCaller = serviceConfig.addElement("caller");
+				usernameCaller.addAttribute("name", "UsernameTokenCaller");
+				usernameCaller.addAttribute("part", "");
+				usernameCaller.addAttribute("uri", "");
+				usernameCaller.addAttribute("localName", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#UsernameToken");
+				
+				ltpaCaller = serviceConfig.addElement("caller");
+				ltpaCaller.addAttribute("name", "LTPATokenCaller");
+				ltpaCaller.addAttribute("part", "");
+				ltpaCaller.addAttribute("uri", "http://www.ibm.com/websphere/appserver/tokentype/5.0.2");
+				ltpaCaller.addAttribute("localName", "LTPA");
+				
+				usernameToken = serviceConfig.addElement("requiredSecurityToken");
+				usernameToken.addAttribute("name", "OptionalUsernameToken");
+				usernameToken.addAttribute("uri", "");
+				usernameToken.addAttribute("localName", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#UsernameToken");
 
-			ltpaToken = serviceConfig.addElement("requiredSecurityToken");
-			ltpaToken.addAttribute("name", "OptionalLTPAToken");
-			ltpaToken.addAttribute("uri", "http://www.ibm.com/websphere/appserver/tokentype/5.0.2");
-			ltpaToken.addAttribute("localName", "LTPA");
-			
-			getLog().info("'ibm-webservices-ext.xmi' updated!");
-			XMLUtils.writeXMLDocument(doc, webservicesExt);
-		}else{
-			getLog().warn(consModuleFolder.getName() + " does not contain 'pcBinding' definition in 'ibm-webservices-ext.xmi'");
+				ltpaToken = serviceConfig.addElement("requiredSecurityToken");
+				ltpaToken.addAttribute("name", "OptionalLTPAToken");
+				ltpaToken.addAttribute("uri", "http://www.ibm.com/websphere/appserver/tokentype/5.0.2");
+				ltpaToken.addAttribute("localName", "LTPA");
+				
+				getLog().info("'ibm-webservices-ext.xmi' updated!");
+				XMLUtils.writeXMLDocument(doc, webservicesExt);
+			}else{
+				getLog().warn(consModuleFolder.getName() + " does not contain 'pcBinding' definition in 'ibm-webservices-ext.xmi'");
+			}
 		}
+		
 	}
 	
 	private void setupIbmWebservicesBnd() throws DocumentException, IOException{
 		Document doc = null;
-		Element pcBinding, bindingConfig, tokenConsumer, tokenConsumer2, valueType, jaasConfig, partRef;
+		Element bindingConfig, tokenConsumer, tokenConsumer2, valueType, jaasConfig, partRef;
+		ArrayList<Element> pcBindings ;
 		XPath search = null;
 		SAXReader reader = null;
 		Map<String, String> uris = null;
@@ -112,44 +119,48 @@ public class AddUsernameToken extends AbstractMojo {
 
 		search = doc.createXPath("//pcBindings");
 		search.setNamespaceURIs(uris);
-		pcBinding = (Element)search.selectSingleNode(doc);
-		if(pcBinding != null){
-			pcBinding.clearContent();
-			
-			bindingConfig = pcBinding.addElement("securityRequestConsumerBindingConfig");
-			
-			tokenConsumer = bindingConfig.addElement("tokenConsumer");
-			tokenConsumer.addAttribute("classname", "com.ibm.wsspi.wssecurity.token.UsernameTokenConsumer");
-			tokenConsumer.addAttribute("name", "UsernameTokenConsumer");
-			
-			valueType = tokenConsumer.addElement("valueType");
-			valueType.addAttribute("localName", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#UsernameToken");
-			valueType.addAttribute("uri", "");
-			valueType.addAttribute("name", "UsernameToken");
-			
-			jaasConfig = tokenConsumer.addElement("jAASConfig");
-			jaasConfig.addAttribute("configName", "system.wssecurity.UsernameToken");
-			
-			partRef = tokenConsumer.addElement("partReference");
-			partRef.addAttribute("part", "OptionalUsernameToken");
-			
-			tokenConsumer2 = bindingConfig.addElement("tokenConsumer");
-			tokenConsumer2.addAttribute("classname", "com.ibm.wsspi.wssecurity.token.LTPATokenConsumer");
-			tokenConsumer2.addAttribute("name", "LTPATokenConsumer");
-			
-			valueType = tokenConsumer2.addElement("valueType");
-			valueType.addAttribute("localName", "LTPA");
-			valueType.addAttribute("uri", "http://www.ibm.com/websphere/appserver/tokentype/5.0.2");
-			valueType.addAttribute("name", "LTPA Token");
-			
-			partRef = tokenConsumer2.addElement("partReference");
-			partRef.addAttribute("part", "OptionalLTPAToken");
-			
-			getLog().info("'ibm-webservices-bnd.xmi' updated!");
-			XMLUtils.writeXMLDocument(doc, webservicesBnd);
-		}else{
-			getLog().warn(consModuleFolder.getName() + " does not contain 'securityRequestConsumerBindingConfig' definition in 'ibm-webservices-ext.xmi'");
+		pcBindings = (ArrayList<Element>)search.selectNodes(doc);
+		
+		for(Element pcBinding : pcBindings){
+			if(pcBinding != null){
+				pcBinding.clearContent();
+				
+				bindingConfig = pcBinding.addElement("securityRequestConsumerBindingConfig");
+				
+				tokenConsumer = bindingConfig.addElement("tokenConsumer");
+				tokenConsumer.addAttribute("classname", "com.ibm.wsspi.wssecurity.token.UsernameTokenConsumer");
+				tokenConsumer.addAttribute("name", "UsernameTokenConsumer");
+				
+				valueType = tokenConsumer.addElement("valueType");
+				valueType.addAttribute("localName", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#UsernameToken");
+				valueType.addAttribute("uri", "");
+				valueType.addAttribute("name", "UsernameToken");
+				
+				jaasConfig = tokenConsumer.addElement("jAASConfig");
+				jaasConfig.addAttribute("configName", "system.wssecurity.UsernameToken");
+				
+				partRef = tokenConsumer.addElement("partReference");
+				partRef.addAttribute("part", "OptionalUsernameToken");
+				
+				tokenConsumer2 = bindingConfig.addElement("tokenConsumer");
+				tokenConsumer2.addAttribute("classname", "com.ibm.wsspi.wssecurity.token.LTPATokenConsumer");
+				tokenConsumer2.addAttribute("name", "LTPATokenConsumer");
+				
+				valueType = tokenConsumer2.addElement("valueType");
+				valueType.addAttribute("localName", "LTPA");
+				valueType.addAttribute("uri", "http://www.ibm.com/websphere/appserver/tokentype/5.0.2");
+				valueType.addAttribute("name", "LTPA Token");
+				
+				partRef = tokenConsumer2.addElement("partReference");
+				partRef.addAttribute("part", "OptionalLTPAToken");
+				
+				getLog().info("'ibm-webservices-bnd.xmi' updated!");
+				XMLUtils.writeXMLDocument(doc, webservicesBnd);
+			}else{
+				getLog().warn(consModuleFolder.getName() + " does not contain 'securityRequestConsumerBindingConfig' definition in 'ibm-webservices-ext.xmi'");
+			}
 		}
+		
 	}
 
 	public File getConsModuleFolder() {
