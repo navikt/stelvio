b0fc707ff9412316b31feb1320836562ad56292b
ebf2812 <ebf2812@localhost>
2008-09-19 09:58:14 +0000

    Refactored ReportWriter to use libriary for writing CSV files

--- int/BPCCommandLine/BPCHelperAppClient/appClientModule/no/nav/bpchelper/writers/ReportWriter.java
+++ int/BPCCommandLine/BPCHelperAppClient/appClientModule/no/nav/bpchelper/writers/ReportWriter.java
@@ -3,23 +3,43 @@ package no.nav.bpchelper.writers;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.Ostermiller.util.CSVPrinter;
 import com.ibm.bpe.clientmodel.bean.ProcessInstanceBean;
 
 public class ReportWriter {
-    // TODO: Is default locale ok?
-    private Locale locale = Locale.getDefault();
+    /**
+         * Locale used to format labels
+         */
+    private static final Locale LOCALE = Locale.getDefault();
 
-    private Appender appender;
+    /**
+         * Delimiter to use in CSV file
+         */
+    private static final char DELIMITER = ';';
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private CSVPrinter csvPrinter;
 
     public ReportWriter() {
 	this(createDefaultFile());
     }
 
     public ReportWriter(File file) {
-	this.appender = new Appender(file);
-	System.out.println("Writing report to file: " + file.getAbsolutePath());
+	try {
+	    this.csvPrinter = new CSVPrinter(new BufferedWriter(new FileWriter(file)));
+	    this.csvPrinter.changeDelimiter(DELIMITER);
+	} catch (Exception e) {
+	    throw new RuntimeException(e);
+	}
+	logger.info("Writing report to file: " + file.getAbsolutePath());
     }
 
     private static File createDefaultFile() {
@@ -31,73 +51,30 @@ public class ReportWriter {
     }
 
     public void close() {
-	appender.close();
-	appender = null;
+	try {
+	    csvPrinter.close();
+	} catch (Exception e) {
+	    logger.warn("Error closing writer", e);
+	}
+	csvPrinter = null;
     }
 
     public void writeHeader() {
-	appender.write(ProcessInstanceBean.getLabel(ProcessInstanceBean.NAME_PROPERTY, locale));
-	appender.write(ProcessInstanceBean.getLabel(ProcessInstanceBean.PROCESSTEMPLATENAME_PROPERTY, locale));
-	appender.write(ProcessInstanceBean.getLabel(ProcessInstanceBean.EXECUTIONSTATE_PROPERTY, locale));
-	appender.write(ProcessInstanceBean.getLabel(ProcessInstanceBean.STARTTIME_PROPERTY, locale));
-	appender.newLine();
+	Collection<String> line = new ArrayList<String>();
+	line.add(ProcessInstanceBean.getLabel(ProcessInstanceBean.NAME_PROPERTY, LOCALE));
+	line.add(ProcessInstanceBean.getLabel(ProcessInstanceBean.PROCESSTEMPLATENAME_PROPERTY, LOCALE));
+	line.add(ProcessInstanceBean.getLabel(ProcessInstanceBean.EXECUTIONSTATE_PROPERTY, LOCALE));
+	line.add(ProcessInstanceBean.getLabel(ProcessInstanceBean.STARTTIME_PROPERTY, LOCALE));
+	csvPrinter.println(line.toArray(new String[line.size()]));
     }
 
     public void writeProcessInstance(ProcessInstanceBean processInstanceBean) {
-	appender.write(processInstanceBean.getName());
-	appender.write(processInstanceBean.getProcessTemplateName());
-	appender.write(ProcessInstanceBean.getConverter(ProcessInstanceBean.EXECUTIONSTATE_PROPERTY).getAsString(
-		processInstanceBean.getExecutionState(), locale));
-	appender.write(ProcessInstanceBean.getConverter(ProcessInstanceBean.STARTTIME_PROPERTY).getAsString(processInstanceBean.getStartTime(),
-		locale));
-	appender.newLine();
-    }
-
-    /**
-         * TODO: Escaping
-         */
-    private static class Appender {
-	/**
-         * Separator to use in CSV file
-         */
-	public static final String SEPARATOR = ";";
-
-	private BufferedWriter writer;
-
-	public Appender(File file) {
-	    try {
-		this.writer = new BufferedWriter(new FileWriter(file));
-	    } catch (Exception e) {
-		throw new RuntimeException(e);
-	    }
-	}
-
-	public void close() {
-	    try {
-		writer.close();
-		writer = null;
-	    } catch (Exception e) {
-		throw new RuntimeException(e);
-	    }
-	}
-
-	public Appender write(String s) {
-	    try {
-		writer.write(s);
-		writer.write(SEPARATOR);
-		return this;
-	    } catch (Exception e) {
-		throw new RuntimeException(e);
-	    }
-	}
-
-	public Appender newLine() {
-	    try {
-		writer.newLine();
-		return this;
-	    } catch (Exception e) {
-		throw new RuntimeException(e);
-	    }
-	}
+	Collection<String> line = new ArrayList<String>();
+	line.add(processInstanceBean.getName());
+	line.add(processInstanceBean.getProcessTemplateName());
+	line.add(ProcessInstanceBean.getConverter(ProcessInstanceBean.EXECUTIONSTATE_PROPERTY).getAsString(processInstanceBean.getExecutionState(),
+		LOCALE));
+	line.add(ProcessInstanceBean.getConverter(ProcessInstanceBean.STARTTIME_PROPERTY).getAsString(processInstanceBean.getStartTime(), LOCALE));
+	csvPrinter.println(line.toArray(new String[line.size()]));
     }
 }
