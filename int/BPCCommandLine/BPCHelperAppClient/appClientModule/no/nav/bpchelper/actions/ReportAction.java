058aecbb3053ec32c441a2b6dcc123608b9536a9
ebf2812 <ebf2812@localhost>
2008-09-19 13:32:54 +0000

    Refactoring

--- int/BPCCommandLine/BPCHelperAppClient/appClientModule/no/nav/bpchelper/actions/ReportAction.java
+++ int/BPCCommandLine/BPCHelperAppClient/appClientModule/no/nav/bpchelper/actions/ReportAction.java
@@ -10,39 +10,56 @@ import com.ibm.bpe.api.QueryResultSet;
 import com.ibm.bpe.clientmodel.bean.ProcessInstanceBean;
 
 public class ReportAction extends AbstractAction {
-    public void process() {
-	BFMConnectionAdapter bfmConnection = BFMConnectionAdapter.getInstance();
+    private final BFMConnectionAdapter bfmConnection;
 
-	Collection<ProcessInstancePropertyAccessor> propertyAccessors = new ArrayList<ProcessInstancePropertyAccessor>();
+    private final Collection<ProcessInstancePropertyAccessor> propertyAccessors;
+
+    public ReportAction() {
+	bfmConnection = BFMConnectionAdapter.getInstance();
+
+	propertyAccessors = new ArrayList<ProcessInstancePropertyAccessor>();
 	propertyAccessors.add(new ProcessInstancePropertyAccessor(ProcessInstanceBean.NAME_PROPERTY));
 	propertyAccessors.add(new ProcessInstancePropertyAccessor(ProcessInstanceBean.PROCESSTEMPLATENAME_PROPERTY));
 	propertyAccessors.add(new ProcessInstancePropertyAccessor(ProcessInstanceBean.EXECUTIONSTATE_PROPERTY));
 	propertyAccessors.add(new ProcessInstancePropertyAccessor(ProcessInstanceBean.STARTTIME_PROPERTY));
+    }
 
+    public void process() {
+	ReportWriter writer = new ReportWriter();
+
+	writer.writeln(getHeader());
+
+	QueryResultSet queryResultSet = executeQuery();
+	while (queryResultSet.next()) {
+	    ProcessInstanceBean processInstanceBean = new ProcessInstanceBean(queryResultSet, bfmConnection.getAdaptee());
+	    writer.writeln(getRow(processInstanceBean));
+	}
+
+	writer.close();
+    }
+
+    private Collection<String> getRow(ProcessInstanceBean processInstanceBean) {
+	Collection<String> row = new ArrayList<String>();
+	for (ProcessInstancePropertyAccessor propertyAccessor : propertyAccessors) {
+	    row.add(propertyAccessor.getValue(processInstanceBean));
+	}
+	return row;
+    }
+
+    protected Collection<String> getHeader() {
+	Collection<String> header = new ArrayList<String>();
+	for (ProcessInstancePropertyAccessor propertyAccessor : propertyAccessors) {
+	    header.add(propertyAccessor.getLabel());
+	}
+	return header;
+    }
+
+    protected QueryResultSet executeQuery() {
 	// TODO: Can we use constants for any of the clause fragments?
 	String selectClause = "DISTINCT PROCESS_INSTANCE.PIID,PROCESS_INSTANCE.NAME,PROCESS_INSTANCE.TEMPLATE_NAME,PROCESS_INSTANCE.STATE,PROCESS_INSTANCE.STARTED";
 	String whereClause = getCriteria().toSqlString();
 
 	// TODO: Is query the correct service to use, or should queryAll be used?
-	QueryResultSet rs = bfmConnection.getBusinessFlowManagerService().query(selectClause, whereClause, null, null);
-
-	Collection<String> temp = new ArrayList<String>(propertyAccessors.size());
-
-	ReportWriter writer = new ReportWriter();
-	for (ProcessInstancePropertyAccessor propertyAccessor : propertyAccessors) {
-	    temp.add(propertyAccessor.getLabel());
-	}
-	writer.writeln(temp);
-
-	while (rs.next()) {
-	    temp.clear();
-	    ProcessInstanceBean processInstanceBean = new ProcessInstanceBean(rs, bfmConnection.getAdaptee());
-	    for (ProcessInstancePropertyAccessor propertyAccessor : propertyAccessors) {
-		temp.add(propertyAccessor.getValue(processInstanceBean));
-	    }
-	    writer.writeln(temp);
-	}
-	
-	writer.close();
+	return bfmConnection.getBusinessFlowManagerService().query(selectClause, whereClause, null, null);
     }
 }
