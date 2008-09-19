18fe3bc3040156cedbc625acd8cb920ee40df329
ebf2812 <ebf2812@localhost>
2008-09-19 09:20:16 +0000

    Added logging

--- int/BPCCommandLine/BPCHelperAppClient/appClientModule/no/nav/bpchelper/adapters/BusinessFlowManagerServiceAdapter.java
+++ int/BPCCommandLine/BPCHelperAppClient/appClientModule/no/nav/bpchelper/adapters/BusinessFlowManagerServiceAdapter.java
@@ -2,9 +2,14 @@ package no.nav.bpchelper.adapters;
 
 import java.util.TimeZone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.ibm.bpe.api.QueryResultSet;
 
 public class BusinessFlowManagerServiceAdapter {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    
     private com.ibm.bpe.api.BusinessFlowManagerService adaptee;
 
     public BusinessFlowManagerServiceAdapter(com.ibm.bpe.api.BusinessFlowManagerService adaptee) {
@@ -17,6 +22,9 @@ public class BusinessFlowManagerServiceAdapter {
 
     public QueryResultSet query(String selectClause, String whereClause, String orderByClause, Integer threshold) {
 	try {
+	    if (logger.isDebugEnabled()) {
+		logger.debug("Executing query({},{},{},{})", new Object[]{selectClause,whereClause,orderByClause,threshold});
+	    }
 	    return adaptee.query(selectClause, whereClause, orderByClause, threshold, TimeZone.getDefault());
 	} catch (Exception e) {
 	    throw new RuntimeException(e);
@@ -25,6 +33,9 @@ public class BusinessFlowManagerServiceAdapter {
 
     public QueryResultSet queryAll(String selectClause, String whereClause, String orderByClause, Integer threshold) {
 	try {
+	    if (logger.isDebugEnabled()) {
+		logger.debug("Executing queryAll({},{},{},{})", new Object[]{selectClause,whereClause,orderByClause,threshold});
+	    }
 	    return adaptee.queryAll(selectClause, whereClause, orderByClause, null, threshold, TimeZone.getDefault());
 	} catch (Exception e) {
 	    throw new RuntimeException(e);
