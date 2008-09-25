6312231e6106349cd7087f6bf9b0716ec7f46904
ard2812 <ard2812@localhost>
2008-09-25 09:13:57 +0000

    Added functionality to read environment configuration from file (with same structure as the FEM Helper tool)

--- int/BPCCommandLine/BPCHelperAppClient/appClientModule/no/nav/bpchelper/adapters/BFMConnectionAdapter.java
+++ int/BPCCommandLine/BPCHelperAppClient/appClientModule/no/nav/bpchelper/adapters/BFMConnectionAdapter.java
@@ -1,35 +1,39 @@
 package no.nav.bpchelper.adapters;
 
+import java.util.Properties;
+
 import com.ibm.bpe.clientmodel.BFMConnection;
 
 public class BFMConnectionAdapter {
-    private BFMConnection adaptee;
+	private BFMConnection adaptee;
+	
+	private BusinessFlowManagerServiceAdapter businessFlowManagerService;
 
-    private BusinessFlowManagerServiceAdapter businessFlowManagerService;
-
-    private BFMConnectionAdapter(BFMConnection adaptee) {
-	this.adaptee = adaptee;
-    }
-
-    public BFMConnection getAdaptee() {
-	return adaptee;
-    }
-
-    public static BFMConnectionAdapter getInstance() {
-	BFMConnection bfmConnection = new BFMConnection();
-	bfmConnection.setRemote(Boolean.TRUE.toString());
-	bfmConnection.setJndiName("java:comp/env/ejb/BusinessFlowManagerHome");
-	return new BFMConnectionAdapter(bfmConnection);
-    }
-
-    public BusinessFlowManagerServiceAdapter getBusinessFlowManagerService() {
-	if (businessFlowManagerService == null) {
-	    try {
-		businessFlowManagerService = new BusinessFlowManagerServiceAdapter(adaptee.getBusinessFlowManagerService());
-	    } catch (Exception e) {
-		throw new RuntimeException(e);
-	    }
+	private BFMConnectionAdapter(BFMConnection adaptee) {
+		this.adaptee = adaptee;
 	}
-	return businessFlowManagerService;
-    }
+
+	public BFMConnection getAdaptee() {
+		return adaptee;
+	}
+
+	public static BFMConnectionAdapter getInstance(Properties properties) {
+		BFMConnection bfmConnection = new SecuredBFMConnection(properties);
+		bfmConnection.setRemote(Boolean.TRUE.toString());
+		bfmConnection.setJndiName("java:comp/env/ejb/BusinessFlowManagerHome");
+		BFMConnectionAdapter adapter = new BFMConnectionAdapter(bfmConnection); 
+		return adapter;
+	}
+
+	public BusinessFlowManagerServiceAdapter getBusinessFlowManagerService() {
+		if (businessFlowManagerService == null) {
+			try {
+				businessFlowManagerService = new BusinessFlowManagerServiceAdapter(
+						adaptee.getBusinessFlowManagerService());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return businessFlowManagerService;
+	}
 }
