35a772ab5cba3ee37969111d6eb97825670efbbd
agb2812 <agb2812@localhost>
2008-10-01 12:46:37 +0000

    1.0.0 release

--- dev/datapower/datapower-cfg-gen/src/test/java/no/nav/datapower/config/WSDLFileTest.java
+++ dev/datapower/datapower-cfg-gen/src/test/java/no/nav/datapower/config/WSDLFileTest.java
@@ -60,11 +60,12 @@ public class WSDLFileTest extends TestCase {
 	}
 
 	public void testCreateMappedUri() {
-		File root = new File("E:/Develop/wsdl/wsdl-tptilb/wsdl/");
-		File wsdlFile = DPFileUtils.append(root, "/no/nav/tpsamordningregistrering/V0_2/ws/TPSamordningRegistreringWSEXP_TPSamordningRegistreringHttp_Service.wsdl");
-		WSDLFile wsdl = WSDLFile.createMappedUri(wsdlFile, root, "/elsam/tptilb/");
-		String frontsideURI = wsdl.getFrontsideURI();
-		System.out.println("version = " + wsdl.getVersion());
+		
+//		File root = new File("E:/Develop/wsdl/wsdl-tptilb/wsdl/");
+//		File wsdlFile = DPFileUtils.append(root, "/no/nav/tpsamordningregistrering/V0_2/ws/TPSamordningRegistreringWSEXP_TPSamordningRegistreringHttp_Service.wsdl");
+//		WDLFile wsdl = WSDLFile.createMappedUri(wsdlFile, root, "/elsam/tptilb/");
+//		String frontsideURI = wsdl.getFrontsideURI();
+//		System.out.println("version = " + wsdl.getVersion());
 //		assertEquals("", "/elsam/tptilb/TPSamordningRegistreringV0_2", frontsideURI);
 	}
 }
