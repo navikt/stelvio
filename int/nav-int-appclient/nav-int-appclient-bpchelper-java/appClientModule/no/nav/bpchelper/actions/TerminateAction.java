07be87c95d03143396d721444ad15b6a7668680f
ebf2812 <ebf2812@localhost>
2008-10-02 12:30:39 +0000

    Refactored out resuable code

--- int/nav-int-appclient/nav-int-appclient-bpchelper-java/appClientModule/no/nav/bpchelper/actions/TerminateAction.java
+++ int/nav-int-appclient/nav-int-appclient-bpchelper-java/appClientModule/no/nav/bpchelper/actions/TerminateAction.java
@@ -3,7 +3,8 @@ package no.nav.bpchelper.actions;
 import java.util.Collection;
 import java.util.Properties;
 
-import com.ibm.bpe.api.BusinessFlowManagerService;
+import no.nav.appclient.adapter.ServiceException;
+
 import com.ibm.bpe.api.PIID;
 import com.ibm.bpe.clientmodel.bean.ProcessInstanceBean;
 
@@ -30,23 +31,9 @@ public class TerminateAction extends ReportAction {
 		PIID id = processInstanceBean.getID();
 		String result;
 		try {
-			BusinessFlowManagerService businessFlowManagerService = bfmConnection.getBusinessFlowManagerService().getAdaptee();
-			if (logger.isDebugEnabled()) {
-				logger.debug("Attempting to terminate process instance with id=<" + id + ">");
-			}
-			businessFlowManagerService.forceTerminate(id);
-			if (logger.isDebugEnabled()) {
-				logger.debug("Successfully terminated process instance with id=<" + id + ">");
-			}
-			if (logger.isDebugEnabled()) {
-				logger.debug("Attempting to delete process instance with id=<" + id + ">");
-			}
-			businessFlowManagerService.delete(id);
-			if (logger.isDebugEnabled()) {
-				logger.debug("Successfully deleted process instance with id=<" + id + ">");
-			}
+			bfmConnection.getBusinessFlowManagerService().forceTerminate(id.toString());
 			result = "OK";
-		} catch (Exception e) {
+		} catch (ServiceException e) {
 			logger.warn("Error terminating/deleting process instance with id=<" + id + ">", e);
 			result = e.getMessage();
 		}
