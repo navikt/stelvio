1a1637c8d66467224b90b518c76660d5623b88e8
ebf2812 <ebf2812@localhost>
2008-10-10 14:18:56 +0000

    Refactoring

--- int/nav-int-appclient/nav-int-appclient-common-java/src/main/java/no/nav/appclient/adapter/BusinessFlowManagerServiceAdapter.java
+++ int/nav-int-appclient/nav-int-appclient-common-java/src/main/java/no/nav/appclient/adapter/BusinessFlowManagerServiceAdapter.java
@@ -1,13 +1,20 @@
 package no.nav.appclient.adapter;
 
+import java.rmi.RemoteException;
 import java.util.TimeZone;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.ibm.bpe.api.EngineNotAuthorizedException;
+import com.ibm.bpe.api.EngineWrongKindException;
+import com.ibm.bpe.api.EngineWrongStateException;
+import com.ibm.bpe.api.IdWrongFormatException;
+import com.ibm.bpe.api.ObjectDoesNotExistException;
 import com.ibm.bpe.api.PIID;
 import com.ibm.bpe.api.ProcessInstanceData;
 import com.ibm.bpe.api.QueryResultSet;
+import com.ibm.bpe.api.UnexpectedFailureException;
 
 public class BusinessFlowManagerServiceAdapter {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
@@ -46,16 +53,15 @@ public class BusinessFlowManagerServiceAdapter {
 		}
 	}
 
-	public PIID getProcessInstanceID(String activityID) {
+	public PIID getProcessInstanceIdByActivityId(String activityId) {
 		String selectClause = "ACTIVITY.PIID";
 		StringBuilder whereClause = new StringBuilder("ACTIVITY.AIID=");
-		whereClause.append("ID('").append(activityID).append("')");
+		whereClause.append("ID('").append(activityId).append("')");
 		QueryResultSet rs = queryAll(selectClause, whereClause.toString(), null, null);
 		if (rs.next()) {
 			return (PIID) rs.getOID(1);
 		} else {
-			throw new ServiceException("Activity with id=<" + activityID
-					+ "> not found. Process already deleted? Nothing to terminate.");
+			throw new ServiceException("Activity with id=<" + activityId + "> not found.");
 		}
 	}
 
@@ -67,36 +73,24 @@ public class BusinessFlowManagerServiceAdapter {
 		}
 	}
 
-	public void forceTerminate(PIID piid) {
-		forceTerminate(piid, true);
-	}
-
-	public void forceTerminate(PIID piid, boolean delete) {
+	public void delete(PIID piid) {
 		try {
+			forceTerminate(piid);
 			if (logger.isDebugEnabled()) {
-				logger.debug("Attempting to terminate process instance with id=<{}>", piid);
+				logger.debug("Attempting to delete process instance with id=<{}>", piid);
 			}
-			adaptee.forceTerminate(piid);
+			adaptee.delete(piid);
 			if (logger.isDebugEnabled()) {
-				logger.debug("Successfully terminated process instance with id=<{}>", piid);
-			}
-			if (delete) {
-				if (logger.isDebugEnabled()) {
-					logger.debug("Attempting to delete process instance with id=<{}>", piid);
-				}
-				adaptee.delete(piid);
-				if (logger.isDebugEnabled()) {
-					logger.debug("Successfully deleted process instance with id=<{}>", piid);
-				}
+				logger.debug("Successfully deleted process instance with id=<{}>", piid);
 			}
 		} catch (Exception e) {
 			throw new ServiceException(e);
 		}
 	}
 
-	public void forceTerminateFromActivity(String aiid) {
-		PIID piid = getProcessInstanceID(aiid);
-		forceTerminate(piid);
+	public void deleteProcessInstanceByActivityId(String activityId) {
+		PIID piid = getProcessInstanceIdByActivityId(activityId);
+		delete(piid);
 	}
 
 	public void restart(PIID piid) {
@@ -113,4 +107,25 @@ public class BusinessFlowManagerServiceAdapter {
 			throw new ServiceException(e);
 		}
 	}
+
+	private void forceTerminate(PIID piid) throws EngineNotAuthorizedException, EngineWrongKindException,
+			IdWrongFormatException, ObjectDoesNotExistException, UnexpectedFailureException, RemoteException {
+		try {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Attempting to force terminate process instance with id=<{}>", piid);
+			}
+			adaptee.forceTerminate(piid);
+			if (logger.isDebugEnabled()) {
+				logger.debug("Successfully force terminated process instance with id=<{}>", piid);
+			}
+		} catch (EngineWrongStateException e) {
+			// Catching wrong state exception - the processes is probably
+			// already terminated
+			if (logger.isDebugEnabled()) {
+				logger.debug("Caught wrong state exception when attempting to force terminate process instance with id=<"
+						+ piid + ">; ignoring", e);
+			}
+		}
+	}
+
 }
