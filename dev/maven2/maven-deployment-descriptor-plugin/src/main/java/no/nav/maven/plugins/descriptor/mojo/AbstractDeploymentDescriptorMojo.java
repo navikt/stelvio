d093d782f6893e6528f2d5460b5e339709c1ed78
agb2812 <agb2812@localhost>
2008-10-18 10:21:21 +0000

    1.0.1 release

--- dev/maven2/maven-deployment-descriptor-plugin/src/main/java/no/nav/maven/plugins/descriptor/mojo/AbstractDeploymentDescriptorMojo.java
+++ dev/maven2/maven-deployment-descriptor-plugin/src/main/java/no/nav/maven/plugins/descriptor/mojo/AbstractDeploymentDescriptorMojo.java
@@ -2,38 +2,29 @@ package no.nav.maven.plugins.descriptor.mojo;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import javax.enterprise.deploy.spi.DeploymentConfiguration;
-
+import no.nav.maven.moose.utility.EarFileRetriever;
+import no.nav.maven.moose.utility.VersionRetriever;
+import no.nav.maven.moose.utility.spring.loader.MooseConfigLoader;
+import no.nav.maven.moose.utility.spring.loader.MooseFileSystemSpringConfigLoader;
 import no.nav.maven.plugins.descriptor.config.DeploymentDescriptorMojoConfig;
 import no.nav.maven.plugins.descriptor.config.EarConfig;
-import no.nav.maven.plugins.descriptor.config.EjbMethodPermissionConfig;
-import no.nav.maven.plugins.descriptor.config.EjbJarConfig;
 import no.nav.maven.plugins.descriptor.config.SecurityRoleConfig;
-import no.nav.maven.plugins.descriptor.config.EjbMethodPermissionConfig.MethodInterfaceType;
 import no.nav.maven.plugins.descriptor.config.loader.ConfigLoader;
 import no.nav.maven.plugins.descriptor.config.loader.FileSystemSpringConfigLoader;
 import no.nav.maven.plugins.descriptor.jee.ApplicationDescriptorEditor;
-import no.nav.maven.plugins.descriptor.jee.EjbJarAssemblyDescriptorEditor;
-import no.nav.maven.plugins.descriptor.jee.EjbJarEditor;
 import no.nav.maven.plugins.descriptor.websphere.bnd.IbmApplicationBndEditor;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.eclipse.jst.j2ee.commonarchivecore.internal.Archive;
-import org.eclipse.jst.j2ee.commonarchivecore.internal.CommonArchiveFactoryRegistry;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.CommonarchiveFactory;
-import org.eclipse.jst.j2ee.commonarchivecore.internal.CommonarchivePackage;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.EARFile;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.exception.OpenFailureException;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.exception.ReopenException;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.exception.SaveFailureException;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.helpers.ArchiveOptions;
-import org.eclipse.jst.j2ee.commonarchivecore.internal.helpers.FileIterator;
 import org.eclipse.jst.j2ee.commonarchivecore.internal.impl.CommonarchiveFactoryImpl;
 
 
@@ -48,6 +39,28 @@ public abstract class AbstractDeploymentDescriptorMojo extends AbstractMojo{
 	 * @parameter
 	 */
 	private String springConfigFile; 
+	
+	/**
+	 * MOOSE SPESIFIC: The Moose ID used to retrieve the version from the mapping file.
+	 * @parameter
+	 */
+	private String mooseID; 
+	
+	/**
+	 * MOOSE SPESIFIC: This is the location of the mapping file used to retrieve the correct ear name when fixing from Moose
+	 * @parameter 
+	 */
+	private File mappingFile; 
+	
+	/**
+	 * MOOSE SPESIFIC: This is the location of The ear File used from Moose
+	 * @parameter 
+	 */
+	private String earLocation; 
+	/**
+	 * MOOSE SPESIFIC: This one is populated with spring with a map with a prefix used to match the Moose ID and mapped to ear name prefix (Version will be postfixed to complete a ear file name)
+	 */
+	private EarFileRetriever earFileRetriever;
 	
 	
 	private DeploymentDescriptorMojoConfig mojoConfig;
@@ -70,6 +83,7 @@ public abstract class AbstractDeploymentDescriptorMojo extends AbstractMojo{
 			if(springConfigFile != null){
 				ConfigLoader configLoader = new FileSystemSpringConfigLoader(springConfigFile);
 				mojoConfig = configLoader.getDeploymentDescriptorMojoConfig();
+
 			} 
 		}
 		return mojoConfig;
@@ -131,6 +145,14 @@ public abstract class AbstractDeploymentDescriptorMojo extends AbstractMojo{
 		try {
 			getLog().info("Executing deployment descriptor mojo.");
 			getLog().info("EAR file = " + getEar());
+			getLog().info("MooseID = " + getMooseID());
+			
+			//Kicks in only if the plugin is used by Moose
+			if (getMooseID() != null){
+				setEar(retrieveMooseEar());
+				getLog().info("Spring loaded the following map for ear names " +  getEarFileRetriever().getMooseIdToEarNameMathcer());
+				getLog().info("MooseEAR = " + getEar());
+			}
 			String earPath = getEar().getAbsolutePath();
 			EARFile earFile = openEarFile(earPath);
 			String applicationName = earFile.getName();
@@ -150,6 +172,52 @@ public abstract class AbstractDeploymentDescriptorMojo extends AbstractMojo{
 		} 
 	}
 	
+	
 	public abstract void executeDescriptorOperations(EARFile earFile, EarConfig earConfig) throws MojoExecutionException;
+	
+	protected File retrieveMooseEar() throws MojoExecutionException {
+		try{
+		return getEarFileRetriever().getEarDescriptorConfig(getMooseID(), new VersionRetriever().getVersionId(getMooseID(), getMappingFile()), getEarLocation());
+		}
+		catch (IOException ie){
+			throw new MojoExecutionException("Failed to retrieve Moose version ", ie);
+		}
+	}
+	
+	//Moose Specific methods
+	
+	public String getMooseID() {
+		return mooseID;
+	}
+	public void setMooseID(String mooseID) {
+		this.mooseID = mooseID;
+	}
+	
+	public File getMappingFile() {
+		return mappingFile;
+	}
+	public void setMappingFile(File mappingFile) {
+		this.mappingFile = mappingFile;
+	}
+	public void setEarFileRetriever(EarFileRetriever earFileRetriever) {
+		this.earFileRetriever = earFileRetriever;
+	}
+	
+	public EarFileRetriever getEarFileRetriever(){
+		if(earFileRetriever == null){
+			if(springConfigFile != null){
+				MooseConfigLoader mooseConfigLoader = new MooseFileSystemSpringConfigLoader(springConfigFile);
+				earFileRetriever = mooseConfigLoader.getEarFileRetriever();
+
+			} 
+		}
+		return earFileRetriever;
+	}
+	public String getEarLocation() {
+		return earLocation;
+	}
+	public void setEarLocation(String earLocation) {
+		this.earLocation = earLocation;
+	}
 	
 }
