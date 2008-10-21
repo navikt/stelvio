e5c2ceb880b25e5bf546a7d1a237c2e79642415f
ebf2812 <ebf2812@localhost>
2008-10-21 14:48:26 +0000

    Upgraded versions to 1.0-SNAPSHOT

--- int/nav-int-appclient/nav-int-wpshelper-bundle/src/main/bin/femstatus.sh
+++ int/nav-int-appclient/nav-int-wpshelper-bundle/src/main/bin/femstatus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-command="/cygdrive/C/IBM/WID61/pf/wps01/bin/launchClient.bat ./femhelper-ear-0.9.ear -CCtrace=no.nav -CCpropfile=$1 -cf $1 -a STATUS" 
+command="/cygdrive/C/IBM/WID61/pf/wps01/bin/launchClient.bat ./femhelper-ear-1.0-SNAPSHOT.ear -CCtrace=no.nav -CCpropfile=$1 -cf $1 -a STATUS" 
 echo "Executing command: $command"
 $command
