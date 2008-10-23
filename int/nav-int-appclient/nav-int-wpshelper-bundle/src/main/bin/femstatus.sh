05375144d747ee7d9dac45fa8b2e74c0c76efad6
sbc2812 <sbc2812@localhost>
2008-10-23 13:57:48 +0000

    Changed fileformat from windows to unix, chenged path to launchclient to the correct for WPS dmgr on Linux.

--- int/nav-int-appclient/nav-int-wpshelper-bundle/src/main/bin/femstatus.sh
+++ int/nav-int-appclient/nav-int-wpshelper-bundle/src/main/bin/femstatus.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-command="/cygdrive/C/IBM/WID61/pf/wps01/bin/launchClient.bat ./femhelper-ear-1.0-SNAPSHOT.ear -CCtrace=no.nav -CCpropfile=$1 -cf $1 -a STATUS" 
+command="/opt/IBM/WebSphere/ProcServer/profiles/Dmgr01/bin/launchClient.sh ./femhelper-ear-0.9.1.ear -CCtrace=no.nav -CCpropfile=$1 -cf $1 -a STATUS"
 echo "Executing command: $command"
 $command
