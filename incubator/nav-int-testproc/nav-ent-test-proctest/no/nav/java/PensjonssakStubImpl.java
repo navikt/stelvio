9de8f5e34b68007b38506b286de18b76003fbf2f
hnc2812 <hnc2812@localhost>
2008-12-12 12:57:19 +0000

    Første innsjekk

---
+++ incubator/nav-int-testproc/nav-ent-test-proctest/no/nav/java/PensjonssakStubImpl.java
@@ -0,0 +1,100 @@
+package no.nav.java;
+
+import commonj.sdo.DataObject;
+
+import com.ibm.websphere.sca.ServiceBusinessException;
+import com.ibm.websphere.sca.ServiceManager;
+import com.ibm.websphere.sca.sdo.DataFactory;
+
+public class PensjonssakStubImpl {
+
+	private final String nameSpace="http://nav-lib-test/no/nav/lib/test/gbo";
+	private final String faultNameSpace = "http://nav-lib-test/no/nav/lib/test/fault";
+	
+	/**
+	 * Default constructor.
+	 */
+	public PensjonssakStubImpl() {
+		super();
+	}
+
+	/**
+	 * Return a reference to the component service instance for this implementation
+	 * class.  This method should be used when passing this service to a partner reference
+	 * or if you want to invoke this component service asynchronously.    
+	 *
+	 * @generated (com.ibm.wbit.java)
+	 */
+	@SuppressWarnings("unused")
+	private Object getMyService() {
+		return (Object) ServiceManager.INSTANCE.locateService("self");
+	}
+
+	/**
+	 * Method generated to support implemention of operation "opprettKontrollpunkt" defined for WSDL port type 
+	 * named "Pensjonssak".
+	 * 
+	 * The presence of commonj.sdo.DataObject as the return type and/or as a parameter 
+	 * type conveys that its a complex type. Please refer to the WSDL Definition for more information 
+	 * on the type of input, output and fault(s).
+	 */
+	public DataObject opprettKontrollpunkt(
+			DataObject opprettKontrollpunktRequest) {
+		//TODO Needs to be implemented.
+		return null;
+	}
+
+	/**
+	 * Method generated to support implemention of operation "hentKontrollpunktListe" defined for WSDL port type 
+	 * named "Pensjonssak".
+	 * 
+	 * The presence of commonj.sdo.DataObject as the return type and/or as a parameter 
+	 * type conveys that its a complex type. Please refer to the WSDL Definition for more information 
+	 * on the type of input, output and fault(s).
+	 */
+	public DataObject hentKontrollpunktListe(
+			DataObject hentKontrollpunktListeRequest) {
+		//TODO Needs to be implemented.
+		return null;
+	}
+
+	/**
+	 * Method generated to support implemention of operation "hentPensjonssakListe" defined for WSDL port type 
+	 * named "Pensjonssak".
+	 * 
+	 * The presence of commonj.sdo.DataObject as the return type and/or as a parameter 
+	 * type conveys that its a complex type. Please refer to the WSDL Definition for more information 
+	 * on the type of input, output and fault(s).
+	 */
+	public DataObject hentPensjonssakListe(
+			DataObject hentPensjonssakListeRequest) {
+		//TODO Needs to be implemented.
+		return null;
+	}
+
+	/**
+	 * Method generated to support implemention of operation "hentPensjonssak" defined for WSDL port type 
+	 * named "Pensjonssak".
+	 * 
+	 * The presence of commonj.sdo.DataObject as the return type and/or as a parameter 
+	 * type conveys that its a complex type. Please refer to the WSDL Definition for more information 
+	 * on the type of input, output and fault(s).
+	 */
+	public DataObject hentPensjonssak(DataObject hentPensjonssakRequest) {
+		if (hentPensjonssakRequest.getBoolean("triggerSbe"))
+		{
+			DataObject faultSakIkkeFunnet = DataFactory.INSTANCE.create(faultNameSpace, "FaultSakIkkeFunnet");
+			faultSakIkkeFunnet.setString("errorMessage", "Simulerer feil i hentPensjonssak" );
+			throw new ServiceBusinessException(faultSakIkkeFunnet);
+		}
+		DataObject gboPensjonssak = createGboPensjonssak();
+		return gboPensjonssak;
+	}
+
+	private DataObject createGboPensjonssak() {
+		DataObject gboPensjonssak = DataFactory.INSTANCE.create(nameSpace,"GBOPensjonssak" );
+		gboPensjonssak.setString("saksId", "789");
+		return gboPensjonssak;
+	}
+
+}
---
+++ incubator/nav-int-testproc/nav-ent-test-proctest/no/nav/java/VedtakStubImpl.java
@@ -0,0 +1,124 @@
+package no.nav.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import commonj.sdo.DataObject;
+
+import com.ibm.websphere.bo.BOFactory;
+import com.ibm.websphere.sca.ServiceBusinessException;
+import com.ibm.websphere.sca.ServiceManager;
+import com.ibm.websphere.sca.sdo.DataFactory;
+
+public class VedtakStubImpl {
+	
+	
+	private final String nameSpace="http://nav-lib-test/no/nav/lib/test/gbo";
+	private final String faultNameSpace = "http://nav-lib-test/no/nav/lib/test/fault";
+	/**
+	 * Default constructor.
+	 */
+	public VedtakStubImpl() {
+		super();
+	}
+
+	/**
+	 * Return a reference to the component service instance for this implementation
+	 * class.  This method should be used when passing this service to a partner reference
+	 * or if you want to invoke this component service asynchronously.    
+	 *
+	 * @generated (com.ibm.wbit.java)
+	 */
+	@SuppressWarnings("unused")
+	private Object getMyService() {
+		return (Object) ServiceManager.INSTANCE.locateService("self");
+	}
+
+	/**
+	 * Method generated to support implemention of operation "hentVedtakListe" defined for WSDL port type 
+	 * named "Vedtak".
+	 * 
+	 * The presence of commonj.sdo.DataObject as the return type and/or as a parameter 
+	 * type conveys that its a complex type. Please refer to the WSDL Definition for more information 
+	 * on the type of input, output and fault(s).
+	 */
+	public DataObject hentVedtakListe(DataObject hentVedtakListeRequest) {
+		if (hentVedtakListeRequest.getBoolean("returnTomVedtakListe"))
+			return null;
+		
+		List<DataObject> vedtakListe = new ArrayList<DataObject>();
+		DataObject gboVedtak = createGboVedtak(true);
+		vedtakListe.add(gboVedtak);
+		DataObject gboVedtakListe = DataFactory.INSTANCE.create(nameSpace, "GBOVedtakListe");
+		gboVedtakListe.setList("vedtakListe", vedtakListe);
+		return gboVedtakListe;
+	}
+
+	/**
+	 * Method generated to support implemention of operation "hentVedtak" defined for WSDL port type 
+	 * named "Vedtak".
+	 * 
+	 * The presence of commonj.sdo.DataObject as the return type and/or as a parameter 
+	 * type conveys that its a complex type. Please refer to the WSDL Definition for more information 
+	 * on the type of input, output and fault(s).
+	 */
+	public DataObject hentVedtak(DataObject hentVedtakRequest) {
+		if(hentVedtakRequest.getBoolean("triggerSbe"))
+		{
+			DataObject faultVedtakIkkeFunnet = DataFactory.INSTANCE.create(faultNameSpace, "FaultVedtakIkkeFunnet");
+			faultVedtakIkkeFunnet.setString("errorMessage", "Simulerer feil faultVedtakIkkeFunnet");
+			throw new ServiceBusinessException(faultVedtakIkkeFunnet);
+		}
+		DataObject gboVedtak = createGboVedtak(hentVedtakRequest.getBoolean("setDatoBefore"));
+		return gboVedtak;
+	}
+
+	/**
+	 * Bygger en stub av GBOVedtak. Man kan manipulere resultat ved å bruke følgende inputparametre i request:
+	 * 1. setDatoBefore - Dersom denne er true, settes dato til en dato før 10.10.2008 for å trigge DatoFor10102008 case.
+	 * 2. triggerSbe - Dersom denne er true, vil koden kaste ServiceBusinessException som er definert for kallet (faultVedtakIkkeFunnet).
+	 * 3. triggerSre - Dersom denne er true, vil koden kaste en ServiceRuntimeException.
+	 * @param hentVedtakRequest
+	 * @return
+	 */
+	private DataObject createGboVedtak(boolean setDatoBefore) {
+		DataObject gboVedtak = DataFactory.INSTANCE.create(nameSpace, "GBOVedtak");
+		gboVedtak.setString("vedtakId", "567");
+		gboVedtak.setString("saksId", "1234");
+		if(setDatoBefore)
+		{
+			gboVedtak.setString("virkningFom", "2007-11-18T23:00:00Z");
+		}else{
+			gboVedtak.setString("virkningFom", "2008-11-18T23:00:00Z");
+		}
+		
+		return gboVedtak;
+	}
+
+	/**
+	 * Method generated to support implemention of operation "lagreVedtakStatus" defined for WSDL port type 
+	 * named "Vedtak".
+	 * 
+	 * The presence of commonj.sdo.DataObject as the return type and/or as a parameter 
+	 * type conveys that its a complex type. Please refer to the WSDL Definition for more information 
+	 * on the type of input, output and fault(s).
+	 */
+	public String lagreVedtakStatus(DataObject lagreVedtakStatusRequest) {
+		//TODO Needs to be implemented.
+		return null;
+	}
+
+	/**
+	 * Method generated to support implemention of operation "lagreVedtak" defined for WSDL port type 
+	 * named "Vedtak".
+	 * 
+	 * The presence of commonj.sdo.DataObject as the return type and/or as a parameter 
+	 * type conveys that its a complex type. Please refer to the WSDL Definition for more information 
+	 * on the type of input, output and fault(s).
+	 */
+	public String lagreVedtak(DataObject lagreVedtakRequest) {
+		//TODO Needs to be implemented.
+		return null;
+	}
+
+}
