08525eef238acb425151626eeecd049b7ab48c9e
ebf2812 <ebf2812@localhost>
2009-01-15 10:47:19 +0000

    Changes after making project directory the source directory

---
+++ incubator/maven/maven-poc/poc-not-working-module/no/nav/java/Main.java
@@ -0,0 +1,11 @@
+package no.nav.java;
+
+import no.nav.util.LoggerUtil;
+import no.nav.util.LoggerUtil2;
+
+public class Main {
+	public static void main(String args[]) {
+		LoggerUtil.log("Logging using poc-lib1");
+		LoggerUtil2.log("Logging using poc-lib2");
+	}
+}
