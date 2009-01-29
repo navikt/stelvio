cafeee970ff794d6f5209674fec1165304f426b3
ebf2812 <ebf2812@localhost>
2009-01-29 14:51:02 +0000

    Extracted reusable superclass

--- incubator/maven/maven-plugins/maven-vertical-plugin/src/main/java/no/nav/maven/plugin/sca/CheckOutMojo.java
+++ incubator/maven/maven-plugins/maven-vertical-plugin/src/main/java/no/nav/maven/plugin/sca/CheckOutMojo.java
@@ -1,23 +1,9 @@
 package no.nav.maven.plugin.sca;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.MavenProjectBuilder;
-import org.apache.maven.project.ProjectBuildingException;
 import org.apache.maven.scm.ScmException;
-import org.apache.maven.scm.ScmFileSet;
-import org.apache.maven.scm.manager.ScmManager;
-import org.apache.maven.scm.repository.ScmRepository;
 
 /**
  * @author test@example.com
@@ -26,75 +12,8 @@ import org.apache.maven.scm.repository.ScmRepository;
  * @requiresDependencyResolution compile
  */
 @SuppressWarnings("unchecked")
-public class CheckOutMojo extends AbstractMojo {
-	private static final Collection<String> SUPPORTED_PACKAGINGS = new HashSet<String>(Arrays.asList(new String[] {
-			"sca-module-ear", "sca-library-jar" }));
-
-	/**
-	 * The working directory
-	 * 
-	 * @parameter default-value="${project.basedir}"
-	 */
-	private File workingDirectory;
-
-	/**
-	 * @parameter expression="${project.artifacts}"
-	 * @required
-	 * @readonly
-	 */
-	private Collection<Artifact> artifacts;
-
-	/**
-	 * @parameter expression="${project.remoteArtifactRepositories}"
-	 * @required
-	 * @readonly
-	 */
-	private List remoteArtifactRepositories;
-
-	/**
-	 * @parameter expression="${localRepository}"
-	 * @required
-	 * @readonly
-	 */
-	private ArtifactRepository localRepository;
-
-	/**
-	 * @component
-	 */
-	private MavenProjectBuilder projectBuilder;
-
-	/**
-	 * @component
-	 */
-	private ScmManager scmManager;
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public void execute() throws MojoExecutionException {
-		Collection<MavenProject> projects = getProjects();
-		Collection<ScmProject> scmProjects = new ArrayList<ScmProject>(projects.size());
-		for (MavenProject project : projects) {
-			String packaging = project.getPackaging();
-			if (SUPPORTED_PACKAGINGS.contains(packaging)) {
-				ScmRepository scmRepository = getScmRepository(project.getScm().getDeveloperConnection());
-				File projectWorkingDirectory = new File(workingDirectory, project.getArtifactId());
-				ScmFileSet scmFileSet = new ScmFileSet(projectWorkingDirectory);
-				scmProjects.add(new ScmProject(scmRepository, scmFileSet));
-			}
-		}
-		checkOut(scmProjects);
-	}
-
-	private ScmRepository getScmRepository(String scmUrl) throws MojoExecutionException {
-		try {
-			return scmManager.makeScmRepository(scmUrl);
-		} catch (ScmException e) {
-			throw new MojoExecutionException("Error creating SCM repository", e);
-		}
-	}
-
-	private void checkOut(Collection<ScmProject> scmProjects) throws MojoExecutionException {
+public class CheckOutMojo extends VerticalMojo {
+	protected void execute(Collection<ScmProject> scmProjects) throws MojoExecutionException {
 		try {
 			for (ScmProject scmProject : scmProjects) {
 				scmProject.getScmFileSet().getBasedir().mkdirs();
@@ -102,18 +21,6 @@ public class CheckOutMojo extends AbstractMojo {
 			}
 		} catch (ScmException e) {
 			throw new MojoExecutionException("Error checking out projects", e);
-		}
-	}
-
-	private Collection<MavenProject> getProjects() throws MojoExecutionException {
-		try {
-			Collection<MavenProject> projects = new ArrayList<MavenProject>();
-			for (Artifact artifact : artifacts) {
-				projects.add(projectBuilder.buildFromRepository(artifact, remoteArtifactRepositories, localRepository));
-			}
-			return projects;
-		} catch (ProjectBuildingException e) {
-			throw new MojoExecutionException("Error building projects from repository", e);
 		}
 	}
 }
---
+++ incubator/maven/maven-plugins/maven-vertical-plugin/src/main/java/no/nav/maven/plugin/sca/VerticalMojo.java
@@ -0,0 +1,97 @@
+package no.nav.maven.plugin.sca;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectBuilder;
+import org.apache.maven.project.ProjectBuildingException;
+import org.apache.maven.scm.ScmException;
+import org.apache.maven.scm.ScmFileSet;
+import org.apache.maven.scm.manager.ScmManager;
+import org.apache.maven.scm.repository.ScmRepository;
+
+public abstract class VerticalMojo extends AbstractMojo {
+	private static final Collection<String> SUPPORTED_PACKAGINGS = new HashSet<String>(Arrays.asList(new String[] {
+			"sca-module-ear", "sca-library-jar" }));
+	/**
+	 * The working directory
+	 * 
+	 * @parameter default-value="${project.basedir}"
+	 */
+	private File workingDirectory;
+	/**
+	 * @parameter expression="${project.artifacts}"
+	 * @required
+	 * @readonly
+	 */
+	private Collection<Artifact> artifacts;
+	/**
+	 * @parameter expression="${project.remoteArtifactRepositories}"
+	 * @required
+	 * @readonly
+	 */
+	private List remoteArtifactRepositories;
+	/**
+	 * @parameter expression="${localRepository}"
+	 * @required
+	 * @readonly
+	 */
+	private ArtifactRepository localRepository;
+	/**
+	 * @component
+	 */
+	private MavenProjectBuilder projectBuilder;
+	/**
+	 * @component
+	 */
+	protected ScmManager scmManager;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void execute() throws MojoExecutionException {
+		Collection<MavenProject> projects = getProjects();
+		Collection<ScmProject> scmProjects = new ArrayList<ScmProject>(projects.size());
+		for (MavenProject project : projects) {
+			String packaging = project.getPackaging();
+			if (SUPPORTED_PACKAGINGS.contains(packaging)) {
+				ScmRepository scmRepository = getScmRepository(project.getScm().getDeveloperConnection());
+				File projectWorkingDirectory = new File(workingDirectory, project.getArtifactId());
+				ScmFileSet scmFileSet = new ScmFileSet(projectWorkingDirectory);
+				scmProjects.add(new ScmProject(scmRepository, scmFileSet));
+			}
+		}
+		execute(scmProjects);
+	}
+
+	private ScmRepository getScmRepository(String scmUrl) throws MojoExecutionException {
+		try {
+			return scmManager.makeScmRepository(scmUrl);
+		} catch (ScmException e) {
+			throw new MojoExecutionException("Error creating SCM repository", e);
+		}
+	}
+
+	private Collection<MavenProject> getProjects() throws MojoExecutionException {
+		try {
+			Collection<MavenProject> projects = new ArrayList<MavenProject>();
+			for (Artifact artifact : artifacts) {
+				projects.add(projectBuilder.buildFromRepository(artifact, remoteArtifactRepositories, localRepository));
+			}
+			return projects;
+		} catch (ProjectBuildingException e) {
+			throw new MojoExecutionException("Error building projects from repository", e);
+		}
+	}
+
+	protected abstract void execute(Collection<ScmProject> scmProjects) throws MojoExecutionException;
+}
