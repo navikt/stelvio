15f76b3fd717c7ce5374af225110faf512cb56ac
sfa2812 <sfa2812@localhost>
2009-02-05 10:49:55 +0000

    Refactoring the module

---
+++ incubator/maven/maven-core-artifacts/maven-plugins/maven-websphere-plugin/src/main/java/no/nav/maven/plugin/websphere/plugin/mojo/LoadRuntimeConfigurationMojo.java
@@ -0,0 +1,49 @@
+package no.nav.maven.plugin.websphere.plugin.mojo;
+
+import java.io.File;
+
+import no.nav.busconfiguration.configuration.ArtifactConfiguration;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+
+
+
+/**
+ * Goal that loads the configuration for all artifacts into the jvm 
+ * 
+ * @author test@example.com
+ * 
+ * @goal load-runtime-configuration
+ * @requiresDependencyResolution
+ */
+public class LoadRuntimeConfigurationMojo extends RuntimeModifierMojo {
+    
+	/**
+     * Name of module configuration artifact.
+     *
+     * @parameter default-value="wpsconfiguration"
+     */
+	private String moduleConfigurationArtifactName;
+	
+	protected final void doExecute() throws MojoExecutionException, MojoFailureException {
+		
+		if(ArtifactConfiguration.isConfigurationLoaded() == true) {
+			return;
+		} else {
+			for(Artifact a : dependencyArtifacts) {
+				if(a.getArtifactId().equals(moduleConfigurationArtifactName)) {
+					File extractedFolder = jarArchiveManager.unArchive(a.getFile(), new File(baseDirectory,scriptDirectory));
+					ArtifactConfiguration.loadConfiguration(new File(extractedFolder, "moduleconfig"));
+					break;
+				}
+			}
+			
+			if(ArtifactConfiguration.isConfigurationLoaded() == false) {
+				getLog().warn("The depoyment does not contain dependency to a wps configuration");
+			}
+		}
+	}
+}	
---
+++ incubator/maven/maven-core-artifacts/maven-plugins/maven-websphere-plugin/src/main/java/no/nav/maven/plugin/websphere/plugin/mojo/RuntimeModifierMojo.java
@@ -0,0 +1,91 @@
+package no.nav.maven.plugin.websphere.plugin.mojo;
+
+import java.io.File;
+import java.util.Set;
+
+import no.nav.busconfiguration.managers.ArchiveManager;
+import no.nav.busconfiguration.managers.IArchiveManager;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.archiver.Archiver;
+import org.codehaus.plexus.archiver.UnArchiver;;
+
+
+/**
+ * Abstract class using the template pattern for child mojos.
+ * 
+ * @author test@example.com 
+ */
+public abstract class RuntimeModifierMojo extends AbstractMojo {
+
+	/**
+	 * @parameter expression="${component.org.codehaus.plexus.archiver.Archiver#ear}"
+	 * @required
+	 */
+	protected Archiver earArchiver;
+
+	/**
+	 * @parameter expression="${component.org.codehaus.plexus.archiver.UnArchiver#ear}"
+	 * @required
+	 */
+	protected UnArchiver earUnArchiver;
+
+	/**
+	 * @parameter expression="${component.org.codehaus.plexus.archiver.Archiver#jar}"
+	 * @required
+	 */
+	protected Archiver jarArchiver;
+
+	/**
+	 * @parameter expression="${component.org.codehaus.plexus.archiver.UnArchiver#jar}"
+	 * @required
+	 */
+	protected UnArchiver jarUnArchiver;	
+
+	/**
+	 * @parameter expression="${project.build.directory}"
+	 * @required
+	 */
+	protected String targetDirectory;
+
+	/**
+	 * @parameter expression="${project.basedir}"
+	 * @required
+	 */
+	protected File baseDirectory;
+	
+	/**
+	 * @parameter expression="${project.build.scriptSourceDirectory}"
+	 * @required
+	 */
+	protected String scriptDirectory;
+
+	/**
+	 * @parameter expression="${project.dependencyArtifacts}"
+	 * @required
+	 */
+	protected Set<Artifact> dependencyArtifacts;	
+	
+	/**
+	 * @parameter expression="${project.artifacts}"
+	 * @required
+	 */
+	protected Set<Artifact> artifacts;	
+	
+	protected abstract void doExecute() throws MojoExecutionException, MojoFailureException;
+
+	protected IArchiveManager earArchiveManager;
+	protected IArchiveManager jarArchiveManager;
+	
+	public void execute() throws MojoExecutionException, MojoFailureException {
+
+		earArchiveManager = new ArchiveManager(earArchiver, earUnArchiver);
+		jarArchiveManager = new ArchiveManager(jarArchiver, jarUnArchiver);
+		doExecute();
+	}
+}
