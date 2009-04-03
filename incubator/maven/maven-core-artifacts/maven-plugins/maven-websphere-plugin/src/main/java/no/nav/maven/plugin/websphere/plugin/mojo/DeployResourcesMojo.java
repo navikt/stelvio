4a56ef0cb0d3ffe4a06280420ef9c25696c7a7a4
sfa2812 <sfa2812@localhost>
2009-04-03 11:12:41 +0000

    changed to install phase req.

--- incubator/maven/maven-core-artifacts/maven-plugins/maven-websphere-plugin/src/main/java/no/nav/maven/plugin/websphere/plugin/mojo/DeployResourcesMojo.java
+++ incubator/maven/maven-core-artifacts/maven-plugins/maven-websphere-plugin/src/main/java/no/nav/maven/plugin/websphere/plugin/mojo/DeployResourcesMojo.java
@@ -13,7 +13,7 @@ import org.codehaus.plexus.util.cli.Commandline;
  * @author test@example.com
  * 
  * @goal deploy-resources
- * @execute phase="package"
+ * @execute phase="install"
  * @requiresDependencyResolution
  */
 public class DeployResourcesMojo extends WebsphereUpdaterMojo {
