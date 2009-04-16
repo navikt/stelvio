2f7e8045a49d2951ee6e8be8d92f7a6c114d0395
tza2812 <tza2812@localhost>
2009-04-16 08:33:56 +0000

    blablablabla

--- incubator/maven/maven-core-artifacts/maven-plugins/maven-confluence-plugin/src/main/java/no/nav/maven/plugin/confluence/mojo/AddCommentMojo.java
+++ incubator/maven/maven-core-artifacts/maven-plugins/maven-confluence-plugin/src/main/java/no/nav/maven/plugin/confluence/mojo/AddCommentMojo.java
@@ -1,12 +1,13 @@
 package no.nav.maven.plugin.confluence.mojo;
 
 import java.net.MalformedURLException;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import no.nav.maven.plugin.confluence.util.Constants;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -16,81 +17,102 @@ import org.codehaus.swizzle.confluence.Page;
 
 /**
  * @author test@example.com
- *
+ * 
  * 
  * @goal add-comment
- * @requiresDependencyResolution
  */
 @SuppressWarnings("unchecked")
-public class AddCommentMojo extends AbstractMojo {
+public abstract class AddCommentMojo extends AbstractMojo {
 
-	/** 
+	/**
 	 * 
 	 * @parameter expression="${username}" default-value="deployer"
 	 * @required
-	 */ 
+	 */
 	protected String userName;
-	 
-	/** 
+
+	/**
 	 * 
 	 * @parameter expression="${password}" default-value="deployer"
 	 * @required
-	 */ 
+	 */
 	protected String password;
-	
-	/** 
+
+	/**
 	 * 
-	 * @parameter expression="${endpoint}" default-value="http://confluence.adeo.no"
+	 * @parameter expression="${endpoint}"
+	 *            default-value="http://confluence.adeo.no"
 	 * @required
-	 */ 
+	 */
 	protected String endPoint;
-	
+
 	/**
 	 * @parameter expression="${environment}"
 	 * @required
 	 */
 	protected String environment;
-	
+
 	/**
 	 * @parameter expression="${comment}"
 	 */
 	protected String comment;
-	
+
+	/**
+	 * @parameter expression="${project.dependencyArtifacts}"
+	 * @required
+	 */
+	protected Set<Artifact> dependencyArtifacts;
+
+	protected String deployString = "";
+
+	protected String configurationString = "";
+
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		
-		if(comment == null) {
-			getLog().warn("Comment argument is not set. Confluence is not updated with comment");
-			return;
+
+		for (Artifact a : dependencyArtifacts) {
+			if (a.getArtifactId().equals("bus-deploy")) {
+				deployString = a.getVersion();
+			}
+			if (a.getArtifactId().equals("busconfiguration")) {
+				configurationString = a.getVersion();
+			}
 		}
-		
+
 		Confluence confluence = null;
 		try {
 			confluence = new Confluence(endPoint);
 		} catch (MalformedURLException e) {
-			throw new RuntimeException("The URL: " + endPoint + " is not valid", e);
+			throw new RuntimeException(
+					"The URL: " + endPoint + " is not valid", e);
 		}
-		
+
 		try {
 			confluence.login(userName, password);
 		} catch (Exception e) {
 			throw new RuntimeException("Unable to log in to confluence", e);
 		}
-		
+
 		Page page = null;
 		try {
-			page = confluence.getPage("stelvio", Constants.ENVMAPPING.get(environment));
+			page = confluence.getPage("stelvio", Constants.ENVMAPPING
+					.get(environment));
 		} catch (Exception e) {
-			throw new RuntimeException("An error occured trying to retrieve environment page", e);
+			throw new RuntimeException(
+					"An error occured trying to retrieve environment page", e);
 		}
-		
+
 		Map map = new HashMap<String, String>();
 		map.put("pageId", page.getId());
-		map.put("content", comment);
-		Comment comment = new Comment(map);
+		map.put("content", getComment());
+		Comment newcomment = new Comment(map);
 		try {
-			confluence.addComment(comment);
+			confluence.addComment(newcomment);
 		} catch (Exception e) {
-			throw new RuntimeException("An error occured trying to add a comment to the environment page", e);
+			throw new RuntimeException(
+					"An error occured trying to add a comment to the environment page",
+					e);
 		}
 	}
+
+	protected abstract String getComment();
 };
