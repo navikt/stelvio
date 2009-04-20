db2a7799f615995850aff67c5700765aadb8c612
sfa2812 <sfa2812@localhost>
2009-04-20 14:49:57 +0000

    Updated with autoamtic discovery of BP ears

--- incubator/maven/maven-core-artifacts/maven-plugins/maven-websphere-plugin/src/main/java/no/nav/maven/plugin/websphere/plugin/mojo/RemoveBPELMojo.java
+++ incubator/maven/maven-core-artifacts/maven-plugins/maven-websphere-plugin/src/main/java/no/nav/maven/plugin/websphere/plugin/mojo/RemoveBPELMojo.java
@@ -1,7 +1,11 @@
 package no.nav.maven.plugin.websphere.plugin.mojo;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.maven.artifact.Artifact;
@@ -29,12 +33,14 @@ public class RemoveBPELMojo extends WebsphereUpdaterMojo {
 	protected Properties properties;	
 	
 	public final void applyToWebSphere(final Commandline commandLine) throws MojoExecutionException, MojoFailureException {
+		File modules = new File(deployableArtifactsHome);
+		String[] bpFiles = modules.list(new FilenameFilter() {public boolean accept(File file, String name){return name.endsWith(".bp");}}); 
+		
+		List<String> bpFilesList = Arrays.asList(bpFiles); 
+		System.out.println(bpFilesList);
 		for(Artifact a : artifacts) {
-			if(properties.containsKey(a.getArtifactId())) {
-				String property = properties.getProperty(a.getArtifactId());
-				if(("bpel").equals(property) == true) {
-					removeBPEL(commandLine,a.getArtifactId());
-				}
+			if(bpFilesList.contains(a.getArtifactId() + ".bp")) {
+				removeBPEL(commandLine, a.getArtifactId());
 			}
 		}
 	}
