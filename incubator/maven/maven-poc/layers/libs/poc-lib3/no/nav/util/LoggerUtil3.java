9afbc898a855670b0ee738e62c4aa4dc9ccab057
ebh2812 <ebh2812@localhost>
2009-09-21 12:56:12 +0000

    Introduec classes to test intepretation of manifest Class-Path property

---
+++ incubator/maven/maven-poc/layers/libs/poc-lib3/no/nav/util/LoggerUtil3.java
@@ -0,0 +1,7 @@
+package no.nav.util;
+
+public class LoggerUtil3 {
+	public static void log(Object object) {
+		LoggerUtil.log(object);
+	}
+}
---
+++ incubator/maven/maven-poc/layers/modules/cons/poc-module1/no/nav/java/Main.java
@@ -0,0 +1,9 @@
+package no.nav.java;
+
+import no.nav.util.LoggerUtil3;
+
+public class Main {
+	public static void main(String args[]) {
+		LoggerUtil3.log("Logging using poc-lib3");
+	}
+}
