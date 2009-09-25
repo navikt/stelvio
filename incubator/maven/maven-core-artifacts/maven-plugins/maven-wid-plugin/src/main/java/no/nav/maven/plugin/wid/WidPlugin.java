e2ffa4e0ee7805f2f04067515184a06518cd6ffb
ebh2812 <ebh2812@localhost>
2009-09-25 13:45:17 +0000

    NAV00135844: Implemented suggested workaround

--- incubator/maven/maven-core-artifacts/maven-plugins/maven-wid-plugin/src/main/java/no/nav/maven/plugin/wid/WidPlugin.java
+++ incubator/maven/maven-core-artifacts/maven-plugins/maven-wid-plugin/src/main/java/no/nav/maven/plugin/wid/WidPlugin.java
@@ -1,18 +1,23 @@
 package no.nav.maven.plugin.wid;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import no.nav.maven.plugin.wid.writers.WidWtpComponentWriter;
 import no.nav.maven.plugin.wid.writers.WidWtpFacetsWriter;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.eclipse.EclipsePlugin;
 import org.apache.maven.plugin.eclipse.EclipseSourceDir;
 import org.apache.maven.plugin.eclipse.writers.EclipseWriterConfig;
+import org.apache.maven.plugin.ide.IdeDependency;
+import org.apache.maven.project.MavenProject;
 
 /**
  * Generates the wid configuration files.
@@ -25,16 +30,59 @@ import org.apache.maven.plugin.eclipse.writers.EclipseWriterConfig;
  * @execute phase="generate-resources"
  */
 public class WidPlugin extends EclipsePlugin {
+	private static final String WPS_MODULE_EAR = "wps-module-ear";
+	private static final String WPS_LIBRARY_JAR = "wps-library-jar";
+
 	@Override
 	protected void writeConfigurationExtras(EclipseWriterConfig eclipseWriterConfig) throws MojoExecutionException {
 		super.writeConfigurationExtras(eclipseWriterConfig);
 
 		String packaging = getProject().getPackaging();
-		if ("wps-library-jar".equals(packaging) || "wps-module-ear".equals(packaging)) {
+		if (WPS_LIBRARY_JAR.equals(packaging) || WPS_MODULE_EAR.equals(packaging)) {
 			setSourceDirs(eclipseWriterConfig);
 			setOutputDir(eclipseWriterConfig);
+
+			fixDependencies(eclipseWriterConfig);
+
 			writeWtpSettings(eclipseWriterConfig);
 		}
+	}
+
+	private void fixDependencies(EclipseWriterConfig eclipseWriterConfig) throws MojoExecutionException {
+		// WID does not support dependencies of type 'var' in modules and
+		// libraries
+		List<IdeDependency> dependencies = new ArrayList<IdeDependency>();
+		for (IdeDependency dependency : eclipseWriterConfig.getDeps()) {
+			String type = dependency.getType();
+			// The OR is just temporary - remove when correct type is added to
+			// dependencies
+			if (WPS_LIBRARY_JAR.equals(type) || reactorContainsProject(dependency)) {
+				dependency.setReferencedProject(true);
+				dependency.setEclipseProjectName(dependency.getArtifactId());
+				dependency.setFile(null);
+				dependency.setJavadocAttachment(null);
+				dependency.setSourceAttachment(null);
+				dependencies.add(dependency);
+			} else if (WPS_MODULE_EAR.equals(getProject().getPackaging())) {
+				try {
+					FileUtils.copyFileToDirectory(dependency.getFile(), getEclipseProjectDir());
+				} catch (IOException e) {
+					throw new MojoExecutionException("Unable to copy file", e);
+				}
+			}
+		}
+		eclipseWriterConfig.setDeps(dependencies.toArray(new IdeDependency[dependencies.size()]));
+	}
+
+	private boolean reactorContainsProject(IdeDependency dependency) {
+		for (Object temp : getReactorProjects()) {
+			MavenProject reactorProject = (MavenProject) temp;
+			if (dependency.getGroupId().equals(reactorProject.getGroupId())
+					&& dependency.getArtifactId().equals(reactorProject.getArtifactId())) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private void writeWtpSettings(EclipseWriterConfig eclipseWriterConfig) throws MojoExecutionException {
