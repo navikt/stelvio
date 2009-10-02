88f2cd441dd42e74f34cfd04f596712a7563845d
tia2812 <tia2812@localhost>
2009-10-02 11:09:43 +0000

    Støtte for cons-moduler som ikke har web-modul

--- int/maven/plugins/maven-wsdl-export-plugin/src/main/java/no/nav/maven/plugins/WSDLExportMojo.java
+++ int/maven/plugins/maven-wsdl-export-plugin/src/main/java/no/nav/maven/plugins/WSDLExportMojo.java
@@ -16,10 +16,6 @@ package no.nav.maven.plugins;
  * limitations under the License.
  */
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.util.Collections;
-
 import org.apache.maven.artifact.handler.ArtifactHandler;
 import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
 import org.apache.maven.plugin.AbstractMojo;
@@ -29,18 +25,27 @@ import org.apache.maven.project.MavenProjectHelper;
 import org.codehaus.plexus.archiver.Archiver;
 import org.codehaus.plexus.archiver.UnArchiver;
 
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Collections;
+
+
 /**
  * Goal which touches a timestamp file.
- * 
+ *
  * @goal exportwsdl
  * 
  * @phase package
  */
-public class WSDLExportMojo extends AbstractMojo {
+public class WSDLExportMojo
+    extends AbstractMojo
+{
 	private static final String ZIP_SUFFIX = "zip";
 	private static final String WAR_SUFFIX = "war";
 	private static final String WSDL_PATH_IN_WAR = "/META-INF/wsdl";
 	private static final String WSDL_INTERFACE_ARTIFACT_TYPE = "wsdl-interface";
+
 
 	/**
 	 * @parameter expression="${project}"
@@ -48,11 +53,12 @@ public class WSDLExportMojo extends AbstractMojo {
 	 * @readonly
 	 */
 	private MavenProject project;
-
+	
 	/**
 	 * @component
 	 */
 	private MavenProjectHelper projectHelper;
+
 
 	/**
 	 * @component roleHint="zip"
@@ -78,63 +84,62 @@ public class WSDLExportMojo extends AbstractMojo {
 	 */
 	private UnArchiver unArchiver;
 
-	/**
-	 * EAR-file.
-	 * 
-	 * @parameter expression="${project.artifact.file}"
-	 * @required
-	 */
-	private File earFile;
 
-	public void execute() throws MojoExecutionException {
+    /**
+     * EAR-file.
+     * @parameter expression="${project.artifact.file}"
+     * @required
+     */
+    private File earFile;
+    
+    public void execute()
+        throws MojoExecutionException
+    {
+    	
+    	try {
 
-		try {
-			// earFile=project.getArtifact().getFile();
-			if (!wsdlInterfaceArtifactHandler.equals(artifactHandlerManager.getArtifactHandler(WSDL_INTERFACE_ARTIFACT_TYPE))) {
-				getLog().debug("Adding wsdlif interchange artifact handler to artifact handler manager");
-				artifactHandlerManager.addHandlers(Collections.singletonMap(WSDL_INTERFACE_ARTIFACT_TYPE,
-						wsdlInterfaceArtifactHandler));
-			}
-			unArchiver.setSourceFile(earFile);
-			String tempDir = project.getBuild().getDirectory() + "/wsdltemp/" + System.currentTimeMillis() + "/";
-			File tempDirfile = new File(tempDir);
-			tempDirfile.mkdirs();
-
-			unArchiver.setDestDirectory(tempDirfile);
-			try {
+    		if (!wsdlInterfaceArtifactHandler.equals(artifactHandlerManager
+    				.getArtifactHandler(WSDL_INTERFACE_ARTIFACT_TYPE))) {
+    			getLog().debug("Adding wsdlif interchange artifact handler to artifact handler manager");
+    			artifactHandlerManager.addHandlers(Collections.singletonMap(WSDL_INTERFACE_ARTIFACT_TYPE,
+    					wsdlInterfaceArtifactHandler));
+    		}	    	
+	    	unArchiver.setSourceFile(earFile);
+	    	String tempDir=project.getBuild().getDirectory()+"/wsdltemp/"+System.currentTimeMillis()+"/";	    	
+	    	File tempDirfile = new File(tempDir);
+	    	tempDirfile.mkdirs();
+		    	
+	    	unArchiver.setDestDirectory(tempDirfile);
+	    	try{
 				unArchiver.extract();
-			} catch (Exception e) {
-				throw new RuntimeException("Unable to unzip ear file " + earFile.getPath() + " to directory "
-						+ unArchiver.getDestDirectory(), e);
+			} catch (Exception e) {			
+				throw new RuntimeException("Unable to unzip ear file "+earFile.getPath()+" to directory "+unArchiver.getDestDirectory(), e);
 			}
-			String[] warFileNames = tempDirfile.list(new FilenameFilter() {
+			String[] warFileNames = tempDirfile.list(new FilenameFilter(){
 				public boolean accept(File dir, String name) {
 					return name.endsWith(WAR_SUFFIX);
-				}
+				}			
 			});
-			if (warFileNames.length == 0) {
-				throw new MojoExecutionException("Unable to find a war-file under directory " + tempDir);
+			if (warFileNames.length!=0){ //Does this module have WSDL exports?					
+				String warFileName=tempDir+warFileNames[0];
+				File warFile = new File(warFileName);
+		    	unArchiver.setSourceFile(warFile);    	
+				unArchiver.extract();
+				
+				File wsdlDirectory=new File(tempDir+WSDL_PATH_IN_WAR);				
+				File wsdlZipArtifactFile = new File(project.getBuild().getDirectory(), project.getBuild().getFinalName()+"-"+wsdlInterfaceArtifactHandler.getClassifier()
+						+ "." + ZIP_SUFFIX);
+				archiver.setDestFile(wsdlZipArtifactFile);
+				
+				archiver.addDirectory(wsdlDirectory);
+				archiver.createArchive();
+				projectHelper.attachArtifact(project, WSDL_INTERFACE_ARTIFACT_TYPE, wsdlInterfaceArtifactHandler
+						.getClassifier(), wsdlZipArtifactFile);							
 			}
-			String warFileName = tempDir + warFileNames[0];
-			File warFile = new File(warFileName);
-			unArchiver.setSourceFile(warFile);
-			unArchiver.extract();
-
-			File wsdlDirectory = new File(tempDir + WSDL_PATH_IN_WAR);
-
-			File wsdlZipArtifactFile = new File(project.getBuild().getDirectory(), project.getBuild().getFinalName() + "-"
-					+ wsdlInterfaceArtifactHandler.getClassifier() + "." + ZIP_SUFFIX);
-			archiver.setDestFile(wsdlZipArtifactFile);
-
-			archiver.addDirectory(wsdlDirectory);
-			archiver.createArchive();
-			projectHelper.attachArtifact(project, WSDL_INTERFACE_ARTIFACT_TYPE, wsdlInterfaceArtifactHandler.getClassifier(),
-					wsdlZipArtifactFile);
-		} catch (Exception e) {
-			throw new MojoExecutionException("Creating wsdl export file failed ", e);
-		}
-
-		// project.addAttachedArtifact(artifact)
-
-	}
+    	}catch(Exception e){
+    		throw new MojoExecutionException("Creating wsdl export file failed ",e);
+    	}
+		
+    	
+    }
 }
