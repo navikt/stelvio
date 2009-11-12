a6fcc3219d050ff84efe34d2d3e390795faa9623
tle2812 <tle2812@localhost>
2009-11-12 06:28:48 +0000

    Improve error handling for action types

--- int/nav-int-appclient/nav-int-wpshelper/femhelper-java/src/main/java/no/nav/femhelper/actions/ActionFactory.java
+++ int/nav-int-appclient/nav-int-wpshelper/femhelper-java/src/main/java/no/nav/femhelper/actions/ActionFactory.java
@@ -9,20 +9,16 @@ public class ActionFactory {
 	
 	public static AbstractAction getAction (String actionType, Properties properties) {
 
-		AbstractAction action= null;
-		
 		if (Constants.ACTION_REPORT.equalsIgnoreCase(actionType)) {
-			action = new ReportAction(properties);
+			return new ReportAction(properties);
 		} else if (Constants.ACTION_DISCARD.equalsIgnoreCase(actionType)) {
-			action = new DeleteAction(properties);
+			return new DeleteAction(properties);
 		} else if (Constants.ACTION_RESUBMIT.equalsIgnoreCase(actionType)) {
-			action = new ResubmitAction(properties);
+			return new ResubmitAction(properties);
 		} else if (Constants.ACTION_STATUS.equalsIgnoreCase(actionType)) {
-			action = new StatusAction(properties);
-		} 
-
-		// Since the input allready is validated to be a valid option 
-		// based on these constants
-		return action;
+			return new StatusAction(properties);
+		} else {
+			throw new RuntimeException("ActionType '" + actionType + "' is not supported");
+		}
 	}
 }
