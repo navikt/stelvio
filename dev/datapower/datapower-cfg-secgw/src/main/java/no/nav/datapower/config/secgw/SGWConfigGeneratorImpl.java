c7647e20bd845616d31f31446ed8fc2fde558559
oga2812 <oga2812@localhost>
2009-12-08 17:13:08 +0000

    Move AAA-files out of generator

--- dev/datapower/datapower-cfg-secgw/src/main/java/no/nav/datapower/config/secgw/SGWConfigGeneratorImpl.java
+++ dev/datapower/datapower-cfg-secgw/src/main/java/no/nav/datapower/config/secgw/SGWConfigGeneratorImpl.java
@@ -28,8 +28,6 @@ public class SGWConfigGeneratorImpl extends FreemarkerConfigGenerator {
 	private static final Logger LOG = Logger.getLogger(SGWConfigGeneratorImpl.class);
 	private static final String GENERATOR_NAME = "secgw";
 	private static final String TEMPLATE_CFG = "secgw-configuration.ftl";
-	private static final String TEMPLATE_AAA = "aaa-mapping-file.ftl";
-	private static final String TEMPLATE_AAA_SBLUTB = "aaa-mapping-file-SBLUTB.ftl";
 	private static final String REQUIRED_PROPERTIES_NAME = "/cfg-secgw-required.properties";
 	private static final Properties REQUIRED_PROPERTIES = DPPropertiesUtils.load(SGWConfigGeneratorImpl.class,REQUIRED_PROPERTIES_NAME);
 	private static final TemplateLoader SECGW_TEMPLATE_LOADER = new StreamTemplateLoader(SGWConfigGeneratorImpl.class, "/");
@@ -42,7 +40,6 @@ public class SGWConfigGeneratorImpl extends FreemarkerConfigGenerator {
 	public ConfigPackage generate() {
 		EnvironmentResources cfg = getEnvironmentResources();
 		Properties props = cfg.getProperties();
-//		System.out.println("Properties:\r\n" + DPPropertiesUtils.toString(props));
 		props = new PropertiesBuilder(props).interpolate().buildProperties();
 		LOG.debug("Properties:\r\n" + DPPropertiesUtils.toString(props));
 		PropertiesValidator validator = new PropertiesValidator(cfg.getProperties(), getRequiredProperties());
@@ -55,35 +52,6 @@ public class SGWConfigGeneratorImpl extends FreemarkerConfigGenerator {
 		LOG.debug("Files local directory = " + unit.getFilesLocalDir());
 		LOG.debug("Files local wsdl directory = " + unit.getFilesLocalWsdlDir());
 		
-		// Generate AAAInfo files
-		try {
-			String aaaFilename = cfg.getProperty("aaaFileName");
-			LOG.debug("Generating AAA file " + aaaFilename);
-			File aaaMappingFile = DPFileUtils.append(unit.getFilesLocalAaaDir(), aaaFilename);
-			FileWriter aaaWriter = new FileWriter(aaaMappingFile);
-			processTemplate(TEMPLATE_AAA, cfg.getProperties(), aaaWriter);
-			aaaWriter.close();
-			LOG.debug("Done generating AAA file " + aaaFilename);
-		} catch (IOException e) {
-			throw new IllegalStateException("Caught IOException while building AAAInfo file", e);
-		} catch (TemplateException e) {
-			throw new IllegalStateException("Template processing failed for AAAInfo file", e);
-		}
-		
-		try {
-			String SBLUTBaaaFilename = cfg.getProperty("SBLUTBWPSaaaFileName");
-			LOG.debug("Generating AAA file " + SBLUTBaaaFilename);
-			File SBLUTBaaaMappingFile = DPFileUtils.append(unit.getFilesLocalAaaDir(), SBLUTBaaaFilename);
-			FileWriter SBLUTBaaaWriter = new FileWriter(SBLUTBaaaMappingFile);
-			processTemplate(TEMPLATE_AAA_SBLUTB, cfg.getProperties(), SBLUTBaaaWriter);
-			SBLUTBaaaWriter.close();
-			LOG.debug("Done generating AAA file " + SBLUTBaaaFilename);
-		} catch (IOException e) {
-			throw new IllegalStateException("Caught IOException while building AAAInfo file", e);
-		} catch (TemplateException e) {
-			throw new IllegalStateException("Template processing failed for AAAInfo file", e);
-		}
-
 		// Generate XCFG configuration
 		try {			
 			File cfgFile = DPFileUtils.append(unit.getImportConfigDir(), cfg.getConfigFilename());
