a5eb3a071709ece392b35d68e37dab12b158bc4a
ebh2812 <ebh2812@localhost>
2010-01-21 10:40:53 +0000

    Fixed SIR 158228: Feil i kode som beregner alder basert på dato og fødselsnummer.

--- int/stelvio-commons-lib-test/src/test/java/no/stelvio/common/bus/util/FnrUtilTest.java
+++ int/stelvio-commons-lib-test/src/test/java/no/stelvio/common/bus/util/FnrUtilTest.java
@@ -6,124 +6,48 @@ import junit.framework.TestCase;
 
 public class FnrUtilTest extends TestCase {
 	//	 individsifre 500-759
-	String fnr1855to1899 = "12345678901"; //01.01.1860
+	private String fnr1855to1899 = "12345678901"; //01.01.1860
 	//	 individsifre 000-499
-	String fnr1900to1999 = "12345678901"; //01.01.1930
+	private String fnr1900to1999 = "12345678901"; //01.01.1930
 	//	 individsifre 900-999
-	String fnr1940to1999 = "12345678901"; //01.01.1940
+	private String fnr1940to1999 = "12345678901"; //01.01.1940
 	//	 individsifre 500-999
-	String fnr2000to2039 = "12345678901"; //01.01.2030
+	private String fnr2000to2039 = "12345678901"; //01.01.2030
 
 	/* -----VALID D-NUMMER ----*/
 	//	add 4 to first number (day)
-	String dnr = "41018031416"; // 01.01.1980
-	String dnr2 = "71018031439"; // 31.01.1980
+	private String dnr = "41018031416"; // 01.01.1980
+	private String dnr2 = "71018031439"; // 31.01.1980
 	
 	/*----- VALID BOSTNR -----*/
 	// 	add 2 to third number
 	//	 individsifre 500-759
-	String bost1855to1899 = "01216050270"; //01.01.1860
+	private String bost1855to1899 = "01216050270"; //01.01.1860
 	//	 individsifre 000-499
-	String bost1900to1999 = "01323010192"; //01.12.1930
+	private String bost1900to1999 = "01323010192"; //01.12.1930
 	//	 individsifre 900-999
-	String bost1940to1999 = "01324090122"; //01.12.1940
+	private String bost1940to1999 = "01324090122"; //01.12.1940
 	//	 individsifre 500-999
-	String bost2000to2039 = "01213090247"; //01.01.2030
+	private String bost2000to2039 = "01213090247"; //01.01.2030
 	
 	/*---- VALID H-NUMMER ----*/
 	//	 add 4 to third number
-	String hnr1855to1899 = "01416050186"; //01.01.1860
-	String hnr1900to1999 = "01523010189"; //01.12.1930
-	String hnr1940to1999 = "01524090119"; //01.12.1940
-	String hnr2000to2039 = "01413090152"; //01.01.2030
+	private String hnr1855to1899 = "01416050186"; //01.01.1860
+	private String hnr1900to1999 = "01523010189"; //01.12.1930
+	private String hnr1940to1999 = "01524090119"; //01.12.1940
+	private String hnr2000to2039 = "01413090152"; //01.01.2030
 	
-	String fdat = "12345678901"; //19.04.2001
+	private String fdat = "12345678901"; //19.04.2001
 	
 	/* ---- INVALID FNR, H-NUMMER, BOSTNR AND D-NUMMER */
-	String invalid_dnr1 = "72016050146"; // invalid dnr
-	String invalid_dnr2 = "40013010146"; // invalid dnr
-	String invalid_hnr1 = "01406050196"; // invalid hnr
-	String invalid_hnr2 = "01533010179"; // invalid hnr
-	String invalid_bost1 = "01200502708"; // invalid bost
-	String invalid_bost2 = "01334090122"; // invalid bost
-	String invalid_fnr1= "01804731446"; // invalid bost
-	String invalid_fnr2 = "12345678901"; // invalid bost
-	
-	public void testMakeDnrOrBostnrAdjustments() 
-	{
-		String adjusted_fnr1855to1899 = FnrUtil.makeDnrOrBostnrAdjustments(fnr1855to1899);
-		String adjusted_fnr1900to1999 = FnrUtil.makeDnrOrBostnrAdjustments(fnr1900to1999);
-		String adjusted_fnr1940to1999 = FnrUtil.makeDnrOrBostnrAdjustments(fnr1940to1999);
-		String adjusted_fnr2000to2039 = FnrUtil.makeDnrOrBostnrAdjustments(fnr2000to2039);
-		
-		// we should get same fnr back
-		assertEquals(fnr1855to1899, adjusted_fnr1855to1899);
-		assertEquals(fnr1900to1999, adjusted_fnr1900to1999);
-		assertEquals(fnr1940to1999, adjusted_fnr1940to1999);
-		assertEquals(fnr2000to2039, adjusted_fnr2000to2039);
-
-		// test dnr
-		String expected_dnr = "12345678901"; //01.01.1980
-		String expected_dnr2 = "12345678901"; // 31.01.1980
-		
-		String adjusted_dnr = FnrUtil.makeDnrOrBostnrAdjustments(dnr);
-		String adjusted_dnr2 = FnrUtil.makeDnrOrBostnrAdjustments(dnr2);
-		
-		assertEquals(expected_dnr, adjusted_dnr);
-		assertEquals(expected_dnr2, adjusted_dnr2);
-		
-		// test hnr
-		String expected_hnr1855to1899 = "12345678901";
-		String expected_hnr1900to1999 = "12345678901";
-		String expected_hnr1940to1999 = "12345678901";
-		String expected_hnr2000to2039 = "12345678901";
-		
-		String adjusted_hnr1855to1899 = FnrUtil.makeDnrOrBostnrAdjustments(hnr1855to1899);
-		String adjusted_hnr1900to1999 = FnrUtil.makeDnrOrBostnrAdjustments(hnr1900to1999);
-		String adjusted_hnr1940to1999 = FnrUtil.makeDnrOrBostnrAdjustments(hnr1940to1999);
-		String adjusted_hnr2000to2039 = FnrUtil.makeDnrOrBostnrAdjustments(hnr2000to2039);
-		
-		assertEquals(expected_hnr1855to1899, adjusted_hnr1855to1899);
-		assertEquals(expected_hnr1900to1999, adjusted_hnr1900to1999);
-		assertEquals(expected_hnr1940to1999, adjusted_hnr1940to1999);
-		assertEquals(expected_hnr2000to2039, adjusted_hnr2000to2039);
-
-		// test bost
-		String expected_bost1855to1899 = "12345678901";
-		String expected_bost1900to1999 = "12345678901";
-		String expected_bost1940to1999 = "12345678901";
-		String expected_bost2000to2039 = "12345678901";
-		
-		String adjusted_bost1855to1899 = FnrUtil.makeDnrOrBostnrAdjustments(bost1855to1899);
-		String adjusted_bost1900to1999 = FnrUtil.makeDnrOrBostnrAdjustments(bost1900to1999);
-		String adjusted_bost1940to1999 = FnrUtil.makeDnrOrBostnrAdjustments(bost1940to1999);
-		String adjusted_bost2000to2039 = FnrUtil.makeDnrOrBostnrAdjustments(bost2000to2039);
-		
-		assertEquals(expected_bost1855to1899, adjusted_bost1855to1899);
-		assertEquals(expected_bost1900to1999, adjusted_bost1900to1999);
-		assertEquals(expected_bost1940to1999, adjusted_bost1940to1999);
-		assertEquals(expected_bost2000to2039, adjusted_bost2000to2039);
-		
-		// test invalid
-		String adjusted_invalid_dnr1 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_dnr1);
-		String adjusted_invalid_dnr2= FnrUtil.makeDnrOrBostnrAdjustments(invalid_dnr2);
-		String adjusted_invalid_hnr1 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_hnr1);
-		String adjusted_invalid_hnr2 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_hnr2);
-		String adjusted_invalid_bost1 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_bost1);
-		String adjusted_invalid_bost2 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_bost2);
-		String adjusted_invalid_fnr1 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_fnr1);
-		String adjusted_invalid_fnr2 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_fnr2);
-		
-		// We should get same no-valid fnr back
-		assertEquals(invalid_dnr1, adjusted_invalid_dnr1);
-		assertEquals(invalid_dnr2, adjusted_invalid_dnr2);
-		assertEquals(invalid_hnr1, adjusted_invalid_hnr1);
-		assertEquals(invalid_hnr2, adjusted_invalid_hnr2);
-		assertEquals(invalid_bost1, adjusted_invalid_bost1);
-		assertEquals(invalid_bost2, adjusted_invalid_bost2);
-		assertEquals(invalid_fnr1, adjusted_invalid_fnr1);
-		assertEquals(invalid_fnr2, adjusted_invalid_fnr2);
-	}
+	private String invalid_dnr1 = "72016050146"; // invalid dnr
+	private String invalid_dnr2 = "40013010146"; // invalid dnr
+	private String invalid_hnr1 = "01406050196"; // invalid hnr
+	private String invalid_hnr2 = "01533010179"; // invalid hnr
+	private String invalid_bost1 = "01200502708"; // invalid bost
+	private String invalid_bost2 = "01334090122"; // invalid bost
+	private String invalid_fnr1= "01804731446"; // invalid bost
+	private String invalid_fnr2 = "12345678901"; // invalid bost
 	
 	public void testIsValidFnr()
 	{	
@@ -159,108 +83,16 @@ public class FnrUtilTest extends TestCase {
 		assertFalse(FnrUtil.isValidFnr(invalid_fnr2));
 	}
 	
-	public void testGet4DigitYearOfBirth()
-	{	
-		// Test year of birth for fnr
-		String adjusted_fnr1855to1899 = FnrUtil.makeDnrOrBostnrAdjustments(fnr1855to1899); //01.01.1847
-		String adjusted_fnr1900to1999 = FnrUtil.makeDnrOrBostnrAdjustments(fnr1900to1999); //01.01.1930
-		String adjusted_fnr1940to1999 = FnrUtil.makeDnrOrBostnrAdjustments(fnr1940to1999); //01.01.1940
-		String adjusted_fnr2000to2039 = FnrUtil.makeDnrOrBostnrAdjustments(fnr2000to2039); //01.01.2030
-		
-		int fnr_aar1 = FnrUtil.get4DigitYearOfBirth(adjusted_fnr1855to1899);
-		int fnr_aar2 = FnrUtil.get4DigitYearOfBirth(adjusted_fnr1900to1999);
-		int fnr_aar3 = FnrUtil.get4DigitYearOfBirth(adjusted_fnr1940to1999);
-		int fnr_aar4 = FnrUtil.get4DigitYearOfBirth(adjusted_fnr2000to2039);
-		
-		assertEquals(1860, fnr_aar1);
-		assertEquals(1930, fnr_aar2);
-		assertEquals(1940, fnr_aar3);
-		assertEquals(2030, fnr_aar4);
-		
-		// Test year of birth for dnr
-		String adjusted_dnr1= FnrUtil.makeDnrOrBostnrAdjustments(dnr); //01.01.1980
-		String adjusted_dnr2 = FnrUtil.makeDnrOrBostnrAdjustments(dnr2); //31.01.1980
-		
-		int dnr1_aar1 = FnrUtil.get4DigitYearOfBirth(adjusted_dnr1);
-		int dnr2_aar2 = FnrUtil.get4DigitYearOfBirth(adjusted_dnr2);
-		
-		assertEquals(1980, dnr1_aar1);
-		assertEquals(1980, dnr2_aar2);
-		
-		// test year of birth for bost
-		String adjusted_bost1855to1899 = FnrUtil.makeDnrOrBostnrAdjustments(bost1855to1899); //01.01.1860
-		String adjusted_bost1900to1999 = FnrUtil.makeDnrOrBostnrAdjustments(bost1900to1999); //01.01.1930
-		String adjusted_bost1940to1999 = FnrUtil.makeDnrOrBostnrAdjustments(bost1940to1999); //01.01.1940
-		String adjusted_bost2000to2039 = FnrUtil.makeDnrOrBostnrAdjustments(bost2000to2039); //01.01.2030
-		
-		int bost_aar1 = FnrUtil.get4DigitYearOfBirth(adjusted_bost1855to1899);
-		int bost_aar2 = FnrUtil.get4DigitYearOfBirth(adjusted_bost1900to1999);
-		int bost_aar3 = FnrUtil.get4DigitYearOfBirth(adjusted_bost1940to1999);
-		int bost_aar4 = FnrUtil.get4DigitYearOfBirth(adjusted_bost2000to2039);
-		
-		assertEquals(1860, bost_aar1);
-		assertEquals(1930, bost_aar2);
-		assertEquals(1940, bost_aar3);
-		assertEquals(2030, bost_aar4);
-
-		// Test year of birth for hnr
-		String adjusted_hnr1855to1899 = FnrUtil.makeDnrOrBostnrAdjustments(hnr1855to1899); //01.01.1860
-		String adjusted_hnr1900to1999 = FnrUtil.makeDnrOrBostnrAdjustments(hnr1900to1999); //01.01.1930
-		String adjusted_hnr1940to1999 = FnrUtil.makeDnrOrBostnrAdjustments(hnr1940to1999); //01.01.1940
-		String adjusted_hnr2000to2039 = FnrUtil.makeDnrOrBostnrAdjustments(hnr2000to2039); //01.01.2030
-		
-		int hnr_aar1 = FnrUtil.get4DigitYearOfBirth(adjusted_hnr1855to1899);
-		int hnr_aar2 = FnrUtil.get4DigitYearOfBirth(adjusted_hnr1900to1999);
-		int hnr_aar3 = FnrUtil.get4DigitYearOfBirth(adjusted_hnr1940to1999);
-		int hnr_aar4 = FnrUtil.get4DigitYearOfBirth(adjusted_hnr2000to2039);
-		
-		assertEquals(1860, hnr_aar1);
-		assertEquals(1930, hnr_aar2);
-		assertEquals(1940, hnr_aar3);
-		assertEquals(2030, hnr_aar4);
-		
-		// Test year of birth for invalid fnr, hnr, dnr and bostnr
-		String adjusted_invalid_dnr1 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_dnr1);
-		String adjusted_invalid_dnr2 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_dnr2);
-		String adjusted_invalid_hnr1 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_hnr1);
-		String adjusted_invalid_hnr2 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_hnr2);
-		
-		String adjusted_invalid_bost1 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_bost1);
-		String adjusted_invalid_bost2 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_bost2);
-		String adjusted_invalid_fnr1 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_fnr1);
-		String adjusted_invalid_fnr2 = FnrUtil.makeDnrOrBostnrAdjustments(invalid_fnr2);
-		
-		int invalid_aar1 = FnrUtil.get4DigitYearOfBirth(adjusted_invalid_dnr1);
-		int invalid_aar2 = FnrUtil.get4DigitYearOfBirth(adjusted_invalid_dnr2);
-		int invalid_aar3 = FnrUtil.get4DigitYearOfBirth(adjusted_invalid_hnr1);
-		int invalid_aar4 = FnrUtil.get4DigitYearOfBirth(adjusted_invalid_hnr2);
-		
-		int invalid_aar5 = FnrUtil.get4DigitYearOfBirth(adjusted_invalid_bost1);
-		int invalid_aar6 = FnrUtil.get4DigitYearOfBirth(adjusted_invalid_bost2);
-		int invalid_aar7 = FnrUtil.get4DigitYearOfBirth(adjusted_invalid_fnr1);
-		int invalid_aar8 = FnrUtil.get4DigitYearOfBirth(adjusted_invalid_fnr2);
-		
-		assertEquals(1860, invalid_aar1);
-		assertEquals(1930, invalid_aar2);
-		assertEquals(1860, invalid_aar3);
-		assertEquals(1930, invalid_aar4);
-		
-		assertEquals(1905, invalid_aar5);
-		assertEquals(1940, invalid_aar6);
-		assertEquals(1947, invalid_aar7);
-		assertEquals(1930, invalid_aar8);
-	}
-	
 	public void testGetAgeAtDateForFnr()
 	{
 		Calendar atDate = Calendar.getInstance();
-		atDate.set(2008,1,1);
+		atDate.set(2008,Calendar.JANUARY,1);
 		
 		//----------- Test fnr 
 		assertEquals(148,FnrUtil.getAgeAtDateForFnr(fnr1855to1899, atDate)); //01.01.1860
 		assertEquals(78,FnrUtil.getAgeAtDateForFnr(fnr1900to1999, atDate)); //01.01.1930
 		assertEquals(68,FnrUtil.getAgeAtDateForFnr(fnr1940to1999, atDate)); //01.01.1940
-		assertEquals(-1,FnrUtil.getAgeAtDateForFnr(fnr2000to2039, atDate)); //01.01.2030, should return -1 since birth date is in the future
+		assertEquals(0,FnrUtil.getAgeAtDateForFnr(fnr2000to2039, atDate)); //01.01.2030, should return -1 since birth date is in the future
 		
 		// ----------- Test dnr
 		assertEquals(28,FnrUtil.getAgeAtDateForFnr(dnr, atDate)); //01.01.1980
@@ -270,12 +102,12 @@ public class FnrUtilTest extends TestCase {
 		assertEquals(148,FnrUtil.getAgeAtDateForFnr(bost1855to1899, atDate)); //01.01.1860
 		assertEquals(77,FnrUtil.getAgeAtDateForFnr(bost1900to1999, atDate)); // 01.12.1930, 77 since atDate = 01.01.2008
 		assertEquals(67,FnrUtil.getAgeAtDateForFnr(bost1940to1999, atDate)); //01.12.1940, 67 since atDate = 01.01.2008
-		assertEquals(-1,FnrUtil.getAgeAtDateForFnr(bost2000to2039, atDate)); //01.01.2030
+		assertEquals(0,FnrUtil.getAgeAtDateForFnr(bost2000to2039, atDate)); //01.01.2030
 			
 		assertEquals(148,FnrUtil.getAgeAtDateForFnr(hnr1855to1899, atDate)); //01.01.1860
 		assertEquals(77,FnrUtil.getAgeAtDateForFnr(hnr1900to1999, atDate)); //01.12.1930
 		assertEquals(67,FnrUtil.getAgeAtDateForFnr(hnr1940to1999, atDate)); //01.12.1940
-		assertEquals(-1,FnrUtil.getAgeAtDateForFnr(hnr2000to2039, atDate)); //01.01.2030
+		assertEquals(0,FnrUtil.getAgeAtDateForFnr(hnr2000to2039, atDate)); //01.01.2030
 		
 		//------------ Test with invalid fnr
 		assertEquals(-1,FnrUtil.getAgeAtDateForFnr(invalid_dnr1, atDate));
@@ -289,7 +121,7 @@ public class FnrUtilTest extends TestCase {
 		
 		//------------- Test other
 		Calendar atDate2 = Calendar.getInstance();
-		atDate2.set(2008,5,7); // 07.05.2008
+		atDate2.set(2008,Calendar.MAY,7); // 07.05.2008
 		
 		String fnr1 = "12345678901"; //08.05.1860
 		String fnr2 = "12345678901"; //07.05.1860
--- int/stelvio-commons-lib/no/stelvio/common/bus/util/FnrUtil.java
+++ int/stelvio-commons-lib/no/stelvio/common/bus/util/FnrUtil.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 import no.stelvio.domain.person.Pid;
+import no.stelvio.domain.person.PidValidationException;
 
 
 /**
@@ -81,122 +82,6 @@ public final class FnrUtil{
 	public static boolean isWhitespaceCompliant(String pid) {
 		return Pid.isWhitespaceCompliant(pid);
 	}
-
-	/**
-	 * Adjusts DNR, HNR and BostNr so that the first 6 numbers represents a valid date. In the case where DNR or BostNr is the input,
-	 * the return value will fail a modulus 11 check.
-	 * 
-	 * @param value a personal identification number
-	 * @return the inparam if it wasn't a DNR or BostNr, otherwise the BostNr/DNR/HNR where the 6 first digits can be converted to a
-	 * valid date
-	 */
-	protected static String makeDnrOrBostnrAdjustments(String value) {
-		if (StringUtils.isBlank(value)) {
-			return value;
-		}
-
-		// fnr format will be <DDMMAAXXXYY>
-		int day = Integer.parseInt(value.substring(0, 2));
-		int month = Integer.parseInt(value.substring(2, 4));
-
-		// DNR adjustment
-		if (day > 40 && day <= 71) {
-			day -= 40;
-			StringBuffer fnr = new StringBuffer(value);
-
-			if (day < 10) {
-				fnr.replace(0, 2, "0" + Integer.toString(day));
-			} else {
-				fnr.replace(0, 2, Integer.toString(day));
-			}
-
-			return fnr.toString();
-		} else if (month > 20 && month <= 32) {
-			// BOST adjustments
-			month -= 20;
-			StringBuffer fnr = new StringBuffer(value);
-
-			if (month < 10) {
-				fnr.replace(2, 4, "0" + Integer.toString(month));
-			} else {
-				fnr.replace(2, 4, Integer.toString(month));
-			}
-
-			return fnr.toString();
-		} else if(month > 40 && month <=52 ) {
-			// H-Nummer adjustment
-			month -= 40;
-			StringBuffer fnr = new StringBuffer(value);
-
-			if (month < 10) {
-				fnr.replace(2, 4, "0" + Integer.toString(month));
-			} else {
-				fnr.replace(2, 4, Integer.toString(month));
-			}
-
-			return fnr.toString();
-		}
-		
-
-		// value was neither bostnr nor dnr nor hnr
-		return value;
-	}
-
-	/**
-	 * Returns a 4-digit birth date
-	 * 
-	 * @param dnrOrBnrAdjustedFnr a fnr, adjusted if it's a bnr or dnr
-	 * @return 4 digit birth date, -1 if invalid
-	 */
-	protected static int get4DigitYearOfBirth(String dnrOrBnrAdjustedFnr) {
-		int year = Integer.parseInt(dnrOrBnrAdjustedFnr.substring(4, 6));
-		int individnr = Integer.parseInt(dnrOrBnrAdjustedFnr.substring(6, 9));
-		// stilborn baby (dødfødt barn)
-		if (Integer.parseInt(dnrOrBnrAdjustedFnr.substring(6)) < 10) {
-			year += 2000;
-		} else {
-			// recalculating year
-			if (individnr < 500) {
-				year += 1900;
-			} else if ((individnr < 750) && (54 < year)) {
-				year += 1800;
-			} else if ((individnr < 1000) && (year < 40)) {
-				year += 2000;
-			} else if ((900 <= individnr) && (individnr < 1000) && (39 < year)) {
-				year += 1900;
-			} else {
-				// invalid fnr
-				return -1;
-			}
-		}
-		return year;
-	}
-	
-	/**
-	 * Returns day of birth
-	 * 
-	 * @param dnrOrBnrAdjustedFnr a fnr, adjusted if it's a bnr, dnr or hnr
-	 * @return 2 digit day of date
-	 */
-	protected static int getDayOfBirth(String dnrOrBnrAdjustedFnr)
-	{
-		// dnrOrBnrAdjustedFnr format will be ddmmyyiiicc
-		int day = Integer.parseInt(dnrOrBnrAdjustedFnr.substring(0, 2));
-		return day;
-	}
-	
-	/**
-	 * Returns month of birth
-	 * 
-	 * @param dnrOrBnrAdjustedFnr a fnr, adjusted if it's a bnr, dnr or hnr
-	 * @return 2 digit month of date
-	 */
-	protected static int getMonthOfBirth(String dnrOrBnrAdjustedFnr)
-	{
-		// dnrOrBnrAdjustedFnr format will be ddmmyyiiicc
-		int month = Integer.parseInt(dnrOrBnrAdjustedFnr.substring(2, 4));
-		return month;
-	}
 	
 	/**
 	 * Returns age of the person at certain date
@@ -207,33 +92,11 @@ public final class FnrUtil{
 	 */
 	public static int getAgeAtDateForFnr(String fnr, Calendar atDate)
 	{
-		// needs to validate first because get4DigitYearOfBirth returns year of birth even if the pid is not valid
-		boolean valid = isValidFnr(fnr);
-		int age = -1;
-		if( valid )
-		{
-			String adjustedFnr = makeDnrOrBostnrAdjustments(fnr);
-			
-			int yearOfBirth = get4DigitYearOfBirth(adjustedFnr);
-			int monthOfBirth = getMonthOfBirth(adjustedFnr);
-			int dayOfBirth = getDayOfBirth(adjustedFnr);
-
-			int atYear = atDate.get(Calendar.YEAR);
-			int atMonth = atDate.get(Calendar.MONTH);
-			int atDay = atDate.get(Calendar.DAY_OF_MONTH);
-			
-			age = atYear - yearOfBirth;
-			
-			/* Has the persons month and day of birth passed. */
-			if ((monthOfBirth > atMonth) || (monthOfBirth == atMonth && dayOfBirth > atDay))
-				age--; // day of birth not yet passed
-			
-			/* Age can not be less than 0 years. */
-			if (age < 0) 
-				age = -1;
+		try {
+			return Pid.calculateAge(new Pid(fnr, true), atDate.getTime());
+		} catch (PidValidationException e) {
+			return -1;
 		}
-		
-		return age;
 	}
 	
 	/**
@@ -243,8 +106,10 @@ public final class FnrUtil{
 	 * @return birthdate for given fnr 
 	 */
 	public static Date getBirthdateForFnr(String fnr) {
-		if(fnr == null || fnr.length()<6) return null;
-		
-		return DateUtil.parseInputString(makeDnrOrBostnrAdjustments(fnr).substring(0,6), false);
+		try {
+			return new Pid(fnr, true).getFodselsdato();
+		} catch (PidValidationException e) {
+			return null;
+		}
 	}
 }
