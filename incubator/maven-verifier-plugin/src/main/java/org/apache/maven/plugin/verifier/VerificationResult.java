57d502b7508f8525533083453d7595c611577975
oga2812 <oga2812@localhost>
2010-01-26 06:52:53 +0000

    Add maven-verifier-plugin from Apache to make customized version

---
+++ incubator/maven-verifier-plugin/src/main/java/org/apache/maven/plugin/verifier/VerificationResult.java
@@ -0,0 +1,70 @@
+package org.apache.maven.plugin.verifier;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.plugin.verifier.model.File;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VerificationResult
+{
+    private List existenceFailures = new ArrayList();
+
+    private List nonExistenceFailures = new ArrayList();
+
+    private List contentFailures = new ArrayList();
+
+    public void addExistenceFailure( File file )
+    {
+        existenceFailures.add( file );
+    }
+
+    public void addNonExistenceFailure( File file )
+    {
+        nonExistenceFailures.add( file );
+    }
+
+    public void addContentFailure( File file )
+    {
+        contentFailures.add( file );
+    }
+
+    public List getExistenceFailures()
+    {
+        return existenceFailures;
+    }
+
+    public List getNonExistenceFailures()
+    {
+        return nonExistenceFailures;
+    }
+
+    public List getContentFailures()
+    {
+        return contentFailures;
+    }
+
+    public boolean hasFailures()
+    {
+        return !getExistenceFailures().isEmpty() || !getNonExistenceFailures().isEmpty()
+            || !getContentFailures().isEmpty();
+    }
+}
