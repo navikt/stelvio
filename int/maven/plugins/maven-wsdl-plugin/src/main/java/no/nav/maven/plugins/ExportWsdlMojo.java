b2c9281325f0970379021b961a1a1561df5d2b8a
ebh2812 <ebh2812@localhost>
2010-01-29 11:52:39 +0000

    Reimplemented ExportWsdlMojo to work with WPS 7

--- int/maven/plugins/maven-wsdl-plugin/src/main/java/no/nav/maven/plugins/ExportWsdlMojo.java
+++ int/maven/plugins/maven-wsdl-plugin/src/main/java/no/nav/maven/plugins/ExportWsdlMojo.java
@@ -17,47 +17,74 @@ package no.nav.maven.plugins;
  */
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
+import javax.wsdl.Definition;
+import javax.wsdl.Import;
+import javax.wsdl.Types;
+import javax.wsdl.WSDLException;
+import javax.wsdl.extensions.ExtensibilityElement;
+import javax.wsdl.extensions.schema.Schema;
+import javax.wsdl.extensions.schema.SchemaImport;
+import javax.wsdl.extensions.schema.SchemaReference;
+import javax.wsdl.factory.WSDLFactory;
+import javax.wsdl.xml.WSDLReader;
+import javax.xml.namespace.QName;
+
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.handler.ArtifactHandler;
 import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
+import org.apache.maven.project.path.PathTranslator;
+import org.apache.maven.shared.filtering.MavenFilteringException;
+import org.apache.maven.shared.filtering.MavenResourcesExecution;
+import org.apache.maven.shared.filtering.MavenResourcesFiltering;
 import org.codehaus.plexus.archiver.Archiver;
 import org.codehaus.plexus.archiver.ArchiverException;
 import org.codehaus.plexus.archiver.UnArchiver;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.StringUtils;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+import org.jdom.xpath.XPath;
 
 /**
  * Goal which touches a timestamp file.
  * 
  * @goal exportwsdl
- * 
- * @phase package
+ * @requiresDependencyResolution compile
  */
+@SuppressWarnings("unchecked")
 public class ExportWsdlMojo extends AbstractMojo {
-	private static final String ZIP_SUFFIX = "zip";
-
-	private static final String WAR_SUFFIX = "war";
-
-	private static final String WSDL_PATH_IN_WAR = "/META-INF/wsdl";
-
-	private static final String WSDL_INTERFACE_ARTIFACT_TYPE = "wsdl-interface";
-
-	/**
-	 * @parameter expression="${project}"
-	 * @required
-	 * @readonly
-	 */
-	private MavenProject project;
+	private static final String TYPE_WSDL_INTERFACE = "wsdl-interface";
+	private static final String TYPE_WPS_LIBRARY_JAR = "wps-library-jar";
 
 	/**
 	 * @component
 	 */
 	private MavenProjectHelper projectHelper;
+
+	/**
+	 * @component roleHint="zip"
+	 * @required
+	 * @readonly
+	 */
+	private UnArchiver unArchiver;
 
 	/**
 	 * @component roleHint="zip"
@@ -77,80 +104,241 @@ public class ExportWsdlMojo extends AbstractMojo {
 	private ArtifactHandlerManager artifactHandlerManager;
 
 	/**
-	 * @component roleHint="zip"
+	 * @component
+	 */
+	private MavenResourcesFiltering mavenResourcesFiltering;
+
+	/**
+	 * @component
+	 */
+	private PathTranslator pathTranslator;
+
+	/**
+	 * @parameter expression="${project}"
 	 * @required
 	 * @readonly
 	 */
-	private UnArchiver unArchiver;
+	private MavenProject project;
 
 	/**
-	 * EAR-file.
-	 * 
-	 * @parameter expression="${project.build.directory}/${project.build.finalName}.${project.artifact.artifactHandler.extension}"
+	 * @parameter expression="${session}"
+	 * @required
+	 * @readonly
 	 */
-	private File earFile;
+	private MavenSession session;
 
-	public void execute() throws MojoExecutionException {
+	/**
+	 * @parameter default-value="${project.build.sourceEncoding}"
+	 */
+	private String encoding;
+	
+	/**
+	 * @parameter default-value="false"
+	 */
+	private boolean verbose;
+
+	private SAXBuilder saxBuilder;
+	private XPath webServiceExportXPath;
+	private WSDLReader wsdlReader;
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
 		String packaging = project.getPackaging();
 		// This plugin is only applicable to WPS Module artifacts
 		if ("wps-module-ear".equals(packaging)) {
+			init();
 			executeInternal();
 		} else {
 			getLog().debug("Skipping exportwsdl because packaging is " + packaging + " and not wps-module-ear.");
 		}
 	}
 
-	private void executeInternal() throws MojoExecutionException {
+	private void init() throws MojoExecutionException {
 		try {
-			// TODO: The following must be done because of one (or more) bug(s)
-			// in Maven. See Maven bugs MNG-3506 and MNG-2426 for more info.
-			if (!wsdlInterfaceArtifactHandler.equals(artifactHandlerManager.getArtifactHandler(WSDL_INTERFACE_ARTIFACT_TYPE))) {
-				getLog().debug("Adding wsdlif interchange artifact handler to artifact handler manager");
-				artifactHandlerManager.addHandlers(Collections.singletonMap(WSDL_INTERFACE_ARTIFACT_TYPE,
-						wsdlInterfaceArtifactHandler));
-			}
-
-			File tempDir = new File(project.getBuild().getDirectory(), "wsdltemp");
-			tempDir.mkdir();
-
-			File tempEarFileDir = extractFile(earFile, tempDir);
-			File[] warFiles = tempEarFileDir.listFiles(new FilenameFilter() {
-				public boolean accept(File dir, String name) {
-					return name.endsWith(WAR_SUFFIX);
-				}
-			});
-
-			// Does this module have WSDL exports?
-			if (warFiles.length != 0) {
-				File warFile = warFiles[0];
-
-				File tempWarFileDir = extractFile(warFile, tempDir);
-
-				File wsdlDirectory = new File(tempWarFileDir, WSDL_PATH_IN_WAR);
-
-				File wsdlZipArtifactFile = new File(project.getBuild().getDirectory(), project.getBuild().getFinalName() + "-"
-						+ wsdlInterfaceArtifactHandler.getClassifier() + "." + ZIP_SUFFIX);
-				archiver.setDestFile(wsdlZipArtifactFile);
-
-				archiver.addDirectory(wsdlDirectory);
-				archiver.createArchive();
-				projectHelper.attachArtifact(project, WSDL_INTERFACE_ARTIFACT_TYPE, wsdlInterfaceArtifactHandler
-						.getClassifier(), wsdlZipArtifactFile);
-			}
-		} catch (Exception e) {
-			throw new MojoExecutionException("Creating wsdl export file failed ", e);
+			saxBuilder = new SAXBuilder();
+			webServiceExportXPath = XPath.newInstance("//esbBinding[@xsi:type='webservice:WebServiceExportBinding']");
+			wsdlReader = WSDLFactory.newInstance().newWSDLReader();
+			wsdlReader.setFeature("javax.wsdl.verbose", verbose);
+			wsdlReader.setFeature("javax.wsdl.importDocuments", false);
+		} catch (JDOMException e) {
+			throw new MojoExecutionException("Error initalizing XML processing framework", e);
+		} catch (WSDLException e) {
+			throw new MojoExecutionException("Error initalizing WSDL framework", e);
 		}
 	}
 
-	private File extractFile(File file, File parentDirectory) throws IOException, ArchiverException {
-		File extractDir = new File(parentDirectory, file.getName());
-		extractDir.delete();
-		extractDir.mkdir();
+	private void executeInternal() throws MojoExecutionException, MojoFailureException {
+		try {
+			// TODO: The following must be done because of one (or more) bug(s)
+			// in Maven. See Maven bugs MNG-3506 and MNG-2426 for more info.
+			if (!wsdlInterfaceArtifactHandler.equals(artifactHandlerManager.getArtifactHandler(TYPE_WSDL_INTERFACE))) {
+				getLog().debug("Adding wsdlif interchange artifact handler to artifact handler manager");
+				artifactHandlerManager.addHandlers(Collections.singletonMap(TYPE_WSDL_INTERFACE, wsdlInterfaceArtifactHandler));
+			}
 
-		unArchiver.setSourceFile(file);
-		unArchiver.setDestDirectory(extractDir);
-		unArchiver.extract();
+			Collection<QName> exportedWebServices = getExportedWebServices();
 
-		return extractDir;
+			if (!exportedWebServices.isEmpty()) {
+				File workingDir = createWorkingDir();
+
+				extractDependendentResources(workingDir);
+				copyProjectResources(workingDir);
+
+				Collection<String> documentUris = new HashSet();
+
+				Map<QName, Definition> webServiceToWsdlMap = getWebServiceToWsdlMap(workingDir);
+				for (QName exportedWebService : exportedWebServices) {
+					Definition wsdl = webServiceToWsdlMap.get(exportedWebService);
+					if (wsdl == null) {
+						throw new MojoFailureException("WSDL for exported web service " + exportedWebService + " not found.");
+					}
+					addFilesForWsdl(documentUris, wsdl);
+				}
+
+				Collection<File> files = new ArrayList<File>(documentUris.size());
+				for (String documentUri : documentUris) {
+					// TODO: WSDL4J generates invalid URIs (with whitespace)
+					// that will have to be escaped
+					URI uri = URI.create(documentUri.replace(" ", "%20"));
+					files.add(new File(uri));
+				}
+
+				createArchive(workingDir, files);
+			}
+		} catch (IOException e) {
+			throw new MojoExecutionException("Error accessing file system", e);
+		} catch (JDOMException e) {
+			throw new MojoExecutionException("Error processing XML", e);
+		} catch (ArchiverException e) {
+			throw new MojoExecutionException("Error accessing archive", e);
+		} catch (MavenFilteringException e) {
+			throw new MojoExecutionException("Error filtering resources", e);
+		} catch (WSDLException e) {
+			throw new MojoExecutionException("Error processing WSDL", e);
+		}
+
+	}
+
+	private void addFilesForWsdl(Collection<String> documentUris, Definition wsdl) throws WSDLException {
+		String documentBaseURI = wsdl.getDocumentBaseURI();
+		documentUris.add(documentBaseURI);
+
+		for (Collection<Import> imports : (Collection<Collection<Import>>) wsdl.getImports().values()) {
+			for (Import _import : imports) {
+				Definition importedWsdl = wsdlReader.readWSDL(documentBaseURI,_import.getLocationURI());
+				addFilesForWsdl(documentUris, importedWsdl);
+			}
+		}
+
+		Types types = wsdl.getTypes();
+		if (types != null) {
+			for (ExtensibilityElement extensibilityElement : (Collection<ExtensibilityElement>) types
+					.getExtensibilityElements()) {
+				if (extensibilityElement instanceof Schema) {
+					Schema schema = (Schema) extensibilityElement;
+					for (Collection<SchemaImport> schemaImports : (Collection<Collection<SchemaImport>>) schema.getImports()
+							.values()) {
+						for (SchemaImport schemaImport : schemaImports) {
+							documentUris.add(schemaImport.getReferencedSchema().getDocumentBaseURI());
+						}
+					}
+					for (SchemaReference schemaInclude : (Collection<SchemaReference>) schema.getIncludes()) {
+						documentUris.add(schemaInclude.getReferencedSchema().getDocumentBaseURI());
+					}
+				}
+			}
+		}
+	}
+
+	private Map<QName, Definition> getWebServiceToWsdlMap(File workingDir) throws IOException, WSDLException,
+			MojoFailureException {
+		Map<QName, Definition> webServiceToWsdlMap = new HashMap<QName, Definition>();
+
+		Collection<File> wsdlFiles = FileUtils.getFiles(workingDir, "**/*.wsdl", null);
+		for (File wsdlFile : wsdlFiles) {
+			Definition wsdl = wsdlReader.readWSDL(wsdlFile.getAbsolutePath());
+			Collection<QName> services = wsdl.getServices().keySet();
+			for (QName service : services) {
+				Definition previousWsdl = webServiceToWsdlMap.put(service, wsdl);
+				if (previousWsdl != null) {
+					throw new MojoFailureException("Duplicate service definition for service " + service
+							+ ". Service defined in " + wsdl.getDocumentBaseURI() + ", but already defined in "
+							+ previousWsdl.getDocumentBaseURI() + ".");
+				}
+			}
+		}
+
+		return webServiceToWsdlMap;
+	}
+
+	private void extractDependendentResources(File workingDir) throws ArchiverException, IOException {
+		for (Artifact artifact : (Collection<Artifact>) project.getCompileArtifacts()) {
+			if (TYPE_WPS_LIBRARY_JAR.equals(artifact.getType())) {
+				File artifactFile = artifact.getFile();
+				unArchiver.setSourceFile(artifactFile);
+				unArchiver.setDestDirectory(workingDir);
+				unArchiver.extract();
+			}
+		}
+	}
+
+	private void copyProjectResources(File workingDir) throws MavenFilteringException {
+		MavenResourcesExecution mavenResourcesExecution = new MavenResourcesExecution();
+		mavenResourcesExecution.setMavenProject(project);
+		mavenResourcesExecution.setMavenSession(session);
+		mavenResourcesExecution.setResources(project.getResources());
+		mavenResourcesExecution.setInjectProjectBuildFilters(true);
+		mavenResourcesExecution.setOutputDirectory(workingDir);
+		mavenResourcesExecution.setEncoding(encoding);
+
+		mavenResourcesFiltering.filterResources(mavenResourcesExecution);
+	}
+
+	private File createWorkingDir() throws IOException {
+		File tempDir = new File(project.getBuild().getDirectory(), "wsdltemp");
+		if (tempDir.exists()) {
+			if (tempDir.isDirectory()) {
+				FileUtils.deleteDirectory(tempDir);
+			} else {
+				tempDir.delete();
+			}
+		}
+		tempDir.mkdir();
+		return tempDir;
+	}
+
+	private void createArchive(File baseDir, Collection<File> files) throws ArchiverException, IOException {
+		File wsdlZipArtifactFile = new File(project.getBuild().getDirectory(), project.getBuild().getFinalName() + "-"
+				+ wsdlInterfaceArtifactHandler.getClassifier() + "." + wsdlInterfaceArtifactHandler.getExtension());
+		archiver.setDestFile(wsdlZipArtifactFile);
+
+		for (File file : files) {
+			String destFileName = pathTranslator.unalignFromBaseDirectory(file.getAbsolutePath(), baseDir);
+			archiver.addFile(file, destFileName);
+		}
+
+		archiver.createArchive();
+		projectHelper.attachArtifact(project, TYPE_WSDL_INTERFACE, wsdlInterfaceArtifactHandler.getClassifier(),
+				wsdlZipArtifactFile);
+	}
+
+	private Collection<QName> getExportedWebServices() throws IOException, JDOMException {
+		Collection<QName> exportedWebServices = new HashSet<QName>();
+
+		Collection<File> exportFiles = FileUtils.getFiles(project.getBasedir(), "**/*.export", null);
+		for (File exportFile : exportFiles) {
+			Document exportDocument = saxBuilder.build(exportFile);
+			Collection<Element> webServiceBindingElements = webServiceExportXPath.selectNodes(exportDocument);
+			for (Element webServiceBindingElement : webServiceBindingElements) {
+				String exportedWebServiceAttributeValue = webServiceBindingElement.getAttributeValue("service");
+				String[] exportedWebServiceAttributeValueParts = StringUtils.split(exportedWebServiceAttributeValue, ":");
+				String exportedWebServicePrefix = exportedWebServiceAttributeValueParts[0];
+				Namespace exportedWebServiceNamespace = webServiceBindingElement.getNamespace(exportedWebServicePrefix);
+				String exportedWebServiceName = exportedWebServiceAttributeValueParts[1];
+				QName exportedWebService = new QName(exportedWebServiceNamespace.getURI(), exportedWebServiceName);
+				exportedWebServices.add(exportedWebService);
+			}
+		}
+
+		return exportedWebServices;
 	}
 }
