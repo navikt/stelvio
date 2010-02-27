713a83cc1444210ccbc6394b07849572a6e81b16
ebh2812 <ebh2812@localhost>
2010-02-27 14:16:03 +0000

    Total rewrite of the service deploy assembly mojo

--- int/maven/plugins/maven-service-deploy-plugin/src/main/java/no/nav/maven/plugin/sca/ServiceDeployAssemblyMojo.java
+++ int/maven/plugins/maven-service-deploy-plugin/src/main/java/no/nav/maven/plugin/sca/ServiceDeployAssemblyMojo.java
@@ -1,21 +1,44 @@
 package no.nav.maven.plugin.sca;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.jar.Attributes;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
 
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.ArtifactUtils;
 import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
+import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.resolver.AbstractArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactCollector;
 import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.artifact.resolver.filter.ScopeArtifactFilter;
 import org.apache.maven.model.Build;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.dependency.tree.DependencyNode;
+import org.apache.maven.shared.dependency.tree.DependencyTreeBuilder;
+import org.apache.maven.shared.dependency.tree.DependencyTreeBuilderException;
+import org.apache.maven.shared.dependency.tree.filter.ArtifactDependencyNodeFilter;
+import org.apache.maven.shared.dependency.tree.traversal.DependencyNodeVisitor;
+import org.apache.maven.shared.dependency.tree.traversal.FilteringDependencyNodeVisitor;
 import org.codehaus.plexus.archiver.Archiver;
 import org.codehaus.plexus.archiver.ArchiverException;
 
@@ -28,6 +51,8 @@ import org.codehaus.plexus.archiver.ArchiverException;
  * @requiresDependencyResolution
  */
 public class ServiceDeployAssemblyMojo extends AbstractMojo {
+	private static final String JAR_CLASSIFIER = "jar";
+
 	/**
 	 * @component
 	 */
@@ -41,12 +66,20 @@ public class ServiceDeployAssemblyMojo extends AbstractMojo {
 	/**
 	 * @component
 	 */
-	private ArtifactHandlerManager artifactHandlerManager;
+	private ArtifactMetadataSource artifactMetadataSource;
+
+	/**
+	 * @component
+	 */
+	private ArtifactCollector artifactCollector;
+
+	/**
+	 * @component
+	 */
+	private DependencyTreeBuilder dependencyTreeBuilder;
 
 	/**
 	 * @component roleHint="zip"
-	 * @required
-	 * @readonly
 	 */
 	private Archiver archiver;
 
@@ -65,103 +98,40 @@ public class ServiceDeployAssemblyMojo extends AbstractMojo {
 	private ArtifactRepository localRepository;
 
 	/**
-	 * Defines the assembly type to use. Valid values are zip [default] and pi (project interchange).
+	 * Defines the service deploy assembly type to use. Only zip is supported.
 	 * 
-	 * @parameter default-value="zip"
+	 * @parameter
+	 * @deprecated Parameter is ignored. Only zip is supported.
 	 */
+	@SuppressWarnings("unused")
+	// TODO: Delete variable when all usage is removed.
 	private String assemblyType;
 
-	private Map<String, MavenProject> projectReferences;
+	private ProjectArtifactResolver projectArtifactResolver = new ProjectArtifactResolver();
+
+	private ProjectDependencyResolver projectDependencyResolver = new ProjectDependencyResolver();
+
+	private ServiceDeployAssemblyBuilder serviceDeployAssemblyBuilder = new ServiceDeployAssemblyBuilder();
 
 	/**
 	 * {@inheritDoc}
 	 */
-	public void execute() throws MojoExecutionException {
-		try {
-			ServiceDeployAssembly serviceDeployAssembly = getServiceDeployAssembly();
-
-			Build build = project.getBuild();
-			File outputFile = new File(build.getDirectory(), build.getFinalName() + "-sd" + ".zip");
-			archiver.setDestFile(outputFile);
-
-			Collection<Artifact> artifacts = serviceDeployAssembly.getArtifacts(project);
-			for (Artifact artifact : artifacts) {
-				File artifactFile = artifact.getFile();
-				if (artifactFile == null) {
-					artifact = resolveArtifact(artifact);
-				}
-			}
-			serviceDeployAssembly.addArtifacts(project, archiver, artifacts);
-
-			archiver.createArchive();
-		} catch (ArchiverException e) {
-			throw new MojoExecutionException("Error creating service deploy assembly", e);
-		} catch (IOException e) {
-			throw new MojoExecutionException("Error creating service deploy assembly", e);
-		}
-	}
-
-	private ServiceDeployAssembly getServiceDeployAssembly() throws MojoExecutionException {
-		if ("zip".equals(assemblyType)) {
-			return new ZipServiceDeployAssembly();
-		} else if ("pi".equals(assemblyType)) {
-			return new ProjectInterchangeServiceDeployAssembly(artifactFactory, artifactHandlerManager);
-		} else {
-			throw new MojoExecutionException("Illegal assembly type (" + assemblyType + "). Must be one of [zip,pi].");
-		}
-	}
-
-	private Artifact resolveArtifact(Artifact artifact) throws MojoExecutionException {
-		// Trying to resolve artifact from project references first. This will
-		// work if the artifact to resolve is built as part of this
-		// (multi-module) build.
-		Artifact resolvedArtifact = resolveArtifactFromProjectReferences(artifact);
-		if (resolvedArtifact != null) {
-			return resolvedArtifact;
-		}
-		return resolveArtifactFromRepositories(artifact);
-	}
-
 	@SuppressWarnings("unchecked")
-	private Artifact resolveArtifactFromProjectReferences(Artifact artifact) throws MojoExecutionException {
-		MavenProject projectReference = getProjectReference(artifact);
-		if (projectReference != null) {
-			getLog().info("Matching project reference found: " + projectReference.getBasedir());
-			Artifact projectReferenceArtifact = projectReference.getArtifact();
-			if (projectReferenceArtifact.equals(artifact)) {
-				getLog().debug("Matching artifact found in project reference: " + projectReferenceArtifact);
-				return projectReferenceArtifact;
+	public void execute() throws MojoExecutionException {
+		Collection<Artifact> artifacts = new ArrayList<Artifact>();
+		for (Artifact artifact : (Collection<Artifact>) project.getAttachedArtifacts()) {
+			if (JAR_CLASSIFIER.equals(artifact.getClassifier())) {
+				artifacts.add(artifact);
+				break;
 			}
-			for (Artifact attachedArtifact : (Collection<Artifact>) projectReference.getAttachedArtifacts()) {
-				if (attachedArtifact.equals(artifact)) {
-					getLog().debug("Matching artifact found in project reference: " + projectReferenceArtifact);
-					return attachedArtifact;
-				}
-			}
-			throw new MojoExecutionException(
-					"Matching project reference found, but a matching artifact was not found in matching project reference.");
-		} else {
-			return null;
 		}
-	}
+		for (Artifact artifact : (Collection<Artifact>) project.getRuntimeArtifacts()) {
+			artifacts.add(artifact);
+		}
 
-	private Artifact resolveArtifactFromRepositories(Artifact artifact) throws MojoExecutionException {
-		try {
-			artifactResolver.resolve(artifact, project.getRemoteArtifactRepositories(), localRepository);
-			return artifact;
-		} catch (AbstractArtifactResolutionException e) {
-			throw new MojoExecutionException("Error resolving artifact", e);
-		}
-	}
-
-	private MavenProject getProjectReference(Artifact artifact) {
-		if (projectReferences == null) {
-			projectReferences = new HashMap<String, MavenProject>();
-			addProjectReferences(projectReferences, project);
-		}
-		String projectReferenceId = getProjectReferenceId(artifact.getGroupId(), artifact.getArtifactId(), artifact
-				.getVersion());
-		return projectReferences.get(projectReferenceId);
+		projectArtifactResolver.resolveArtifacts(artifacts);
+		Map<Artifact, Set<Artifact>> artifactDependencyMap = projectDependencyResolver.getArtifactDependencyMap(artifacts);
+		serviceDeployAssemblyBuilder.createAssembly(artifactDependencyMap);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -176,5 +146,211 @@ public class ServiceDeployAssemblyMojo extends AbstractMojo {
 
 	private static String getProjectReferenceId(String groupId, String artifactId, String version) {
 		return groupId + ":" + artifactId + ":" + version;
+	}
+
+	private class ProjectArtifactResolver {
+		public void resolveArtifacts(Collection<Artifact> artifacts) throws MojoExecutionException {
+			Map<String, MavenProject> projectReferences = new LinkedHashMap<String, MavenProject>();
+			addProjectReferences(projectReferences, project);
+
+			for (Artifact artifact : artifacts) {
+				File artifactFile = artifact.getFile();
+				if (artifactFile == null) {
+					artifact = resolveArtifact(artifact, projectReferences);
+				}
+			}
+		}
+
+		private Artifact resolveArtifact(Artifact artifact, Map<String, MavenProject> projectReferences)
+				throws MojoExecutionException {
+			// Trying to resolve artifact from project references first. This will
+			// work if the artifact to resolve is built as part of this
+			// (multi-module) build.
+			Artifact resolvedArtifact = resolveArtifactFromProjectReferences(artifact, projectReferences);
+			if (resolvedArtifact != null) {
+				return resolvedArtifact;
+			}
+			return resolveArtifactFromRepositories(artifact);
+		}
+
+		@SuppressWarnings("unchecked")
+		private Artifact resolveArtifactFromProjectReferences(Artifact artifact, Map<String, MavenProject> projectReferences)
+				throws MojoExecutionException {
+			String projectReferenceId = getProjectReferenceId(artifact.getGroupId(), artifact.getArtifactId(), artifact
+					.getVersion());
+			MavenProject projectReference = projectReferences.get(projectReferenceId);
+			if (projectReference != null) {
+				getLog().info("Matching project reference found: " + projectReference.getBasedir());
+				Artifact projectReferenceArtifact = projectReference.getArtifact();
+				if (projectReferenceArtifact.equals(artifact)) {
+					getLog().debug("Matching artifact found in project reference: " + projectReferenceArtifact);
+					return projectReferenceArtifact;
+				}
+				for (Artifact attachedArtifact : (Collection<Artifact>) projectReference.getAttachedArtifacts()) {
+					if (attachedArtifact.equals(artifact)) {
+						getLog().debug("Matching artifact found in project reference: " + projectReferenceArtifact);
+						return attachedArtifact;
+					}
+				}
+				throw new MojoExecutionException(
+						"Matching project reference found, but a matching artifact was not found in matching project reference.");
+			} else {
+				return null;
+			}
+		}
+
+		private Artifact resolveArtifactFromRepositories(Artifact artifact) throws MojoExecutionException {
+			try {
+				artifactResolver.resolve(artifact, project.getRemoteArtifactRepositories(), localRepository);
+				return artifact;
+			} catch (AbstractArtifactResolutionException e) {
+				throw new MojoExecutionException("Error resolving artifact", e);
+			}
+		}
+	}
+
+	private class ProjectDependencyResolver {
+		private ScopeArtifactFilter scopeArtifactFilter = new ScopeArtifactFilter("runtime");
+
+		public Map<Artifact, Set<Artifact>> getArtifactDependencyMap(final Collection<Artifact> artifacts)
+				throws MojoExecutionException {
+			try {
+				final Map<Artifact, Set<Artifact>> artifactDependencyMap = new LinkedHashMap<Artifact, Set<Artifact>>();
+
+				DependencyNodeVisitor visitor = new DependencyNodeVisitor() {
+					private Map<String, Artifact> artifactMap;
+
+					public boolean visit(DependencyNode node) {
+						if (!node.hasChildren()) {
+							Set<Artifact> emptySet = Collections.emptySet();
+							artifactDependencyMap.put(getArtifact(node), emptySet);
+						}
+						return true;
+					}
+
+					public boolean endVisit(DependencyNode node) {
+						DependencyNode parent = node.getParent();
+						if (parent != null) {
+							Artifact parentArtifact = getArtifact(parent);
+							Set<Artifact> parentDependencies = artifactDependencyMap.get(parentArtifact);
+							if (parentDependencies == null) {
+								parentDependencies = new LinkedHashSet<Artifact>();
+								artifactDependencyMap.put(parentArtifact, parentDependencies);
+							}
+
+							Artifact artifact = getArtifact(node);
+							parentDependencies.add(artifact);
+							Set<Artifact> dependencies = artifactDependencyMap.get(artifact);
+							if (dependencies != null) {
+								parentDependencies.addAll(dependencies);
+							}
+						}
+						return true;
+					}
+
+					private Artifact getArtifact(DependencyNode node) {
+						if (artifactMap == null) {
+							artifactMap = new HashMap<String, Artifact>(artifacts.size());
+							for (Artifact artifact : artifacts) {
+								artifactMap.put(getArtifactKey(artifact), artifact);
+							}
+						}
+						String artifactKey = getArtifactKey(node.getArtifact());
+						Artifact artifact = artifactMap.get(artifactKey);
+						return artifact;
+					}
+
+					private String getArtifactKey(Artifact artifact) {
+						return ArtifactUtils.versionlessKey(artifact);
+					}
+				};
+
+				DependencyNode rootNode = dependencyTreeBuilder.buildDependencyTree(project, localRepository, artifactFactory,
+						artifactMetadataSource, scopeArtifactFilter, artifactCollector);
+				visitor = new FilteringDependencyNodeVisitor(visitor, new ArtifactDependencyNodeFilter(scopeArtifactFilter));
+				rootNode.accept(visitor);
+
+				return artifactDependencyMap;
+			} catch (DependencyTreeBuilderException e) {
+				throw new MojoExecutionException("Error resolving dependency tree", e);
+			}
+		}
+	}
+
+	private class ServiceDeployAssemblyBuilder {
+		public void createAssembly(Map<Artifact, Set<Artifact>> artifactDependencyMap) throws MojoExecutionException {
+			try {
+				Build build = project.getBuild();
+				File outputFile = new File(build.getDirectory(), build.getFinalName() + "-sd" + ".zip");
+				archiver.setDestFile(outputFile);
+				File workingDir = createWorkingDir();
+				for (Map.Entry<Artifact, Set<Artifact>> entry : artifactDependencyMap.entrySet()) {
+					Artifact artifact = entry.getKey();
+					Set<Artifact> dependencyArtifacts = entry.getValue();
+					File newArtifactFile = createNewArtifactFile(workingDir, artifact, dependencyArtifacts);
+					archiver.addFile(newArtifactFile, newArtifactFile.getName());
+				}
+				archiver.createArchive();
+			} catch (ArchiverException e) {
+				throw new MojoExecutionException("Error creating service deploy assembly", e);
+			} catch (IOException e) {
+				throw new MojoExecutionException("Error creating service deploy assembly", e);
+			}
+		}
+
+		private File createWorkingDir() {
+			File parentDir = new File(project.getBuild().getDirectory(), "service-deploy-assembly");
+			File workingDir = new File(parentDir, String.valueOf(System.currentTimeMillis()));
+			workingDir.mkdirs();
+			return workingDir;
+		}
+
+		private File createNewArtifactFile(File workingDir, Artifact artifact, Set<Artifact> dependencyArtifacts)
+				throws IOException, ArchiverException {
+			File newArtifactFile = new File(workingDir, getArtifactFileName(artifact));
+
+			JarFile artifactJarFile = new JarFile(artifact.getFile());
+
+			// Build new classpath string for manifest (including also transitive
+			// dependencies)
+			StringBuilder classpath = new StringBuilder();
+			for (Artifact dependencyArtifact : dependencyArtifacts) {
+				if (classpath.length() > 0) {
+					classpath.append(" ");
+				}
+				classpath.append(getArtifactFileName(dependencyArtifact));
+			}
+			// Update Manifest File
+			Manifest manifest = artifactJarFile.getManifest();
+			Attributes manifestMainAttributes = manifest.getMainAttributes();
+			if (classpath.length() > 0) {
+				manifestMainAttributes.put(Attributes.Name.CLASS_PATH, classpath.toString());
+			} else {
+				manifestMainAttributes.remove(Attributes.Name.CLASS_PATH);
+			}
+
+			JarOutputStream out = new JarOutputStream(new BufferedOutputStream(new FileOutputStream(newArtifactFile)), manifest);
+			byte[] buf = new byte[1024];
+			for (JarEntry jarEntry : Collections.list(artifactJarFile.entries())) {
+				if (!"META-INF/MANIFEST.MF".equals(jarEntry.getName())) {
+					JarEntry updatedJarEntry = new JarEntry(jarEntry.getName());
+					out.putNextEntry(updatedJarEntry);
+					InputStream in = new BufferedInputStream(artifactJarFile.getInputStream(updatedJarEntry));
+					int len;
+					while ((len = in.read(buf)) > 0) {
+						out.write(buf, 0, len);
+					}
+					out.closeEntry();
+					in.close();
+				}
+			}
+			out.close();
+
+			return newArtifactFile;
+		}
+
+		private String getArtifactFileName(Artifact artifact) {
+			return artifact.getArtifactId() + "." + artifact.getArtifactHandler().getExtension();
+		}
 	}
 }
