95e6aec8903246c87763806f46dbd962f783586b
jhf2812 <jhf2812@localhost>
2010-03-26 12:27:17 +0000

    Added more constants for the new environments

--- int/maven/plugins/maven-confluence-plugin/src/main/java/no/nav/maven/plugin/confluence/util/Constants.java
+++ int/maven/plugins/maven-confluence-plugin/src/main/java/no/nav/maven/plugin/confluence/util/Constants.java
@@ -16,11 +16,16 @@ public interface Constants {
 			put("Sandbox2", "Sandbox 2");
 			put("Sandbox3", "Sandbox 3");
 			put("Sandbox4", "Sandbox 4");
+			put("Sandbox6", "Sandbox 6");
+			put("Sandbox7", "Sandbox 7");
+			put("Sandbox8", "Sandbox 8");
 			put("K1", "Komponenttest 1");
 			put("K2", "Komponenttest 2");
 			put("K3", "Komponenttest 3");
 			put("K4", "Komponenttest 4");
 			put("K5", "Komponenttest 5");
+			put("K6", "Komponenttest 6");
+			put("K7", "Komponenttest 7");
 			put("I1", "Integrasjonstest 1");
 			put("I2", "Integrasjonstest 2");
 			put("I3", "Integrasjonstest 3");
@@ -33,10 +38,15 @@ public interface Constants {
 			put("T7", "Systemtest 7");
 			put("T8", "Systemtest 8");
 			put("T9", "Systemtest 9");
+			put("T10", "Systemtest 10");
+			put("T11", "Systemtest 11");
+			put("T12", "Systemtest 12");
+			put("T13", "Systemtest 13");
 			put("Q1", "Q1");
 			put("Q2", "Q2");
 			put("Q3", "Q3");
 			put("Q5", "Q5");
+			put("Q6", "Q6");
 			put("P", "Produksjon");
 		}
 	};
