ead3fe726b850f3596fc78929f118598192568e8
ebh2812 <ebh2812@localhost>
2010-06-04 12:32:24 +0000

    Initial version of WSI plugin

---
+++ int/maven/plugins/maven-wsi-plugin/src/main/java/no/nav/maven/plugin/wsi/AnalyzeMojo.java
@@ -0,0 +1,79 @@
+package no.nav.maven.plugin.wsi;
+
+import java.io.File;
+import java.util.Collections;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.wsi.WSIException;
+import org.wsi.test.analyzer.BasicProfileAnalyzer;
+import org.wsi.test.analyzer.config.AnalyzerConfig;
+import org.wsi.test.analyzer.config.WSDLElement;
+import org.wsi.test.analyzer.config.WSDLReference;
+import org.wsi.test.analyzer.config.impl.WSDLElementImpl;
+import org.wsi.test.analyzer.config.impl.WSDLReferenceImpl;
+import org.wsi.test.common.AddStyleSheet;
+import org.wsi.test.common.impl.AddStyleSheetImpl;
+import org.wsi.test.document.DocumentFactory;
+
+/**
+ * 
+ * @author test@example.com
+ * 
+ * @goal analyze
+ */
+public class AnalyzeMojo extends AbstractMojo {
+	/**
+	 * @parameter
+	 */
+	private boolean verbose;
+
+	/**
+	 * @parameter expression="${project.build.directory}"
+	 * @readonly
+	 */
+	private File projectBuildDirectory;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@SuppressWarnings("unchecked")
+	public void execute() throws MojoExecutionException {
+		try {
+			DocumentFactory documentFactory = DocumentFactory.newInstance();
+
+			AnalyzerConfig analyzerConfig = documentFactory.newAnalyzerConfig();
+			analyzerConfig.setVerboseOption(verbose);
+
+			// TODO: Make selection of profile configurable
+			analyzerConfig.setTestAssertionsDocumentLocation("common/profiles/SSBP10_BP11_TAD.xml");
+
+			analyzerConfig.setReportLocation(new File(projectBuildDirectory, "report.xml").getPath());
+			analyzerConfig.setReplaceReport(true);
+
+			AddStyleSheet addStyleSheet = new AddStyleSheetImpl();
+			addStyleSheet.setHref("common/xsl/report.xsl");
+			addStyleSheet.setType("text/xsl");
+			analyzerConfig.setAddStyleSheet(addStyleSheet);
+
+			// TODO: Find all applicable port types
+			WSDLReference wsdlReference = new WSDLReferenceImpl();
+			wsdlReference.setWSDLLocation("no\\nav\\lib\\oko\\inf\\Utbetaling.wsdl");
+			WSDLElement wsdlElement = new WSDLElementImpl();
+			wsdlReference.setWSDLElement(wsdlElement);
+			wsdlElement.setType("portType");
+			wsdlElement.setNamespace("http://nav-lib-oko/no/nav/lib/oko/inf");
+			wsdlElement.setName("Utbetaling");
+
+			analyzerConfig.setWSDLReference(wsdlReference);
+
+			if (getLog().isDebugEnabled()) {
+				getLog().debug(analyzerConfig.toString());
+			}
+
+			new BasicProfileAnalyzer(Collections.singletonList(analyzerConfig)).validateConformance();
+		} catch (WSIException e) {
+			throw new MojoExecutionException("WSIException", e);
+		}
+	}
+}
