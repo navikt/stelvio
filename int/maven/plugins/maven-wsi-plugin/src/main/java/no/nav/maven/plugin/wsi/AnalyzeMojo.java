8ce97ec77a820f8241fcae96387f855b15a8c5d2
ebh2812 <ebh2812@localhost>
2010-06-04 14:17:07 +0000

    Introduced scanning for WSDL files

--- int/maven/plugins/maven-wsi-plugin/src/main/java/no/nav/maven/plugin/wsi/AnalyzeMojo.java
+++ int/maven/plugins/maven-wsi-plugin/src/main/java/no/nav/maven/plugin/wsi/AnalyzeMojo.java
@@ -2,10 +2,21 @@ package no.nav.maven.plugin.wsi;
 
 import java.io.File;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.wsdl.Definition;
+import javax.wsdl.PortType;
+import javax.wsdl.WSDLException;
+import javax.wsdl.factory.WSDLFactory;
+import javax.wsdl.xml.WSDLReader;
+import javax.xml.namespace.QName;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.DirectoryScanner;
 import org.wsi.WSIException;
 import org.wsi.test.analyzer.BasicProfileAnalyzer;
 import org.wsi.test.analyzer.config.AnalyzerConfig;
@@ -36,9 +47,26 @@ public class AnalyzeMojo extends AbstractMojo {
 	private File projectBuildDirectory;
 
 	/**
-	 * @parameter expression=${failOnFailure} default-value="true"
+	 * @parameter expression="${failOnFailure}" default-value="true"
 	 */
 	private boolean failOnFailure;
+
+	/**
+	 * @parameter expression="${project.build.sourceDirectory}"
+	 */
+	private File basedir;
+
+	/**
+	 * @parameter
+	 */
+	private Set<String> includes = new HashSet<String>();
+
+	/**
+	 * @parameter
+	 */
+	private Set<String> excludes = new HashSet<String>();
+
+	private WSDLReader wsdlReader;
 
 	/**
 	 * {@inheritDoc}
@@ -62,14 +90,7 @@ public class AnalyzeMojo extends AbstractMojo {
 			addStyleSheet.setType("text/xsl");
 			analyzerConfig.setAddStyleSheet(addStyleSheet);
 
-			// TODO: Find all applicable port types
-			WSDLReference wsdlReference = new WSDLReferenceImpl();
-			wsdlReference.setWSDLLocation("no\\nav\\lib\\oko\\inf\\Utbetaling.wsdl");
-			WSDLElement wsdlElement = new WSDLElementImpl();
-			wsdlReference.setWSDLElement(wsdlElement);
-			wsdlElement.setType("portType");
-			wsdlElement.setNamespace("http://nav-lib-oko/no/nav/lib/oko/inf");
-			wsdlElement.setName("Utbetaling");
+			WSDLReference wsdlReference = getWSDLReference();
 
 			analyzerConfig.setWSDLReference(wsdlReference);
 
@@ -81,8 +102,58 @@ public class AnalyzeMojo extends AbstractMojo {
 			if (failOnFailure && result > 0) {
 				throw new MojoFailureException("WSI validation failed, check report for details.");
 			}
+
 		} catch (WSIException e) {
 			throw new MojoExecutionException("WSIException", e);
 		}
+	}
+
+	private WSDLReference getWSDLReference() throws MojoExecutionException {
+		// TODO: Find all applicable port types
+		try {
+			WSDLReference wsdlReference = new WSDLReferenceImpl();
+
+			DirectoryScanner scanner = new DirectoryScanner();
+			scanner.setBasedir(basedir);
+			if (!includes.isEmpty()) {
+				scanner.setIncludes(includes.toArray(new String[includes.size()]));
+			} else {
+				scanner.setIncludes(new String[] { "**/*.wsdl" });
+			}
+			if (excludes.isEmpty()) {
+				scanner.setExcludes(excludes.toArray(new String[excludes.size()]));
+			}
+			scanner.scan();
+
+			String[] includedFiles = scanner.getIncludedFiles();
+
+			for (int i = 0; i < includedFiles.length; i++) {
+				String includedFile = includedFiles[i];
+				Definition wsdl = getWsdlReader().readWSDL(basedir.toString(), includedFile);
+				Map<QName, PortType> portTypes = wsdl.getPortTypes();
+				for (QName qName : portTypes.keySet()) {
+					wsdlReference.setWSDLLocation(includedFile);
+					WSDLElement wsdlElement = new WSDLElementImpl();
+					wsdlReference.setWSDLElement(wsdlElement);
+					wsdlElement.setType("portType");
+					wsdlElement.setNamespace(qName.getNamespaceURI());
+					wsdlElement.setName(qName.getLocalPart());
+
+					return wsdlReference;
+				}
+			}
+			return null;
+		} catch (WSDLException e) {
+			throw new MojoExecutionException("Error parsing WSDL", e);
+		}
+	}
+
+	private WSDLReader getWsdlReader() throws WSDLException {
+		if (wsdlReader == null) {
+			wsdlReader = WSDLFactory.newInstance().newWSDLReader();
+			wsdlReader.setFeature("javax.wsdl.verbose", false);
+			wsdlReader.setFeature("javax.wsdl.importDocuments", false);
+		}
+		return wsdlReader;
 	}
 }
