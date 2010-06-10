39ba93c8ca1627615df05838528c101ac593bf13
ebh2812 <ebh2812@localhost>
2010-06-10 10:11:31 +0000

    Fixed issue with requiring local files to run analyze

--- int/maven/plugins/maven-wsi-plugin/src/main/java/no/nav/maven/plugin/wsi/AnalyzeMojo.java
+++ int/maven/plugins/maven-wsi-plugin/src/main/java/no/nav/maven/plugin/wsi/AnalyzeMojo.java
@@ -1,6 +1,8 @@
 package no.nav.maven.plugin.wsi;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
@@ -13,9 +15,13 @@ import javax.wsdl.factory.WSDLFactory;
 import javax.wsdl.xml.WSDLReader;
 import javax.xml.namespace.QName;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.ArtifactUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.archiver.ArchiverException;
+import org.codehaus.plexus.archiver.UnArchiver;
 import org.codehaus.plexus.util.DirectoryScanner;
 import org.wsi.WSIException;
 import org.wsi.test.analyzer.BasicProfileAnalyzer;
@@ -27,6 +33,7 @@ import org.wsi.test.analyzer.config.impl.WSDLReferenceImpl;
 import org.wsi.test.common.AddStyleSheet;
 import org.wsi.test.common.impl.AddStyleSheetImpl;
 import org.wsi.test.document.DocumentFactory;
+import org.wsi.util.WSIProperties;
 
 /**
  * 
@@ -36,15 +43,25 @@ import org.wsi.test.document.DocumentFactory;
  */
 public class AnalyzeMojo extends AbstractMojo {
 	/**
+	 * @component roleHint="zip"
+	 */
+	private UnArchiver unArchiver;
+
+	/**
+	 * @parameter expression="${plugin.artifacts}"
+	 * @readonly
+	 */
+	private Collection<Artifact> pluginDependendencies;
+
+	/**
 	 * @parameter
 	 */
 	private boolean verbose;
 
 	/**
-	 * @parameter expression="${project.build.directory}"
-	 * @readonly
+	 * @parameter expression="${project.build.directory}/wsi-tools"
 	 */
-	private File projectBuildDirectory;
+	private File reportDirectory;
 
 	/**
 	 * @parameter expression="${failOnFailure}" default-value="true"
@@ -74,15 +91,20 @@ public class AnalyzeMojo extends AbstractMojo {
 	@SuppressWarnings("unchecked")
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		try {
+			reportDirectory.mkdirs();
+			extractWsiToolsFiles();
+
 			DocumentFactory documentFactory = DocumentFactory.newInstance();
 
 			AnalyzerConfig analyzerConfig = documentFactory.newAnalyzerConfig();
+			analyzerConfig.setLocation(reportDirectory.getPath());
 			analyzerConfig.setVerboseOption(verbose);
 
 			// TODO: Make selection of profile configurable
-			analyzerConfig.setTestAssertionsDocumentLocation("common/profiles/SSBP10_BP11_TAD.xml");
+			analyzerConfig.setTestAssertionsDocumentLocation(new File(reportDirectory, "common/profiles/SSBP10_BP11_TAD.xml")
+					.getPath());
 
-			analyzerConfig.setReportLocation(new File(projectBuildDirectory, "report.xml").getPath());
+			analyzerConfig.setReportLocation(new File(reportDirectory, "report.xml").getPath());
 			analyzerConfig.setReplaceReport(true);
 
 			AddStyleSheet addStyleSheet = new AddStyleSheetImpl();
@@ -98,6 +120,10 @@ public class AnalyzeMojo extends AbstractMojo {
 				getLog().debug(analyzerConfig.toString());
 			}
 
+			// This is sort of a "hack" to make the WSI Testing Tool find schemas
+			// TODO: This can be improved by setting properties explicitly (see class org.wsi.test.util.TestUtils for details)
+			System.setProperty(WSIProperties.PROP_WSI_HOME, reportDirectory.getPath());
+
 			int result = new BasicProfileAnalyzer(Collections.singletonList(analyzerConfig)).validateConformance();
 			if (failOnFailure && result > 0) {
 				throw new MojoFailureException("WSI validation failed, check report for details.");
@@ -106,6 +132,29 @@ public class AnalyzeMojo extends AbstractMojo {
 		} catch (WSIException e) {
 			throw new MojoExecutionException("WSIException", e);
 		}
+	}
+
+	/**
+	 * TODO: This method is needed because the tool require files to be available on the local file system. Can be improved?
+	 */
+	private void extractWsiToolsFiles() throws MojoExecutionException {
+		try {
+			String versionlessKey = ArtifactUtils.versionlessKey("org.wsi.test", "wsi-test-tools-files");
+			for (Artifact pluginDependency : pluginDependendencies) {
+				if (ArtifactUtils.versionlessKey(pluginDependency).equals(versionlessKey)) {
+					// Found what I was looking for. Extract contents
+					unArchiver.setDestDirectory(reportDirectory);
+					unArchiver.setSourceFile(pluginDependency.getFile());
+					unArchiver.extract();
+					return;
+				}
+			}
+		} catch (ArchiverException e) {
+			throw new MojoExecutionException("Error extracting WSI tools files", e);
+		} catch (IOException e) {
+			throw new MojoExecutionException("Error extracting WSI tools files", e);
+		}
+		throw new MojoExecutionException("Dependency containing WSI Tools files not found. Unable to continue.");
 	}
 
 	private WSDLReference getWSDLReference() throws MojoExecutionException {
