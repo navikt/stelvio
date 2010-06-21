1710b316f983c2e3a0a070bcd3ebff05414cd4ec
ebh2812 <ebh2812@localhost>
2010-06-21 14:09:00 +0000

    Added new JAX-WS handler to process Stelvio Context SOAP header.

--- int/stelvio-commons-lib/no/stelvio/common/context/StelvioContextData.java
+++ int/stelvio-commons-lib/no/stelvio/common/context/StelvioContextData.java
@@ -1,14 +1,19 @@
 package no.stelvio.common.context;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * @author test@example.com
  */
+@XmlRootElement(name = "StelvioContext", namespace = "http://www.nav.no/StelvioContextPropagation")
 public class StelvioContextData {
 	private String applicationId;
 	private String correlationId;
 	private String languageId;
 	private String userId;
 
+	@XmlElement(name = "applicationId")
 	public String getApplicationId() {
 		return applicationId;
 	}
@@ -17,6 +22,7 @@ public class StelvioContextData {
 		this.applicationId = applicationId;
 	}
 
+	@XmlElement(name = "correlationId")
 	public String getCorrelationId() {
 		return correlationId;
 	}
@@ -25,6 +31,7 @@ public class StelvioContextData {
 		this.correlationId = correlationId;
 	}
 
+	@XmlElement(name = "languageId")
 	public String getLanguageId() {
 		return languageId;
 	}
@@ -33,6 +40,7 @@ public class StelvioContextData {
 		this.languageId = languageId;
 	}
 
+	@XmlElement(name = "userId")
 	public String getUserId() {
 		return userId;
 	}
