3a80cf71ee7419d85e553a10c0c0961def22ea96
jhf2812 <jhf2812@localhost>
2010-10-29 11:47:05 +0000

    Fixed the upload functionality, faster better stronger

--- int/maven/plugins/maven-wasdeploy-plugin/src/main/java/no/nav/maven/plugins/utils/SSHUtil.java
+++ int/maven/plugins/maven-wasdeploy-plugin/src/main/java/no/nav/maven/plugins/utils/SSHUtil.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import org.codehaus.plexus.util.FileUtils;
 
 import com.sshtools.j2ssh.FileTransferProgress;
+import com.sshtools.j2ssh.ScpClient;
 import com.sshtools.j2ssh.SftpClient;
 import com.sshtools.j2ssh.SshClient;
 import com.sshtools.j2ssh.authentication.AuthenticationProtocolState;
@@ -52,13 +53,8 @@ public class SSHUtil {
 			throw new IOException("[ERROR] Unable to connect to host: " + hostname + " with username/pw:" + username + "/" + password + ".");
 		}
 		
-		SftpClient client = ssh.openSftpClient();
-
-		// Make sure we have the needed folder structure
-		client.mkdirs(remoteDir);
-		FileTransferProgress ftprog = SSHUtil.getFTProg();
-		client.copyLocalDirectory(tempDirectory.getAbsolutePath(), remoteDir, true, false, true, ftprog);
-		client.quit();
+		ScpClient client = ssh.openScpClient();
+		client.put(tempDirectory.getAbsolutePath(), remoteDir, true);
 		ssh.disconnect();
 
 		System.out.println("[INFO] Deleting temp folder: " + tempDirectory.getParentFile().getAbsolutePath());
@@ -66,21 +62,4 @@ public class SSHUtil {
 
 		System.out.println("[INFO] Successfully uploaded the directory: " + localDir + " => " + hostname + ":" + remoteDir);
 	}
-
-	// Useless, but needed for copyLocalDirectory method.
-	// Couldn't find any implementations of the FileTransferProgress interface.
-	private static FileTransferProgress getFTProg() {
-		return new FileTransferProgress() {
-			public void started(long arg0, String arg1) {
-			}
-			public void progressed(long arg0) {
-			}
-			public void completed() {
-			}
-			public boolean isCancelled() {
-				return false;
-			}
-		};
-	}
-
 }
