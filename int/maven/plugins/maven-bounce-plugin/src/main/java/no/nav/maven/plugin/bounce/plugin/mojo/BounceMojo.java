bc47dfd8b0b80239cda4ce3c308c5797d653906e
dsc2812 <dsc2812@localhost>
2010-11-05 09:05:52 +0000

    deleted debugging output

--- int/maven/plugins/maven-bounce-plugin/src/main/java/no/nav/maven/plugin/bounce/plugin/mojo/BounceMojo.java
+++ int/maven/plugins/maven-bounce-plugin/src/main/java/no/nav/maven/plugin/bounce/plugin/mojo/BounceMojo.java
@@ -178,15 +178,6 @@ public class BounceMojo extends AbstractMojo {
 	}
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
-
-		String executing = "[DEBUG] Executing: mvn clean install " +
-				"-Dapps="+this.apps + " " +
-				"-Denv="+this.env + " " + 
-				"-Dda-configuration-version="+ "7.0.1-SNAPSHOT " + 
-				"-DexcludeBus="+this.excludeBus + " " + 
-				"-Daction="+this.action + " " + 
-				"-DrestartJoark="+this.includeJoark;
-		getLog().info(executing);
 		envFile = envFilesDir + "/" + env.toUpperCase() + ".xml";
 
 		try {
@@ -203,9 +194,7 @@ public class BounceMojo extends AbstractMojo {
 				getLog().info("");
 				getLog().info("Parsing restart configuration file ...");
 				getLog().info("");
-				getLog().info("[BEFORE] was_ss: "+this.was_ss_operation+" was_is: "+this.was_is_operation+" wps: "+this.wps_operation);
 				this.ResolveRestart();
-				getLog().info("[AFTER] was_ss: "+this.was_ss_operation+" was_is: "+this.was_is_operation+" wps: "+this.wps_operation);
 			}
 
 			getLog().info("Parsing environment file for parameters: \n " + this.envFile);
