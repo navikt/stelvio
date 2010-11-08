27d01a986208e63731ee0216a470d69bba6ebde6
jhf2812 <jhf2812@localhost>
2010-11-08 08:08:18 +0000

    Removed required flag for excludeBus

--- int/maven/plugins/maven-bounce-plugin/src/main/java/no/nav/maven/plugin/bounce/plugin/mojo/BounceMojo.java
+++ int/maven/plugins/maven-bounce-plugin/src/main/java/no/nav/maven/plugin/bounce/plugin/mojo/BounceMojo.java
@@ -75,7 +75,6 @@ public class BounceMojo extends AbstractMojo {
 	 * Indicates whether bus deploy is needed.
 	 * 
 	 * @parameter expression="${excludeBus}"
-	 * @required
 	 */
 	private boolean excludeBus;
 
