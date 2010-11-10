a4d8b7713c60030987e6b1e5272f36699832e5ab
jhf2812 <jhf2812@localhost>
2010-11-10 17:15:05 +0000

    - Added support for importing configuration directories as well as files - Changed inputparameter to be "config", it is also required

--- int/maven/plugins/maven-datapower-plugin/src/main/java/no/nav/maven/plugins/ImportConfigMojo.java
+++ int/maven/plugins/maven-datapower-plugin/src/main/java/no/nav/maven/plugins/ImportConfigMojo.java
@@ -10,30 +10,53 @@ import org.apache.maven.plugin.MojoFailureException;
 
 /**
  * Goal which imports a DataPower configuration into a specified device.
- *
+ * 
  * @goal importConfig
  * 
  */
 public class ImportConfigMojo extends AbstractDeviceMgmtMojo {
 
-
 	/**
-	 * @parameter expression="${format}" default-value="XML" alias="format"
+	 * @parameter expression="${config}"
+	 * @required
 	 */
-	private String importFormat;
-	
+	private File config;
 
-	/**
-	 * @parameter expression="${config}" alias="config"
-	 */
-	private File configFile;
-	
 	protected void doExecute() throws MojoExecutionException, MojoFailureException {
+
+		if (config.isDirectory()) {
+
+			for (File f : config.listFiles()) {
+				importConfigFile(f.getAbsolutePath(), getFormat(f));
+			}
+			
+		} else {
+			importConfigFile(config.getAbsolutePath(), getFormat(config));
+		}
+
+	}
+
+	private void importConfigFile(String configFile, ImportFormat importFormat) throws MojoExecutionException {
 		try {
-			String response = getXMLMgmtSession().importConfig(configFile, ImportFormat.valueOf(importFormat));
+
+			getLog().info("Importing config file: " + configFile + " ...");
+			String response = getXMLMgmtSession().importConfig(configFile, importFormat);
 			getLog().debug(response);
+
 		} catch (XMLMgmtException e) {
 			throw new MojoExecutionException("Failed to import configuration", e);
 		}
 	}
+
+	private ImportFormat getFormat(File f) throws MojoFailureException {
+
+		if (f.getName().endsWith(".xml")) {
+			return ImportFormat.valueOf("XML");
+		} else if (f.getName().endsWith(".zip")) {
+			return ImportFormat.valueOf("ZIP");
+		} else {
+			throw new MojoFailureException("Unsupported file format for file: " + f + ". Only XML and ZIP are supported.");
+		}
+	}
+
 }
