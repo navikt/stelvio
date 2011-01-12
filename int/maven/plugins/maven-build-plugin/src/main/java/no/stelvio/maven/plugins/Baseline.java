dbfbc74b5d982dad8d5ee4e195b2c11307ddc910
dsc2812 <dsc2812@localhost>
2011-01-12 14:37:22 +0000

    dsc2812: created code for making baseline

--- int/maven/plugins/maven-build-plugin/src/main/java/no/stelvio/maven/plugins/Baseline.java
+++ int/maven/plugins/maven-build-plugin/src/main/java/no/stelvio/maven/plugins/Baseline.java
@@ -1,5 +1,7 @@
 package no.stelvio.maven.plugins;
 
+import no.stelvio.maven.build.plugin.utils.CleartoolCommandLine;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -13,10 +15,24 @@ import org.apache.maven.plugin.MojoFailureException;
  */
 public class Baseline extends AbstractMojo{
 
+	/**
+	 * Stream name - BUILD_TEST
+	 * 
+	 * @parameter expression="${stream}"
+	 * @required
+	 */
+	private String stream;
+	
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		// TODO Auto-generated method stub
+		this.getLog().info("-------------------------");
+		this.getLog().info("--- Creating baseline ---");
+		this.getLog().info("-------------------------");
 		
+		String workingDir = "D:/cc/"+this.stream+"_int";
+		String subcommand = "-mkbl -force";
+		if (CleartoolCommandLine.runClearToolCommand(workingDir, subcommand) != 0) 
+			throw new MojoExecutionException("Unable to perform delivery");
 	}
 
 }
