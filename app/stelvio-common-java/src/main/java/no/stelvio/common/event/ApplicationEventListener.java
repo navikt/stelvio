129eed9f4a3d7be09b1a4a6424ba271fa533fd4b
w132847 <w132847@localhost>
2011-02-24 13:45:22 +0000

    Removed test migration to perform a proper migration.

--- app/stelvio-common-java/src/main/java/no/stelvio/common/event/ApplicationEventListener.java
+++
@@ -1,9 +0,0 @@
-package no.stelvio.common.event;
-
-import org.springframework.context.ApplicationListener;
-
-/**
- * @author personf8e9850ed756
- */
-public interface ApplicationEventListener extends ApplicationListener {
-}
