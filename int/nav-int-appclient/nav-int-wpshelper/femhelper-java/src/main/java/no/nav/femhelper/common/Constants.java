36a981ec33c551cfc5f0a30150fcbb84a1dc86d7
jmf2812 <jmf2812@localhost>
2011-03-03 12:28:18 +0000

    Added constants and queries to support BPC, JMS, MQ and SCA events

--- int/nav-int-appclient/nav-int-wpshelper/femhelper-java/src/main/java/no/nav/femhelper/common/Constants.java
+++ int/nav-int-appclient/nav-int-wpshelper/femhelper-java/src/main/java/no/nav/femhelper/common/Constants.java
@@ -20,17 +20,17 @@ public class Constants {
 	public static final String ACTION_STATUS = "STATUS";
 	public static final String ACTION_COUNT = "COUNT";
 
-	public static final List<String> ACTIONS = Arrays.asList(ACTION_REPORT, ACTION_DISCARD, ACTION_RESUBMIT, ACTION_STATUS, ACTION_COUNT);
+	public static final List<String> ACTIONS = Arrays.asList(ACTION_REPORT, ACTION_DISCARD, ACTION_RESUBMIT, ACTION_STATUS,
+			ACTION_COUNT);
 
 	/**
-	 * Public constant to represent the formatter pattern to be used together
-	 * with <code>Constants.timeFrame</code> operation
+	 * Public constant to represent the formatter pattern to be used together with <code>Constants.timeFrame</code> operation
 	 */
 	public static final String TIME_FRAME_FORMAT = "dd.MM.yyyy:hhmm";
 
 	/**
-	 * Public constant to represent the Date operation and parameter string from
-	 * the configuration file and properties related to that
+	 * Public constant to represent the Date operation and parameter string from the configuration file and properties related
+	 * to that
 	 */
 	public static String DEFAULT_DATE_FORMAT = "yyyyMMddHHmmss";
 	public static String DEFAULT_DATE_FORMAT_MILLS = "yyyy-MM-dd HH:mm:ss:S";
@@ -39,5 +39,13 @@ public class Constants {
 	public static final int MONTH_RANGE = 3;
 	public static final int MAX_DELETE = 100;
 	public static final String FILE_PREFIX = "LOG";
+
+	/**
+	 * Public properties representing failed event type
+	 */
+	public static final String EVENT_TYPE_BPC = "BPC";
+	public static final String EVENT_TYPE_JMS = "JMS";
+	public static final String EVENT_TYPE_MQ = "MQ";
+	public static final String EVENT_TYPE_SCA = "SCA";
 
 }
--- int/nav-int-appclient/nav-int-wpshelper/femhelper-java/src/main/java/no/nav/femhelper/common/Queries.java
+++ int/nav-int-appclient/nav-int-wpshelper/femhelper-java/src/main/java/no/nav/femhelper/common/Queries.java
@@ -32,6 +32,22 @@ public class Queries {
 	public static final String QUERY_DISCARD_ALL_FAILED_EVENTS = "discardAll";
 	
 	/** Public constant to represent the query to all failed events from the failure event manager (FEM)
-	 *  Replaces the deprecated methods getFailedEventForDestination and getFailedEventsForTimePeriod in WPS 7*/
+	 *  Replaces the deprecated methods getFailedEventForDestination and getFailedEventsForTimePeriod in WPS6.1*/
 	public static final String QUERY_FAILED_EVENTS = "queryFailedEvents";	
+	
+	/** Public constant to represent the query to get details about SCA events from the failure event manager (FEM)
+	 *  Replaces the deprecated methods getFailedEventWithParameters in WPS6.1*/
+	public static final String QUERY_EVENT_DETAIL_FOR_SCA = "getEventDetailForSCA";
+	
+	/** Public constant to represent the query to get details about BPC events from the failure event manager (FEM)
+	 *  Replaces the deprecated methods getFailedEventWithParameters in WPS6.1*/
+	public static final String QUERY_EVENT_DETAIL_FOR_BPC = "getEventDetailForBPC";
+	
+	/** Public constant to represent the query to get details about JMS events from the failure event manager (FEM)
+	 *  Replaces the deprecated methods getFailedEventWithParameters in WPS6.1*/
+	public static final String QUERY_EVENT_DETAIL_FOR_JMS = "getEventDetailForJMS";
+	
+	/** Public constant to represent the query to get details about JMS events from the failure event manager (FEM)
+	 *  Replaces the deprecated methods getFailedEventWithParameters in WPS6.1*/
+	public static final String QUERY_EVENT_DETAIL_FOR_MQ = "getEventDetailForMQ";
 }
