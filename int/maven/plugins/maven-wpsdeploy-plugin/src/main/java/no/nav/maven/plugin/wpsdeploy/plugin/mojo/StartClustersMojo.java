2e643ea43ecf6099986bee6c3267051031b19a64
h129009 <h129009@localhost>
2011-03-17 16:21:18 +0000

    Added option to not start clusters

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/java/no/nav/maven/plugin/wpsdeploy/plugin/mojo/StartClustersMojo.java
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/java/no/nav/maven/plugin/wpsdeploy/plugin/mojo/StartClustersMojo.java
@@ -5,12 +5,18 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.codehaus.plexus.util.cli.Commandline;
 
 /**
- * Goal that contacts the deployment manager to start MECluster,SupportCluster and WPSCluster.
+ * Goal that contacts the deployment manager to start MECluster,SupportCluster
+ * and WPSCluster.
  * 
  * @goal start-clusters
  * @requiresDependencyResolution
  */
 public class StartClustersMojo extends WebsphereUpdaterMojo {
+
+	/**
+	 * @parameter expression="${start-clusters.skip}"
+	 */
+	protected boolean startClustersSkip;
 
 	public final void applyToWebSphere(final Commandline commandLine) throws MojoExecutionException, MojoFailureException {
 
@@ -19,28 +25,34 @@ public class StartClustersMojo extends WebsphereUpdaterMojo {
 			return;
 		}
 
-		startClusters(commandLine);
+		if (!startClustersSkip) {
+			startClusters(commandLine);
+		} else {
+			getLog().info("Skipping cluster start-up.");
+		}
 	}
 
 	private final void startClusters(final Commandline commandLine) {
-	
+
 		Commandline.Argument arg = new Commandline.Argument();
 		arg.setLine("-f " + scriptsHome + "/scripts/ClusterStartStop.py " + scriptsHome + " start");
 		commandLine.addArg(arg);
-		
-		// Handling SOAPException, SocketTimeoutException (retval 105), retrying five times.
+
+		// Handling SOAPException, SocketTimeoutException (retval 105), retrying
+		// five times.
 		int attempt = 0;
 		int maxattempt = 5;
-		
-		while (attempt <= maxattempt){
+
+		while (attempt <= maxattempt) {
 			int retval = executeCommand(commandLine);
 			getLog().info("[RETVAL = " + retval + "]");
-			
-			if (retval != 105){
+
+			if (retval != 105) {
 				break;
 			}
-			
-			if (attempt != maxattempt) getLog().info("Caught exception, retrying ... " + "[" + ++attempt + "/" + maxattempt + "]" );
+
+			if (attempt != maxattempt)
+				getLog().info("Caught exception, retrying ... " + "[" + ++attempt + "/" + maxattempt + "]");
 			else {
 				getLog().info("Could not perform the operation. Continuing ...");
 				break;
