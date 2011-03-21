182cdd4de6c239e92a56951997c875f98474985b
isc2812 <isc2812@localhost>
2011-03-21 13:49:45 +0000

    Synkroniserte alle plug-in versjonene til 1.0.4

--- int/eclipse/plugins/stelvio-models-transformation-uml2servicemodel/src/no/stelvio/esb/models/transformation/uml2servicemodel/transforms/PropertyMappingToAttributeTransform.java
+++ int/eclipse/plugins/stelvio-models-transformation-uml2servicemodel/src/no/stelvio/esb/models/transformation/uml2servicemodel/transforms/PropertyMappingToAttributeTransform.java
@@ -34,113 +34,113 @@ import org.eclipse.uml2.uml.UMLPackage;
 public class PropertyMappingToAttributeTransform extends MapTransform {
 
 	/**
-	 * The transform id <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
+   * The transform id <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
 	public static final String PROPERTYMAPPINGTOATTRIBUTE_TRANSFORM = "PropertyMappingToAttribute_Transform";//$NON-NLS-1$
 
 	/**
-	 * The create rule id <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
+   * The create rule id <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
 	public static final String PROPERTYMAPPINGTOATTRIBUTE_CREATE_RULE = PROPERTYMAPPINGTOATTRIBUTE_TRANSFORM
-			+ "_Create_Rule";//$NON-NLS-1$
+      + "_Create_Rule";//$NON-NLS-1$
 
 	/**
-	 * The 'PropertyMappingToAttribute TilFeltNavn to MappingToAttributeName
-	 * rule' id <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
+   * The 'PropertyMappingToAttribute TilFeltNavn to MappingToAttributeName
+   * rule' id <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
 	public static final String PROPERTYMAPPINGTOATTRIBUTE_TIL_FELT_NAVN_TO_MAPPING_TO_ATTRIBUTE_NAME_RULE = PROPERTYMAPPINGTOATTRIBUTE_TRANSFORM
-			+ "_TilFeltNavnToMappingToAttributeName_Rule";//$NON-NLS-1$
+      + "_TilFeltNavnToMappingToAttributeName_Rule";//$NON-NLS-1$
 
 	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
+   * <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
 	public PropertyMappingToAttributeTransform(Registry registry,
 			FeatureAdapter referenceAdapter) {
-		this(PROPERTYMAPPINGTOATTRIBUTE_TRANSFORM,
-				Uml2servicemodelMessages.PropertyMappingToAttribute_Transform,
-				registry, referenceAdapter);
-	}
+    this(PROPERTYMAPPINGTOATTRIBUTE_TRANSFORM,
+        Uml2servicemodelMessages.PropertyMappingToAttribute_Transform,
+        registry, referenceAdapter);
+  }
 
 	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
+   * <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
 	public PropertyMappingToAttributeTransform(String id, String name,
 			Registry registry, FeatureAdapter referenceAdapter) {
-		super(id, name, registry, referenceAdapter);
-		addTransformElements(registry);
-	}
+    super(id, name, registry, referenceAdapter);
+    addTransformElements(registry);
+  }
 
 	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
+   * <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
 	private void addTransformElements(Registry registry) {
-		// You may add more transform element before the generated ones here
-		// Remember to remove the @generated tag or add NOT to it
-		addGeneratedTransformElements(registry);
-		// You may add more transform element after the generated ones here
-		// Remember to remove the @generated tag or add NOT to it
-	}
+    // You may add more transform element before the generated ones here
+    // Remember to remove the @generated tag or add NOT to it
+    addGeneratedTransformElements(registry);
+    // You may add more transform element after the generated ones here
+    // Remember to remove the @generated tag or add NOT to it
+  }
 
 	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
+   * <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
 	private void addGeneratedTransformElements(Registry registry) {
-		add(getTilFeltNavnToMappingToAttributeName_Rule());
-	}
+    add(getTilFeltNavnToMappingToAttributeName_Rule());
+  }
 
 	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
+   * <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
 	protected Condition getAccept_Condition() {
-		return new InstanceOfCondition(UMLPackage.Literals.PROPERTY)
-				.AND(new StereotypeCondition(
-						new String[] { "NAV UML Profile::Mapping" }));
-	}
+    return new InstanceOfCondition(UMLPackage.Literals.PROPERTY)
+        .AND(new StereotypeCondition(
+            new String[] { "NAV UML Profile::Mapping" }));
+  }
 
 	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
+   * <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
 	protected CreateRule getCreate_Rule(FeatureAdapter referenceAdapter) {
-		CreateRule rule = new CreateRule(
-				PROPERTYMAPPINGTOATTRIBUTE_CREATE_RULE,
-				Uml2servicemodelMessages.PropertyMappingToAttribute_Transform_Create_Rule,
-				this, referenceAdapter,
-				ServiceMetamodelPackage.Literals.ATTRIBUTE);
-		return rule;
-	}
+    CreateRule rule = new CreateRule(
+        PROPERTYMAPPINGTOATTRIBUTE_CREATE_RULE,
+        Uml2servicemodelMessages.PropertyMappingToAttribute_Transform_Create_Rule,
+        this, referenceAdapter,
+        ServiceMetamodelPackage.Literals.ATTRIBUTE);
+    return rule;
+  }
 
 	/**
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
+   * <!-- begin-user-doc --> <!-- end-user-doc -->
+   * 
+   * @generated
+   */
 	protected AbstractRule getTilFeltNavnToMappingToAttributeName_Rule() {
-		MoveRule rule = new MoveRule(
-				PROPERTYMAPPINGTOATTRIBUTE_TIL_FELT_NAVN_TO_MAPPING_TO_ATTRIBUTE_NAME_RULE,
-				Uml2servicemodelMessages.PropertyMappingToAttribute_Transform_TilFeltNavnToMappingToAttributeName_Rule,
-				new StereotypeFeatureAdapter(
-						"NAV UML Profile::Mapping::tilFeltNavn"),
-				new DirectFeatureAdapter(
-						ServiceMetamodelPackage.Literals.ATTRIBUTE__MAPPING_TO_ATTRIBUTE_NAME));
-		return rule;
-	}
+    MoveRule rule = new MoveRule(
+        PROPERTYMAPPINGTOATTRIBUTE_TIL_FELT_NAVN_TO_MAPPING_TO_ATTRIBUTE_NAME_RULE,
+        Uml2servicemodelMessages.PropertyMappingToAttribute_Transform_TilFeltNavnToMappingToAttributeName_Rule,
+        new StereotypeFeatureAdapter(
+            "NAV UML Profile::Mapping::tilFeltNavn"),
+        new DirectFeatureAdapter(
+            ServiceMetamodelPackage.Literals.ATTRIBUTE__MAPPING_TO_ATTRIBUTE_NAME));
+    return rule;
+  }
 
 }
