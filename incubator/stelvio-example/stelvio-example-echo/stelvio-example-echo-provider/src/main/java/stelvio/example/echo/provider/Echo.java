273b97aac7666a13b400277c49a1ba4560a6cc7d
oga2812 <oga2812@localhost>
2011-03-21 19:59:18 +0000

    Legg til avhengigheter i Eclipse Endret relativ path for JAX-WS handler (fant ikke filen)

--- incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
+++ incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
@@ -5,21 +5,21 @@ import javax.jws.WebService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import no.stelvio.common.context.RequestContextHolder;
 import no.stelvio.example.services.v1.echoservice.binding2.EchoEchoFault1Msg;
 
 // name="EchoService", wsdlLocation="WEB-INF/wsdl/stelvio-example-echo-service_EchoServiceWSEXP.wsdl", targetNamespace="http://www.stelvio.no/example/services/V1/EchoService/Binding2", serviceName="EchoServiceWSEXP_EchoServiceHttpService", portName="EchoServiceWSEXP_EchoServiceHttpPort" 
-@WebService(endpointInterface="no.stelvio.example.services.v1.echoservice.binding2.EchoService")
-@HandlerChain(file="EchoHandler.xml")
+@WebService(endpointInterface = "no.stelvio.example.services.v1.echoservice.binding2.EchoService")
+@HandlerChain(file = "/EchoHandler.xml")
 public class Echo {
 
 	protected final Log log = LogFactory.getLog(this.getClass());
-	
+
 	public String echo(String input) throws EchoEchoFault1Msg {
 
-		if( log.isDebugEnabled() ) {
-			   log.debug("Echoing message:" + input);
-			}
-		
+		log.debug("Echoing message:" + input);
+		log.debug("Context: " + RequestContextHolder.currentRequestContext());
+
 		return input;
 	}
 }
