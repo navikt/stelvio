929b6317915f883807716e95fa26bee62042c68c
oga2812 <oga2812@localhost>
2011-03-22 16:59:49 +0000

    Flytt Handler.xml til riktig sted

--- incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
+++ incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
@@ -10,7 +10,7 @@ import no.stelvio.example.services.v1.echoservice.binding2.EchoEchoFault1Msg;
 
 // name="EchoService", wsdlLocation="WEB-INF/wsdl/stelvio-example-echo-service_EchoServiceWSEXP.wsdl", targetNamespace="http://www.stelvio.no/example/services/V1/EchoService/Binding2", serviceName="EchoServiceWSEXP_EchoServiceHttpService", portName="EchoServiceWSEXP_EchoServiceHttpPort" 
 @WebService(endpointInterface = "no.stelvio.example.services.v1.echoservice.binding2.EchoService")
-@HandlerChain(file = "/EchoHandler.xml")
+@HandlerChain(file = "EchoHandler.xml")
 public class Echo {
 
 	protected final Log log = LogFactory.getLog(this.getClass());
