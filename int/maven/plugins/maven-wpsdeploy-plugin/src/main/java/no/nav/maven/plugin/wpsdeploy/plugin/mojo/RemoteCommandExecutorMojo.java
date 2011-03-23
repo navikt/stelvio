872c9bb346748541ff75920efc9bdc21c0d41cea
h129009 <h129009@localhost>
2011-03-23 08:41:04 +0000

    Moved the exposing of envClass variable to correct location

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/java/no/nav/maven/plugin/wpsdeploy/plugin/mojo/RemoteCommandExecutorMojo.java
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/java/no/nav/maven/plugin/wpsdeploy/plugin/mojo/RemoteCommandExecutorMojo.java
@@ -1,42 +1,16 @@
 package no.nav.maven.plugin.wpsdeploy.plugin.mojo;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import no.nav.maven.plugin.wpsdeploy.plugin.utils.PropertyUtils;
-
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
 /**
 * Abstract class using the template pattern for child mojos.
-* 
-* @author test@example.com 
 */
 public abstract class RemoteCommandExecutorMojo extends WebsphereMojo {
-    
-	/**
-	 * @parameter expression="${environment}"
-	 * @required
-	 */
-	protected String environment;
 	
 	protected abstract void executeRemoteCommand() throws MojoExecutionException, MojoFailureException;
 	
-	protected final void doExecute() throws MojoExecutionException, MojoFailureException {
-		
-		String tmpEnvironmentFile = baseDirectory + tmpBusConfigurationExtractDirectory + "/environments/" + environment + ".properties";
-		
-		try {
-			PropertyUtils pf = new PropertyUtils(tmpEnvironmentFile, project);
-			pf.exposeProperty("envClass", pf.getProperty("envClass"), false);
-			
-		} catch (FileNotFoundException e) {
-			throw new MojoExecutionException("[ERROR]: " + e);
-		} catch (IOException e) {
-			throw new MojoExecutionException("[ERROR]: " + e);
-		}
-		
+	protected final void doExecute() throws MojoExecutionException, MojoFailureException {		
 		executeRemoteCommand();
 	}
 }	
