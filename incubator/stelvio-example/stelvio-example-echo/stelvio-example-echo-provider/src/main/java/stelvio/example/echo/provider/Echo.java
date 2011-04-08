35916a50545f57207b5e8cc9f70484991943c68f
oga2812 <oga2812@localhost>
2011-04-08 14:22:17 +0000

    Annoter providerklassen som en EJB

--- incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
+++ incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
@@ -1,5 +1,6 @@
 package stelvio.example.echo.provider;
 
+import javax.ejb.Stateless;
 import javax.jws.HandlerChain;
 import javax.jws.WebService;
 import org.apache.commons.logging.Log;
@@ -7,11 +8,13 @@ import org.apache.commons.logging.LogFactory;
 
 import no.stelvio.common.context.RequestContextHolder;
 import no.stelvio.example.services.v1.echoservice.binding2.EchoEchoFault1Msg;
+import no.stelvio.example.services.v1.echoservice.binding2.EchoService;
 
 // name="EchoService", wsdlLocation="WEB-INF/wsdl/stelvio-example-echo-service_EchoServiceWSEXP.wsdl", targetNamespace="http://www.stelvio.no/example/services/V1/EchoService/Binding2", serviceName="EchoServiceWSEXP_EchoServiceHttpService", portName="EchoServiceWSEXP_EchoServiceHttpPort" 
 @WebService(endpointInterface = "no.stelvio.example.services.v1.echoservice.binding2.EchoService")
 @HandlerChain(file = "EchoHandler.xml")
-public class Echo {
+@Stateless
+public class Echo implements EchoService {
 
 	protected final Log log = LogFactory.getLog(this.getClass());
 
