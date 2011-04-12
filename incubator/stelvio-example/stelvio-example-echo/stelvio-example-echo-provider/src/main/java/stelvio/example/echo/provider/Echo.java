d7f11b253098d6b2eed1449da2ff50784b7dfb64
w132847 <w132847@localhost>
2011-04-12 15:10:49 +0000

    Added the (experimantal) context handler to the framework

--- incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
+++ incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
@@ -14,7 +14,7 @@ import no.stelvio.example.services.v1.echoservice.binding2.EchoService;
 
 // name="EchoService", wsdlLocation="WEB-INF/wsdl/stelvio-example-echo-service_EchoServiceWSEXP.wsdl", targetNamespace="http://www.stelvio.no/example/services/V1/EchoService/Binding2", serviceName="EchoServiceWSEXP_EchoServiceHttpService", portName="EchoServiceWSEXP_EchoServiceHttpPort" 
 @WebService(endpointInterface = "no.stelvio.example.services.v1.echoservice.binding2.EchoService")
-@HandlerChain(file = "EchoHandler.xml")
+@HandlerChain(file = "no.stelvio.provider.EchoHandler.xml")
 @Stateless
 public class Echo implements EchoService {
 
