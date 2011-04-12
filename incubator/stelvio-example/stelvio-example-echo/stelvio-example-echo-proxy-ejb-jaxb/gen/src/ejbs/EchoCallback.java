4bb67e9fc50bcdf4c55934c46673812227335660
oga2812 <oga2812@localhost>
2011-04-12 08:45:32 +0000

    Legg til proxy-modul for EJB

---
+++ incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-proxy-ejb-jaxb/gen/src/ejbs/EchoCallback.java
@@ -0,0 +1,13 @@
+  
+package ejbs;	
+
+/**
+ * @generated
+ */
+public interface EchoCallback {
+
+  /**
+   * @generated
+   */		
+  public void onEchoResponse(com.ibm.websphere.sca.Ticket __ticket, java.lang.String __result, java.lang.Exception __exception);
+}
---
+++ incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-proxy-ejb-jaxb/gen/src/ejbs/EchoImplAsync.java
@@ -0,0 +1,13 @@
+  
+package ejbs;	
+
+/**
+ * @generated
+ */
+public interface EchoImplAsync {
+
+  /**
+   * @generated
+   */		
+  public void echoAsync(java.lang.String input, ejbs.EchoCallback __callback, com.ibm.websphere.sca.Ticket __ticket);
+}
