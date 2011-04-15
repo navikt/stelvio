d6268b5a29b6ee26f4afd36ac319e303c51791ae
oga2812 <oga2812@localhost>
2011-04-15 13:05:20 +0000

    Legg til targetNamespace, serviceName og portName på @WebService

--- incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
+++ incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
@@ -12,7 +12,11 @@ import org.apache.commons.logging.LogFactory;
 
 import stelvio.example.echo.service.EchoServiceBi;
 
-@WebService(endpointInterface = "no.stelvio.example.services.v1.echoservice.binding2.EchoService", wsdlLocation="WEB-INF/wsdl/no/stelvio/example/services/V1/stelvio-example-echo-service_EchoServiceWSEXP.wsdl")
+@WebService(endpointInterface = "no.stelvio.example.services.v1.echoservice.binding2.EchoService",
+			wsdlLocation="WEB-INF/wsdl/no/stelvio/example/services/V1/stelvio-example-echo-service_EchoServiceWSEXP.wsdl",
+			targetNamespace="http://www.stelvio.no/example/services/V1/EchoService/Binding2",
+			serviceName="EchoServiceWSEXP_EchoServiceHttpService",
+			portName="EchoServiceWSEXP_EchoServiceHttpPort")
 @HandlerChain(file = "EchoHandler.xml")
 public class Echo implements EchoService {
 
