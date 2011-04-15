479c665d63431ef161f8ae18143365917e0e086b
oga2812 <oga2812@localhost>
2011-04-15 09:55:37 +0000

    Flytt HandlerConfiguration tilbake til JAX-WS bean implementasjonsprosjektet Hent inn WSDL-fil Fjern @Stateless

--- incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
+++ incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
@@ -1,21 +1,20 @@
 package stelvio.example.echo.provider;
 
-import javax.ejb.Stateless;
 import javax.jws.HandlerChain;
 import javax.jws.WebService;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import stelvio.example.echo.service.EchoServiceBi;
 
 import no.stelvio.common.context.RequestContextHolder;
 import no.stelvio.example.services.v1.echoservice.binding2.EchoEchoFault1Msg;
 import no.stelvio.example.services.v1.echoservice.binding2.EchoService;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import stelvio.example.echo.service.EchoServiceBi;
+
 // name="EchoService", wsdlLocation="WEB-INF/wsdl/stelvio-example-echo-service_EchoServiceWSEXP.wsdl", targetNamespace="http://www.stelvio.no/example/services/V1/EchoService/Binding2", serviceName="EchoServiceWSEXP_EchoServiceHttpService", portName="EchoServiceWSEXP_EchoServiceHttpPort" 
-@WebService(endpointInterface = "no.stelvio.example.services.v1.echoservice.binding2.EchoService")
-@HandlerChain(file = "no.stelvio.provider.EchoHandler.xml")
-@Stateless
+@WebService(endpointInterface = "no.stelvio.example.services.v1.echoservice.binding2.EchoService", wsdlLocation="WEB-INF/wsdl/no/stelvio/example/services/V1/stelvio-example-echo-service_EchoServiceWSEXP.wsdl")
+@HandlerChain(file = "EchoHandler.xml")
 public class Echo implements EchoService {
 
 	protected final Log log = LogFactory.getLog(this.getClass());
