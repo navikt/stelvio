1351ce6f7e68b96f01ec1b7a1b29e9bfbdd5ee22
dsc2812 <dsc2812@localhost>
2011-05-04 11:24:09 +0000

    dsc2812: changed tabs to spaces in python

--- int/maven/poms/maven-bounce-pom/bouncePerform.py
+++ int/maven/poms/maven-bounce-pom/bouncePerform.py
@@ -5,9 +5,9 @@ import subprocess
 
 if (len(sys.argv) < 9):
     print "[ERROR] Missing parameters. Usage: bouncePerform.py envs action da_config_verson wasSs wasIs wps joark onlyAppTarget [env_class zone bounce_app_servers bounce_dmgr_na install_ifix]"
-	print "[EXAMPLE 1] bouncePerform.py T1,T2 stop 7.0.1 true false true true false - this will stop WAS SS (both PensjonsCluster adn Joark) and WPS (all clusters) in T1 and T2"
-	print "[EXAMPLE 2] bouncePerform.py K1 start 7.0.1 false false true false true T sensitiv false true true  - this will start only dmgr and nodeagents for WPS in K1 after trying to install ifixes"
-	print "[EXAMPLE 3] bouncePerform.py K1 restart 7.0.1 true true false false false T sensitiv true true false - this will restart WAS SS and IS (only AppTarget) in K1, bounce dmgr and node agents without installation of ifixes"
+    print "[EXAMPLE 1] bouncePerform.py T1,T2 stop 7.0.1 true false true true false - this will stop WAS SS (both PensjonsCluster adn Joark) and WPS (all clusters) in T1 and T2"
+    print "[EXAMPLE 2] bouncePerform.py K1 start 7.0.1 false false true false true T sensitiv false true true  - this will start only dmgr and nodeagents for WPS in K1 after trying to install ifixes"
+    print "[EXAMPLE 3] bouncePerform.py K1 restart 7.0.1 true true false false false T sensitiv true true false - this will restart WAS SS and IS (only AppTarget) in K1, bounce dmgr and node agents without installation of ifixes"
     sys.exit(1)
 
 ENVS = sys.argv[1].split(",")
