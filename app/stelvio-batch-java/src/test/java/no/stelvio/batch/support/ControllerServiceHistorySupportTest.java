d6500988a72faa6c484d40cbafeab13edb21158a
f133008 <f133008@localhost>
2011-06-09 13:29:18 +0000

    cleaned up tests (batch hist)

--- app/stelvio-batch-java/src/test/java/no/stelvio/batch/support/ControllerServiceHistorySupportTest.java
+++ app/stelvio-batch-java/src/test/java/no/stelvio/batch/support/ControllerServiceHistorySupportTest.java
@@ -21,6 +21,7 @@ import org.hibernate.SessionFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 
@@ -28,25 +29,24 @@ public class ControllerServiceHistorySupportTest {
 
 	private static final String BATCH_TEST_CONTEXT = "btc-testbatch-context.xml";
 
-	DefaultBatchControllerService batchControllerService;
-	HibernateTemplate ht;
-	HibernateBatchHistRepository histRepository;
-	ControllerServiceHistorySupport controllerServiceHistorySupport;
+	private DefaultBatchControllerService batchControllerService;
+	private HibernateBatchHistRepository histRepository;
+	private ControllerServiceHistorySupport controllerServiceHistorySupport;
 	
-	private SessionFactory sessionFactory;
-
+	private ApplicationContext ctx;
+	
 	/**
 	 * 
 	 */
 	public ControllerServiceHistorySupportTest() {
 	}
 
-	public SessionFactory getSessionFactory() {
-		return sessionFactory;
+	public DefaultBatchControllerService getBatchControllerService() {
+		return batchControllerService;
 	}
 
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
+	public void setBatchControllerService(DefaultBatchControllerService batchControllerService) {
+		this.batchControllerService = batchControllerService;
 	}
 
 	public HibernateBatchHistRepository getHistRepository() {
@@ -56,29 +56,28 @@ public class ControllerServiceHistorySupportTest {
 	public void setHistRepository(HibernateBatchHistRepository histRepository) {
 		this.histRepository = histRepository;
 	}
+	
+	public ControllerServiceHistorySupport getControllerServiceHistorySupport() {
+		return controllerServiceHistorySupport;
+	}
+
+	public void setControllerServiceHistorySupport(ControllerServiceHistorySupport controllerServiceHistorySupport) {
+		this.controllerServiceHistorySupport = controllerServiceHistorySupport;
+	}
 
 	@Before
 	public void prepareForTests() {
 		
-	
-		batchControllerService = new DefaultBatchControllerService();
+		ctx = new ClassPathXmlApplicationContext(BATCH_TEST_CONTEXT);
+		batchControllerService = (DefaultBatchControllerService) ctx.getBean("DefaultBatchControllerService");
+		
 		batchControllerService.setBatchNameMap(new HashMap());
 		batchControllerService.getBatchNameMap().put("btc.testbatch.dummyBatch", "btc.testbatch.dummyBatch");
+		batchControllerService.getBatchNameMap().put("btc.testbatch.dummyBatchWithInputParameters", "btc.testbatch.dummyBatchWithInputParameters");
 
-		ClassPathXmlApplicationContext springContext = new ClassPathXmlApplicationContext(BATCH_TEST_CONTEXT);
-		sessionFactory = (SessionFactory) springContext.getBean("btc.testbatch.sessionFactory");
-		
-		ht = new HibernateTemplate(sessionFactory);
-		batchControllerService.setApplicationContext(springContext);
-		
-		histRepository = new HibernateBatchHistRepository();
+		histRepository = (HibernateBatchHistRepository) ctx.getBean("batchHistoryRepository");
 
-		histRepository.setHibernateTemplate(ht);
-		
-		controllerServiceHistorySupport = new ControllerServiceHistorySupport();
-		controllerServiceHistorySupport.setBatchHistoryRepository(histRepository);
-		
-		batchControllerService.setControllerServiceHistorySupport(controllerServiceHistorySupport);
+		controllerServiceHistorySupport = (ControllerServiceHistorySupport) ctx.getBean("ControllerServiceHistorySupport");
 
 	}
 
@@ -114,6 +113,22 @@ public class ControllerServiceHistorySupportTest {
 		List <BatchHistDO> batchHistory = (List<BatchHistDO>) histRepository.findByNameAndSlice("dummyBatch", 0);
 		assertEquals(batchHistory.get(0).getBatchname(), "dummyBatch");
 	}
+	
+	@Test
+	public void shouldSaveBatchHistToDatabaseBatchWithParams() {
+
+		//This "test" need a more complete case
+		batchControllerService.executeBatch("btc.testbatch.dummyBatchWithInputParameters", 0);
+
+		BatchHistDO batchHistDO = new BatchHistDO();
+		batchHistDO.setBatchname("dummyBatchWithInputParameters");
+
+		//TODO This tests the repository, not the implementation class
+		long batchID = histRepository.setHist(batchHistDO);
+		
+		List <BatchHistDO> batchHistory = (List<BatchHistDO>) histRepository.findByNameAndSlice("dummyBatchWithInputParameters", 0);
+		assertEquals(batchHistory.get(0).getBatchname(), "dummyBatchWithInputParameters");
+	}
 
 	//TODO rename test
 	//TODO Mange av disse testene er veldig ad-hoc og bærer preg av å ville teste database og ikke bare metodesignaturer
@@ -124,14 +139,10 @@ public class ControllerServiceHistorySupportTest {
 		Date fromDate = new Date();
 		//fromDate.setTime(fromDate.getTime()-1500000);
 		
-		//Providing the controller service with correct setup		ControllerServiceHistorySupport controller = new ControllerServiceHistorySupport();
-		controller.setBatchHistoryRepository(histRepository);
-		batchControllerService.setControllerServiceHistorySupport(controller);	
-
+		//Providing the controller service with correct setup
+		batchControllerService = (DefaultBatchControllerService) ctx.getBean("DefaultBatchControllerService");
 
 		batchControllerService.executeBatch(batchName, 0);
-		
-		ControllerServiceHistorySupport testController = batchControllerService.getControllerServiceHistorySupport();
 
 		Collection <BatchHistDO> batchHistory = fetchBatchHistory(batchName, fromDate, new Date());
 		Iterator<BatchHistDO> iterator = batchHistory.iterator();
@@ -141,12 +152,33 @@ public class ControllerServiceHistorySupportTest {
 		Assert.assertFalse(iterator.hasNext());
 
 	}
+	
+	@Test
+	public void endToEndTestBatchWithParams(){
+		String batchName = "btc.testbatch.dummyBatchWithInputParameters";
+		Date testDay = new Date();
+		Date fromDate = new Date();
+		//fromDate.setTime(fromDate.getTime()-1500000);
+		
+		//Providing the controller service with correct setup
+		batchControllerService = (DefaultBatchControllerService) ctx.getBean("DefaultBatchControllerService");
+		
+		batchControllerService.executeBatch(batchName, 0);
+		
+		Collection <BatchHistDO> batchHistory = fetchBatchHistory(batchName, fromDate, new Date());
+		Iterator<BatchHistDO> iterator = batchHistory.iterator();
+		Assert.assertTrue(iterator.hasNext());
+		BatchHistDO batchHistDO = iterator.next();
+		Assert.assertEquals(BatchStatus.BATCH_ERROR, Integer.parseInt(batchHistDO.getStatus()));
+		Assert.assertFalse(iterator.hasNext());
+
+	}
 		
 	@Test(expected = InvalidBatchEntryException.class)
 	public void shouldThrowExceptionIfNonexistingBatch(){
 
 		histRepository.findByNameAndSlice("non-existing-batch", 1);
-		
+
 		insertDummyBatches(controllerServiceHistorySupport);
 		fetchBatchHistory("dummyBatch", 1);	
 		
@@ -166,6 +198,21 @@ public class ControllerServiceHistorySupportTest {
 		long interval = retrievedHistory.getEndtime().getTime() - retrievedHistory.getStartTime().getTime();
 		
 		System.out.println("Runtime: " + interval + " ms");
+
+	}
+	
+	@Test 
+	public void shouldSaveAdditionalDataToExistingBatchHistoryEntryBatchWithParams(){
+		int finishedCode = 1;
+		
+		long batchNr = controllerServiceHistorySupport.saveInitialBatchInformation("dummyBatchWithInputParameters", 1);
+		BatchHistDO historyDO = controllerServiceHistorySupport.fetchBatchHistory(batchNr);
+		assertEquals(historyDO.getBatchname(), "dummyBatchWithInputParameters");
+		
+		controllerServiceHistorySupport.saveAdditionalBatchInformation(batchNr, finishedCode);
+				
+		BatchHistDO retrievedHistory = controllerServiceHistorySupport.fetchBatchHistory(batchNr);
+		long interval = retrievedHistory.getEndtime().getTime() - retrievedHistory.getStartTime().getTime();
 
 	}
 	
