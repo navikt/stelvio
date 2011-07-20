5f6be43d9e0918deb42d94249ed0aedb79490074
f133008 <f133008@localhost>
2011-07-20 09:47:47 +0000

    Added functionality to check for missing properties in all files in a directory

--- int/maven/plugins/maven-properties-plugin/src/main/java/no/nav/maven/plugins/DetectUnresolvedPropertiesMojo.java
+++ int/maven/plugins/maven-properties-plugin/src/main/java/no/nav/maven/plugins/DetectUnresolvedPropertiesMojo.java
@@ -2,16 +2,18 @@ package no.nav.maven.plugins;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
 /**
- * Goal which scans through a properties file, prints warning for unresolved properties and fails at the end if
+ * Goal which scans through a properties file or a directory containing property files, prints warning for unresolved properties and fails at the end if
  * one or more properties are unresolved
  * 
  * Delimiters on the form ${propertyname} and @propertyname@ are recognized.
@@ -19,6 +21,7 @@ import org.apache.maven.plugin.MojoFailureException;
  * @goal detect-unresolved-properties
  * 
  * @author Øystein Gisnås, Accenture
+ * @author person4fdbf4cece95, Accenture
  */
 public class DetectUnresolvedPropertiesMojo extends AbstractMojo {
 
@@ -34,26 +37,38 @@ public class DetectUnresolvedPropertiesMojo extends AbstractMojo {
 	
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-			String line;
+			File[] files;
+			if (file.isDirectory()){
+				files = file.listFiles((FileFilter) FileFilterUtils.directoryFileFilter());
+			}else{
+				files = new File[1];
+				files[0] = file;
+			}
+			
 			int numUnresolvedProperties = 0;
-			while ((line = reader.readLine()) != null) {
-				int index = 0;
-				for (Delimter delimiter : DELIMITERS) {
-					int startIndex = line.indexOf(delimiter.getBegin(), index);
-					if (startIndex > -1) {
-						index = startIndex + 1;
-						int endIndex = line.indexOf(delimiter.getEnd(), index);
-						if (endIndex > -1) {
-							index = endIndex + 1;
-							numUnresolvedProperties++;
-							getLog().warn("Found unresolved property '" + line.substring(startIndex + 1, endIndex) + "'");
-							getLog().info("Context: '" + line + "'");
+			
+			for (File inputFile : files){
+				BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+				String line;
+	
+				while ((line = reader.readLine()) != null) {
+					int index = 0;
+					for (Delimter delimiter : DELIMITERS) {
+						int startIndex = line.indexOf(delimiter.getBegin(), index);
+						if (startIndex > -1) {
+							index = startIndex + 1;
+							int endIndex = line.indexOf(delimiter.getEnd(), index);
+							if (endIndex > -1) {
+								index = endIndex + 1;
+								numUnresolvedProperties++;
+								getLog().warn("Found unresolved property '" + line.substring(startIndex + 1, endIndex) + "'");
+								getLog().info("Context: '" + line + "'");
+							}
 						}
 					}
 				}
+				reader.close();
 			}
-			reader.close();
 			
 			if (numUnresolvedProperties > 0) {
 				throw new MojoFailureException("Found " + numUnresolvedProperties + " unresolved properties");
