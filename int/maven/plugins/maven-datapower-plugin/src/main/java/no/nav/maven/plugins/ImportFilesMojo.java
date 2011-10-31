d53bc58670d325c4a5ba0d7be712bdcc1c2f8022
f133008 <f133008@localhost>
2011-10-31 09:42:10 +0000

    added possibility to upload single files

--- int/maven/plugins/maven-datapower-plugin/src/main/java/no/nav/maven/plugins/ImportFilesMojo.java
+++ int/maven/plugins/maven-datapower-plugin/src/main/java/no/nav/maven/plugins/ImportFilesMojo.java
@@ -2,6 +2,7 @@ package no.nav.maven.plugins;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.List;
 
 import no.nav.datapower.xmlmgmt.DeviceFileStore;
 import no.nav.datapower.xmlmgmt.XMLMgmtException;
@@ -9,6 +10,8 @@ import no.nav.datapower.xmlmgmt.XMLMgmtException;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+
+import java.util.Arrays;
 
 /**
  * Goal which imports a DataPower configuration into a specified device.
@@ -34,21 +37,23 @@ public class ImportFilesMojo extends AbstractDeviceMgmtMojo {
 		if (singleFiles){
 			File[] children = importDirectory.listFiles();
 			System.out.println(importDirectory + " mappe, lengde " + children.length);
-			for (File child : children) {
-				getLog().info("File = " + child);
+			List<File> filelist = Arrays.asList(children);
+			
+//			for (File child : children) {
+			
+//				getLog().info("File = " + child);
 				try {
-					DeviceFileStore childLocation = DeviceFileStore.fromString(child.getName());
-					DeviceFileStore location = childLocation != null ? childLocation : DeviceFileStore.LOCAL;
+					//DeviceFileStore childLocation = DeviceFileStore.fromString(child.getName());
+					//DeviceFileStore location = childLocation != null ? childLocation : DeviceFileStore.LOCAL;
+					DeviceFileStore location = DeviceFileStore.LOCAL;
 					getLog().info("Importing files to device location '" + location + "'");
-					getXMLMgmtSession().importFiles(child, location);
+					getXMLMgmtSession().importFiles(filelist, importDirectory, location);
 				} catch (XMLMgmtException e) {
 					throw new MojoExecutionException("Failed to import files from directory '" + importDirectory + "' to domain '" + getDomain() + "'", e);
 				}
-			}
+//			}
 		} else {
 			File[] children = importDirectory.listFiles((FileFilter)FileFilterUtils.directoryFileFilter());
-	
-			System.out.println(importDirectory + " mappe, lengde " + children.length);
 			for (File child : children) {
 				getLog().info("File = " + child);
 				try {
