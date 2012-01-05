69e58c3d20fcd1602245dbff489e0b02d22b78bf
f133008 <f133008@localhost>
2012-01-05 10:02:01 +0000

    IRPORT-1613 Skript for å hente versjoner og skrive dem til en fil

---
+++ int/maven/plugins/maven-datapower-config-generator-plugin/src/main/resources/scripts/GetVersions.py
@@ -0,0 +1,28 @@
+#****************************************************************************
+#
+# File name: GetVersions.py
+#
+# Description: This script is used to get the versions of modules deployed on 
+#				a specific WPS, and then writing them to a file.  The modules 
+#				are specified in the datapower-deploy pom.xml.  
+#
+#              Used by the plugin:
+#              maven-datapower-config-generator-plugin: no.nav.maven.plugins
+#
+# Author: test@example.com
+#
+#****************************************************************************
+
+variableNames	= sys.argv[0]
+
+varNameList = variableNames.split(",")
+
+filename = "temp.txt"
+FILE = open(filename,"w")
+
+for varName in varNameList:
+	AdminOperations = AdminControl.completeObjectName('WebSphere:*,type=AdminOperations')
+	version = AdminControl.invoke(AdminOperations, 'expandVariable', '${'+varName+'}')
+	FILE.writelines(varName+"="+version+"\n")
+
+FILE.close()
