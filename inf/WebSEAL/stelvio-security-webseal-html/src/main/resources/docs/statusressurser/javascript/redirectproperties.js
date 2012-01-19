abc8d759140c1f6bd7b6c713f30ec952f2f8d32f
b133553 <b133553@localhost>
2012-01-19 14:16:21 +0000

    Har ryddet opp i scriptet, og bygget en versjon som lar deg kjøre flere PSELV-miljø på samme WebSEAL instans (uten å bryte med eksisterende grensesnitt).

--- inf/WebSEAL/stelvio-security-webseal-html/src/main/resources/docs/statusressurser/javascript/redirectproperties.js
+++ inf/WebSEAL/stelvio-security-webseal-html/src/main/resources/docs/statusressurser/javascript/redirectproperties.js
@@ -1,92 +1,76 @@
+//------------------------------------------------------------------- Environment Properties --------------------------------------------------------------------------------------------
 
-//------------------------------------------------------------------- Properties --------------------------------------------------------------------------------------------
-
-//Pensjon properties
+// Default PSELV junction name, usually "/dinpensjon".
 var pselvJunction = "/dinpensjon";
+
+// Default PSELV context root, usually "/pselv".
 var pselvContextRoot = "/pselv";
-var pselvTilgangNektetURI = "/tilleggsfunksjonalitet/tilgangnektet.jsf?_flowId=tilgangnektet-flow";
+
+// Path to "access denied" page in PSELV.
+var pselvTilgangNektetURI = "/tilleggsfunksjonalitet/tilgangnektet.jsf";
+
+// Path to "login" page in PSELV.
+var endagspassordLoginURI = "/tilleggsfunksjonalitet/innlogging.jsf";
+
+// Endagspassord login is usually set up identically to PSELV.
 var endagspassordJunction = pselvJunction;
 var endagspassordContextRoot = pselvContextRoot;
-var endagspassordLoginURI = "/tilleggsfunksjonalitet/innlogging.jsf?_flowId=innlogging-flow"; 
-var minsideLogoutUrl = "https://applikasjoner-t.nav.no/fim/sps/PSELV-MinIDtest/saml20/sloinitial?RequestBinding=HTTPArtifact";
+
+// Endpoint for federated single log-off.  Environment specific.
+var minSideLogoutURL = "https://applikasjoner-t0.nav.no/FIM/sps/PSELVIDPorten/saml20/soapinitial?RequestBinding=HTTPRedirect";
 
 //------------------------------------------------------------------- End Properties --------------------------------------------------------------------------------------------
 
-//Returns the URL for the PSELV application
-function getPselvURL(){
-	return pselvJunction + pselvContextRoot;
-}
-
-function getMinSideLogoutURL(){
-	return minsideLogoutUrl;
-}
-
-//Returns the URL for the endagspassord authentication login page.
-function endagsPassordLoginURL() {
-	return endagspassordJunction + endagspassordContextRoot + endagspassordLoginURI;
-}
-
-function defaultLoginURL(){
-	return endagsPassordLoginURL();
-}
-
-
-//Returns the URL to the page that a user should receive when denied access to PSELV.
-function pselvTilgangNektetURL() {
-	return pselvJunction + pselvContextRoot + pselvTilgangNektetURI;
-}
-
-// Returns the junction name for the current request
+// Returns the junction name for the current request.  This is needed to use a shared WebSEAL in test environments.
 function getRequestedJunction(){
-	var url = document.URL;
-	var array = url.split("/");
-	if(array.length > 3){
-		var junction = array[3] 
-		return "/" + junction;
-	} else {
-		return "";
-	}
+		var url = document.URL;
+		var array = url.split("/");
+		if(array.length > 3){
+				var junction = array[3]
+				return "/" + junction;
+		} else {
+				return "";
+		}
 }
 
-
-function getDefaultURLAfterAuthentication(){
-	var requestedJunction = getRequestedJunction();
-	if (requestedJunction == pselvJunction){
-	
-		return getPselvURL();
-		
-	} else if (requestedJunction == "/junctionToAnotherApplication" ){
-		
-		return "/junctionToAnotherEai/default.html";
-		
-	} else {
-		
-		return "";
-	}
+// Builds the base URL for the PSELV instance serving the current request
+function getPselvURL(){
+		return getRequestedJunction() + pselvContextRoot;
 }
 
+// Returns the logout URL (for the default PSELV instance).
+function getMinSideLogoutURL(){
+		return minSideLogoutURL;
+}
+
+// Builds the URL to the default login page for the PSELV instance serving the current request.
+function endagsPassordLoginURL() {
+		var requestedJunction = getRequestedJunction();
+		if (requestedJunction == pselvJunction) {
+				return endagspassordJunction + endagspassordContextRoot + endagspassordLoginURI;
+		} else {
+				return requestedJunction + endagspassordContextRoot + endagspassordLoginURI;
+		}
+}
+
+// Builds the URL to the default access denied page for the PSELV instance serving the current request.
+function pselvTilgangNektetURL() {
+		return getPselvURL() + pselvTilgangNektetURI;
+}
+
+// Change URL string from http to https.
 function redirectToHttps(){
-	var url = document.URL;
-	var str = url.replace(/http/,"https");
-	return str; 
+		var url = document.URL;
+		var str = url.replace(/http:/,"https:");
+		return str;
 }
 
-//Returns the URL to the login page of the appropriate External Authentication Interface matching the junction the user is trying to access. 
+// After authentication, just hand control back to PSELV.
+function getDefaultURLAfterAuthentication(){
+		return getPselvURL();
+}
+
+// Before authentication, send the user to the correct login page.
 function getAuthenticationURL() {
-	var requestedJunction = getRequestedJunction();
-	if (requestedJunction == pselvJunction){
-	
 		return endagsPassordLoginURL();
-		
-	} else if (requestedJunction == "/junctionToAnotherApplication" ){
-		
-		return "/junctionToAnotherEai/loginpage.html";
-		
-	} else {
-		
-		return defaultLoginURL();
-	}
 }
-
-
-
