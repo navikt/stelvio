7cf8bb982cef2c764d992ac1d596e0c49d425075
g115484 <g115484@localhost>
2012-01-21 17:14:20 +0000

    IRPORT-91: Legg til jaxws-artifact som vedlegg til service-specificaiton

--- incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
+++ incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/Echo.java
@@ -4,15 +4,15 @@ import javax.jws.HandlerChain;
 import javax.jws.WebService;
 
 import no.stelvio.common.context.RequestContextHolder;
-import no.stelvio.example.services.v1.echoservice.binding2.EchoEchoFault1Msg;
-import no.stelvio.example.services.v1.echoservice.binding2.EchoService;
+import no.stelvio.example.services.v1.echoservice.exampleprovider.EchoFault;
+import no.stelvio.example.services.v1.echoservice.exampleprovider.EchoService;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import stelvio.example.echo.service.EchoServiceBi;
 
-@WebService(endpointInterface = "no.stelvio.example.services.v1.echoservice.binding2.EchoService",
+@WebService(endpointInterface = "no.stelvio.example.services.v1.echoservice.exampleprovider.EchoService",
 			wsdlLocation="WEB-INF/wsdl/no/stelvio/example/services/V1/stelvio-example-echo-service_EchoServiceWSEXP.wsdl",
 			targetNamespace="http://www.stelvio.no/example/services/V1/EchoService/Binding2",
 			serviceName="EchoServiceWSEXP_EchoServiceHttpService",
@@ -24,7 +24,7 @@ public class Echo implements EchoService {
 	
 	EchoServiceBi echoServiceBi;
 
-	public String echo(String input) throws EchoEchoFault1Msg {
+	public String echo(String input) throws EchoFault {
 
 		log.debug("Echoing message:" + input);
 		log.debug("Context: " + RequestContextHolder.currentRequestContext());
--- incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/EchoLTPA.java
+++ incubator/stelvio-example/stelvio-example-echo/stelvio-example-echo-provider/src/main/java/stelvio/example/echo/provider/EchoLTPA.java
@@ -2,13 +2,13 @@ package stelvio.example.echo.provider;
 
 import javax.jws.WebService;
 
-import no.stelvio.example.services.v1.echoservice.binding2.EchoEchoFault1Msg;
+import no.stelvio.example.services.v1.echoservice.exampleprovider.EchoFault;
 
 // name="EchoService", wsdlLocation="WEB-INF/wsdl/stelvio-example-echo-service_EchoServiceWSEXP.wsdl", targetNamespace="http://www.stelvio.no/example/services/V1/EchoService/Binding2", serviceName="EchoServiceWSEXP_EchoServiceHttpService", portName="EchoServiceWSEXP_EchoServiceHttpPort" 
-@WebService(endpointInterface="no.stelvio.example.services.v1.echoservice.binding2.EchoService")
+@WebService(endpointInterface="no.stelvio.example.services.v1.echoservice.exampleprovider.EchoService")
 public class EchoLTPA {
 
-	public String echo(String input) throws EchoEchoFault1Msg {
+	public String echo(String input) throws EchoFault {
 		return input;
 	}
 }
