8057da7dfba8d89491906857f62e5df8998c4213
r132736 <r132736@localhost>
2012-03-29 15:48:51 +0000

    Nå fungerer enhetstesting i windows

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/runTests.bat
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/runTests.bat
@@ -1,1 +1,6 @@
-C:\apps\Java\j2re1.4.2\bin\java.exe -Dlogging.level=EXCEPTION -cp C:\IBM\WID7_WTE\runtimes\bi_v7\optionalLibraries\jython\jython.jar org.python.util.jython runTests.py
+@echo off 
+set path=lib\x64;%path%
+
+ansicon C:\IBM\WID7_WTE\runtimes\bi_v7\java\bin\java.exe -Dlogging.level=EXCEPTION -cp C:\IBM\WID7_WTE\runtimes\bi_v7\optionalLibraries\jython\jython.jar org.python.util.jython runTests.py
+
+pause
