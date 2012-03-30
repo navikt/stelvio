b6ec126d29a294401b7cb278406d9f5a13101c7f
r132736 <r132736@localhost>
2012-03-30 06:26:07 +0000

    .bat skriptet tar nå -exit som argument, og da slipper du å måtte trykke en tast for at skriptet skal avslutte.

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/runTests.bat
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/runTests.bat
@@ -1,6 +1,8 @@
 @echo off 
-set path=lib\x64;%path%
+set path=lib\WindowsCmdColors_x64;%path%
 
 ansicon C:\IBM\WID7_WTE\runtimes\bi_v7\java\bin\java.exe -Dlogging.level=EXCEPTION -cp C:\IBM\WID7_WTE\runtimes\bi_v7\optionalLibraries\jython\jython.jar org.python.util.jython runTests.py
 
+IF (%1)==(-exit) GOTO END
 pause
+:END
