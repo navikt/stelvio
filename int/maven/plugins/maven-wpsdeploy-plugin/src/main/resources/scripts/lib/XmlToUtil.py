5779208b1f97fa0fbc254df18d3d6e5fc800a644
r132736 <r132736@localhost>
2012-04-02 13:05:49 +0000

    IRPORT-2402: DownloadEARFiles.py er ikke i bruk lenger. Og DownloadEARFiles.py er den eneste som refererer til pomUtil.py

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/lib/XmlToUtil.py
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/lib/XmlToUtil.py
@@ -1,18 +1,4 @@
 from lib.XMLUtil import parseXML
-from lib.pomUtil import PomModule
-def pomXmlDependensiesToPomModules(pom, repository):
-	modules = []
-	
-	xml = parseXML(pom)
-	
-	for d in xml.findAll("dependency"):
-		artifactId = d.getChild('artifactId').get()
-		groupId = d.getChild('groupId').get()
-		version = d.getChild('version').get()
-		type = d.getChild('type').get()
-	
-		modules.append(PomModule(artifactId=artifactId, groupId=groupId, version=version, type=type, repository=repository))
-	return modules
 	
 def blaGroupXmlToStringList(blaGroupXmlFile):
 	xml = parseXML(blaGroupXmlFile)
