d693ae62f328c306f14f3387f66de4d2652f74de
r132736 <r132736@localhost>
2012-04-14 15:33:48 +0000

    IRPORT-2102: La til timer

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/DeleteBLAGroups.py
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/DeleteBLAGroups.py
@@ -1,22 +1,31 @@
 from lib.saveUtil import save
+from lib.timerUtil import Timer
 import lib.logUtil as log
 l = log.getLogger(__name__)
 
 def main():
-	for blaGroupId in getBLAGrups():
-		l.info('Deleting CompUnits in', blaGroupId)
-		for unitId in getCompUnitsInGroup(blaGroupId):
-			if deleteCompUnit(blaGroupId, unitId):
-				l.info('Deleted', unitId)
-			else:
-				l.error('Could not delete', unitId)
-				
-		if deleteEmptyBlaGroup(blaGroupId):
-			l.info('Deleted', blaGroupId)
-		else:
-			l.error('An error ocured when deleting', blaGroupId)
+	myTimer = Timer()
+	l.info('Getting BLA-groups...')
+	blaGroups = getBLAGrups()
+	l.info('It took', myTimer, 'to get', len(blaGroups), 'BLA-groups.')
 	
-	save()
+	if blaGroups:
+		for blaGroupId in blaGroups:
+			l.info('Deleting CompUnits in', blaGroupId)
+			for unitId in getCompUnitsInGroup(blaGroupId):
+				if deleteCompUnit(blaGroupId, unitId):
+					l.info('Deleted', unitId)
+				else:
+					l.error('Could not delete', unitId)
+					
+			if deleteEmptyBlaGroup(blaGroupId):
+				l.info('Deleted', blaGroupId)
+			else:
+				l.error('An error ocured when deleting', blaGroupId)
+		
+		save()
+	else:
+		l.info('Found no BLA-groups, exiting!')
 
 def getBLAGrups():
 	allBLAs = AdminTask.listBLAs().splitlines()
