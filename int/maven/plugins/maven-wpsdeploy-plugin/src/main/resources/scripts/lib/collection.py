73f1377c65d1ab6f1b82e7c757393f553edc9317
r132736 <r132736@localhost>
2012-04-14 15:33:23 +0000

    IRPORT-2102: La til sortering på nøkkel og reversering i sorted metoden

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/lib/collection.py
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/lib/collection.py
@@ -1,5 +1,27 @@
-def dict(sequence):
+'''http://en.wikipedia.org/wiki/Insertion_sort'''
+
+def dict(sequence, **kwargs):
 	returnDict = {}
 	for key, value in sequence:
 		returnDict[key] = value
 	return returnDict
+	
+	
+def sorted(list, key=None, reverse=False):
+	if key:
+		if not list: 
+			return []
+		else:
+			pivot = list[0]
+			pivotKey = key(pivot)
+			lesser = sorted([x for x in list[1:] if key(x) < pivotKey], key)
+			greater = sorted([x for x in list[1:] if key(x) >= pivotKey], key)
+			list = lesser + [pivot] + greater
+	else:
+		list = list[:]
+		list.sort()
+		
+	if reverse:
+		list.reverse()
+	
+	return list
