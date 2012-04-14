489db856acd6d78b1d65185b9d8e50d150164b26
r132736 <r132736@localhost>
2012-04-14 19:21:49 +0000

    IRPORT-2102: Bruker nå den nye cvs filen isdede for EARsToDeploy mappen.

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/java/no/nav/maven/plugin/wpsdeploy/plugin/mojo/RemoveBPMojo.java
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/java/no/nav/maven/plugin/wpsdeploy/plugin/mojo/RemoveBPMojo.java
@@ -1,6 +1,5 @@
 package no.nav.maven.plugin.wpsdeploy.plugin.mojo;
 
-import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.codehaus.plexus.util.cli.Commandline;
@@ -9,33 +8,16 @@ import org.codehaus.plexus.util.cli.Commandline;
  * Goal that removes the modules containing short-running business processes.
  * Note: The modules which are removed are defined statically inside this class' constructor.
  * 
- * @author test@example.com
- * 
  * @goal remove-bp
  * @requiresDependencyResolution
  */ 
 
 public class RemoveBPMojo extends WebsphereUpdaterMojo {
-
-	private String modulesToRemove;
-
-	/* 
-	 * Builds up a command line to send as a argument to the RemoveOldBPModule.py script
-	 */
 	@Override
 	protected void applyToWebSphere(Commandline wsadminCommandLine) throws MojoExecutionException, MojoFailureException {
 		
-		modulesToRemove = "";
-		
-		for (Artifact a : artifacts){
-			if (a.getArtifactId().contains("-microflow-") 
-					|| a.getArtifactId().equals("nav-bsrv-frg-hentinstitusjonsoppholdliste")){
-				modulesToRemove += a.getArtifactId() +"="+ a.getVersion() +" ";
-			}
-		}
-		
 		Commandline.Argument arg = new Commandline.Argument();
-		arg.setLine("RemoveOldBPModule.py " + modulesToRemove);
+		arg.setLine("RemoveOldBPModule.py");
 		wsadminCommandLine.addArg(arg);
 		
 		executeCommand(wsadminCommandLine);
--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/RemoveOldBPModule.py
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/RemoveOldBPModule.py
@@ -1,66 +1,14 @@
-#****************************************************************************
-#							                              
-# File name: RemoveOldBPModule.py 			                    
-#							                    
-# Description: This script removes the older version of the business process                     
-# 	       provided as argument. 
-# 	       
-# 	       Used by the plugin: 
-# 	       maven-wpsdeploy-plugin:remove-bp (RemoveBPMojo.java)
-#							                    
-# Author:      test@example.com
-#	       test@example.com		                   
-#							 		
-#****************************************************************************
 import sys, re
+import lib.scaModuleUtil as sca
+import lib.applicationUtil as app
 import lib.logUtil as log
 l = log.getLogger(__name__)
 
-argumentList = sys.argv[1:]
-appsInstalled = AdminApp.list()
-
-def main():
-	for APPLICATION_NAME, NEW_VERSION in [x.split('=') for x in argumentList]:
-		l.info("RemoveOldBPModule: Checking whether older version of application " + APPLICATION_NAME + " exists.")
-
-		appToUninstall = appsInstalled[appsInstalled.find(APPLICATION_NAME, 0, len(appsInstalled)):].split("\n")[0]
-		
-		if (len(appToUninstall) < 2):
-			l.info("Application not found!")
-			continue
-
-		if not outdatedVersion(appToUninstall, NEW_VERSION):
-			l.info("No newer version to install!")
-			continue
-				
-		l.info("Uninstalling application: " + appToUninstall)
-		try:
-			AdminApp.uninstall(appToUninstall)
-		except:
-			l.exception("Uninstall failed!")
-		AdminConfig.save()
-	
-def outdatedVersion(appToUninstall, newVersion):
-	'''
-	Checks if the application currently installed on the server is older 
-	than the one in the current busrelease, if so - return True, else return False
-	'''
-	pattern = re.compile(r'\s+')
-	oldVersion = re.sub(pattern, '', AdminApp.view(appToUninstall, '-buildVersion').replace('Application Build ID:  ', ''))
-	
-	if (newVersion.find("SNAPSHOT") >= 0):
-		l.info("SNAPSHOT version, will uninstall: " + appToUninstall)
-		return True
-				
-	elif (oldVersion[0] == 'Unknown'):
-		l.info("Unknown version, will uninstall: " + appToUninstall)
-		return True
-		
-	elif (oldVersion == newVersion):
-		l.info("Equal version, will not uninstall: " + appToUninstall)
-		return False
-	else:
-		l.info("Different version, will uninstall: " + appToUninstall)
-		return True
-
-main()
+microflowREGEX = re.compile('-microflow-')
+for scaModule in sca.getModulesToBeInstalled():
+	if microflowREGEX.search(scaModule.shortName):
+		l.info('Checking whether older version of application %s exists...' % scaModule.shortName)
+		if scaModule.uninstallOldVersion:
+			app.uninstall(scaModule)
+		else:
+			l.info('Same version or not installed, continuing without uninstalling.')
