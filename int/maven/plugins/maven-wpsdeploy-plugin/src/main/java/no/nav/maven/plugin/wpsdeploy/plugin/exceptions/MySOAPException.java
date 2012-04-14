382257eb08e37eab3de08d82efffcea82cd65921
r132736 <r132736@localhost>
2012-04-14 15:16:08 +0000

    IRPORT-2102: Endet på exceptions slik at det nå har samme konstruktører som super klassen.

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/java/no/nav/maven/plugin/wpsdeploy/plugin/exceptions/MySOAPException.java
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/java/no/nav/maven/plugin/wpsdeploy/plugin/exceptions/MySOAPException.java
@@ -1,9 +1,21 @@
 package no.nav.maven.plugin.wpsdeploy.plugin.exceptions;
 
 public class MySOAPException extends RuntimeException {
-	private static final long serialVersionUID = 460749273498346737L;
-	public MySOAPException() {}
-	public MySOAPException(String message) {
-		super(message);
+	private static final long serialVersionUID = 7420416597198479884L;
+
+	public MySOAPException() {
+		super();
+	}
+
+	public MySOAPException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public MySOAPException(String arg0) {
+		super(arg0);
+	}
+
+	public MySOAPException(Throwable arg0) {
+		super(arg0);
 	}
 }
