061c3ad0750633dc23ddef92d46136f342b121bb
r132736 <r132736@localhost>
2012-05-18 05:40:56 +0000

    Bruker nå lib.ibmfixes.py

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/Executor.py
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/Executor.py
@@ -1,35 +1,10 @@
 import sys, os, re
+import exceptions
 from lib.timerUtil import Timer
 import lib.logUtil as log
 import lib.exceptionUtil
-import exceptions
+import lib.ibmfixes
 
-
-# Define False, True for all scripts
-try:
-    True and False
-except NameError:
-    class bool(type(1)):
-        def __init__(self, val = 0):
-            if val:
-                type(1).__init__(self, 1)
-            else:
-                type(1).__init__(self, 0)
-        def __repr__(self):
-            if self:
-                return 'True'
-            else:
-                return 'False'
-
-        __str__ = __repr__
-
-    __builtin__.bool = bool
-    __builtin__.False = bool(0)
-    __builtin__.True = bool(1)
-# End fix #3
-
-glo = globals()
-loc = locals()
 def Executor_main():
 	scriptDir = Executor_getScriptDir()	
 	scriptPath = scriptDir + Executor_getScriptName()
@@ -55,7 +30,7 @@ def Executor_executeScript(scriptPath):
 	myTimer = Timer()
 	
 	try:
-		execfile(scriptPath, glo, loc)
+		execfile(scriptPath)
 	except:
 		ex = lib.exceptionUtil.getException()
 		if ex.checkInstance(exceptions.SystemExit):
