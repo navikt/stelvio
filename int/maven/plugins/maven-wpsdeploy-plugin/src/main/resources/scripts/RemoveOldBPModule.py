c9ca451835beddce77c30f28872d13991d1d9c9d
r132736 <r132736@localhost>
2012-05-18 05:40:35 +0000

    Hadde glemt å lagre endringene etter at de gamle prosessene hadde blitt avinstallert

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/RemoveOldBPModule.py
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/RemoveOldBPModule.py
@@ -1,14 +1,20 @@
 import sys, re
 import lib.scaModuleUtil as sca
 import lib.applicationUtil as app
+from  lib.saveUtil import save
 import lib.logUtil as log
 l = log.getLogger(__name__)
 
 microflowREGEX = re.compile('-microflow-')
-for scaModule in sca.getModulesToBeInstalled():
+scaModules = sca.getModulesToBeInstalled()
+for scaModule in scaModules:
 	if microflowREGEX.search(scaModule.shortName):
 		l.info('Checking whether older version of application %s exists...' % scaModule.shortName)
 		if scaModule.uninstallOldVersion:
 			app.uninstall(scaModule)
+			scaModule.uninstallOldVersion = False
 		else:
 			l.info('Same version or not installed, continuing without uninstalling.')
+
+sca.setModulesToBeInstalled(scaModules)
+save()
