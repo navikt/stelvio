efd01e536718100f1c18fc7d08b140c48519f0c6
w132847 <w132847@localhost>
2012-06-07 11:11:53 +0000

    IRPORT-2450 - IRPORT-2821 Refaktorert testkode. Endret konfigurasjon og implementasjon av lyttere. Renamet noen filer.

---
+++ app/stelvio-common-java/src/test/java/no/stelvio/common/cache/support/DefaultCacheEventListener.java
@@ -0,0 +1,69 @@
+package no.stelvio.common.cache.support;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import net.sf.ehcache.CacheException;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.event.CacheEventListener;
+
+/**
+ * 
+ * @author person5dc3535ea7f4
+ */
+public class DefaultCacheEventListener implements CacheEventListener{
+
+	private static final Log LOG = LogFactory.getLog(DefaultCacheEventListener.class);
+
+	private int onCachingCounter;
+	
+	/**
+	 * Get onChachingCounter.
+	 * 
+	 * @return onChachingCounter
+	 */
+	public int getOnCachingCounter() {
+		return onCachingCounter;
+	}
+
+	/**
+	 * Reset onChachingCounter.
+	 */
+	public void resetOnCachingCounter() {
+		this.onCachingCounter = 0;
+	}
+
+	public void notifyElementPut(Ehcache arg0, Element arg1) throws CacheException {
+		System.out.println("DefaultCacheEventListener intercepted: notifyElementPut()");
+		onCachingCounter++;	
+		LOG.debug("Cache hit, incrementing onChacingCounter");		
+	}
+	
+	public Object clone(){
+		throw new UnsupportedOperationException();
+	}
+	
+	public void dispose(){}
+
+	public void notifyElementEvicted(Ehcache arg0, Element arg1) {
+		// No action required
+	}
+
+	public void notifyElementExpired(Ehcache arg0, Element arg1) {
+		// No action required
+	}
+
+	public void notifyElementRemoved(Ehcache arg0, Element arg1) throws CacheException {
+		// No action required
+	}
+
+	public void notifyElementUpdated(Ehcache arg0, Element arg1) throws CacheException {
+		// No action required
+	}
+
+	public void notifyRemoveAll(Ehcache arg0) {
+		// No action required
+	}
+	
+}
