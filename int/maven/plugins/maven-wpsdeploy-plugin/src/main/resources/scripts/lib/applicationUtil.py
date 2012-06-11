cc8314e203030f76b3c4ef6eeaf52da2218de2e1
r132736 <r132736@localhost>
2012-06-11 14:45:13 +0000

    La til mer logikk for å kunne hente ut informasjon om server/cluster osv.

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/lib/applicationUtil.py
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/lib/applicationUtil.py
@@ -5,30 +5,59 @@ l = log.getLogger(__name__)
 clusterName = ""
 
 def uninstall(scaModule):
-	l.info('Uninstalling %s!' % scaModule)
+	l.info('Uninstalling %s (%s)!' % (scaModule.shortName, scaModule.version))
 	AdminApp.uninstall(scaModule.applicationName)
 	l.debug('Uninstall complete')
 		
 def install(scaModule, verbose=False):
+	clusterName = getApplicationClusterName()
 	if verbose:
-		options = "[ -verbose -cluster %s -distributeApp ]" % getClusterName()
+		options = "[ -verbose -cluster %s -distributeApp ]" % clusterName
 	else:
-		options = "[ -cluster %s -distributeApp ]" % getClusterName()
+		options = "[ -cluster %s -distributeApp ]" % clusterName
 
-	l.info("Installing %s with options [%s] on %s." % (scaModule, options, getClusterName()))
+	l.info("Installing %s (%s) with options [%s] on %s." % (scaModule.shortName, scaModule.version, options, clusterName))
 	AdminApp.install(scaModule.earPath, options)
 	l.debug('Installation complete')
-	
-def getClusterName():
+
+def isApplicationClusterRunning():
+	cellName = getName(getCell())
+	cluster = AdminControl.completeObjectName('cell=%s,type=Cluster,name=%s,*' % (cellName, getApplicationClusterName()))
+	status = AdminControl.getAttribute(cluster, 'state')
+	return status == 'websphere.cluster.running'
+
+def getApplicationClusterName():
+	targetCluster = 'AppTarget'
 	global clusterName
 	if clusterName:
 		return clusterName
 	else:	
-		clusters = AdminConfig.list('ServerCluster', AdminConfig.list('Cell')).splitlines()
+		clusters = AdminConfig.list('ServerCluster', getCell()).splitlines()
 		for cluster in clusters:
-			if re.search('AppTarget', cluster):
-				clusterName = AdminConfig.showAttribute(cluster, 'name')
-		if not clusterName:
-			raise Exception, "No cluster name found"
+			if re.search(targetCluster, cluster):
+				return getName(cluster)
 		else:
-			return clusterName
+			raise Exception, targetCluster + " cluster not found!"
+
+def getApplicationManager(nodeName):
+	cellName = getName(getCell())
+	serverName = getName(getAppServer(nodeName))
+	return AdminControl.queryNames('cell=%s,node=%s,type=ApplicationManager,process=%s,*' % (cellName, nodeName, serverName))
+
+def getCell():
+	cell = AdminConfig.list('Cell')
+	if '\n' in cell:
+		raise ValueError('The deployscript only handles one cell!')
+	return cell
+
+def getName(configElement):
+	return AdminConfig.showAttribute(configElement,'name')
+
+def getNodes():
+	cell = getCell()
+	return [n for n in AdminConfig.list('Node',cell).splitlines() if not re.search('CellManager', n)]
+
+def getAppServer(nodeName):
+	for server in AdminConfig.getid('/Node:'+ nodeName +'/Server:/').splitlines():
+		if re.search('AppTarget', server):
+			return server
