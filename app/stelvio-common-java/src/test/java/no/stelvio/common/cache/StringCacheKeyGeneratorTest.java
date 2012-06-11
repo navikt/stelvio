d449716c153436c262100112cea1fb9c8cc6d8e0
h114522 <h114522@localhost>
2012-06-11 10:23:28 +0000

    IRPORT-2450, IRPORT-2819: Splittet testene i 2 klasser etter review.

--- app/stelvio-common-java/src/test/java/no/stelvio/common/cache/StringCacheKeyGeneratorTest.java
+++ app/stelvio-common-java/src/test/java/no/stelvio/common/cache/StringCacheKeyGeneratorTest.java
@@ -15,11 +15,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:common-java_test_cache_beans_cache_annotations.xml" })
-public class DefaultEhCacheKeyGeneratorTest {
+public class StringCacheKeyGeneratorTest {
 
 	@Autowired
-	@Qualifier(value = "hashCodeCacheKeyGeneratorNAVAnsatt")
-	private NavAnsattServiceBi hashCodeCacheKeyGeneratorNAVAnsattService;
+	@Qualifier(value = "stringCacheKeyGeneratorNavAnsatt")
+	private NavAnsattServiceBi stringCacheKeyGeneratorNavAnsattService;
 
 	@Autowired
 	private CacheManager cacheManager;
@@ -31,33 +31,33 @@ public class DefaultEhCacheKeyGeneratorTest {
 
 	@DirtiesContext
 	@Test
-	public void shouldReturnTheSameNumbersWhenTheCacheIsHitWhenUsingDefaultHashCodeKeyGenerator() {
+	public void shouldReturnTheSameNumbersWhenTheCacheIsHitWhenUsingStringCacheKeyGenerator() {
 		TestRequest request = new TestRequest("D101768", "1589");
-		String response = hashCodeCacheKeyGeneratorNAVAnsattService.hentNAVAnsattFagomradeListe(request);
+		String response = stringCacheKeyGeneratorNavAnsattService.hentNAVAnsattFagomradeListe(request);
 		assertThat(response, is("D101768 - 1589 - #1"));
 		
 		request = new TestRequest("A100474", "1289");
-		response = hashCodeCacheKeyGeneratorNAVAnsattService.hentNAVAnsattFagomradeListe(request);
+		response = stringCacheKeyGeneratorNavAnsattService.hentNAVAnsattFagomradeListe(request);
 		assertThat(response, is("A100474 - 1289 - #2"));
 		
 		request = new TestRequest("A100474", "1289");
-		response = hashCodeCacheKeyGeneratorNAVAnsattService.hentNAVAnsattFagomradeListe(request);
+		response = stringCacheKeyGeneratorNavAnsattService.hentNAVAnsattFagomradeListe(request);
 		assertThat(response, is("A100474 - 1289 - #2"));
 		
 		request = new TestRequest("D101858", "0689");
-		response = hashCodeCacheKeyGeneratorNAVAnsattService.hentNAVAnsattFagomradeListe(request);
+		response = stringCacheKeyGeneratorNavAnsattService.hentNAVAnsattFagomradeListe(request);
 		assertThat(response, is("D101858 - 0689 - #3"));
 		
 		request = new TestRequest("D101868", "0589");
-		response = hashCodeCacheKeyGeneratorNAVAnsattService.hentNAVAnsattFagomradeListe(request);
+		response = stringCacheKeyGeneratorNavAnsattService.hentNAVAnsattFagomradeListe(request);
 		assertThat(response, is("D101868 - 0589 - #4"));
 		
 		request = new TestRequest("D101868", "0589");
-		response = hashCodeCacheKeyGeneratorNAVAnsattService.hentNAVAnsattFagomradeListe(request);
+		response = stringCacheKeyGeneratorNavAnsattService.hentNAVAnsattFagomradeListe(request);
 		assertThat(response, is("D101868 - 0589 - #4"));
 		
 		request = new TestRequest("A100564", "0389");
-		response = hashCodeCacheKeyGeneratorNAVAnsattService.hentNAVAnsattFagomradeListe(request);
+		response = stringCacheKeyGeneratorNavAnsattService.hentNAVAnsattFagomradeListe(request);
 		assertThat(response, is("A100564 - 0389 - #5"));
 	}
 
