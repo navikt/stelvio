55d7cfc50962b6d305fa2278cbceeff3cb7a3429
f133008 <f133008@localhost>
2012-11-16 14:16:41 +0000

    Har laget oppsettet for plugin for forsyning av endpoints til service gateway

---
+++ int/maven/plugins/maven-service-registry-updater-plugin/src/main/java/no/nav/maven/plugins/GenerateServiceRegistryFileMojo.java
@@ -0,0 +1,23 @@
+package no.nav.maven.plugins;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Goal which generates a service registry file.
+ * 
+ * @goal generate-file
+ * @phase compile
+ * 
+ * @author person4fdbf4cece95, Accenture
+ */
+public class GenerateServiceRegistryFileMojo extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
