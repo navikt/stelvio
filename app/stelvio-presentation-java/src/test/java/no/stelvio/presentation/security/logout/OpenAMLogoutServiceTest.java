14c1e68e2de76cff5a6d74ef22e7a4eec1858bcc
a100622 <a100622@localhost>
2013-09-09 14:23:42 +0000

    PKIGUAN-117 testdekning logoutservice

---
+++ app/stelvio-presentation-java/src/test/java/no/stelvio/presentation/security/logout/OpenAMLogoutServiceTest.java
@@ -0,0 +1,64 @@
+package no.stelvio.presentation.security.logout;
+
+import static org.junit.Assert.*;
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+
+public class OpenAMLogoutServiceTest {
+
+    @Test
+    public void withoutOpenAM() throws Exception {
+
+        OpenAMLogoutService openAMLogoutService = new OpenAMLogoutService();
+        openAMLogoutService.setLogoutFromOpenAM(false);
+        openAMLogoutService.setPointOfContactHostAddress("http://testaddress");
+        openAMLogoutService.setLogoutAction("testaction");
+        openAMLogoutService.afterPropertiesSet();
+        String result = openAMLogoutService.buildRedirectUrl("http://testurl");
+
+        assertEquals("testaction?logoutExitPage=http://testurl", result);
+    }
+
+    @Test
+    public void withOpenAM() throws Exception {
+
+        OpenAMLogoutService openAMLogoutService = new OpenAMLogoutService();
+        openAMLogoutService.setLogoutFromOpenAM(true);
+        openAMLogoutService.setPointOfContactHostAddress("http://testaddress");
+        openAMLogoutService.setLogoutAction("testaction");
+        openAMLogoutService.afterPropertiesSet();
+        String result = openAMLogoutService.buildRedirectUrl("http://testurl");
+
+        assertEquals("testaction?logoutExitPage=http://testaddress/esso/logout", result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void missingProperty() throws Exception {
+        OpenAMLogoutService openAMLogoutService = new OpenAMLogoutService();
+        openAMLogoutService.afterPropertiesSet();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void wrongPropertyValue() throws Exception {
+        OpenAMLogoutService openAMLogoutService = new OpenAMLogoutService();
+        openAMLogoutService.setPointOfContactHostAddress("addressWithoutHttpFirst");
+        openAMLogoutService.setLogoutAction("testaction");
+        openAMLogoutService.afterPropertiesSet();
+    }
+
+    @Test
+    public void removeUnwantedSlash() throws Exception {
+        OpenAMLogoutService openAMLogoutService = new OpenAMLogoutService();
+        openAMLogoutService.setLogoutFromOpenAM(true);
+        openAMLogoutService.setPointOfContactHostAddress("http://testaddress/");
+        openAMLogoutService.setLogoutAction("testaction");
+        openAMLogoutService.afterPropertiesSet();
+        String result = openAMLogoutService.buildRedirectUrl("http://testurl");
+
+        assertEquals("testaction?logoutExitPage=http://testaddress/esso/logout", result);
+    }
+}
