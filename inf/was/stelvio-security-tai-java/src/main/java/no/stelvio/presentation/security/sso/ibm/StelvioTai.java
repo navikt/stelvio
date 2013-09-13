0db31d2ccceafec828fdedf8b6d59bc81a5a6f31
h103841 <h103841@localhost>
2013-09-13 06:59:34 +0000

    PKIGUAN-126: Removed log statement to avoid spamming SystemOut.log

--- inf/was/stelvio-security-tai-java/src/main/java/no/stelvio/presentation/security/sso/ibm/StelvioTai.java
+++ inf/was/stelvio-security-tai-java/src/main/java/no/stelvio/presentation/security/sso/ibm/StelvioTai.java
@@ -218,7 +218,6 @@ public class StelvioTai implements TrustAssociationInterceptor {
             return TAIResult.create(HttpServletResponse.SC_OK, "notused", subject);
 
         } catch (Exception e) {
-            log.logp(Level.SEVERE, this.getClass().getName(), "negotiateValidateandEstablishTrust", e.getMessage(), e);
             throw new WebTrustAssociationFailedException(e.getMessage());
         }
     }
