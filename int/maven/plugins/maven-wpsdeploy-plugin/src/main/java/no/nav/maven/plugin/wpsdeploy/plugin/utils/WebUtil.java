cfe8ea4515f4821eddcb0b8a1f17849dde37faa9
R132736 <r132736@localhost>
2014-01-10 13:37:46 +0000

    PK-5932: Added a webutil for reading urls

---
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/java/no/nav/maven/plugin/wpsdeploy/plugin/utils/WebUtil.java
@@ -0,0 +1,81 @@
+package no.nav.maven.plugin.wpsdeploy.plugin.utils;
+
+import no.nav.maven.plugin.wpsdeploy.plugin.exceptions.ConfigurationException;
+import no.nav.maven.plugin.wpsdeploy.plugin.exceptions.HttpCode404Exception;
+import no.nav.maven.plugin.wpsdeploy.plugin.exceptions.HttpCodeNon2XXException;
+import org.codehaus.plexus.util.Base64;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WebUtil {
+	public static String readUrl(String urlString) {
+		String outPut;
+
+		try {
+			URL url = new URL(urlString);
+			HttpURLConnection huc =  (HttpURLConnection) url.openConnection();
+			int responsecode = huc.getResponseCode();
+			if (responsecode == 404){
+				throw new HttpCode404Exception(responsecode+" response code returned from URL: "+ urlString);
+			} else {
+				if (responsecode < 200 && responsecode >= 300){
+					throw new HttpCodeNon2XXException(responsecode +" response code returned from URL: "+ urlString);
+				} else {
+					InputStream is = huc.getInputStream();
+					outPut = convertStreamToString(is);
+					is.close();
+				}
+			}
+		} catch (IOException e) {
+			throw new ConfigurationException("Exception when reading url: "+ urlString +"\n"+ e);
+		}
+
+		return outPut.toString();
+	}
+
+	/*
+	URL url = new URL(“location address”);
+URLConnection uc = url.openConnection();
+String userpass = username + ":" + password";
+String basicAuth = "Basic " + new String(new Base64().encode(userpass.getBytes()));
+uc.setRequestProperty ("Authorization", basicAuth);
+InputStream in = uc.getInputStream();
+	 */
+
+	public static String readUrlWithAuth(String urlString, String username, String password) {
+		String outPut;
+
+		try {
+			URL url = new URL(urlString);
+			HttpURLConnection huc =  (HttpURLConnection) url.openConnection();
+			int responsecode = huc.getResponseCode();
+			if (responsecode != 404){
+				if (responsecode >= 200 && responsecode < 300){
+					String userpass = username + ":" + password;
+					String basicAuth = "Basic " + new String(new Base64().encode(userpass.getBytes()));
+					huc.setRequestProperty("Authorization", basicAuth);
+					InputStream is = huc.getInputStream();
+					outPut = convertStreamToString(is);
+					is.close();
+				} else {
+					throw new HttpCodeNon2XXException(responsecode +" response code returned from URL: "+ urlString);
+				}
+			} else {
+				throw new HttpCode404Exception(responsecode+" response code returned from URL: "+ urlString);
+			}
+		} catch (IOException e) {
+			throw new ConfigurationException("Exception when reading url with authorization: "+ urlString +"\n"+ e);
+		}
+
+		return outPut.toString();
+	}
+
+	private static String convertStreamToString(InputStream is) {
+		java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+		return s.hasNext() ? s.next() : "";
+	}
+
+}
