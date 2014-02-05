f7233c2bbabae00bd1159c2d53ece80babaa9c9d
R132736 <r132736@localhost>
2014-02-05 10:02:35 +0000

    PK-5932: Moved the main() function to the top of the file.

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/ClusterStartStop.py
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/ClusterStartStop.py
@@ -11,19 +11,91 @@ if len(sys.argv) != 2:
 	l.error("Syntax: wsadmin -lang jython -f <scriptName>.py <scripts_home> <operation>")
 	sys.exit()
 
+def main():
+	appTarget = ""
+	messaging = ""
+	support = ""
+
+	cell = AdminConfig.list("Cell")
+
+	clusters = AdminConfig.list("ServerCluster").splitlines()
+	for cluster in clusters:
+		if (cluster.find("AppTarget") >= 0 or cluster.find("WPSCluster") >= 0):
+			appTarget = cluster
+		elif (cluster.find("Messaging") >= 0 or cluster.find("MECluster") >= 0):
+			messaging = cluster
+		elif (cluster.find("Support") >= 0):
+			support = cluster
+		else:
+			l.error("Could not identify " + cluster + ".")
+
+	if (OPERATION == "start"):
+		l.info("Starting cluster " + re.split('\(', messaging)[0])
+		result_me = doClusterOperation(re.split('\(', cell)[0], messaging, OPERATION)
+		while (result_me == 1):
+			sleep(10)
+			l.info("Exception detected. Retrying...")
+			result_me = doClusterOperation(re.split('\(', cell)[0], messaging, OPERATION)
+		waitForMessagingEnginesStarted()
+
+		l.info("Starting cluster " + re.split('\(', support)[0])
+		result_support = doClusterOperation(re.split('\(', cell)[0], support, OPERATION)
+		while (result_support == 1):
+			sleep(10)
+			l.info("Exception detected. Retrying...")
+			result_support = doClusterOperation(re.split('\(', cell)[0], support, OPERATION)
+
+		l.info("Starting cluster " + re.split('\(', appTarget)[0])
+		result_app = doClusterOperation(re.split('\(', cell)[0], appTarget, OPERATION)
+		while (result_app == 1):
+			sleep(10)
+			l.info("Exception detected. Retrying...")
+			result_app = doClusterOperation(re.split('\(', cell)[0], appTarget, OPERATION)
+
+		l.info("All clusters are started")
+
+	elif (OPERATION == "stop"):
+		l.info("Stopping cluster " + re.split('\(', appTarget)[0])
+		result_app = doClusterOperation(re.split('\(', cell)[0], appTarget, OPERATION)
+		while (result_app == 1):
+			sleep(10)
+			l.info("Exception detected. Retrying...")
+			result_app = doClusterOperation(re.split('\(', cell)[0], appTarget, OPERATION)
+
+		l.info("Stopping cluster " + re.split('\(', support)[0])
+		result_support = doClusterOperation(re.split('\(', cell)[0], support, OPERATION)
+		while (result_support == 1):
+			sleep(10)
+			l.info("Exception detected. Retrying...")
+			result_support = doClusterOperation(re.split('\(', cell)[0], support, OPERATION)
+
+		l.info("Stopping cluster " + re.split('\(', messaging)[0])
+		result_me = doClusterOperation(re.split('\(', cell)[0], messaging, OPERATION)
+		while (result_me == 1):
+			sleep(10)
+			l.info("Exception detected. Retrying...")
+			result_me = doClusterOperation(re.split('\(', cell)[0], messaging, OPERATION)
+
+		l.info("All clusters are stopped")
+
+	elif (OPERATION == "synch"):
+		sync()
+	else:
+		l.error("Bad operation: [" + OPERATION + "]")
+
 def waitForMessagingEnginesStarted():
 	engines = AdminControl.queryNames("type=SIBMessagingEngine,*").splitlines()
-	
+
 	count = len(engines)
 	i = 0
 	max_failed = 1
-	
+
 	while i < count:
 		if (max_failed == 10):
 			sys.exit(105)
 		started = 'false'
 		engine = engines[i]
-		
+
 		try:
 			started = AdminControl.invoke(engine, "isStarted")
 		except:
@@ -31,7 +103,7 @@ def waitForMessagingEnginesStarted():
 			l.warning("Could not reach messaging engine, trying again...")
 			l.warning("Return value is ["+started+"]")
 			l.warning("Retry #"+str(max_failed))
-		
+
 		if (started == 'true'):
 			l.info(str(re.split(',', re.split('\=', engine)[1])[0] + ' is started'))
 			engines.pop(i)
@@ -102,7 +174,7 @@ def doClusterOperation(cellName, clusterName, operation):
 			l.info("Not all nodes are running, we are entering the Matrix.")
 			members = AdminConfig.showAttribute(clusterName, "members").split(java.lang.System.getProperty(' '))
 			active_nodes = getRunningNodes()
-			
+
 			for member in members:
 				member=member.replace('[','')
 				member=member.replace(']','')
@@ -121,77 +193,5 @@ def doClusterOperation(cellName, clusterName, operation):
 							l.info("Waiting for " + serverName + " to " + operation + ". Current state is: [" + serverState + "].")
 							sleep(10)
 
-def main():
-	appTarget = ""
-	messaging = ""
-	support = ""
-
-	cell = AdminConfig.list("Cell")
-
-	clusters = AdminConfig.list("ServerCluster").split(java.lang.System.getProperty('line.separator'))
-	for cluster in clusters:
-		if (cluster.find("AppTarget") >= 0 or cluster.find("WPSCluster") >= 0):
-			appTarget = cluster
-		elif (cluster.find("Messaging") >= 0 or cluster.find("MECluster") >= 0):
-			messaging = cluster
-		elif (cluster.find("Support") >= 0):
-			support = cluster
-		else:
-			l.error("Could not identify " + cluster + ".")
-			
-	if (OPERATION == "start"):
-		l.info("Starting cluster " + re.split('\(', messaging)[0])
-		result_me = doClusterOperation(re.split('\(', cell)[0], messaging, OPERATION)
-		while (result_me == 1):
-			sleep(10)
-			l.info("Exception detected. Retrying...")
-			result_me = doClusterOperation(re.split('\(', cell)[0], messaging, OPERATION)
-		waitForMessagingEnginesStarted()
-		
-		l.info("Starting cluster " + re.split('\(', support)[0])
-		result_support = doClusterOperation(re.split('\(', cell)[0], support, OPERATION)
-		while (result_support == 1):
-			sleep(10)
-			l.info("Exception detected. Retrying...")
-			result_support = doClusterOperation(re.split('\(', cell)[0], support, OPERATION)
-			
-		l.info("Starting cluster " + re.split('\(', appTarget)[0])
-		result_app = doClusterOperation(re.split('\(', cell)[0], appTarget, OPERATION)	
-		while (result_app == 1):
-			sleep(10)
-			l.info("Exception detected. Retrying...")
-			result_app = doClusterOperation(re.split('\(', cell)[0], appTarget, OPERATION)
-			
-		l.info("All clusters are started")
-		
-	elif (OPERATION == "stop"):
-		l.info("Stopping cluster " + re.split('\(', appTarget)[0])
-		result_app = doClusterOperation(re.split('\(', cell)[0], appTarget, OPERATION)
-		while (result_app == 1):
-			sleep(10)
-			l.info("Exception detected. Retrying...")
-			result_app = doClusterOperation(re.split('\(', cell)[0], appTarget, OPERATION)
-		
-		l.info("Stopping cluster " + re.split('\(', support)[0])
-		result_support = doClusterOperation(re.split('\(', cell)[0], support, OPERATION)
-		while (result_support == 1):
-			sleep(10)
-			l.info("Exception detected. Retrying...")
-			result_support = doClusterOperation(re.split('\(', cell)[0], support, OPERATION)
-		
-		l.info("Stopping cluster " + re.split('\(', messaging)[0])
-		result_me = doClusterOperation(re.split('\(', cell)[0], messaging, OPERATION)
-		while (result_me == 1):
-			sleep(10)
-			l.info("Exception detected. Retrying...")
-			result_me = doClusterOperation(re.split('\(', cell)[0], messaging, OPERATION)
-		
-		l.info("All clusters are stopped")
-		
-	elif (OPERATION == "synch"):
-		sync()
-	else:
-		l.error("Bad operation: [" + OPERATION + "]")
-
-main()
+if __name__ == "__main__": main()
 
