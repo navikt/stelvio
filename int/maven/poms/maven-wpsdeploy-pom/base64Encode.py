888936fae4cc21f1b8708a06b9c5e6151da29d9f
srvsvnldap <srvsvnldap@localhost>
2014-02-05 12:41:59 +0000

    version 2 of the base64Encode.py script

--- int/maven/poms/maven-wpsdeploy-pom/base64Encode.py
+++ int/maven/poms/maven-wpsdeploy-pom/base64Encode.py
@@ -4,8 +4,10 @@ import base64
 import sys
 
 if len(sys.argv) > 1:
-	input = sys.argv[1] or sys.stdin.read()
+	input = sys.argv[1] 
 else:
-	input = sys.stdin.read()
+	if sys.stdin.isatty():
+		print "Write the string you want to encode and hit enter:"
+	input = sys.stdin.readline()
 
 print base64.b64encode(input.strip())
