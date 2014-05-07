8f349acd09a91672712384a1a993d087194ebce0
S140744 <s140744@localhost>
2014-05-07 10:13:52 +0000

    PKFEIL-1600: La til sletting av short lived BPEL modul før stopping av cluster

--- int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/RemoveOldBPModule.py
+++ int/maven/plugins/maven-wpsdeploy-plugin/src/main/resources/scripts/RemoveOldBPModule.py
@@ -5,16 +5,25 @@ from  lib.saveUtil import save
 import lib.logUtil as log
 l = log.getLogger(__name__)
 
-microflowREGEX = re.compile('-microflow-')
-scaModules = sca.getModulesToBeInstalled()
-for scaModule in scaModules:
-	if microflowREGEX.search(scaModule.shortName):
-		l.info('Checking whether older version of application %s exists...' % scaModule.shortName)
-		if scaModule.uninstallOldVersion:
-			app.uninstall(scaModule)
-			scaModule.uninstallOldVersion = False
-		else:
-			l.info('Same version or not installed, continuing without uninstalling.')
+def main():
+	microflowREGEX = re.compile('-microflow-')
+	bpelREGEX = re.compile('nav-bsrv-frg-hentinstitusjonsoppholdliste')
+	scaModules = sca.getModulesToBeInstalled()
+	for scaModule in scaModules:
+		if microflowREGEX.search(scaModule.shortName):
+			l.info('Checking whether older version of microflow application %s exists...' % scaModule.shortName)
+			setUninstallOldVersion(scaModule)
+		elif bpelREGEX.search(scaModule.shortName):
+			l.info('Checking whether older version of short lived BPEL application %s exists...' % scaModule.shortName)
+			setUninstallOldVersion(scaModule)
+	sca.save(scaModules)
+	save()
 
-sca.save(scaModules)
-save()
+def setUninstallOldVersion(scaModule):
+	if scaModule.uninstallOldVersion:
+		app.uninstall(scaModule)
+		scaModule.uninstallOldVersion = False
+	else:
+		l.info('Same version or not installed, continuing without uninstalling.')
+
+if __name__ == '__main__': main()
