ef9311789bf989a61d09075838dbf14701b4a180
Eirik Sletteberg <eirik.sletteberg@nav.no>
2020-03-23 13:58:51 +0100

    Use commit hash in version number

--- release.sh
+++ release.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
+TIME=$(TZ="Europe/Oslo" date +%Y.%m.%d-%H.%M)
+COMMIT=$(git rev-parse --short=12 HEAD)
+VERSION="6.0.0-beta-6-$COMMIT"
+echo "Setting version $VERSION"
+
+mvn -B versions:set -DnewVersion="$VERSION"
+mvn -B versions:commit
+
 echo "Running release"
 mvn -B --settings maven-settings.xml deploy -Dmaven.wagon.http.pool=false
